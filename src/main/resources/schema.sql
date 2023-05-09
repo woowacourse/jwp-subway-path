747193d9da51a769b9bf1fb9cdfc7f366fdb37c3
sosow0212 <sosow0212@naver.com>
2023-05-09 16:45:10 +0900

    refactor: 스키마 변경
    Co-authored-by: kokodak <kokodakadokok@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS section;
 DROP TABLE IF EXISTS station_point;
 DROP TABLE IF EXISTS station_in_line;
-DROP TABLE IF EXISTS line;
 DROP TABLE IF EXISTS station;
+DROP TABLE IF EXISTS line;
 
 CREATE TABLE IF NOT EXISTS station
 (
@@ -10,6 +10,10 @@ CREATE TABLE IF NOT EXISTS station
     name       VARCHAR(255)          NOT NULL UNIQUE,
     primary key (station_id)
 );
+
+INSERT INTO station(station_id, name)
+VALUES (1, '2호선'),
+       (2, '8호선');
 
 CREATE TABLE IF NOT EXISTS line
 (
@@ -19,11 +23,16 @@ CREATE TABLE IF NOT EXISTS line
     PRIMARY KEY (line_id)
 );
 
+INSERT INTO line(line_id, name, color)
+VALUES (1, '잠실역', '초록색'),
+       (2, '잠실새내역', '초록색');
+
 CREATE TABLE IF NOT EXISTS section
 (
     section_id      BIGINT AUTO_INCREMENT NOT NULL,
     up_station_id   BIGINT                NOT NULL,
     down_station_id BIGINT                NOT NULL,
+    distance        BIGINT                NOT NULL,
     PRIMARY KEY (section_id),
     FOREIGN KEY (up_station_id) REFERENCES station (station_id),
     FOREIGN KEY (down_station_id) REFERENCES station (station_id)
@@ -32,9 +41,11 @@ CREATE TABLE IF NOT EXISTS section
 CREATE TABLE IF NOT EXISTS station_end_point
 (
     station_point_id BIGINT AUTO_INCREMENT NOT NULL,
+    line_id          BIGINT                NOT NULL,
     up_station_id    BIGINT                NOT NULL,
     down_station_id  BIGINT                NOT NULL,
     PRIMARY KEY (station_point_id),
+    FOREIGN KEY (line_id) REFERENCES line (line_id),
     FOREIGN KEY (up_station_id) REFERENCES station (station_id),
     FOREIGN KEY (down_station_id) REFERENCES station (station_id)
 );
