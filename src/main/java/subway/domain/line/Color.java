246076e3e7c438e6b6fa567007434aad7be75947
bjk1649 <qhwns1649@naver.com>
2023-05-09 16:47:04 +0900

    feat: 노선 색 검증 기능 추가
    Co-authored-by: woo-chang <clllickme@naver.com>

---
+++ src/main/java/subway/domain/line/Color.java
@@ -0,0 +1,26 @@
+package subway.domain.line;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+import subway.exception.InvalidColorException;
+
+final class Color {
+
+    private static final Pattern PATTERN = Pattern.compile("^[가-힣]{1,10}색$");
+
+    private final String value;
+
+    public Color(final String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final String value) {
+        if (Objects.isNull(value)) {
+            throw new InvalidColorException("노선 색은 존재해야 합니다.");
+        }
+        if (!PATTERN.matcher(value).matches()) {
+            throw new InvalidColorException("색 이름은 '색'으로 끝나야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/subway/domain/line/ColorTest.java
@@ -0,0 +1,30 @@
+package subway.domain.line;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import subway.exception.InvalidColorException;
+
+class ColorTest {
+
+    @ParameterizedTest
+    @DisplayName("색 이름이 존재하지 않으면 예외를 던진다.")
+    @NullSource
+    void validate_with_null(final String input) {
+        assertThatThrownBy(() -> new Color(input))
+                .isInstanceOf(InvalidColorException.class)
+                .hasMessage("노선 색은 존재해야 합니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("색 이름 형식에 맞지 않는다면 예외를 던진다.")
+    @ValueSource(strings = {"색", "하양", "white색", "하양하양하양하양하양하양색"})
+    void validate_with_invalid_color_format(final String input) {
+        assertThatThrownBy(() -> new Color(input))
+                .isInstanceOf(InvalidColorException.class)
+                .hasMessage("색 이름은 '색'으로 끝나야 합니다.");
+    }
+}
