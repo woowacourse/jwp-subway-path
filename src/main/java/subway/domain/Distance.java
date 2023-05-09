5797a8076a568eee5b6ede742a5ad3274a42837a
wugawuga <dnr3655@naver.com>
2023-05-09 21:04:27 +0900

    feat: Distance 객체 구현
    Co-authored-by: donghae-kim <donghae1999@gmail.com>
    Co-authored-by: cpot5620 <wjswjdfhr030@gmail.com>

---
+++ src/main/java/subway/domain/Distance.java
@@ -0,0 +1,25 @@
+package subway.domain;
+
+import subway.exception.GlobalException;
+
+public class Distance {
+    private static final int MIN_DISTANCE = 1;
+    private static final int MAX_DISTANCE = 10;
+
+    private final int distance;
+
+    public Distance(final int distance) {
+        validate(distance);
+        this.distance = distance;
+    }
+
+    private void validate(final int distance) {
+        if (distance < MIN_DISTANCE || distance > MAX_DISTANCE) {
+            throw new GlobalException("역간 거리는 10km이하 양의 정수만 가능합니다.");
+        }
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
