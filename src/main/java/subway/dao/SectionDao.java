c693ab4f9d286b209991564d1aeb95d306e3f630
mcodnjs <codnjs8989@gmail.com>
2023-05-09 22:16:48 +0900

    feat: SectionDao에서 section 조회하는 기능 구현
    Co-authored-by: Go-Jaecheol <gojaech@naver.com>

--- src/main/java/subway/dao/SectionDao.java
+++ src/main/java/subway/dao/SectionDao.java
@@ -1,18 +1,32 @@
 package subway.dao;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import subway.domain.Section;
 import subway.dto.SectionRequest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class SectionDao {
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
+
+    private RowMapper<Section> rowMapper = (rs, rowNum) ->
+            new Section(
+                    rs.getLong("id"),
+                    rs.getLong("line_id"),
+                    rs.getLong("up_station_id"),
+                    rs.getLong("down_station_id"),
+                    rs.getInt("distance")
+            );
 
     public SectionDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -30,9 +44,32 @@ public class SectionDao {
         return simpleJdbcInsert.executeAndReturnKey(params).longValue();
     }
 
-    public void deleteByStationId(final Long stationId) {
-        String sql = "DELETE FROM section WHERE down_station_id = ?";
-        jdbcTemplate.update(sql, stationId);
+    public Optional<Long> findById(final Long id) {
+        String sql = "SELECT * FROM section WHERE id = ?";
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, Long.class, id));
+        } catch (EmptyResultDataAccessException exception) {
+            return Optional.empty();
+        }
+    }
+
+    public void deleteByStationId(final Long lineId, final Long stationId) {
+        String sql = "DELETE FROM section WHERE line_id = ? AND (up_station_id = ? OR down_station_id = ?)";
+        jdbcTemplate.update(sql, lineId, stationId, stationId);
+    }
+
+    public List<Section> findByLineIdAndStationId(final Long lineId, final Long stationId) {
+        String sql = "SELECT * FROM section WHERE line_id = ? AND (up_station_id = ? OR down_station_id = ?)";
+        return jdbcTemplate.query(sql, rowMapper, lineId, stationId, stationId);
+    }
+
+    public Optional<Long> findByLineId(final Long lineId) {
+        String sql = "SELECT * FROM section WHERE line_id = ?";
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, Long.class, lineId));
+        } catch (EmptyResultDataAccessException exception) {
+            return Optional.empty();
+        }
     }
 }
 
--- src/test/java/subway/dao/SectionDaoTest.java
+++ src/test/java/subway/dao/SectionDaoTest.java
@@ -5,11 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import subway.dto.SectionRequest;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,22 +38,14 @@ class SectionDaoTest {
     void deleteByStationId() {
         // given
         Long stationId = 2L;
-        SectionRequest sectionRequest = new SectionRequest(stationId, 1L, 1L, 1);
+        Long lineId = 1L;
+        SectionRequest sectionRequest = new SectionRequest(stationId, lineId, 1L, 1);
         Long sectionId = sectionDao.insert(sectionRequest);
 
         // when
-        sectionDao.deleteByStationId(stationId);
+        sectionDao.deleteByStationId(lineId, stationId);
 
         // then
-        assertThat(findById(sectionId).isEmpty()).isTrue();
-    }
-
-    private Optional<Long> findById(final Long id) {
-        String sql = "SELECT * FROM section WHERE id = ?";
-        try {
-            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, Long.class, id));
-        } catch (EmptyResultDataAccessException exception) {
-            return Optional.empty();
-        }
+        assertThat(sectionDao.findById(sectionId).isEmpty()).isTrue();
     }
 }
