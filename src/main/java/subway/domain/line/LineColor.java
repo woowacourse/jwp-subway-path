1db0917f1724821f176951220ce8ac48ce9a436c
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-09 16:41:00 +0900

    ✨ feat: 노선 색상 값 객체 추가

---
+++ src/main/java/subway/domain/line/LineColor.java
@@ -0,0 +1,9 @@
+package subway.domain.line;
+
+public class LineColor {
+    private final String colorValue;
+
+    public LineColor(final String colorValue) {
+        this.colorValue = colorValue;
+    }
+}
