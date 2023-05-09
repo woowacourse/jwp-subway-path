88217f604d8ccda82380e120c2b6fefc3d1fa181
sosow0212 <sosow0212@naver.com>
2023-05-09 14:51:25 +0900

    feat: 테이블 DDL 추가 및 h2-console 설정
    Co-authored-by: kokodak <kokodakadokok@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,50 @@
-create table if not exists STATION
+DROP TABLE IF EXISTS section;
+DROP TABLE IF EXISTS station_point;
+DROP TABLE IF EXISTS station_in_line;
+DROP TABLE IF EXISTS line;
+DROP TABLE IF EXISTS station;
+
+CREATE TABLE IF NOT EXISTS station
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
+    station_id BIGINT AUTO_INCREMENT NOT NULL,
+    name       VARCHAR(255)          NOT NULL UNIQUE,
+    primary key (station_id)
 );
 
-create table if not exists LINE
+CREATE TABLE IF NOT EXISTS line
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(id)
+    line_id BIGINT AUTO_INCREMENT NOT NULL,
+    name    VARCHAR(255)          NOT NULL UNIQUE,
+    color   VARCHAR(20)           NOT NULL,
+    PRIMARY KEY (line_id)
+);
+
+CREATE TABLE IF NOT EXISTS section
+(
+    section_id      BIGINT AUTO_INCREMENT NOT NULL,
+    up_station_id   BIGINT                NOT NULL,
+    down_station_id BIGINT                NOT NULL,
+    PRIMARY KEY (section_id),
+    FOREIGN KEY (up_station_id) REFERENCES station (station_id),
+    FOREIGN KEY (down_station_id) REFERENCES station (station_id)
+);
+
+CREATE TABLE IF NOT EXISTS station_end_point
+(
+    station_point_id BIGINT AUTO_INCREMENT NOT NULL,
+    up_station_id    BIGINT                NOT NULL,
+    down_station_id  BIGINT                NOT NULL,
+    PRIMARY KEY (station_point_id),
+    FOREIGN KEY (up_station_id) REFERENCES station (station_id),
+    FOREIGN KEY (down_station_id) REFERENCES station (station_id)
+);
+
+CREATE TABLE IF NOT EXISTS station_in_line
+(
+    station_in_line BIGINT AUTO_INCREMENT NOT NULL,
+    station_id      BIGINT                NOT NULL,
+    line_id         BIGINT                NOT NULL,
+    PRIMARY KEY (station_in_line),
+    FOREIGN KEY (station_id) REFERENCES station (station_id),
+    FOREIGN KEY (line_id) REFERENCES line (line_id)
 );
