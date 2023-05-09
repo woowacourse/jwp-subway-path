2175527183c55750880f7563b8935e2d3fc2e6e4
bjk1649 <qhwns1649@naver.com>
2023-05-09 15:22:45 +0900

    feat: 역 이름 형식 검증 기능 추가
    Co-authored-by: woo-chang <clllickme@naver.com>

--- src/test/java/subway/domain/station/NameTest.java
+++ src/test/java/subway/domain/station/NameTest.java
@@ -2,6 +2,7 @@ package subway.domain.station;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.exception.InvalidStationNameException;
@@ -13,11 +14,31 @@ class NameTest {
     void validate_with_invalid_length() {
         assertThatThrownBy(() -> new Name("열한글자가넘는역이름입니다역"))
                 .isInstanceOf(InvalidStationNameException.class)
-                .hasMessage("역 이름은 11글자를 넘을 수 없습니다.");
+                .hasMessage("역 이름은 1글자에서 11글자까지 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("역 이름이 역으로 끝나지 않을 경우 예외를 던진다.")
+    void validate_with_invalid_name_format() {
+        assertThatThrownBy(() -> new Name("선릉"))
+                .isInstanceOf(InvalidStationNameException.class)
+                .hasMessage("역 이름은 '역'으로 끝나야 합니다.");
+    }
+
+    @Test
+    @DisplayName("역 이름이 한글, 숫자로 구성되지 않을 경우 예외를 던진다.")
+    void validate_with_invalid_name_element() {
+        assertThatThrownBy(() -> new Name("NewYork역"))
+                .isInstanceOf(InvalidStationNameException.class)
+                .hasMessage("역 이름은 한글, 숫자만 가능합니다.");
     }
 }
 
 class Name {
+
+    private static final int MAXIMUM_LENGTH = 11;
+    private static final String SUFFIX = "역";
+    private static final Pattern PATTERN = Pattern.compile("^[가-힣0-9]+$");
 
     private final String value;
 
@@ -27,8 +48,14 @@ class Name {
     }
 
     private void validate(final String value) {
-        if (value.length() > 11) {
-            throw new InvalidStationNameException("역 이름은 11글자를 넘을 수 없습니다.");
+        if (value.isBlank() || value.length() > MAXIMUM_LENGTH) {
+            throw new InvalidStationNameException("역 이름은 1글자에서 11글자까지 가능합니다.");
+        }
+        if (!value.endsWith(SUFFIX)) {
+            throw new InvalidStationNameException("역 이름은 '역'으로 끝나야 합니다.");
+        }
+        if (!PATTERN.matcher(value).matches()) {
+            throw new InvalidStationNameException("역 이름은 한글, 숫자만 가능합니다.");
         }
     }
 }
