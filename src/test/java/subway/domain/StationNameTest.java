29e893c908b4226464383de4dc4e2f9790300f93
jiwon <ljwon77@daum.net>
2023-05-09 19:17:40 +0900

    feat: 역 이름 길이 검증 구현
    Co-authored-by: iamjooon2 <iamjooon2@gmail.com>

--- src/test/java/subway/domain/StationNameTest.java
+++ src/test/java/subway/domain/StationNameTest.java
@@ -7,24 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class LineNameTest {
+class StationNameTest {
 
     @ParameterizedTest(name = "세글자부터 열글자 사이의 이름은 정상 생성된다.")
     @ValueSource(strings = {"일이삼", "일이삼사오륙칠팔구십"})
-    void line_name_success_test(final String name) {
-        final LineName lineName = assertDoesNotThrow(() -> new LineName(name));
-        assertThat(lineName)
+    void station_name_success_test(final String name) {
+        final StationName stationName = assertDoesNotThrow(() -> new StationName(name));
+        assertThat(stationName)
             .extracting("name")
             .isEqualTo(name);
     }
 
     @ParameterizedTest(name = "세글자 미만 열글자 초과의 이름은 예외가 발생한다.")
     @ValueSource(strings = {"일이", "영일이삼사오륙칠팔구십"})
-    void line_name_fail_test(final String name) {
-        assertThatThrownBy(() -> new LineName(name))
+    void station_name_fail_test(final String name) {
+        assertThatThrownBy(() -> new StationName(name))
             .isInstanceOf(IllegalArgumentException.class)
             .extracting("message")
-            .isEqualTo("노선 이름 길이는 3~10여야 합니다.");
+            .isEqualTo("역 이름 길이는 3~10여야 합니다.");
     }
 
 }
