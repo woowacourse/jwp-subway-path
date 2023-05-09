61176e84db5e564f1c1dad65ac87c12e41a1aa00
mcodnjs <codnjs8989@gmail.com>
2023-05-09 15:13:38 +0900

    docs: 기능 구현 명세 작성
    Co-authored-by: Go-Jaecheol <gojaech@naver.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,17 @@ create table if not exists LINE
     color varchar(20) not null,
     primary key(id)
 );
+
+create table if not exists SECTION
+(
+    id                  bigint auto_increment not null,
+    line_id             bigint                not null,
+    up_station_id       bigint,
+    down_station_id     bigint,
+    distance            int                   not null default 0,
+    primary key (id),
+    foreign key (line_id) references LINE (id),
+    foreign key (up_station_id) references STATION (id),
+    foreign key (down_station_id) references STATION (id)
+);
+
