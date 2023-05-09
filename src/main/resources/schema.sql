b68101b9ed54cc3e06e9be86f3878498fdad1291
jeonghoon <shb03323@naver.com>
2023-05-09 17:24:25 +0900

    feat: Section 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,13 @@ create table if not exists LINE
     color varchar(20) not null,
     primary key(id)
 );
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    line_id bigint not null,
+    distance int not null,
+    previous_station_id bigint not null,
+    next_station_id bigint not null,
+    primary key(id)
+);
