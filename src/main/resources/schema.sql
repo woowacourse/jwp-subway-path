163562a8ea7b9d8631f971e0193b1af511becfec
ChoiYoungHoon <dudgns2865@naver.com>
2023-05-09 17:52:12 +0900

    feat: table 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,26 @@
 create table if not exists STATION
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
-);
+    id   bigint auto_increment not null,
+    name varchar(255)          not null unique,
+    primary key (id)
+    );
 
 create table if not exists LINE
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(id)
-);
+    id    bigint auto_increment not null,
+    name  varchar(255)          not null unique,
+    color varchar(20)           not null unique,
+    primary key (id)
+    );
+
+create table if not exists INTERSTATION
+(
+    id               bigint auto_increment not null,
+    line_id          bigint                not null,
+    start_station_id bigint                not null,
+    end_station_id   bigint                not null,
+    primary key (id),
+    foreign key (line_id) references LINE (id),
+    foreign key (start_station_id) references STATION (id),
+    foreign key (end_station_id) references STATION (id)
+    );
