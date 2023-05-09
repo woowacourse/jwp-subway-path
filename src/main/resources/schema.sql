f9196b221d13cb30327541cbb3f8766dda700309
swonny <wonny921@gmail.com>
2023-05-09 20:14:29 +0900

    feat: 데이터 베이스 DDL 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,6 +9,17 @@ create table if not exists LINE
 (
     id bigint auto_increment not null,
     name varchar(255) not null unique,
-    color varchar(20) not null,
     primary key(id)
+);
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    upstream_id bigint not null,
+    downstream_id bigint not null,
+    line_id bigint not null,
+    distance int not null,
+    primary key(id),
+    foreign key(upstream_id) references STATION(id),
+    foreign key(downstream_id) references STATION(id)
 );
