d4e78ef0a750687f80905ad989f4316ec3cea113
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-05-09 15:49:16 +0900

    feat: 스키마 작성
    Co-authored-by: hong-sile <gurwns9325@naver.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,27 @@
 create table if not exists STATION
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
+    name varchar(255) not null,
+
+    primary key (name)
+);
+
+create table if not exists LINE_NODE
+(
+    id              BIGINT        not null AUTO_INCREMENT,
+    before_node     BIGINT,
+    station         varchar(255)  not null,
+    next_node       BIGINT,
+    before_distance INT default 0 not null,
+    next_distance   INT default 0 not null,
+    primary key (id),
+    foreign key (before_node) references LINE_NODE (id),
+    foreign key (next_node) references LINE_NODE (id),
+    foreign key (station) references STATION (name)
 );
 
 create table if not exists LINE
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(id)
+    id   bigint auto_increment not null,
+    name varchar(255)          not null unique,
+    primary key (id)
 );
