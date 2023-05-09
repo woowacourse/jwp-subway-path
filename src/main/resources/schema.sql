da8df054affa7f1479de9dbbc344693d7edce1d3
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-09 15:47:40 +0900

    📝 docs: 기능 구현 목록 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,19 @@
-create table if not exists STATION
+CREATE TABLE IF NOT EXISTS line
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
-);
-
-create table if not exists LINE
-(
-    id bigint auto_increment not null,
+    line_id bigint auto_increment not null,
     name varchar(255) not null unique,
     color varchar(20) not null,
-    primary key(id)
+    primary key(line_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS station
+(
+    station_id BIGINT AUTO_INCREMENT NOT NULL,
+    name varchar(255) NOT NULL,
+    next_distance INT,
+    previous_distance INT,
+    line_id BIGINT,
+    FOREIGN KEY (line_id) REFERENCES line(line_id) ON DELETE CASCADE,
+    PRIMARY KEY(station_id)
 );
