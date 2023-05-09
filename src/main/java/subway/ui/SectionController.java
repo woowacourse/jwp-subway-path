2e5ca1d9d88d85ee31537e6d8a96962d34a583c7
mcodnjs <codnjs8989@gmail.com>
2023-05-09 17:08:05 +0900

    test: Section 통합테스트 구현
    Co-authored-by: Go-Jaecheol <gojaech@naver.com>

--- src/main/java/subway/ui/SectionController.java
+++ src/main/java/subway/ui/SectionController.java
@@ -2,16 +2,10 @@ package subway.ui;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import subway.domain.Station;
-import subway.dto.LineResponse;
 import subway.dto.SectionRequest;
 import subway.dto.SectionResponse;
-import subway.dto.StationResponse;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/sections")
@@ -26,15 +20,5 @@ public class SectionController {
     @DeleteMapping
     public ResponseEntity<Void> deleteSection(@RequestParam final Long stationId) {
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<Map<String, StationResponse>> showAllSections() {
-        final Map<String, StationResponse> result = new HashMap<>();
-        StationResponse 강남역 = StationResponse.of(new Station("강남역"));
-        StationResponse 잠실역 = StationResponse.of(new Station("잠실역"));
-        result.put("2호선", 강남역);
-        result.put("2호선", 잠실역);
-        return ResponseEntity.ok().body(result);
     }
 }
