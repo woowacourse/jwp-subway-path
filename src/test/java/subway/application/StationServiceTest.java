79e2e9bac0fb3d911d2b49c95e84901d16087a74
songusika <sws981202@gmail.com>
2023-05-10 16:23:59 +0900

    feat: findStationResponseById 구현
    Co-authored-by: wonyongChoi05 <rltgjqmduftlagl@gmail.com>

--- src/test/java/subway/application/StationServiceTest.java
+++ src/test/java/subway/application/StationServiceTest.java
@@ -1,5 +1,8 @@
 package subway.application;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -10,7 +13,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import subway.dao.StationDao;
+import subway.dto.StationResponse;
 import subway.dto.StationSaveRequest;
+import subway.entity.Station;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -27,10 +32,25 @@ class StationServiceTest {
     void saveRequest를_받아서_역을_저장한다() {
         // given
         StationSaveRequest request = new StationSaveRequest("잠실역");
-        Mockito.when(stationDao.insert(request.toEntity())).thenReturn(1L);
+        when(stationDao.insert(request.toEntity())).thenReturn(1L);
 
         // when, then
-        Assertions.assertThat(stationService.saveStation(request)).isEqualTo(1L);
+        assertThat(stationService.saveStation(request)).isEqualTo(1L);
     }
 
+    @Test
+    void id_를_받아_해당_역을_조회한다() {
+        // given
+        Long id = 1L;
+        final Station response = Station.of(id, "잠실역");
+        doReturn(response).when(stationDao).findById(id);
+
+        // when
+        final StationResponse result = stationService.findStationResponseById(id);
+
+        // then
+        assertThat(result)
+                .usingRecursiveComparison()
+                .isEqualTo(StationResponse.of(response));
+    }
 }
