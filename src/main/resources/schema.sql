0d7b146649ebee82f2e2c8dc0971f60c05d3dc87
shin-mallang <huipulci1@naver.com>
2023-05-11 02:39:16 +0900

    style: 스키마 파일 형식 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,86 +1,26 @@
 create table if not exists STATION
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    primary key
-(
-    id
-)
-    );
+    id bigint auto_increment not null,
+    name varchar(255) not null unique,
+    primary key (id)
+);
 
 create table if not exists LINE
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    primary key
-(
-    id
-)
-    );
+    id bigint auto_increment not null,
+    name varchar(255) not null unique,
+    primary key (id)
+);
 
 create table if not exists SECTIONS
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    up_station_id
-    bigint
-    not
-    null,
-    down_station_id
-    bigint
-    not
-    null,
-    distance
-    int
-    not
-    null,
-    line_id
-    bigint
-    not
-    null,
-    primary
-    key
-(
-    id
-),
-    FOREIGN KEY
-(
-    up_station_id
-) REFERENCES STATION
-(
-    id
-),
-    FOREIGN KEY
-(
-    down_station_id
-) REFERENCES STATION
-(
-    id
-),
-    FOREIGN KEY
-(
-    line_id
-) REFERENCES LINE
-(
-    id
-)
-    );
+    id bigint auto_increment not null,
+    up_station_id bigint not null,
+    down_station_id bigint not null,
+    distance int not null,
+    line_id bigint not null,
+    primary key (id),
+    FOREIGN KEY (up_station_id) REFERENCES STATION(id),
+    FOREIGN KEY (down_station_id) REFERENCES STATION(id),
+    FOREIGN KEY (line_id) REFERENCES LINE(id)
+);
