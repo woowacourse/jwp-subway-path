e6e73a941f3668e0d4821e77c6b6bc30656406c3
Woojin Lee <dhkep03@gmail.com>
2023-05-10 16:22:19 +0900

    feat: 이미 역이 존재하는 경우 예외 발생

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,18 +1,54 @@
 package subway.domain;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 public class Line {
-    private Long id;
-    private String name;
+    private final Long id;
+    private final String name;
+    private final List<Section> sections;
 
-    public Line(String name) {
+    public Line(String name, List<Section> sections) {
+        this.id = null;
         this.name = name;
+        this.sections = sections;
     }
 
-    public Line(Long id, String name) {
+    public Line(Long id, String name, List<Section> sections) {
         this.id = id;
         this.name = name;
+        this.sections = sections;
+    }
+
+    public static Line of(String name, String upwardStationName, String downwardStationName, int distance) {
+        Station upwardStation = new Station(upwardStationName);
+        Station downwardStation = new Station(downwardStationName);
+        List<Section> sections = new LinkedList<>();
+        sections.add(new Section(upwardStation, downwardStation, distance));
+        return new Line(name, sections);
+    }
+
+    public void addStation(String stationName, String neighborhoodStationName, Direction direction, int distance) {
+        // TODO 로직 분리
+        Station station = new Station(stationName);
+        Station neighborhoodStation = new Station(neighborhoodStationName);
+
+        if (isStationExist(station)) {
+            throw new IllegalArgumentException(String.format(
+                    "이미 노선에 존재하는 역은 추가할 수 없습니다." + System.lineSeparator() +
+                            "추가하려는 노선 : %s" + System.lineSeparator() +
+                            "추가하려는 역 : %s", name, station.getName()));
+        }
+    }
+
+    private boolean isStationExist(Station station) {
+        for (Section section : sections) {
+            if (section.hasStation(station)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Long getId() {
