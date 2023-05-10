b3e150a6cb4bb5b51690969883d72c0a1c51cd4a
Rosie <dev.kayoung@gmail.com>
2023-05-10 09:53:36 +0900

    refactor: 협력객체를 실제 객체로 사용

--- src/test/java/subway/service/StationServiceTest.java
+++ src/test/java/subway/service/StationServiceTest.java
@@ -3,35 +3,28 @@ package subway.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import subway.domain.Station;
 import subway.exception.DuplicatedStationNameException;
+import subway.repository.SimpleStationRepository;
 import subway.repository.StationRepository;
 
-@ExtendWith(MockitoExtension.class)
 class StationServiceTest {
 
-    @InjectMocks
     private StationService stationService;
-    @Mock
     private StationRepository stationRepository;
+
+    @BeforeEach
+    void setUp() {
+        stationRepository = new SimpleStationRepository();
+        stationService = new StationService(stationRepository);
+    }
 
     @Test
     @DisplayName("역을 추가한다.")
     void createStation() {
-        //given
-        BDDMockito.given(stationRepository.findByName(Mockito.any(String.class)))
-                .willReturn(Optional.empty());
-        BDDMockito.given(stationRepository.create(Mockito.any(Station.class)))
-                .willReturn(1L);
+        //give
         final String name = "잠실";
 
         //when
@@ -46,8 +39,6 @@ class StationServiceTest {
     void createStationWithDuplicatedName() {
         //given
         final String name = "잠실";
-        BDDMockito.given(stationRepository.findByName(Mockito.any(String.class)))
-                .willReturn(Optional.of(new Station(name)));
 
         //when
         //then
