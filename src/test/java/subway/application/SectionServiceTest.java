29e6c4c60f7aeb41b7aef4e9a96fb9b5260a7d0e
ReO <yt930310@gmail.com>
2023-05-10 17:18:12 +0900

    test: 역이 존재하는 노선에 역을 추가하는 기능에 대한 테스트코드 작성

--- src/test/java/subway/application/SectionServiceTest.java
+++ src/test/java/subway/application/SectionServiceTest.java
@@ -5,8 +5,8 @@ import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +22,8 @@ import subway.domain.Line;
 import subway.domain.Section;
 import subway.domain.Station;
 import subway.dto.InitialSectionAddRequest;
-import subway.dto.SectionResponse;
+import subway.dto.SectionAddRequest;
+import subway.dto.SectionAddResponse;
 import subway.exception.DomainException;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,38 +39,108 @@ class SectionServiceTest {
 
     @Test
     @DisplayName("구간이 등록되지 않은 라인에 구간을 추가한다.")
-    void addSectionTest() {
-        Station station1 = new Station(1L, "잠실");
-        Station station2 = new Station(2L, "잠실새내");
-        Line line = new Line(1L, "2호선", "초록");
+    void addInitialSectionTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long lineId = 1L;
         when(sectionDao.findAllSectionByLineId(1L)).thenReturn(new ArrayList<>());
-        when(stationDao.findById(1L)).thenReturn(station1);
-        when(stationDao.findById(2L)).thenReturn(station2);
-        when(lineDao.findById(1L)).thenReturn(line);
-        when(sectionDao.insert(new Section(station1, station2, line, new Distance(2)))).thenReturn(1L);
+        when(sectionDao.insert(new Section(null, station1Id, station2Id, lineId, 2))).thenReturn(1L);
 
-        SectionResponse result = sectionService.addSection(new InitialSectionAddRequest(1L, 1L, 2L, 2));
+        SectionAddResponse result = sectionService.addSection(new InitialSectionAddRequest(1L, 1L, 2L, 2));
 
-        Assertions.assertAll(
-            () -> assertThat(result.getId()).isEqualTo(1L),
-            () -> assertThat(result.getDistance()).isEqualTo(2),
-            () -> assertThat(result.getLineId()).isEqualTo(1L),
-            () -> assertThat(result.getFirstStationId()).isEqualTo(1L),
-            () -> assertThat(result.getSecondStationId()).isEqualTo(2L)
-        );
+        assertThat(result.getId()).isEqualTo(1L);
     }
 
     @Test
     @DisplayName("구간이 등록된 라인에 구간을 추가하면 에러를 반환한다.")
-    void addSectionFailTest() {
-        Station station1 = new Station(1L, "잠실");
-        Station station2 = new Station(2L, "잠실새내");
-        Line line = new Line(1L, "2호선", "초록");
-        List<Section> sections = List.of(new Section(station1, station2, line, new Distance(2)),
-            new Section(station2, station1, line, new Distance(2)));
+    void addInitialSectionFailTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long lineId = 1L;
+        List<Section> sections = List.of(new Section(1L, station1Id, station2Id, lineId, 2));
         when(sectionDao.findAllSectionByLineId(1L)).thenReturn(sections);
 
         assertThatThrownBy(() -> sectionService.addSection(new InitialSectionAddRequest(1L, 1L, 2L, 2))).isInstanceOf(
             DomainException.class);
+    }
+
+    @Test
+    @DisplayName("구간이 등록된 라인에 구간을 추가한다. - 구간 사이에 역을 넣는다.")
+    void addSectionBetweenTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long newStationId = 3L;
+        Long lineId = 1L;
+        Section section = new Section(1L, station1Id, station2Id, lineId, 10);
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(lineId, newStationId, station1Id, station2Id, 3);
+        when(sectionDao.findAllSectionByLineId(lineId)).thenReturn(List.of(section));
+
+        List<SectionAddResponse> result = sectionService.addSection(sectionAddRequest);
+
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("구간이 등록된 라인에 구간을 추가한다. - 종점 다음에 역을 넣는다.")
+    void addSectionTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long newStationId = 3L;
+        Long lineId = 1L;
+        Section section = new Section(1L, station1Id, station2Id, lineId, 10);
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(lineId, newStationId, station1Id, null, 3);
+        when(sectionDao.findAllSectionByLineId(lineId)).thenReturn(List.of(section));
+
+        List<SectionAddResponse> result = sectionService.addSection(sectionAddRequest);
+
+        assertThat(result).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("구간이 등록되지 않은 라인에 구간을 추가하면, 예외를 발생시킨다.")
+    void addSectionFailTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long newStationId = 3L;
+        Long lineId = 1L;
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(lineId, newStationId, station1Id, station2Id, 3);
+        when(sectionDao.findAllSectionByLineId(lineId)).thenReturn(List.of());
+
+        assertThatThrownBy(() -> sectionService.addSection(sectionAddRequest))
+            .isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    @DisplayName("종점이 아닌곳에 종점을 추가하려 하면, 예외를 발생시킨다.")
+    void addSectionFailNotLastStopTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long station3Id = 3L;
+        Long newStationId = 4L;
+        Long lineId = 1L;
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(lineId, newStationId, station2Id, null, 3);
+
+        Section section1 = new Section(1L, station1Id, station2Id, lineId, 10);
+        Section section2 = new Section(2L, station2Id, station3Id, lineId, 10);
+        when(sectionDao.findAllSectionByLineId(lineId)).thenReturn(List.of(section1, section2));
+
+        assertThatThrownBy(() -> sectionService.addSection(sectionAddRequest))
+            .isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    @DisplayName("새로 등록하려는 구간의 길이가 기존의 구간 길이보다 길면 예외를 발생시킨다.")
+    void addSectionFailDistanceTest() {
+        Long station1Id = 1L;
+        Long station2Id = 2L;
+        Long newStationId = 3L;
+        Long lineId = 1L;
+        SectionAddRequest sectionAddRequest = new SectionAddRequest(lineId, newStationId, station1Id, station2Id, 10);
+
+        Section section = new Section(1L, station1Id, station2Id, lineId, 5);
+        when(sectionDao.findAllSectionByLineId(lineId)).thenReturn(List.of(section));
+
+        assertThatThrownBy(() -> sectionService.addSection(sectionAddRequest))
+            .isInstanceOf(DomainException.class);
     }
 }
--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -1,128 +1,20 @@
 package subway.integration;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.jdbc.Sql;
 
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import subway.dto.InitialSectionAddRequest;
-import subway.dto.SectionAddRequest;
 
-@Sql("classpath:/InitializeTable.sql")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
     @LocalServerPort
     int port;
 
-    static Stream<Arguments> addInitialSectionInvalidRequest() {
-        return Stream.of(
-            Arguments.of(new InitialSectionAddRequest(0L, 1L, 2L, 1)),
-            Arguments.of(new InitialSectionAddRequest(1L, 0L, 2L, 1)),
-            Arguments.of(new InitialSectionAddRequest(1L, 1L, 0L, 1)),
-            Arguments.of(new InitialSectionAddRequest(1L, 1L, 2L, 0)),
-            Arguments.of(new InitialSectionAddRequest(2L, 1L, 2L, 1))
-        );
-    }
-
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
     }
-
-    @Test
-    @DisplayName("구간이 없는 노선에 구간을 추가한다.")
-    void addInitialSectionTest() {
-        InitialSectionAddRequest initialSectionAddRequest = new InitialSectionAddRequest(1L, 1L, 2L, 1);
-
-        ExtractableResponse<Response> response = RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(initialSectionAddRequest)
-            .when().post("/sections/initial")
-            .then().log().all()
-            .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("Location")).isNotBlank();
-    }
-
-    //TODO: 1L에는 없음 2L 아이디 호선에는 역 있음
-    @ParameterizedTest(name = "구간이 있는 노선에 첫 구간을 추가한다.")
-    @MethodSource("addInitialSectionInvalidRequest")
-    void addInitialSectionTestFail(InitialSectionAddRequest initialSectionAddRequest) {
-        ExtractableResponse<Response> response = RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(initialSectionAddRequest)
-            .when().post("/sections/initial")
-            .then().log().all()
-            .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    @Test
-    @DisplayName("구간이 있는 노선에 구간을 추가한다.")
-    void addSectionTest(SectionAddRequest sectionAddRequest) {
-        ExtractableResponse<Response> response = RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionAddRequest)
-            .when().post("/sections")
-            .then().log().all()
-            .extract();
-
-        //then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    // TODO: 2023/05/09
-    // 1. 역 아이디 없음 (세개 역 전부다)
-    // 2. 노선 아이디 없음
-    // 3. 노선에 해당 구간 없음
-    // 4. 길이 에러(길이 자체 에러, 기존 길이보다 긴 에러)
-
-    @ParameterizedTest(name = "구간이 있는 노선에 첫 구간을 추가한다.")
-    @MethodSource("addSectionInvalidRequest")
-    void addSectionTestFail(SectionAddRequest sectionAddRequest) {
-        ExtractableResponse<Response> response = RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionAddRequest)
-            .when().post("/sections")
-            .then().log().all()
-            .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    static Stream<Arguments> addSectionInvalidRequest() {
-        return Stream.of(
-            Arguments.of(new SectionAddRequest(0L, 1L, 2L, 1L, 0)),
-            Arguments.of(new SectionAddRequest(1L, 0L, 2L, 1)),
-            Arguments.of(new SectionAddRequest(1L, 1L, 0L, 1)),
-            Arguments.of(new SectionAddRequest(1L, 1L, 2L, 0)),
-            Arguments.of(new SectionAddRequest(2L, 1L, 2L, 1))
-        );
-    }
-
 }
