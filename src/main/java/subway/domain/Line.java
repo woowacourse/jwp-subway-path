b21d51586e9d34cdebdb33ccda0b0dec6c8ac605
xxeol2 <jhj01177@naver.com>
2023-05-10 16:21:39 +0900

    feat: Line 도메인 추가
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

---
+++ src/main/java/subway/domain/Line.java
@@ -0,0 +1,52 @@
+package subway.domain;
+
+import subway.exception.ApiIllegalArgumentException;
+
+public class Line {
+
+    private static final int MAX_NAME_LENGTH = 50;
+    private static final int MAX_COLOR_LENGTH = 20;
+
+    private final Long id;
+    private final String name;
+    private final String color;
+
+    public Line(final String name, final String color) {
+        validateName(name);
+        validateColor(color);
+        this.id = null;
+        this.name = name.strip();
+        this.color = color.strip();
+    }
+
+    private void validateName(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new ApiIllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
+        if (name.strip().length() > MAX_NAME_LENGTH) {
+            throw new ApiIllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하여야합니다.");
+        }
+    }
+
+    private void validateColor(final String color) {
+        if (color == null || color.isBlank()) {
+            throw new ApiIllegalArgumentException("색상은 비어있을 수 없습니다.");
+        }
+        if (color.strip().length() > MAX_COLOR_LENGTH) {
+            throw new ApiIllegalArgumentException("색상은 " + MAX_COLOR_LENGTH + "자 이하여야합니다.");
+        }
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
