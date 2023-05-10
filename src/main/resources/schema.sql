0a7d266f04b4392b3f9c59bf612d03917330261b
drunkenhw <drunkenhw@gmail.com>
2023-05-10 14:41:19 +0900

    docs: schema 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,16 @@ create table if not exists LINE
     name varchar(255) not null unique,
     primary key(id)
 );
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    line_id bigint  not null,
+    source_station_id bigint not null,
+    destination_station_id bigint,
+    distance int,
+    foreign key (line_id) references line(id),
+    foreign key (source_station_id) references station(id),
+    foreign key (destination_station_id) references station(id),
+    primary key(id)
+)
