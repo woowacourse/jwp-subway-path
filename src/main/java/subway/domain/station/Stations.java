fe06936d8b714f404e9cfaf3a8c04ba92846985d
seokhwan-an <asok369258@naver.com>
2023-05-10 16:50:41 +0900

    feat: 역 중복 이름 검증 기능 구현

--- src/main/java/subway/domain/station/Stations.java
+++ src/main/java/subway/domain/station/Stations.java
@@ -1,8 +1,9 @@
 package subway.domain.station;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 import subway.domain.section.Sections;
-import subway.domain.station.Station;
 
 public class Stations {
 
@@ -12,12 +13,23 @@ public class Stations {
 
     public Stations(final List<Station> stations) {
         validateStationCount(stations);
+        validateStationDuplicateName(stations);
         this.stations = stations;
     }
 
     private void validateStationCount(final List<Station> stations) {
         if (stations.size() == INVALID_STATION_COUNT) {
             throw new IllegalArgumentException("노선의 역은 1개만 존재할 수 없습니다.");
+        }
+    }
+
+    private void validateStationDuplicateName(final List<Station> stations) {
+        List<String> names = stations.stream()
+            .map(Station::getName)
+            .collect(Collectors.toList());
+
+        if (names.size() != new HashSet<>(names).size()) {
+            throw new IllegalArgumentException("역 이름은 중복될 수 없습니다.");
         }
     }
 
--- src/test/java/subway/domain/station/StationsTest.java
+++ src/test/java/subway/domain/station/StationsTest.java
@@ -24,6 +24,17 @@ class StationsTest {
             .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("stations는 중복된 이름의 station을 가질 수 없다.")
+    void validate_duplicated_name_station() {
+        // given
+        List<Station> stations = List.of(new Station(1L, "잠실역"), new Station(2L, "잠실역"));
+
+        // when + then
+        assertThatThrownBy(() -> new Stations(stations))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @ParameterizedTest
     @MethodSource("createStations")
     @DisplayName("stations는 0개 또는 2개 이상의 역으로 생성된다.")
