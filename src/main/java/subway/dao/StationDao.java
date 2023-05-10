2ed6dea7a0d015b0dc1407cbba54867b61d5ce07
bjk1649 <qhwns1649@naver.com>
2023-05-10 20:58:33 +0900

    feat: DB에 역을 저장하는 기능 추가
    Co-authored-by: woo-chang <clllickme@naver.com>

--- src/main/java/subway/dao/StationDao.java
+++ src/main/java/subway/dao/StationDao.java
@@ -1,17 +1,47 @@
 package subway.dao;
 
 import java.util.Optional;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import subway.entity.StationEntity;
 
 @Repository
 public class StationDao {
 
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+
+    public StationDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("station")
+                .usingGeneratedKeyColumns("id")
+                .usingColumns("name");
+    }
+
     public StationEntity save(final StationEntity stationEntity) {
-        return null;
+        final SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(stationEntity);
+        final Long id = jdbcInsert.executeAndReturnKey(parameterSource).longValue();
+        return new StationEntity(id, stationEntity.getName());
     }
 
     public Optional<StationEntity> findById(final Long id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM station WHERE id = ?";
+        try {
+            final StationEntity result = jdbcTemplate.queryForObject(
+                    sql,
+                    (rs, rowNum) -> new StationEntity(
+                            rs.getLong("id"),
+                            rs.getString("name")
+                    ),
+                    id);
+            return Optional.ofNullable(result);
+        } catch (EmptyResultDataAccessException exception) {
+            return Optional.empty();
+        }
     }
 }
