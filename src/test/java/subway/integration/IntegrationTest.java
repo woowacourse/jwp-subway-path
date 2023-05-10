38bf01053affc50dfdacabbe012be47475305c26
greeng00se <thegreengoose7@gmail.com>
2023-05-10 19:47:39 +0900

    chore: DB 스키마 추가
    Co-authored-by: miseongk <miseongkinn@gmail.com>

--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -2,10 +2,12 @@ package subway.integration;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
+@Disabled
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
