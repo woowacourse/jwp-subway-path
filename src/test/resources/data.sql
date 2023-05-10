7c3ac81137e32e5015ce7194638037bbc58323a6
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-05-10 22:04:58 +0900

    chore: 테스트 환경 구성 및 데이터 추가
    Co-authored-by: hong-sile <gurwns9325@naver.com>

---
+++ src/test/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO LINE (name)
+VALUES ('2호선');
+
+INSERT INTO STATION (name)
+VALUES ('잠실');
+INSERT INTO STATION (name)
+VALUES ('강남');
+INSERT INTO STATION (name)
+VALUES ('선릉');
+INSERT INTO STATION (name)
+VALUES ('삼성');
