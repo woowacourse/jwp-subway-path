6e6fef91c41f671ac497f56a13301c2568a9f838
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-10 17:58:54 +0900

    🔨 chore: 데이터베이스 구성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,28 @@
 CREATE TABLE IF NOT EXISTS line
 (
     line_id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(line_id)
+    name    varchar(255)          not null UNIQUE,
+    color   varchar(20)           not null,
+    primary key (line_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS station
 (
     station_id BIGINT AUTO_INCREMENT NOT NULL,
-    name varchar(255) NOT NULL,
-    next_distance INT,
-    previous_distance INT,
-    line_id BIGINT,
-    FOREIGN KEY (line_id) REFERENCES line(line_id) ON DELETE CASCADE,
-    PRIMARY KEY(station_id)
+    name       varchar(255)          NOT NULL UNIQUE,
+    PRIMARY KEY (station_id)
+);
+
+CREATE TABLE IF NOT EXISTS section
+(
+    section_id     BIGINT AUTO_INCREMENT NOT NULL,
+    first_station  varchar(255)          NOT NULL,
+    second_station varchar(255)          NOT NULL,
+    distance       INT,
+    line_id        BIGINT,
+    FOREIGN KEY (line_id) REFERENCES line (line_id) ON DELETE CASCADE,
+    FOREIGN KEY (first_station) REFERENCES station (name) ON DELETE CASCADE,
+    FOREIGN KEY (second_station) REFERENCES station (name) ON DELETE CASCADE,
+    PRIMARY KEY (section_id)
 );
