620d6234d465e255ee61c7de6ede8bbda50573ed
sosow0212 <sosow0212@naver.com>
2023-05-10 16:50:28 +0900

    refactor: 스키마 변경
    Co-authored-by: kokodak <kokodakadokok@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS section;
 DROP TABLE IF EXISTS station_point;
-DROP TABLE IF EXISTS station_in_line;
 DROP TABLE IF EXISTS station;
 DROP TABLE IF EXISTS line;
 
@@ -11,10 +10,6 @@ CREATE TABLE IF NOT EXISTS station
     primary key (station_id)
 );
 
-INSERT INTO station(station_id, name)
-VALUES (1, '2호선'),
-       (2, '8호선');
-
 CREATE TABLE IF NOT EXISTS line
 (
     line_id BIGINT AUTO_INCREMENT NOT NULL,
@@ -23,39 +18,26 @@ CREATE TABLE IF NOT EXISTS line
     PRIMARY KEY (line_id)
 );
 
-INSERT INTO line(line_id, name, color)
-VALUES (1, '잠실역', '초록색'),
-       (2, '잠실새내역', '초록색');
-
 CREATE TABLE IF NOT EXISTS section
 (
     section_id      BIGINT AUTO_INCREMENT NOT NULL,
+    line_id         BIGINT                NOT NULL,
     up_station_id   BIGINT                NOT NULL,
     down_station_id BIGINT                NOT NULL,
     distance        BIGINT                NOT NULL,
     PRIMARY KEY (section_id),
-    FOREIGN KEY (up_station_id) REFERENCES station (station_id),
-    FOREIGN KEY (down_station_id) REFERENCES station (station_id)
-);
-
-CREATE TABLE IF NOT EXISTS station_end_point
-(
-    station_point_id BIGINT AUTO_INCREMENT NOT NULL,
-    line_id          BIGINT                NOT NULL,
-    up_station_id    BIGINT                NOT NULL,
-    down_station_id  BIGINT                NOT NULL,
-    PRIMARY KEY (station_point_id),
     FOREIGN KEY (line_id) REFERENCES line (line_id),
     FOREIGN KEY (up_station_id) REFERENCES station (station_id),
     FOREIGN KEY (down_station_id) REFERENCES station (station_id)
 );
 
-CREATE TABLE IF NOT EXISTS station_in_line
-(
-    station_in_line BIGINT AUTO_INCREMENT NOT NULL,
-    station_id      BIGINT                NOT NULL,
-    line_id         BIGINT                NOT NULL,
-    PRIMARY KEY (station_in_line),
-    FOREIGN KEY (station_id) REFERENCES station (station_id),
-    FOREIGN KEY (line_id) REFERENCES line (line_id)
-);
+INSERT INTO station(station_id, name)
+VALUES (1, '잠실역'),
+       (2, '잠실새내역'),
+       (3, '종합운동장역'),
+       (4, '삼성역'),
+       (5, '선릉역');
+
+INSERT INTO line(line_id, name, color)
+VALUES (1, '2호선', '초록색'),
+       (2, '8호선', '핑크색');
