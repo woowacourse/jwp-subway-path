bdec8da1f6a75b07cd9152ac84a1513347915a77
songusika <sws981202@gmail.com>
2023-05-10 17:36:26 +0900

    feat: showStations 구현
    Co-authored-by: wonyongChoi05 <rltgjqmduftlagl@gmail.com>

--- src/test/java/subway/ui/StationControllerTest.java
+++ src/test/java/subway/ui/StationControllerTest.java
@@ -1,29 +1,39 @@
 package subway.ui;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.net.URI;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import subway.application.StationService;
+import subway.dto.StationResponse;
 import subway.dto.StationSaveRequest;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest()
 class StationControllerTest {
+
+    @MockBean
+    private StationService stationService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -44,5 +54,23 @@ class StationControllerTest {
                 .andExpect(status().isCreated())
                 .andDo(print())
                 .andReturn();
+    }
+
+    @Test
+    void 모든_역을_조회한다() throws Exception {
+        // given
+        final List<StationResponse> responses = List.of(
+                new StationResponse(1L, "잠실역"),
+                new StationResponse(2L, "선릉역")
+        );
+        when(stationService.findAllStationResponses()).thenReturn(responses);
+        final String responseJson = objectMapper.writeValueAsString(responses);
+
+        // when, then
+        mockMvc.perform(get("/stations")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(print());
     }
 }
