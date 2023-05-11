95249ddaddec45d798c6e15832e7c6cf6e2c8af3
bjk1649 <qhwns1649@naver.com>
2023-05-11 16:44:02 +0900

    fix: 역간 거리의 최대길이 제한 삭제
    Co-authored-by: woo-chang <clllickme@naver.com>

--- src/main/java/subway/domain/section/Distance.java
+++ src/main/java/subway/domain/section/Distance.java
@@ -5,7 +5,6 @@ import subway.exception.InvalidDistanceException;
 final class Distance {
 
     private static final int MINIMUM_VALUE = 0;
-    private static final int MAXIMUM_VALUE = 15;
 
     private final int value;
 
@@ -15,7 +14,7 @@ final class Distance {
     }
 
     private void validate(final int value) {
-        if (value < MINIMUM_VALUE || value > MAXIMUM_VALUE) {
+        if (value < MINIMUM_VALUE) {
             throw new InvalidDistanceException("역 사이의 거리는 0이상, 15이하여야합니다.");
         }
     }
