f5d974a27ea2ead8f6a0da78445dd2eac8270752
Youngho <cyh6099@gmail.com>
2023-05-12 00:01:03 +0900

    refacor: fixture수정

--- src/test/java/subway/domain/LineFixture.java
+++ src/test/java/subway/domain/LineFixture.java
@@ -3,5 +3,6 @@ package subway.domain;
 public class LineFixture {
     public static Line FIRST_LINE = new Line(1L, "1호선", "blue");
     public static Line SECOND_LINE = new Line(2L, "2호선", "green");
+    public static Line FIRST_LINE_NO_ID = new Line(null, "1호선", "blue");
     public static Line SECOND_LINE_NO_ID = new Line(null, "2호선", "green");
 }
