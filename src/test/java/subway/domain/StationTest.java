1fcc6e2bcedba65af4c077be6df9789200442c6b
Cyma-s <ssol0319@naver.com>
2023-05-11 15:42:56 +0900

    test: StationTest 추가
    Co-authored-by: ezzanzzan <eunzzann@gmail.com>

---
+++ src/test/java/subway/domain/StationTest.java
@@ -0,0 +1,26 @@
+package subway.domain;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+
+class StationTest {
+    @Test
+    @DisplayName("생성 테스트")
+    void create() {
+        // expected
+        assertDoesNotThrow(() -> new Station("잠실역"));
+    }
+
+    @ParameterizedTest(name = "{displayName}[{index}] = ''{0}''")
+    @EmptySource
+    @DisplayName(value = "이름에 공백이 입력되면 예외가 발생한다.")
+    void createEmpty(String name) {
+        // expected
+        Assertions.assertThatThrownBy(() -> new Station(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
