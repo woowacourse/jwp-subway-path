a71d8afba195dcf0345ec6ec5eae08414c0243a0
woosung1223 <dntjd991223@naver.com>
2023-05-11 11:39:16 +0900

    feat: 역 조회/삭제 서비스 구현

--- src/main/java/subway/application/SubwayService.java
+++ src/main/java/subway/application/SubwayService.java
@@ -1,16 +1,56 @@
 package subway.application;
 
 import org.springframework.stereotype.Service;
+import subway.dao.StationDao;
+import subway.dao.SubwayDao;
+import subway.domain.Distance;
+import subway.domain.Line;
+import subway.domain.RouteMap;
+import subway.domain.Section;
+import subway.domain.Station;
 import subway.dto.StationEnrollRequest;
+import subway.dto.StationResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SubwayService {
 
-    public void enrollStation(Integer lineId, StationEnrollRequest request) {
+    private final SubwayDao subwayDao;
+    private final StationDao stationDao;
 
+    public SubwayService(SubwayDao subwayDao, StationDao stationDao) {
+        this.subwayDao = subwayDao;
+        this.stationDao = stationDao;
     }
 
-    public void deleteStation(Integer lineId, Integer stationId) {
+    public void enrollStation(Integer lineId, StationEnrollRequest request) {
+        Line line = subwayDao.findById(lineId);
+        Section section = new Section(
+                stationDao.findById(request.getFromStation()),
+                stationDao.findById(request.getToStation()),
+                new Distance(request.getDistance())
+        );
+        line.addSection(section);
+        subwayDao.save(line);
+    }
 
+    public void deleteStation(Integer lineId, Long stationId) {
+        Line line = subwayDao.findById(lineId);
+
+        line.deleteStation(stationDao.findById(stationId));
+        subwayDao.save(line);
+    }
+
+    public List<StationResponse> findRouteMap(Integer lineId) {
+        Line line = subwayDao.findById(lineId);
+        return line.routeMap()
+                .getRouteMap()
+                .stream()
+                .map(station -> new StationResponse(
+                        station.getId(),
+                        station.getName()
+                )).collect(Collectors.toList());
     }
 }
