0ce10f6d822a14e24222d79415a50ed7e8f4c119
Youngho <cyh6099@gmail.com>
2023-05-11 11:38:03 +0900

    feat: 노선에 이미 역들이 존재할 때 새로운 역 2개를 추가하려고 하면 예외

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -15,14 +15,20 @@ public class Sections {
     public AddResultDto add(Station upStation, Station downStation, Distance distance, Line line) {
         validateIsEqualStations(upStation, downStation);
         if (sections.isEmpty()) {
-            Section section = new Section(upStation, downStation, distance, line);
-            return new AddResultDto(
-                    List.of(section),
-                    List.of(),
-                    List.of(upStation, downStation)
-            );
+            return addInitStations(upStation, downStation, distance, line);
         }
+        validateIsNonExistStations(upStation, downStation);
+
         return new AddResultDto(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+    }
+
+    private static AddResultDto addInitStations(Station upStation, Station downStation, Distance distance, Line line) {
+        Section section = new Section(upStation, downStation, distance, line);
+        return new AddResultDto(
+                List.of(section),
+                List.of(),
+                List.of(upStation, downStation)
+        );
     }
 
     private void validateIsEqualStations(Station upStation, Station downStation) {
@@ -30,4 +36,17 @@ public class Sections {
             throw new IllegalArgumentException("동일한 역 2개가 입력으로 들어왔습니다. 이름을 다르게 설정해주세요.");
         }
     }
+
+    private void validateIsNonExistStations(Station upStation, Station downStation) {
+        if (isNonExistInSection(upStation) && isNonExistInSection(downStation)) {
+            throw new IllegalArgumentException("이미 노선에 역들이 존재하기 때문에 한 번에 새로운 역 2개를 추가할 수 없습니다.");
+        }
+    }
+
+    private boolean isNonExistInSection(Station station) {
+        return sections.stream()
+                .noneMatch(section -> section.contains(station));
+    }
+
+
 }
--- src/test/java/subway/domain/SectionsTest.java
+++ src/test/java/subway/domain/SectionsTest.java
@@ -9,17 +9,16 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static subway.domain.LineFixture.SECOND_LINE;
+import static subway.domain.StationFixture.*;
 
 @SuppressWarnings("NonAsciiCharacters")
 class SectionsTest {
     @Test
     void 동일한_역으로_추가하려고_하면_예외() {
         Sections sections = new Sections(new ArrayList<>());
-        Station jamsil1 = new Station(1L, "잠실");
-        Station jamsil2 = new Station(2L, "잠실");
-        Line second = new Line("2호선", "green");
         Distance distance = new Distance(10);
-        assertThatThrownBy(() -> sections.add(jamsil1, jamsil2, distance, second))
+        assertThatThrownBy(() -> sections.add(JAMSIL, JAMSIL, distance, SECOND_LINE))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("동일한 역 2개가 입력으로 들어왔습니다. 이름을 다르게 설정해주세요.");
     }
@@ -28,13 +27,10 @@ class SectionsTest {
     void 노선에_역이_없을_때_새로운_역을_추가한다() {
         //given
         Sections sections = new Sections(new ArrayList<>());
-        Station jamsil = new Station(1L, "잠실");
-        Station seonleung = new Station(2L, "선릉");
-        Line second = new Line(1L, "2호선", "green");
         Distance distance = new Distance(10);
 
         //when
-        AddResultDto addResult = sections.add(jamsil, seonleung, distance, second);
+        AddResultDto addResult = sections.add(StationFixture.JAMSIL, StationFixture.SEONLEUNG, distance, LineFixture.SECOND_LINE);
 
         //then
         List<Section> addedResults = addResult.getAddedResults();
@@ -44,13 +40,23 @@ class SectionsTest {
 
         Assertions.assertAll(
                 () -> assertThat(addedResults).hasSize(1),
-                () -> assertThat(newSection.getLine()).isEqualTo(second),
-                () -> assertThat(newSection.getUpStation()).isEqualTo(jamsil),
-                () -> assertThat(newSection.getDownStation()).isEqualTo(seonleung),
+                () -> assertThat(newSection.getLine()).isEqualTo(SECOND_LINE),
+                () -> assertThat(newSection.getUpStation()).isEqualTo(JAMSIL),
+                () -> assertThat(newSection.getDownStation()).isEqualTo(SEONLEUNG),
                 () -> assertThat(newSection.getDistance()).isEqualTo(distance),
                 () -> assertThat(deletedResults).hasSize(0),
                 () -> assertThat(addedStation).hasSize(2),
-                () -> assertThat(addedStation).containsExactlyInAnyOrder(jamsil, seonleung)
+                () -> assertThat(addedStation).containsExactlyInAnyOrder(JAMSIL, SEONLEUNG)
         );
+    }
+
+    @Test
+    void 노선에_역이_존재할_때_새로운_역_2개를_추가하려고_하면_예외() {
+        Section section = new Section(JAMSIL, SEONLEUNG, new Distance(10), SECOND_LINE);
+        Sections sections = new Sections(List.of(section));
+
+        assertThatThrownBy(() -> sections.add(GANGNAM, YUKSAM, new Distance(5), SECOND_LINE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 노선에 역들이 존재하기 때문에 한 번에 새로운 역 2개를 추가할 수 없습니다.");
     }
 }
