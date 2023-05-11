71e6a5b855372e642f4e4ab9615220cfe92659d4
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-11 15:42:04 +0900

    feat: DB 테이블 DML, 더미 데이터 추가
    Co-authorized-by: dooboocookie <bkh3030@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,33 @@
-create table if not exists STATION
+create table if not exists stations
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
+    id   bigint not null auto_increment,
+    name varchar(255)          not null unique,
+    primary key (id)
 );
 
-create table if not exists LINE
+create table if not exists lines
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(id)
+    id    bigint not null auto_increment,
+    name  varchar(255)          not null unique,
+    color varchar(20)           not null,
+    primary key (id)
+);
+
+create table if not exists stations_lines
+(
+    id         bigint not null auto_increment,
+    station_id bigint not null,
+    line_id    bigint not null,
+    primary key (id)
+);
+
+create table if not exists sections
+(
+    id              bigint not null auto_increment,
+    distance        int                   not null,
+    is_start        boolean               not null,
+    up_station_id   bigint                not null,
+    down_station_id bigint                not null,
+    line_id         bigint                not null,
+    primary key (id)
 );
