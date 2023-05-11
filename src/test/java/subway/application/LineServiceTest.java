0972569db29114a09fc1a9c05f7edce82b8eb543
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-05-11 11:29:25 +0900

    test: 역 등록 테스트 추가
    Co-authored-by: hong-sile <gurwns9325@naver.com>

--- src/test/java/subway/application/LineServiceTest.java
+++ src/test/java/subway/application/LineServiceTest.java
@@ -2,15 +2,40 @@ package subway.application;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
 import subway.dao.LineDao;
+import subway.dao.SectionDao;
+import subway.dao.StationDao;
+import subway.domain.Line;
+import subway.dto.SectionRequest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@JdbcTest
+@Import({LineService.class, LineDao.class, StationDao.class, SectionDao.class, StationService.class, SectionService.class})
 class LineServiceTest {
 
-    @DisplayName("노선을 생성하고 그 노선에 역을 등록한다.")
+    @Autowired
+    private LineService lineService;
+
+    @DisplayName("노선에 역을 등록한다.")
     @Test
     void registerLine() {
-        final LineDao mock = Mockito.mock(LineDao.class);
+        final long lineId = 1L;
+        final SectionRequest sectionRequest =
+                new SectionRequest(lineId, "잠실", "강남", 10);
 
+        lineService.registerStation(sectionRequest);
+
+        //검증
+        final Line line = lineService.findById(lineId);
+        assertAll(
+                () -> assertThat(line.getSections().getSections().get(0).getBeforeStation().getName()).isEqualTo("잠실"),
+                () -> assertThat(line.getSections().getSections().get(0).getNextStation().getName()).isEqualTo("강남"),
+                () -> assertThat(line.getSections().getSections().get(0).getDistance().getValue()).isEqualTo(10)
+        );
     }
 }
