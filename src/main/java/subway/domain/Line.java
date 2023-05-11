8118d42a885bf68f872bde15f9e8cc1a53591f15
This2sho <zx00018@naver.com>
2023-05-11 11:19:52 +0900

    feat(Line): 노선에 역을 추가하는 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,22 +3,68 @@ package subway.domain;
 import java.util.Objects;
 
 public class Line {
-    private Long id;
-    private String name;
-    private String color;
 
-    public Line() {
-    }
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final Stations stations;
 
     public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
+        this(null, name, color);
     }
 
     public Line(Long id, String name, String color) {
+        this(id, name, color, Stations.emptyStations());
+    }
+
+    public Line(Long id, String name, String color, Stations stations) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.stations = stations;
+    }
+
+    /**
+     * Line: 2호선
+     * Stations: 강남 - 서초 - 잠실 3333 - 방배 - 사당
+     */
+    public void initStations(Station previousStation, Station nextStation) {
+        if (stations.isEmpty()) {
+            stations.addLast(previousStation);
+            stations.addLast(nextStation);
+            return;
+        }
+        throw new IllegalStateException("비어있지 않은 노선에 역을 1개만 추가할 수 있습니다.");
+    }
+
+    public void addBeforeAt(Station beforeStation, Station station) {
+        validateEmpty();
+        stations.addBeforeAt(beforeStation, station);
+    }
+
+    private void validateEmpty() {
+        if (stations.isEmpty()) {
+            throw new IllegalStateException();
+        }
+    }
+
+    public void addLast(Station station) {
+        validateEmpty();
+        stations.addLast(station);
+    }
+
+    // 기존 라인에 역 1개 추가
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(id, line.id) && Objects.equals(name, line.name) && Objects.equals(color, line.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
     }
 
     public Long getId() {
@@ -33,16 +79,7 @@ public class Line {
         return color;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Line line = (Line) o;
-        return Objects.equals(id, line.id) && Objects.equals(name, line.name) && Objects.equals(color, line.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, color);
+    public Stations getStations() {
+        return stations;
     }
 }
