83232560ec278967a23e5b1d534e07d5029e2218
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-11 15:45:56 +0900

    feat: 노선의 역 목록 조회 기능 추가
    Co-authorized-by: dooboocookie <bkh3030@gmail.com>

--- src/main/java/subway/application/LineService.java
+++ src/main/java/subway/application/LineService.java
@@ -1,20 +1,34 @@
 package subway.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import subway.dao.LineDao;
+import subway.dao.SectionDao;
+import subway.dao.StationDao;
+import subway.dao.dto.SectionDto;
 import subway.domain.Line;
-import subway.dto.LineRequest;
+import subway.domain.Station;
 import subway.dto.LineResponse;
+import subway.dto.StationResponse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+@Transactional(readOnly = true)
 @Service
 public class LineService {
-    private final LineDao lineDao;
 
-    public LineService(LineDao lineDao) {
+    private final LineDao lineDao;
+    private final SectionDao sectionDao;
+    private final StationDao stationDao;
+
+    public LineService(final LineDao lineDao, final SectionDao sectionDao, final StationDao stationDao) {
         this.lineDao = lineDao;
+        this.sectionDao = sectionDao;
+        this.stationDao = stationDao;
     }
 
     public LineResponse saveLine(LineRequest request) {
@@ -29,25 +43,40 @@ public class LineService {
                 .collect(Collectors.toList());
     }
 
-    public List<Line> findLines() {
-        return lineDao.findAll();
-    }
+    public LineResponse findStationsByLineName(final String lineName) {
+        final Line line = lineDao.findByName(lineName)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 노선명입니다."));
 
-    public LineResponse findLineResponseById(Long id) {
-        Line persistLine = findLineById(id);
-        return LineResponse.of(persistLine);
-    }
+        final List<SectionDto> sectionDtos = sectionDao.findByLineId(line.getId());
 
-    public Line findLineById(Long id) {
-        return lineDao.findById(id);
-    }
+        final Map<Long, Long> upToDown = sectionDtos.stream()
+                .collect(Collectors.toMap(SectionDto::getUpStationId, SectionDto::getDownStationId));
 
-    public void updateLine(Long id, LineRequest lineUpdateRequest) {
-        lineDao.update(new Line(id, lineUpdateRequest.getName(), lineUpdateRequest.getColor()));
-    }
+        final Map<Long, Station> stationsById = new HashMap<>();
 
-    public void deleteLineById(Long id) {
-        lineDao.deleteById(id);
-    }
+        final List<Station> stations = stationDao.findAll();
+        for (Station station : stations) {
+            stationsById.put(station.getId(), station);
+        }
 
+        final Long firstStationIdByLineId = sectionDao.findFirstStationIdByLineId(line.getId());
+        final List<Station> sortedStation = new ArrayList<>();
+
+        Long currId = firstStationIdByLineId;
+        while(upToDown.containsKey(currId)) {
+            Long nextId = upToDown.get(currId);
+            sortedStation.add(stationsById.get(currId));
+            currId = nextId;
+        }
+        sortedStation.add(stationsById.get(currId));
+
+        final List<StationResponse> stationResponses = sortedStation.stream()
+                .map(station -> new StationResponse(
+                        station.getId(),
+                        station.getName()
+                ))
+                .collect(Collectors.toList());
+
+        return LineResponse.of(line, stationResponses);
+    }
 }
