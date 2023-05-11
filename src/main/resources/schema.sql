4f852c452ea5a82d3b9dcf8f8e09374b4596ce8a
java-saeng <gusdn85554@naver.com>
2023-05-11 13:08:21 +0900

    feat : 스키마 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,20 +8,9 @@ create table if not exists LINE
 create table if not exists SECTION
 (
     id bigint auto_increment not null,
-    next_section_id bigint,
-    starter_yn varchar(1) not null,
     current_station_name varchar(255) not null,
     next_station_name varchar(255) not null,
     distance int not null,
     line_id bigint not null,
     primary key(id)
 );
-
-create table if not exist STATION
-(
-    id bigint auto_increment not null,
-    from_station varchar(255) not null,
-    to_station varchar(255) not null,
-    section_id bigint not null,
-    primary key(id)
-)
