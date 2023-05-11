e4a9b69ae91a9bfea7c5d555b9d2d8ba38c651ce
be-student <presentsong@naver.com>
2023-05-11 17:10:14 +0900

    feat: repository 작성

--- src/main/java/subway/repository/LineRepository.java
+++ src/main/java/subway/repository/LineRepository.java
@@ -1,6 +1,7 @@
 package subway.repository;
 
 import java.util.List;
+import java.util.Optional;
 import subway.domain.Line;
 
 public interface LineRepository {
@@ -11,5 +12,8 @@ public interface LineRepository {
 
     Line update(Line line);
 
-    void deleteById(long id);
+    Optional<Line> findByName(String lineName);
+
+    void delete(Line line);
+
 }
