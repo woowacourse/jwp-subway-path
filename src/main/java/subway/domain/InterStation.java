69000065a2a16d048bf0cef6c93f10fe262a4643
be-student <presentsong@naver.com>
2023-05-11 17:10:48 +0900

    feat: domain 작성

--- src/main/java/subway/domain/InterStation.java
+++ src/main/java/subway/domain/InterStation.java
@@ -1,23 +1,44 @@
 package subway.domain;
 
+import lombok.Getter;
 import subway.exception.BusinessException;
 
+@Getter
 public class InterStation {
 
-    private final Station start;
-    private final Station end;
+    private final Long id;
+    private final Station firstStation;
+    private final Station secondStation;
     private final long distance;
 
-    public InterStation(final Station start, final Station end, final long distance) {
+    public InterStation(final Long id, final Station firstStation, final Station secondStation,
+            final long distance) {
         validateDistance(distance);
-        this.start = start;
-        this.end = end;
+        this.id = id;
+        this.firstStation = firstStation;
+        this.secondStation = secondStation;
         this.distance = distance;
+    }
+
+    public InterStation(final Station firstStation, final Station secondStation, final long distance) {
+        this(null, firstStation, secondStation, distance);
     }
 
     private void validateDistance(final long distance) {
         if (distance < 0) {
             throw new BusinessException("거리는 양수이어야 합니다.");
         }
+    }
+
+    public boolean containsAll(final Station frontStation, final Station backStation) {
+        return firstStation.equals(frontStation) && secondStation.equals(backStation)
+                || firstStation.equals(backStation) && secondStation.equals(firstStation);
+    }
+
+    public boolean contains(final Station station) {
+        if (station == null) {
+            return false;
+        }
+        return firstStation.equals(station) || secondStation.equals(station);
     }
 }
