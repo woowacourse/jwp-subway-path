15fdfd626095e9a511b2f021c12421771ce5d338
2014012606 <2014012606@hanyang.ac.kr>
2023-05-11 10:54:25 +0900

    feat: 라인 도메인 재구성 기능

--- src/main/java/subway/application/LineService.java
+++ src/main/java/subway/application/LineService.java
@@ -1,21 +1,18 @@
 package subway.application;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
-
 import subway.dao.LineDao;
 import subway.dao.SectionDao;
-import subway.dto.SectionDto;
 import subway.dao.StationDao;
 import subway.domain.Line;
 import subway.domain.Station;
-import subway.dto.LineRequest;
-import subway.dto.LineResponse;
-import subway.dto.StationDeleteRequest;
-import subway.dto.StationRegisterRequest;
-import subway.dto.StationsRegisterRequest;
+import subway.dto.*;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class LineService {
@@ -51,7 +48,40 @@ public class LineService {
     }
 
     public Line findLineById(Long id) {
-        return lineDao.findById(id);
+        Line rawLine = lineDao.findById(id);
+        List<SectionDto> sections = sectionDao.findAllByLineId(id);
+        if (sections.isEmpty()) {
+            return rawLine;
+        }
+
+        Map<Map.Entry<Station, Station>, Integer> distances = new HashMap<>();
+        sections.forEach(section ->
+            distances.put(Map.entry(stationDao.findById(section.getStationId()), stationDao.findById(section.getNextStationId())), section.getDistance()));
+
+        SectionDto section = sections.remove(0);
+        LinkedList<Long> stationIds = new LinkedList<>(List.of(section.getStationId(), section.getNextStationId()));
+
+        while(!sections.isEmpty()) {
+            for (int i = 0; i < sections.size(); i++) {
+                section = sections.get(i);
+                if (section.getStationId().equals(stationIds.getLast())) {
+                    stationIds.addLast(section.getNextStationId());
+                    sections.remove(i);
+                    break;
+                }
+                if (section.getNextStationId().equals(stationIds.getFirst())) {
+                    stationIds.addFirst(section.getStationId());
+                    sections.remove(i);
+                    break;
+                }
+            }
+        }
+
+        List<Station> stations = stationIds.stream()
+                .map(stationDao::findById)
+                .collect(Collectors.toList());
+
+        return new Line(rawLine.getId(), rawLine.getName(), rawLine.getColor(), new LinkedList<>(stations), distances);
     }
 
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
@@ -90,6 +120,16 @@ public class LineService {
         sectionDao.insertAll(toSectionDtos(line));
     }
 
+    public void deleteStation(Long lineId, StationDeleteRequest request) {
+        Line line = findLineById(lineId);
+        Station station = stationDao.findById(request.getStationId());
+        line.delete(station);
+
+        sectionDao.deleteAllByLineId(line.getId());
+        sectionDao.insertAll(toSectionDtos(line));
+    }
+    //현재 상행, 하행 모든 내용을 저장하고 있음
+
     private List<SectionDto> toSectionDtos(Line line) {
         return line.getAdjacentStations().stream()
                 .map(entry -> toSectionDto(
@@ -101,14 +141,5 @@ public class LineService {
 
     private SectionDto toSectionDto(Line line, Station station, Station nextStation) {
         return new SectionDto(line.getId(), station.getId(), nextStation.getId(), line.getDistanceBetween(station, nextStation));
-    }
-
-    public void deleteStation(Long lineId, StationDeleteRequest request) {
-        Line line = lineDao.findById(lineId);
-        Station station = stationDao.findById(request.getStationId());
-        line.delete(station);
-
-        sectionDao.deleteAllByLineId(line.getId());
-        sectionDao.insertAll(toSectionDtos(line));
     }
 }
