eab909fba4713874cfbf60bdcd52b1cdcad8fb24
zillionme <beatus96@naver.com>
2023-05-11 17:30:12 +0900

    feat: entity, dao 영속성 계층 추가

--- src/main/java/subway/dao/StationDao.java
+++ src/main/java/subway/dao/StationDao.java
@@ -1,5 +1,7 @@
 package subway.dao;
 
+import java.util.Optional;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -41,9 +43,13 @@ public class StationDao {
         return jdbcTemplate.query(sql, rowMapper);
     }
 
-    public Station findById(Long id) {
+    public Optional<Station> findById(Long id) {
         String sql = "select * from STATION where id = ?";
-        return jdbcTemplate.queryForObject(sql, rowMapper, id);
+        try {
+            return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, id));
+        } catch (DataAccessException exception) {
+            return Optional.empty();
+        }
     }
 
     public void update(Station newStation) {
