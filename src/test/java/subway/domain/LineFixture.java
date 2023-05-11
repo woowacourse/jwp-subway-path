cf804b8a93d706949e5abada90cefb29a8a721f2
Youngho <cyh6099@gmail.com>
2023-05-11 21:39:39 +0900

    chore: fixture추가

--- src/test/java/subway/domain/LineFixture.java
+++ src/test/java/subway/domain/LineFixture.java
@@ -3,5 +3,5 @@ package subway.domain;
 public class LineFixture {
     public static Line FIRST_LINE = new Line(1L, "1호선", "blue");
     public static Line SECOND_LINE = new Line(2L, "2호선", "green");
-    public static Line THIRD_LINE = new Line(3L, "3호선", "orange");
+    public static Line SECOND_LINE_NO_ID = new Line(null, "2호선", "green");
 }
--- src/test/java/subway/domain/StationFixture.java
+++ src/test/java/subway/domain/StationFixture.java
@@ -5,4 +5,9 @@ public class StationFixture {
     public static Station SEONLEUNG = new Station(2L, "선릉");
     public static Station GANGNAM = new Station(3L, "강남");
     public static Station YUKSAM = new Station(4L, "역삼");
+
+    public static Station JAMSIL_NO_ID = new Station(null, "잠실");
+    public static Station SEONLEUNG_NO_ID = new Station(null, "선릉");
+    public static Station GANGNAM_NO_ID = new Station(null, "강남");
+    public static Station YUKSAM_NO_ID = new Station(null, "역삼");
 }
