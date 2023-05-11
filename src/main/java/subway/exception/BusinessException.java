f6feb1f85787a15b9b1f46e44ecf77a439aa8f03
be-student <presentsong@naver.com>
2023-05-11 17:09:29 +0900

    feat: 예외 처리 작성

--- src/main/java/subway/exception/BusinessException.java
+++ src/main/java/subway/exception/BusinessException.java
@@ -2,6 +2,10 @@ package subway.exception;
 
 public class BusinessException extends RuntimeException {
 
+    public BusinessException() {
+        super();
+    }
+
     public BusinessException(final String message) {
         super(message);
     }
