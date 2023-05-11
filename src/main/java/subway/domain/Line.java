96b9c06e51f66835c4624e7c142bcd825562f580
2014012606 <2014012606@hanyang.ac.kr>
2023-05-11 10:55:06 +0900

    feat: Line 도메인 예외 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -14,21 +14,29 @@ public class Line {
     private String color;
 
     private final LinkedList<Station> stations;
-    private final Map<Map.Entry<Station, Station>, Integer> distances = new HashMap<>();
+    private final Map<Map.Entry<Station, Station>, Integer> distances;
 
     public Line(String name, String color) {
-        this(null, name, color);
+        this(null, name, color, new LinkedList<>(), new HashMap<>());
     }
 
     public Line(Long id, String name, String color) {
+        this(id, name, color, new LinkedList<>(), new HashMap<>());
+    }
+
+    public Line(Long id, String name, String color, LinkedList<Station> stations, Map<Map.Entry<Station, Station>, Integer> distances) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = new LinkedList<>();
+        this.stations = stations;
+        this.distances = distances;
     }
 
     public void insertBoth(Station top, Station bottom, int distance) {
         validateEmptyStations();
+        if (distance <= 0) {
+            throw new IllegalArgumentException("등록하려는 역의 거리 정보가 잘못되어 등록에 실패했습니다.");
+        }
 
         stations.add(top);
         stations.add(bottom);
@@ -43,7 +51,7 @@ public class Line {
     }
 
     public void insertUpper(Station station, Station base, int distance) {
-
+        validateStation(station, base);
         stations.add(stations.indexOf(base), station);
         insertDistanceBetween(station, base, distance);
 
@@ -53,14 +61,18 @@ public class Line {
 
         int previousDistance = getDistanceBetween(getUpperOf(station), base);
         int upperDistance = Math.abs(previousDistance - distance);
-        insertDistanceBetween(station, getUpperOf(station), upperDistance);
+        if (previousDistance <= distance || distance <= 0) {
+            throw new IllegalArgumentException("등록하려는 역의 거리 정보가 잘못되어 등록에 실패했습니다.");
+        }
 
+        insertDistanceBetween(getUpperOf(station), station, upperDistance);
         deleteDistanceBetween(getUpperOf(station), base);
     }
 
     public void insertLower(Station station, Station base, int distance) {
+        validateStation(station, base);
         stations.add(stations.indexOf(base) + 1, station);
-        insertDistanceBetween(station, base, distance);
+        insertDistanceBetween(base, station, distance);
 
         if (isBottom(station)) {
             return;
@@ -68,8 +80,22 @@ public class Line {
 
         int previousDistance = getDistanceBetween(getLowerOf(station), base);
         int lowerDistance = Math.abs(previousDistance - distance);
+        if (previousDistance <= distance || distance <= 0) {
+            throw new IllegalArgumentException("등록하려는 역의 거리 정보가 잘못되어 등록에 실패했습니다.");
+        }
+
         insertDistanceBetween(station, getLowerOf(station), lowerDistance);
-        deleteDistanceBetween(getLowerOf(station), base);
+        deleteDistanceBetween(base, getLowerOf(station));
+    }
+
+    private void validateStation(Station station, Station base) {
+        if(stations.contains(station)) {
+            throw new IllegalArgumentException("이미 등록된 역을 등록할 수 없습니다.");
+        }
+
+        if(!stations.contains(base)) {
+            throw new IllegalArgumentException("노선에 기준 역이 존재하지 않아 등록할 수 없습니다.");
+        }
     }
 
     public void delete(Station station) {
@@ -91,28 +117,28 @@ public class Line {
         }
 
         if (isBottom(station)) {
-            deleteDistanceBetween(station, getUpperOf(station));
+            deleteDistanceBetween(getUpperOf(station), station);
             stations.remove(station);
             return;
         }
 
         int upperDistance = getDistanceBetween(station, getUpperOf(station));
         int lowerDistance = getDistanceBetween(station, getLowerOf(station));
-        deleteDistanceBetween(station, getUpperOf(station));
+        deleteDistanceBetween(getUpperOf(station), station);
         deleteDistanceBetween(station, getLowerOf(station));
         insertDistanceBetween(getUpperOf(station), getLowerOf(station), upperDistance + lowerDistance);
         stations.remove(station);
     }
 
     public int getDistanceBetween(Station from, Station to) {
-        if (distances.get(Map.entry(from, to)) != null) {
-            return distances.get(Map.entry(from, to));
-        }
-
-        int distance = 0;
         int startInclusive = Math.min(stations.indexOf(from), stations.indexOf(to));
         int endInclusive = Math.max(stations.indexOf(from), stations.indexOf(to));
 
+        if (distances.get(Map.entry(stations.get(startInclusive), stations.get(endInclusive))) != null) {
+            return distances.get(Map.entry(stations.get(startInclusive), stations.get(endInclusive)));
+        }
+
+        int distance = 0;
         for (int i = startInclusive; i < endInclusive; i++) {
             Station station = stations.get(i);
             Station other = stations.get(i + 1);
@@ -122,14 +148,12 @@ public class Line {
         return distance;
     }
 
-    private void insertDistanceBetween(Station station, Station other, int distance) {
-        distances.put(Map.entry(station, other), distance);
-        distances.put(Map.entry(other, station), distance);
+    private void insertDistanceBetween(Station upper, Station lower, int distance) {
+        distances.put(Map.entry(upper, lower), distance);
     }
 
-    private void deleteDistanceBetween(Station station, Station other) {
-        distances.remove(Map.entry(station, other));
-        distances.remove(Map.entry(other, station));
+    private void deleteDistanceBetween(Station upper, Station lower) {
+        distances.remove(Map.entry(upper, lower));
     }
 
     private boolean isTop(Station station) {
---
+++ src/main/java/subway/exception/ApiExceptionHandler.java
@@ -0,0 +1,23 @@
+package subway.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        logger.error("예기치 못한 오류: " + e);
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
