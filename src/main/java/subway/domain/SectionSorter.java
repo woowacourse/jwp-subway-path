6447e08a3fba64b18533b312aa7550d63c5513ec
junpakPark <junpak.park@gmail.com>
2023-05-11 11:20:45 +0900

    refactor : SectionSorter 리팩터링

---
+++ src/main/java/subway/domain/SectionSorter.java
@@ -0,0 +1,52 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SectionSorter {
+
+	private static final SectionSorter instance = new SectionSorter();
+
+	private SectionSorter() {
+	}
+
+	public static SectionSorter getInstance() {
+		return instance;
+	}
+
+	public List<Section> sortSections(List<Section> sections) {
+		Map<Station, Section> departureToSection = createDepartureToSection(sections);
+		Station currentStation = getFirstStationFromSections(departureToSection);
+
+		List<Section> sortedSections = new ArrayList<>();
+
+		while (!departureToSection.isEmpty()) {
+			Section currentSection = departureToSection.remove(currentStation);
+			sortedSections.add(currentSection);
+			currentStation = currentSection.getArrival();
+		}
+
+		return sortedSections;
+	}
+
+	private Station getFirstStationFromSections(Map<Station, Section> sectionsMap) {
+		Set<Station> arrivalStations = sectionsMap.values().stream()
+			.map(Section::getArrival)
+			.collect(Collectors.toSet());
+
+		return sectionsMap.keySet().stream()
+			.filter(departureStation -> !arrivalStations.contains(departureStation))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("해당 노선에 시작역이 존재하지 않습니다."));
+	}
+
+	private Map<Station, Section> createDepartureToSection(List<Section> sections) {
+		return sections.stream()
+			.collect(Collectors.toMap(Section::getDeparture, Function.identity()));
+	}
+
+}
---
+++ src/test/java/subway/domain/StationSorterTest.java
@@ -0,0 +1,40 @@
+package subway.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+class StationSorterTest {
+	@Test
+	void sortingTest() {
+		// given
+		final List<Section> sections = createSections();
+		final List<Section> expectedSections = new ArrayList<>(sections);
+
+		Collections.shuffle(sections);
+
+		// when
+		final SectionSorter sectionSorter = SectionSorter.getInstance();
+		List<Section> sortedSections = sectionSorter.sortSections(sections);
+
+		// then
+		assertThat(sortedSections).isEqualTo(expectedSections);
+	}
+
+	private List<Section> createSections() {
+		final List<String> stationNames = List.of("잠실역", "잠실새내역", "종합운동장역", "삼성역", "선릉역");
+
+		return IntStream.range(0, stationNames.size() - 1)
+			.mapToObj(i -> new Section(1L,
+				new Station(1L, stationNames.get(i)),
+				new Station(1L, stationNames.get(i + 1)),
+				new Distance(1)))
+			.collect(Collectors.toList());
+	}
+}
