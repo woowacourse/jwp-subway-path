e4890f7db6519bce3f669bbf05bbfc675b50149c
jjongwa <troas96@naver.com>
2023-05-11 16:58:24 +0900

    feat: 더미 데이터 추가
    Co-authored-by: Combi153 <jcm04@naver.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,10 +20,29 @@ create table if not exists SECTION
     distance bigint                not null
 );
 
-create table if not exists ENDPOINT
-(
-    id      bigint auto_increment not null,
-    line_id bigint                not null,
-    top_id  bigint                not null,
-    down_id bigint                not null
-);
+insert into Station (name)
+values ('후추');
+insert into Station (name)
+values ('디노');
+insert into Station (name)
+values ('조앤');
+insert into Station (name)
+values ('로운');
+insert into Station (name)
+values ('포비');
+
+insert into Line (name, color)
+values ('2호선', 'Green');
+insert into Line (name, color)
+values ('8호선', 'pink');
+
+insert into SECTION (line_id, from_id, to_id, distance)
+values (1, 1, 2, 5);
+insert into SECTION (line_id, from_id, to_id, distance)
+values (1, 2, 3, 4);
+insert into SECTION (line_id, from_id, to_id, distance)
+values (1, 3, 4, 6);
+insert into SECTION (line_id, from_id, to_id, distance)
+values (2, 5, 4, 3);
+insert into SECTION (line_id, from_id, to_id, distance)
+values (2, 3, 5, 3);
