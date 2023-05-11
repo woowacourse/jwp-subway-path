3fcc840a8a5e5984642ac7864409388925bcd098
ingpyo <juninpyo@naver.com>
2023-05-11 17:02:17 +0900

    feat: 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,6 +9,18 @@ create table if not exists LINE
 (
     id bigint auto_increment not null,
     name varchar(255) not null unique,
-    color varchar(20) not null,
     primary key(id)
+);
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    up_station varchar(255) not null,
+    down_station varchar(255) not null,
+    distance int not null,
+    line_id int not null,
+    foreign key (line_id) references line (id) on delete cascade,
+    foreign key (up_station) references station (name) on delete cascade,
+    foreign key (down_station) references station (name) on delete cascade,
+    primary key (id)
 );
