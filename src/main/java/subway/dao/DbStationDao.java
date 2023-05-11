9c213804b8c84e08f0cefba561f31a525c43e310
gitchan <eunkeeeee@gmail.com>
2023-05-11 15:13:08 +0900

    feat(DbStationDao): 전체 조회 기능 추가
    Co-authored-by: aiaiaiai1 <cvc100326@naver.com>

--- src/main/java/subway/dao/DbStationDao.java
+++ src/main/java/subway/dao/DbStationDao.java
@@ -1,23 +1,32 @@
 package subway.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import subway.domain.Station;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class DbStationDao implements StationDao {
 
+    public static final RowMapper<Station> STATION_ROW_MAPPER = (resultSet, rowNum) -> new Station(
+            resultSet.getLong("id"),
+            resultSet.getString("name")
+    );
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final SimpleJdbcInsert insertStation;
 
     public DbStationDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
         this.insertStation = new SimpleJdbcInsert(dataSource)
                 .withTableName("station")
                 .usingGeneratedKeyColumns("id");
@@ -25,20 +34,22 @@ public class DbStationDao implements StationDao {
 
     @Override
     public Station saveStation(Station station) {
-        Map<String, Object> parameters = new HashMap<>(3);
-        parameters.put("name", station.getName());
+        SqlParameterSource parameters = new BeanPropertySqlParameterSource(station);
         final long newId = insertStation.executeAndReturnKey(parameters).longValue();
         return new Station(newId, station.getName());
     }
 
     @Override
-    public List<Number> saveStations(final List<Station> stations) {
-        return null;
+    public List<Station> findAll() {
+        final String sql = "select * from STATION";
+        return jdbcTemplate.query(sql, STATION_ROW_MAPPER);
     }
 
-//    public Long delete(Long stationId) {
-//        String sql = "delete from station where id = ?";
-//        jdbcTemplate.update(sql,stationId);
-//    }
+    @Override
+    public Station findById(final Long id) {
+        final String sql = "select * from STATION WHERE id = :id";
+        final Map<String, Long> parameter = Map.of("id", id);
+        return namedParameterJdbcTemplate.queryForObject(sql, parameter, STATION_ROW_MAPPER);
+    }
 }
 
--- src/test/java/subway/dao/DbStationDaoTest.java
+++ src/test/java/subway/dao/DbStationDaoTest.java
@@ -8,8 +8,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import subway.domain.Station;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static subway.fixture.StationFixture.EXPRESS_BUS_TERMINAL_STATION;
 import static subway.fixture.StationFixture.SAPYEONG_STATION;
 
 @JdbcTest
@@ -34,5 +36,24 @@ class DbStationDaoTest {
 
         assertThat(savedStation.getId()).isGreaterThanOrEqualTo(1L);
         assertThat(savedStation.getName()).isEqualTo(SAPYEONG_STATION.getName());
+    }
+
+    @Test
+    void findAll() {
+        stationDao.saveStation(SAPYEONG_STATION);
+        stationDao.saveStation(EXPRESS_BUS_TERMINAL_STATION);
+
+        List<Station> stations = stationDao.findAll();
+
+        assertThat(stations.size()).isEqualTo(2);
+    }
+
+    @Test
+    void findById() {
+        final Station savedStation = stationDao.saveStation(SAPYEONG_STATION);
+
+        final Station foundStation = stationDao.findById(savedStation.getId());
+
+        assertThat(foundStation).isEqualTo(savedStation);
     }
 }
