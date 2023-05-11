38cb9ffe80d4ad813de4dfb9bc51156215fac103
hyunseo <ohseo8813@gmail.com>
2023-05-11 15:29:00 +0900

    refactor: MethodArgumentNotValidException에 대한 예외 처리 추가

--- src/main/java/subway/exception/ExceptionControllerAdvice.java
+++ src/main/java/subway/exception/ExceptionControllerAdvice.java
@@ -1,14 +1,21 @@
 package subway.exception;
 
+import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -16,6 +23,13 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleAll(final Exception e) {
+        log.error("Ex : ", e);
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(e.getMessage()));
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequest(final IllegalArgumentException e) {
@@ -35,11 +49,15 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
                 .body(new ExceptionResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleAll(final Exception e) {
-        log.error("Ex : ", e);
-        return ResponseEntity.internalServerError()
-                .body(new ExceptionResponse(e.getMessage()));
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        String exceptionMessage = ex.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(joining(System.lineSeparator()));
+        return ResponseEntity.status(UNPROCESSABLE_ENTITY)
+                .body(new ExceptionResponse(exceptionMessage));
     }
 
     public static class ExceptionResponse {
