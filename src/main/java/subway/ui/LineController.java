19cb2453ed587ab622f538070f3696b71700af2b
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-11 15:51:33 +0900

    feat: GET /lines/stations 전체 노선 조회 기능 추가
    Co-authorized-by: dooboocookie <bkh3030@gmail.com>

--- src/main/java/subway/ui/LineController.java
+++ src/main/java/subway/ui/LineController.java
@@ -21,26 +21,13 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @PostMapping
-    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        LineResponse line = lineService.saveLine(lineRequest);
-        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
-    }
-
-    @GetMapping
+    @GetMapping("/stations")
     public ResponseEntity<List<LineResponse>> findAllLines() {
-        return ResponseEntity.ok(lineService.findLineResponses());
-    }
+        final List<LineResponse> findAllLines = lineService.findAllLines();
 
-    @GetMapping("/{id}")
-    public ResponseEntity<LineResponse> findLineById(@PathVariable Long id) {
-        return ResponseEntity.ok(lineService.findLineResponseById(id));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequest lineUpdateRequest) {
-        lineService.updateLine(id, lineUpdateRequest);
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(findAllLines);
     }
 
     @GetMapping("/{lineName}/stations")
