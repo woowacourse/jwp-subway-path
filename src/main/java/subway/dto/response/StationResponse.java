5eb45b76a01a917d677acacb30f0aef3bc73ba34
xxeol2 <jhj01177@naver.com>
2023-05-11 09:58:44 +0900

    refactor: 응답 객체 기본 생성자 추가
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

--- src/main/java/subway/dto/response/StationResponse.java
+++ src/main/java/subway/dto/response/StationResponse.java
@@ -4,8 +4,11 @@ import subway.entity.StationEntity;
 
 public class StationResponse {
 
-    private final Long id;
-    private final String name;
+    private Long id;
+    private String name;
+
+    public StationResponse() {
+    }
 
     public StationResponse(Long id, String name) {
         this.id = id;
