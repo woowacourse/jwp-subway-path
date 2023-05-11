1e409827f8501a8183e5a4802b9bc9ea9582e7de
This2sho <zx00018@naver.com>
2023-05-11 17:32:42 +0900

    feat(Section): Section 테이블 설계 및 insert 기능 구현

---
+++ src/main/java/subway/dao/SectionDao.java
@@ -0,0 +1,36 @@
+package subway.dao;
+
+import java.sql.PreparedStatement;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Repository;
+import subway.domain.Section;
+
+@Repository
+public class SectionDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public SectionDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+            .withTableName("section")
+            .usingGeneratedKeyColumns("id");
+    }
+
+    public long insert(Section section) {
+        String sql = "INSERT INTO SECTION (line_id, up_station_id, down_station_id, distance) VALUES (?, ?, ?, ?)";
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setLong(1, section.getLineId());
+            ps.setLong(2, section.getUpStationId());
+            ps.setLong(3, section.getDownStationId());
+            ps.setInt(4, section.getDistance());
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+}
