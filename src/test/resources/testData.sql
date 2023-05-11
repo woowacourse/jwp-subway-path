1b8850b7fd0ba379892ae9b12174363ad09c5d84
JunDonghyuk <ehdgur4814@naver.com>
2023-05-11 17:24:28 +0900

    refactor: 테스트 데이터 수정

--- src/test/resources/testData.sql
+++ src/test/resources/testData.sql
@@ -1,4 +1,6 @@
 insert into station(id,name) values(1, '잠실');
 insert into station(id,name) values(2, '잠실새내');
+insert into station(id,name) values(3, '종합운동장');
+
 insert into line (id, name, color) values(1, '2호선', 'green');
 insert into line_station (id, up_bound_id, down_bound_id, line_id, distance) values (1, 1, 2, 1, 10);
