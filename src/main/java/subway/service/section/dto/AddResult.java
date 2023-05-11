0a7f535932999ceaf347b35d31fcfff6bf77c3bf
Youngho <cyh6099@gmail.com>
2023-05-12 03:02:22 +0900

    chore: 네이밍 변경

--- src/main/java/subway/service/section/dto/AddResult.java
+++ src/main/java/subway/service/section/dto/AddResult.java
@@ -5,12 +5,12 @@ import subway.service.station.domain.Station;
 
 import java.util.List;
 
-public class AddResultDto {
+public class AddResult {
     private final List<Section> addedResults;
     private final List<Section> deletedResults;
     private final List<Station> addedStation;
 
-    public AddResultDto(List<Section> addedResults, List<Section> deletedResults, List<Station> addedStation) {
+    public AddResult(List<Section> addedResults, List<Section> deletedResults, List<Station> addedStation) {
         this.addedResults = addedResults;
         this.deletedResults = deletedResults;
         this.addedStation = addedStation;
