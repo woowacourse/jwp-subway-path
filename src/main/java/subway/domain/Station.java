dbb89803f13434f038e9d65b7c01f3d86781b971
This2sho <zx00018@naver.com>
2023-05-11 11:23:57 +0900

    refactor(Station): 생성자 수정

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -3,11 +3,9 @@ package subway.domain;
 import java.util.Objects;
 
 public class Station {
-    private Long id;
-    private String name;
 
-    public Station() {
-    }
+    private final Long id;
+    private final String name;
 
     public Station(Long id, String name) {
         this.id = id;
@@ -15,7 +13,7 @@ public class Station {
     }
 
     public Station(String name) {
-        this.name = name;
+        this(null, name);
     }
 
     public Long getId() {
