b655400e58368cb425d0f7161e34b4a61d4ce8ff
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-11 14:49:45 +0900

    feat: 역 검증 로직 추가
    Co-authored-by: jeomxon <jeomxon@gmail.com>

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -3,6 +3,9 @@ package subway.domain;
 import java.util.Objects;
 
 public class Station {
+
+    private static final String BLANK = " ";
+
     private Long id;
     private String name;
 
@@ -10,12 +13,31 @@ public class Station {
     }
 
     public Station(Long id, String name) {
+        validateName(name);
         this.id = id;
         this.name = name;
     }
 
     public Station(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateBlank(name);
+        validateLength(name);
+    }
+
+    private void validateBlank(String name) {
+        if (name.isBlank() || name.contains(BLANK)) {
+            throw new IllegalArgumentException("역 이름에는 공백이 허용되지 않습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() < 2 || name.length() > 10) {
+            throw new IllegalArgumentException("역 이름은 2~10자까지 가능합니다.");
+        }
     }
 
     public Long getId() {
@@ -28,8 +50,12 @@ public class Station {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Station station = (Station) o;
         return id.equals(station.id) && name.equals(station.name);
     }
---
+++ src/test/java/subway/domain/StationTest.java
@@ -0,0 +1,46 @@
+package subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StationTest {
+
+    @DisplayName("역이 정상적으로 생성된다.")
+    @Test
+    void createStationSuccess() {
+        // given
+        String name = "잠실역";
+
+        // when
+        Station station = new Station(name);
+
+        // then
+        assertThat(station.getName()).isEqualTo(name);
+    }
+
+    @DisplayName("역 이름에 공백이 들어가는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   ", "잠실 새내역"})
+    void createStationFailWithBlankName(String name) {
+        // when, then
+        assertThatThrownBy(() -> new Station(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("역 이름에는 공백이 허용되지 않습니다.");
+    }
+
+    @DisplayName("역 이름의 2이상 10이하가 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"강", "우아한테코크스백엔드역"})
+    void createStationFailWithWrongLength(String name) {
+        // when, then
+        assertThatThrownBy(() -> new Station(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("역 이름은 2~10자까지 가능합니다.");
+    }
+
+}
