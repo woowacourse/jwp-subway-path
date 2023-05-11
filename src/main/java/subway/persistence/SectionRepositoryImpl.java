c6eebb11e22307511d6921d8267b8466f7fd8383
ingpyo <juninpyo@naver.com>
2023-05-11 17:10:18 +0900

    feat: Section의 CRUD 기능 구현

---
+++ src/main/java/subway/persistence/SectionRepositoryImpl.java
@@ -0,0 +1,61 @@
+package subway.persistence;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import subway.domain.Section;
+import subway.domain.Station;
+import subway.domain.repository.SectionRepository;
+
+import java.util.List;
+
+@Repository
+public class SectionRepositoryImpl implements SectionRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insert;
+
+    private final RowMapper<Section> sectionRowMapper = (rs, rowNum) ->
+            new Section(
+                    rs.getLong("id"),
+                    new Station(rs.getString("up_station")),
+                    new Station(rs.getString("down_station")),
+                    rs.getLong("distance")
+            );
+
+    public SectionRepositoryImpl(final JdbcTemplate jdbcTemplate) {
+        this.insert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("section")
+                .usingGeneratedKeyColumns("id");
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Section> findAll() {
+        return null;
+    }
+
+    @Override
+    public void createSection(final Long lineId, final List<Section> sections) {
+        jdbcTemplate.update("TRUNCATE TABLE section");
+        List<SectionEntity> sectionEntities = SectionEntity.of(lineId, sections);
+
+        final BeanPropertySqlParameterSource[] parameterSources = sectionEntities.stream()
+                .map(BeanPropertySqlParameterSource::new)
+                .toArray(BeanPropertySqlParameterSource[]::new);
+        insert.executeBatch(parameterSources);
+    }
+
+    @Override
+    public List<Section> findAllByLineId(final Long lineId) {
+        String sql = "select * from section where line_id = ?";
+        return jdbcTemplate.query(sql, sectionRowMapper, lineId);
+    }
+
+    @Override
+    public void deleteBySection(final Long lineId, final Section section) {
+        String sql = "delete from section where line_id = ? and up_station = ? or down_station = ?";
+        jdbcTemplate.update(sql, lineId, section.getUpStation(), section.getDownStation());
+    }
+}
