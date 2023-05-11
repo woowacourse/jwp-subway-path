12597ee669f2b54691bdc50a268258c4e3342cb0
This2sho <zx00018@naver.com>
2023-05-11 17:39:59 +0900

    feat(GlobalExceptionHandler): 존재하지 않는 id가 들어왔을 때 처리하는 기능 구현

---
+++ src/main/java/subway/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package subway.exceptionHandler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(
+        DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
