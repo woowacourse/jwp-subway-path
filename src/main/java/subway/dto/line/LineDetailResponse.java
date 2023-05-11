e18f150b638e177dd134fc5287ff01beb273029f
seokjin8678 <seokjin8678@gmail.com>
2023-05-11 16:27:58 +0900

    refactor: LineDetailResponse 필드명 JsonProperty 적용
    Co-authored-by: somsom13 <somin1113@naver.com>

--- src/main/java/subway/dto/line/LineDetailResponse.java
+++ src/main/java/subway/dto/line/LineDetailResponse.java
@@ -1,10 +1,14 @@
 package subway.dto.line;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import subway.dto.section.SectionResponse;
 
 public class LineDetailResponse {
+
+    @JsonProperty("line")
     private LineResponse lineResponse;
+    @JsonProperty("sections")
     private List<SectionResponse> sectionResponses;
 
     public LineDetailResponse() {
