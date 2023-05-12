01262c39b52332b94c3061af904afe7b226b535d
sosow0212 <sosow0212@naver.com>
2023-05-12 23:15:35 +0900

    test: StationService 통합 및 Mock을 이용한 단위테스트 작성

---
+++ src/test/java/subway/service/station/StationServiceIntegrationTest.java
@@ -0,0 +1,87 @@
+package subway.service.station;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import subway.domain.subway.Station;
+import subway.dto.station.StationCreateRequest;
+import subway.dto.station.StationResponse;
+import subway.dto.station.StationsResponse;
+import subway.repository.StationRepository;
+import subway.service.StationService;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("/data.sql")
+public class StationServiceIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StationService stationService;
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = this.port;
+    }
+
+    @Test
+    @DisplayName("역을 저장한다.")
+    void save_section_success() {
+        // given
+        StationCreateRequest stationCreateRequest = new StationCreateRequest("잠실역");
+
+        // when
+        stationService.saveStation(stationCreateRequest);
+
+        // then
+        StationsResponse expected = stationService.findAllStationResponses();
+
+        assertAll(
+                () -> assertThat(expected.getStations().size()).isEqualTo(1),
+                () -> assertThat(expected.getStations().get(0).getName()).isEqualTo(stationCreateRequest.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("역을 찾는다.")
+    void find_station_success() {
+        // given
+        Station station = new Station("잠실역");
+        Long id = stationRepository.insertStation(station);
+
+        // when
+        StationResponse result = stationService.findStationEntityById(id);
+
+        // then
+        assertThat(result.getName()).isEqualTo(station.getName());
+    }
+
+    @Test
+    @DisplayName("역을 모두 찾는다.")
+    void find_stations_success() {
+        // given
+        Station station = new Station("잠실역");
+        stationRepository.insertStation(station);
+
+        // when
+        StationsResponse result = stationService.findAllStationResponses();
+
+        // then
+        assertAll(
+                () -> assertThat(result.getStations().size()).isEqualTo(1),
+                () -> assertThat(result.getStations().get(0).getName()).isEqualTo(station.getName())
+        );
+    }
+}
---
+++ src/test/java/subway/service/station/StationServiceMockTest.java
@@ -0,0 +1,77 @@
+package subway.service.station;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import subway.domain.subway.Station;
+import subway.dto.station.StationCreateRequest;
+import subway.dto.station.StationResponse;
+import subway.dto.station.StationsResponse;
+import subway.repository.StationRepository;
+import subway.service.StationService;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class StationServiceMockTest {
+
+    @InjectMocks
+    private StationService stationService;
+
+    @Mock
+    private StationRepository stationRepository;
+
+    @Test
+    @DisplayName("역을 저장한다.")
+    void save_section_success() {
+        // given
+        StationCreateRequest stationCreateRequest = new StationCreateRequest("잠실역");
+
+        // when
+        stationService.saveStation(stationCreateRequest);
+
+        // then
+        verify(stationRepository).insertStation(new Station(stationCreateRequest.getName()));
+    }
+
+    @Test
+    @DisplayName("역을 찾는다.")
+    void find_station_success() {
+        // given
+        Long id = 1L;
+        Station station = new Station("잠실역");
+        when(stationRepository.findByStationId(id)).thenReturn(station);
+
+        // when
+        StationResponse result = stationService.findStationEntityById(id);
+
+        // then
+        assertThat(result.getName()).isEqualTo(station.getName());
+    }
+
+    @Test
+    @DisplayName("역을 모두 찾는다.")
+    void find_stations_success() {
+        // given
+        List<Station> stations = List.of(new Station("잠실역"));
+        when(stationRepository.findAll()).thenReturn(stations);
+        when(stationRepository.findStationIdByStationName("잠실역")).thenReturn(1L);
+
+        // when
+        StationsResponse result = stationService.findAllStationResponses();
+
+        // then
+        assertAll(
+                () -> assertThat(result.getStations().size()).isEqualTo(1),
+                () -> assertThat(result.getStations().get(0).getName()).isEqualTo(stations.get(0).getName())
+        );
+    }
+}
