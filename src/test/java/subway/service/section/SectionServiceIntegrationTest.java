6f99eb02018beb3950d7d7424d67012126feb498
sosow0212 <sosow0212@naver.com>
2023-05-12 23:37:53 +0900

    test: SectionService 통합 및 Mock을 이용한 단위테스트 작성

---
+++ src/test/java/subway/service/section/SectionServiceIntegrationTest.java
@@ -0,0 +1,87 @@
+package subway.service.section;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import subway.domain.subway.Sections;
+import subway.domain.subway.Station;
+import subway.dto.section.SectionCreateRequest;
+import subway.dto.section.SectionDeleteRequest;
+import subway.entity.LineEntity;
+import subway.repository.LineRepository;
+import subway.repository.SectionRepository;
+import subway.repository.StationRepository;
+import subway.service.SectionService;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static subway.factory.SectionsFactory.createSections;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("/data.sql")
+public class SectionServiceIntegrationTest {
+
+    @Autowired
+    private SectionService sectionService;
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    @Autowired
+    private LineRepository lineRepository;
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @Test
+    @DisplayName("구간을 삽입한다.")
+    void save_section_success() {
+        // given
+        SectionCreateRequest req = new SectionCreateRequest("2호선", "종합운동장역", "삼성역", 3L);
+
+        stationRepository.insertStation(new Station("잠실역"));
+        stationRepository.insertStation(new Station("잠실새내역"));
+        stationRepository.insertStation(new Station("종합운동장역"));
+        stationRepository.insertStation(new Station("삼성역"));
+
+        lineRepository.insertLine(new LineEntity(1L, 2L, "2호선", "초록색"));
+        lineRepository.updateLine(createSections(), 2L);
+
+        // when
+        sectionService.insertSection(req);
+
+        // then
+        Sections sections = sectionRepository.findSectionsByLineName("2호선");
+        assertAll(
+                () -> assertThat(sections.getSections().size()).isEqualTo(3),
+                () -> assertThat(sections.getSections().get(0).getUpStation().getName()).isEqualTo("잠실역")
+        );
+
+    }
+
+    @Test
+    @DisplayName("구간을 삭제한다.")
+    void delete_section_success() {
+        // given
+        SectionDeleteRequest req = new SectionDeleteRequest(2L, "종합운동장역");
+
+        stationRepository.insertStation(new Station("잠실역"));
+        stationRepository.insertStation(new Station("잠실새내역"));
+        stationRepository.insertStation(new Station("종합운동장역"));
+
+        lineRepository.insertLine(new LineEntity(1L, 2L, "2호선", "초록색"));
+        lineRepository.updateLine(createSections(), 2L);
+
+        // when
+        sectionService.deleteSection(req);
+
+        // then
+        Sections sections = sectionRepository.findSectionsByLineName("2호선");
+        assertAll(
+                () -> assertThat(sections.getSections().size()).isEqualTo(1),
+                () -> assertThat(sections.getSections().get(0).getUpStation().getName()).isEqualTo("잠실역")
+        );
+    }
+}
---
+++ src/test/java/subway/service/section/SectionServiceMockTest.java
@@ -0,0 +1,65 @@
+package subway.service.section;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import subway.domain.subway.Line;
+import subway.domain.subway.Sections;
+import subway.dto.section.SectionCreateRequest;
+import subway.dto.section.SectionDeleteRequest;
+import subway.repository.LineRepository;
+import subway.repository.SectionRepository;
+import subway.service.SectionService;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static subway.factory.SectionsFactory.createSections;
+
+@ExtendWith(MockitoExtension.class)
+public class SectionServiceMockTest {
+
+    @InjectMocks
+    private SectionService sectionService;
+
+    @Mock
+    private SectionRepository sectionRepository;
+
+    @Mock
+    private LineRepository lineRepository;
+
+    @Test
+    @DisplayName("구간을 삽입한다.")
+    void save_section_success() {
+        // given
+        SectionCreateRequest req = new SectionCreateRequest("2호선", "종합운동장역", "삼성역", 3L);
+        Sections sections = createSections();
+        Line line = new Line(sections, 2L, "2호선", "green");
+
+        given(sectionRepository.findSectionsByLineName(req.getLineName())).willReturn(sections);
+        given(lineRepository.findByLineNameAndSections(req.getLineName(), sections)).willReturn(line);
+
+        // when
+        sectionService.insertSection(req);
+
+        // then
+        verify(lineRepository).updateLine(sections, line.getLineNumber());
+    }
+
+    @Test
+    @DisplayName("구간을 삭제한다.")
+    void delete_section_success() {
+        // given
+        SectionDeleteRequest req = new SectionDeleteRequest(2L, "종합운동장역");
+        Sections sections = createSections();
+        given(sectionRepository.findSectionsByLineNumber(req.getLineNumber())).willReturn(sections);
+
+        // when
+        sectionService.deleteSection(req);
+
+        // then
+        verify(lineRepository).updateLine(sections, req.getLineNumber());
+    }
+}
