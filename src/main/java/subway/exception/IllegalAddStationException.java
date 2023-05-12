4012e6a80d1e56f24295415f9e5f4e3a1905decc
kdkdhoho <hkim4410@naver.com>
2023-05-12 15:36:33 +0900

    rename: 역을 추가할 때 발생하는 커스텀 예외 네이밍 수정

---
+++ src/main/java/subway/exception/IllegalAddStationException.java
@@ -0,0 +1,8 @@
+package subway.exception;
+
+public class IllegalAddStationException extends RuntimeException {
+
+    public IllegalAddStationException(final String message) {
+        super(message);
+    }
+}
--- src/main/java/subway/exception/IllegalStationAddException.java
+++
@@ -1,8 +0,0 @@
-package subway.exception;
-
-public class IllegalStationAddException extends RuntimeException {
-
-    public IllegalStationAddException(final String message) {
-        super(message);
-    }
-}
