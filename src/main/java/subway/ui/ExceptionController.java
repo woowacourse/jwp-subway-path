0d63d53108c3e8d13e2eb4fef4c1ccd4a86c06cd
70825 <hello70825@gmail.com>
2023-05-12 17:54:50 +0900

    fix: 인수 테스트 통과하지 못하는 현상 수정
    Co-authored-by: apptie <appti@naver.com>

--- src/main/java/subway/ui/ExceptionController.java
+++ src/main/java/subway/ui/ExceptionController.java
@@ -4,22 +4,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 import subway.ui.dto.response.ExceptionResponse;
+
+import java.sql.SQLException;
 
 @RestControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleException(final Exception ex) {
-        logger.error("Exception : ", ex);
-
-        return ResponseEntity.internalServerError().body(new ExceptionResponse("예상치 못한 예외가 발생했습니다."));
-    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(final IllegalArgumentException ex) {
         logger.error("IllegalArgumentException : ", ex);
 
         return ResponseEntity.badRequest().body(new ExceptionResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler({SQLException.class, DuplicateKeyException.class})
+    public ResponseEntity<ExceptionResponse> handleDuplicateKeyException(final Exception ex) {
+        logger.error("SQLException : ", ex);
+
+        return ResponseEntity.badRequest().body(new ExceptionResponse("이미 존재하는 값입니다."));
     }
 }
