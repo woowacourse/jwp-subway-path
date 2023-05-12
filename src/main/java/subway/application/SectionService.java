91de954baf2fbdb8bb3ba4f42be5ffaf9d137528
kiarakim <kiarakim413@gmail.com>
2023-05-12 15:09:06 +0900

    style: 쓰이지 않는 필드 제거

--- src/main/java/subway/application/SectionService.java
+++ src/main/java/subway/application/SectionService.java
@@ -1,6 +1,7 @@
 package subway.application;
 
 import org.springframework.stereotype.Service;
+
 import subway.domain.Section;
 import subway.domain.Sections;
 import subway.domain.repository.LineRepository;
@@ -12,41 +13,36 @@ import java.util.List;
 
 @Service
 public class SectionService {
-    private final LineRepository lineRepository;
+	private final SectionRepository sectionRepository;
 
-    private final StationRepository stationRepository;
-    private final SectionRepository sectionRepository;
+	public SectionService(final SectionRepository sectionRepository) {
+		this.sectionRepository = sectionRepository;
+	}
 
-    public SectionService(final LineRepository lineRepository, final StationRepository stationRepository, final SectionRepository sectionRepository) {
-        this.lineRepository = lineRepository;
-        this.stationRepository = stationRepository;
-        this.sectionRepository = sectionRepository;
-    }
+	public void createSection(final Long lineId, final SectionCreateRequest sectionCreateRequest) {
+		final Section section = Section.of(
+			sectionCreateRequest.getUpStation(),
+			sectionCreateRequest.getDownStation(),
+			sectionCreateRequest.getDistance()
+		);
 
-    public void createSection(final Long lineId, final SectionCreateRequest sectionCreateRequest) {
-        final Section section = Section.of(
-                sectionCreateRequest.getUpStation(),
-                sectionCreateRequest.getDownStation(),
-                sectionCreateRequest.getDistance()
-        );
+		final Sections sections = new Sections(sectionRepository.findAllByLineId(lineId));
+		sections.addSection(section);
+		sectionRepository.createSection(lineId, sections.getSections());
+	}
 
-        final Sections sections = new Sections(sectionRepository.findAllByLineId(lineId));
-        sections.addSection(section);
-        sectionRepository.createSection(lineId, sections.getSections());
-    }
+	public void deleteSection(final Long lineId, final SectionDeleteRequest sectionDeleteRequest) {
+		final Section section = Section.of(
+			sectionDeleteRequest.getUpStationName(),
+			sectionDeleteRequest.getDownStationName()
+		);
 
-    public void deleteSection(final Long lineId, final SectionDeleteRequest sectionDeleteRequest) {
-        final Section section = Section.of(
-                sectionDeleteRequest.getUpStationName(),
-                sectionDeleteRequest.getDownStationName()
-        );
+		sectionRepository.deleteBySection(lineId, section);
+	}
 
-        sectionRepository.deleteBySection(lineId, section);
-    }
+	public List<StationResponse> findAllByLine(final Long lineId) {
+		final Sections sections = new Sections(sectionRepository.findAllByLineId(lineId));
 
-    public List<StationResponse> findAllByLine(final Long lineId) {
-        final Sections sections = new Sections(sectionRepository.findAllByLineId(lineId));
-
-        return StationResponse.of(sections.getSortedStations());
-    }
+		return StationResponse.of(sections.getSortedStations());
+	}
 }
