34355fd6200e9093f3d817859c25759f773e79dd
JunDonghyuk <ehdgur4814@naver.com>
2023-05-12 17:18:21 +0900

    feat: 테스트 데이터 추가

--- src/test/resources/testData.sql
+++ src/test/resources/testData.sql
@@ -1,6 +1,15 @@
 insert into station(id,name) values(1, '잠실');
 insert into station(id,name) values(2, '잠실새내');
 insert into station(id,name) values(3, '종합운동장');
+insert into station(id,name) values(4, '삼성');
+insert into station(id,name) values(5, '천호');
+
 
 insert into line (id, name, color) values(1, '2호선', 'green');
+insert into line (id, name, color) values(2, '8호선', 'red');
+insert into line (id, name, color) values(3, '1호선', 'blue');
+
 insert into line_station (id, up_bound_id, down_bound_id, line_id, distance) values (1, 1, 2, 1, 10);
+insert into line_station (id, up_bound_id, down_bound_id, line_id, distance) values (2, 2, 3, 1, 10);
+
+insert into line_station (id, up_bound_id, down_bound_id, line_id, distance) values (3, 5, 1, 2, 10);
