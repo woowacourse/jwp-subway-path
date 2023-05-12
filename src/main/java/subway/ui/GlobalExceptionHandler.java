8ef5460beff42d6459a645df416751d95291350b
amaran-th <songsy405@gmail.com>
2023-05-12 09:27:50 +0900

    feat:예외 핸들러 구현

---
+++ src/main/java/subway/ui/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import subway.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(Exception exception) {
+        ErrorResponse errorResponse = new ErrorResponse("[ERROR] "+exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
