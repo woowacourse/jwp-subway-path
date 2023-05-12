af375af86f20aa5c9218f943bda6f54995bd3ae0
nuyh <hjaehyun25@gmail.com>
2023-05-13 02:41:22 +0900

    feat: 서버 내부 예외 로깅 추가

--- src/main/java/subway/ui/GlobalExceptionHandler.java
+++ src/main/java/subway/ui/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package subway.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public final class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger("Internal Error: ");
+
     @ExceptionHandler
     public ResponseEntity<String> handle(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
@@ -13,7 +17,7 @@ public final class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handle(Exception e) {
-        e.printStackTrace();
+        logger.info(e.getMessage(), e);
         return ResponseEntity.internalServerError().body("예상치 못한 에러가 발생했습니다.");
     }
 }
