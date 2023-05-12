e115fe4c3db6ae74de13fc4ce8a89208638bc3f8
gitchan <eunkeeeee@gmail.com>
2023-05-12 15:15:52 +0900

    feat(schema.sql): 테이블 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,27 @@
-create table if not exists STATION
+DROP TABLE IF EXISTS station;
+CREATE TABLE station
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
+    id   BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
-create table if not exists LINE
+DROP TABLE IF EXISTS line;
+CREATE TABLE line
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(id)
+    id    BIGINT AUTO_INCREMENT NOT NULL,
+    name  VARCHAR(50) NOT NULL UNIQUE,
+    color VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS section;
+CREATE TABLE section
+(
+    id              BIGINT AUTO_INCREMENT NOT NULL,
+    line_id         BIGINT NOT NULL,
+    up_station_id   BIGINT NOT NULL,
+    down_station_id BIGINT NOT NULL,
+    distance        INT    NOT NULL,
+    PRIMARY KEY (id)
 );
