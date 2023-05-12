f8c131c8646f54c1d1921052449a64b1648a1694
kevstevie <jianna442@gmail.com>
2023-05-12 18:08:15 +0900

    feat: direction 정적 팩토리 메서드 추가

--- src/main/java/subway/application/PathService.java
+++ src/main/java/subway/application/PathService.java
@@ -27,7 +27,7 @@ public class PathService {
         final Station targetStation = subwayRepository.findStationById(targetStationId);
         final Station addStation = subwayRepository.findStationById(addStationId);
 
-        line.addPath(targetStation, addStation, distance, Direction.valueOf(direction));
+        line.addPath(targetStation, addStation, distance, Direction.of(direction));
 
         subwayRepository.saveLine(line);
     }
