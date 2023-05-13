333ccafa60a9409f52aa66be4c14eb3783edaf02
JunDonghyuk <ehdgur4814@naver.com>
2023-05-14 01:57:29 +0900

    test: given /when /then 적용

--- src/test/java/subway/dao/LineDaoTest.java
+++ src/test/java/subway/dao/LineDaoTest.java
@@ -35,8 +35,13 @@ class LineDaoTest {
     @DisplayName("라인을 추가한다.")
     @Test
     void insert() {
+        // given
         Line line = new Line("2", "green");
+
+        // when
         Line insertedLine = lineDao.insert(line);
+
+        // then
         assertThat(insertedLine.getName()).isEqualTo(line.getName());
         assertThat(insertedLine.getColor()).isEqualTo(line.getColor());
     }
@@ -44,8 +49,13 @@ class LineDaoTest {
     @DisplayName("이름이 같은 라인을 넣으면 예외를 던진다.")
     @Test
     void insertDuplicatedName() {
+        // given
         Line line = new Line("2", "green");
+
+        // when
         Line insertedLine = lineDao.insert(line);
+
+        // then
         assertThatThrownBy(() -> lineDao.insert(line)).isInstanceOf(InvalidDataException.class);
         assertThat(insertedLine.getColor()).isEqualTo(line.getColor());
     }
@@ -53,43 +63,57 @@ class LineDaoTest {
     @DisplayName("전체 조회를 한다.")
     @Test
     void findAll() {
+        // given
         Line firstLine = new Line("1", "blue");
         Line secondLine = new Line("2", "green");
         Line thirdLine = new Line("3", "orange");
+
+        // when
         Line first = lineDao.insert(firstLine);
         Line second = lineDao.insert(secondLine);
         Line third = lineDao.insert(thirdLine);
 
+        // then
         assertThat(lineDao.findAll().containsAll(List.of(first, second, third)));
     }
 
     @DisplayName("아이디로 조회한다.")
     @Test
     void findById() {
+        // given
         Line firstLine = new Line("1", "blue");
+
+        // when
         Line first = lineDao.insert(firstLine);
 
+        // then
         assertThat(lineDao.findById(first.getId())).isEqualTo(first);
     }
 
     @DisplayName("없는 아이디로 조회하면 예외를 던진다.")
     @Test
     void findByNotExistId() {
+        // when, then
         assertThatThrownBy(() -> lineDao.findById(10L)).isInstanceOf(NotFoundException.class);
     }
 
     @DisplayName("이름으로 조회한다.")
     @Test
     void findByName() {
+        // given
         Line firstLine = new Line("1", "blue");
+
+        // when
         Line first = lineDao.insert(firstLine);
 
+        // then
         assertThat(lineDao.findByName(first.getName())).isEqualTo(first);
     }
 
     @DisplayName("없는 이름으로 조회하면 예외를 던진다.")
     @Test
     void findByNotExistName() {
+        // when, then
         assertThatThrownBy(() -> lineDao.findByName("hardy")).isInstanceOf(NotFoundException.class);
     }
 
@@ -97,18 +121,29 @@ class LineDaoTest {
     @DisplayName("업데이트를 한다.")
     @Test
     void update() {
+        // given
         Line line = new Line("1", "blue");
         Line originLine = lineDao.insert(line);
+
+        // when
         lineDao.update(new Line(originLine.getId(), "2", "green"));
-        assertThat(lineDao.findById(originLine.getId())).isNotEqualTo(originLine);
+
+        // then
+        assertThat(lineDao.findById(originLine.getId()).getName()).isNotEqualTo(originLine.getName());
+        assertThat(lineDao.findById(originLine.getId()).getColor()).isNotEqualTo(originLine.getColor());
     }
 
     @DisplayName("삭제를 한다.")
     @Test
     void deleteById() {
+        // given
         Line line = new Line("1", "blue");
         Line originLine = lineDao.insert(line);
+
+        // when
         lineDao.deleteById(originLine.getId());
+
+        // then
         assertThat(lineDao.findAll()).isEmpty();
     }
 }
--- src/test/java/subway/dao/StationDaoTest.java
+++ src/test/java/subway/dao/StationDaoTest.java
@@ -35,23 +35,31 @@ class StationDaoTest {
     @DisplayName("역을 추가한다.")
     @Test
     void insert() {
+        // given
         Station station = new Station("잠실");
+
+        // when
         Station insertedStation = stationDao.insert(station);
+
+        // then
         assertThat(insertedStation.getName()).isEqualTo(station.getName());
     }
 
     @DisplayName("이름이 같은 역을 넣으면 예외를 던진다.")
     @Test
     void insertDuplicatedName() {
+        // given
         Station station = new Station("잠실");
         Station insertedStation = stationDao.insert(station);
 
+        // when, then
         assertThatThrownBy(() -> stationDao.insert(station)).isInstanceOf(InvalidDataException.class);
     }
 
     @DisplayName("전체 조회를 한다.")
     @Test
     void findAll() {
+        // given
         Station firstStation = new Station("잠실");
         Station secondStation = new Station("선릉");
         Station thirdStation = new Station("잠실나루");
@@ -60,58 +68,71 @@ class StationDaoTest {
         Station second = stationDao.insert(secondStation);
         Station third = stationDao.insert(thirdStation);
 
+        // when, then
         assertThat(stationDao.findAll().containsAll(List.of(first, second, third)));
     }
 
     @DisplayName("아이디로 조회한다.")
     @Test
     void findById() {
+        // given
         Station firstStation = new Station("잠실");
         Station first = stationDao.insert(firstStation);
 
+        // when, then
         assertThat(stationDao.findById(first.getId())).isEqualTo(first);
     }
 
     @DisplayName("없는 아이디로 조회하면 예외를 던진다.")
     @Test
     void findByNotExistId() {
+        // when, then
         assertThatThrownBy(() -> stationDao.findById(10L)).isInstanceOf(NotFoundException.class);
     }
 
     @DisplayName("이름으로 조회한다.")
     @Test
     void findByName() {
+        // given
         Station firstStation = new Station("잠실");
         Station first = stationDao.insert(firstStation);
 
+        // when, then
         assertThat(stationDao.findByName(first.getName())).isEqualTo(first);
     }
 
     @DisplayName("없는 이름으로 조회하면 예외를 던진다.")
     @Test
     void findByNotExistName() {
+        // when, then
         assertThatThrownBy(() -> stationDao.findByName("hardy")).isInstanceOf(NotFoundException.class);
     }
 
     @DisplayName("업데이트를 한다.")
     @Test
     void update() {
+        // given
         Station station = new Station("잠실");
         Station originStation = stationDao.insert(station);
 
+        // when
         stationDao.update(new Station(originStation.getId(), "잠실나루"));
 
-        assertThat(stationDao.findById(originStation.getId())).isNotEqualTo(new Station(originStation.getId(), "잠실"));
+        // then
+        assertThat(stationDao.findById(originStation.getId()).getName()).isNotEqualTo(station.getName());
     }
 
     @DisplayName("삭제를 한다.")
     @Test
     void deleteById() {
+        // given
         Station station = new Station("잠실");
         Station originStation = stationDao.insert(station);
 
+        // when
         stationDao.deleteById(originStation.getId());
 
+        // then
         assertThat(stationDao.findAll()).isEmpty();
     }
 }
