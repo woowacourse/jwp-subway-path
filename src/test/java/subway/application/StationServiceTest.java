737d3584c892baaa64119f669f1374317ff3045b
mcodnjs <codnjs8989@gmail.com>
2023-05-13 14:43:06 +0900

    refactor: StationServiceTest 개행 추가

--- src/test/java/subway/application/StationServiceTest.java
+++ src/test/java/subway/application/StationServiceTest.java
@@ -22,45 +22,58 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
+
 @ExtendWith(MockitoExtension.class)
 class StationServiceTest {
+
     @Mock
     private StationDao stationDao;
+
     private StationService stationService;
+
     @BeforeEach
     void setUp() {
         stationService = new StationService(stationDao);
     }
+
     @DisplayName("station이 정상적으로 저장되고 stationId 값을 반환한다.")
     @Test
     void save() {
         // given
         given(stationDao.insert(any())).willReturn(1L);
+
         // when
         Long id = stationService.save(new StationRequest("강남역"));
+
         // then
         assertThat(id).isEqualTo(1L);
     }
+
     @DisplayName("stationId로 station을 조회할 수 있다.")
     @Test
     void findById() {
         // given
         given(stationDao.findById(anyLong())).willReturn(Optional.of(new Station("강남역")));
+
         // when
         StationResponse stationResponse = stationService.findById(1L);
+
         // then
         assertThat(stationResponse.getName()).isEqualTo("강남역");
     }
+
     @DisplayName("stationId로 station을 조회할 때, 역이 존재하지 않으면 예외 처리한다.")
     @Test
     void findByIdWhenEmpty() {
         // given
         given(stationDao.findById(anyLong())).willReturn(Optional.empty());
+
         // when & then
         assertThatThrownBy(() -> stationService.findById(1L))
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessageContaining("해당하는 역이 존재하지 않습니다.");
     }
+
     @DisplayName("모든 station들을 조회할 수 있다.")
     @Test
     void findAll() {
@@ -68,13 +81,16 @@ class StationServiceTest {
         final Station station1 = new Station("강남역");
         final Station station2 = new Station("선릉역");
         given(stationDao.findAll()).willReturn(List.of(station1, station2));
+
         // when
         List<StationResponse> stationResponse = stationService.findAll();
+
         // then
         assertThat(stationResponse.size()).isEqualTo(2);
         assertThat(stationResponse.get(0).getName()).isEqualTo(station1.getName());
         assertThat(stationResponse.get(1).getName()).isEqualTo(station2.getName());
     }
+
     @DisplayName("stationId와 stationRequest로 station을 수정할 수 있다.")
     @Test
     void update() {
@@ -82,17 +98,21 @@ class StationServiceTest {
         given(stationDao.insert(any())).willReturn(1L);
         Long id = stationService.save(new StationRequest("강남역"));
         willDoNothing().given(stationDao).update(anyLong(), any(Station.class));
+
         // when
         stationService.update(id, new StationRequest("서울역"));
         given(stationDao.findById(id)).willReturn(Optional.of(new Station("서울역")));
+
         // then
         assertThat(stationService.findById(id).getName()).isEqualTo("서울역");
     }
+
     @DisplayName("stationId로 station을 삭제할 수 있다.")
     @Test
     void deleteById() {
         // given
         willDoNothing().given(stationDao).deleteById(anyLong());
+
         // when & then
         assertDoesNotThrow(() -> stationService.deleteById(1L));
     }
