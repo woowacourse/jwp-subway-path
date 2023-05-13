e155c099958726d86b104a13ea29ec01ec60e22c
Be-student <presentsong@naver.com>
2023-05-14 03:30:10 +0900

    feat: lineColor 기능 구현

---
+++ src/main/java/subway/exception/line/LineColorException.java
@@ -0,0 +1,10 @@
+package subway.exception.line;
+
+import subway.exception.BusinessException;
+
+public class LineColorException extends BusinessException {
+
+    public LineColorException(final String message) {
+        super(message);
+    }
+}
