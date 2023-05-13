e881454846f248d036fd474b9e067a3b36ddea1a
donghae <donghae1999@gmail.com>
2023-05-13 16:59:24 +0900

    test: @Nested 를 사용한 테스트 코드 수정

--- src/test/java/subway/domain/LineTest.java
+++ src/test/java/subway/domain/LineTest.java
@@ -1,5 +1,7 @@
 package subway.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,17 +14,21 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class LineTest {
 
-    @ParameterizedTest(name = "호선 이름은 1글자 이상, 10글자 이하여야 한다")
-    @ValueSource(strings = {"선", "경의잠실중앙나루호선"})
-    void createLineInfoSuccessTest(String name) {
-        assertDoesNotThrow(() -> new Line(name, new Sections(Collections.emptyList())));
-    }
+    @Nested
+    @DisplayName("노선을 추가한다.")
+    class addLine {
+        @ParameterizedTest(name = "호선 이름은 1글자 이상, 10글자 이하여야 한다")
+        @ValueSource(strings = {"선", "경의잠실중앙나루호선"})
+        void createLineInfoSuccessTest(String name) {
+            assertDoesNotThrow(() -> new Line(name, new Sections(Collections.emptyList())));
+        }
 
-    @ParameterizedTest(name = "호선 이름은 1글자 이상, 10글자 이하여야 한다")
-    @ValueSource(strings = {"", "경의잠실중앙나루호호선"})
-    @NullSource
-    void createLineInfoFailTestByNameLength(String name) {
-        assertThatThrownBy(() -> new Line(name, new Sections(Collections.emptyList())))
-                .isInstanceOf(GlobalException.class);
+        @ParameterizedTest(name = "호선 이름은 1글자 이상, 10글자 이하여야 한다")
+        @ValueSource(strings = {"", "경의잠실중앙나루호호선"})
+        @NullSource
+        void createLineInfoFailTestByNameLength(String name) {
+            assertThatThrownBy(() -> new Line(name, new Sections(Collections.emptyList())))
+                    .isInstanceOf(GlobalException.class);
+        }
     }
 }
