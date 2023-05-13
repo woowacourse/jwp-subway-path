c7d7c72e0fbfb1b3c388ceb635bc33d4b5fa3849
woo-chang <clllickme@naver.com>
2023-05-13 15:56:06 +0900

    refactor: 노선 색에 대한 예외 세분화

--- src/main/java/subway/domain/line/Color.java
+++ src/main/java/subway/domain/line/Color.java
@@ -6,7 +6,8 @@ import subway.exception.InvalidColorException;
 
 final class Color {
 
-    private static final Pattern PATTERN = Pattern.compile("^[가-힣]{1,10}색$");
+    private static final int MAXIMUM_LENGTH = 11;
+    private static final Pattern PATTERN = Pattern.compile("^[가-힣]+색$");
 
     private final String value;
 
@@ -16,11 +17,14 @@ final class Color {
     }
 
     private void validate(final String value) {
-        if (Objects.isNull(value)) {
+        if (Objects.isNull(value) || value.isBlank()) {
             throw new InvalidColorException("노선 색은 존재해야 합니다.");
         }
+        if (value.length() > MAXIMUM_LENGTH) {
+            throw new InvalidColorException("노선 색은 " + MAXIMUM_LENGTH + "글자까지 가능합니다.");
+        }
         if (!PATTERN.matcher(value).matches()) {
-            throw new InvalidColorException("색 이름은 '색'으로 끝나야 합니다.");
+            throw new InvalidColorException("색 이름은 한글로 이루어져 있고, '색'으로 끝나야 합니다.");
         }
     }
 
