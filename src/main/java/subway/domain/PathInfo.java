ecd93812d11dca49c4d4e428206e728dd61125eb
apptie <appti@naver.com>
2023-05-14 00:18:43 +0900

    feat: PathInfo 메소드 추가

--- src/main/java/subway/domain/PathInfo.java
+++ src/main/java/subway/domain/PathInfo.java
@@ -14,8 +14,20 @@ public class PathInfo {
         return new PathInfo(distance, direction);
     }
 
+    public Distance calculateMiddleDistance(final Distance other) {
+        if (other.isGreaterThanOrEqualTo(distance)) {
+            throw new IllegalArgumentException("등록되는 구간 중간에 다른 역이 존재합니다.");
+        }
+
+        return distance.minus(other);
+    }
+
     public boolean matchesByDirection(final Direction direction) {
         return this.direction.matches(direction);
+    }
+
+    public int distance() {
+        return distance.getDistance();
     }
 
     public Distance getDistance() {
