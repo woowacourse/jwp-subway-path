4322da9e01fd1bbe8626bd085620d9c462d96d18
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-13 11:40:25 +0900

    test: 역 등록, 조회 인수테스트 수정

--- src/test/java/subway/domain/StationDomainTest.java
+++ src/test/java/subway/domain/StationDomainTest.java
@@ -1,36 +1,31 @@
 package subway.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class StationDomainTest {
 
-    @Test
-    void 역을_생성한다() {
-        assertAll(
-                () -> assertDoesNotThrow(() -> new StationDomain("루카")),
-                () -> assertDoesNotThrow(() -> new StationDomain("헤나"))
-        );
+    @ParameterizedTest
+    @ValueSource(strings = {"헤나", "루카", "현구막"})
+    void 역을_생성한다(final String value) {
+        assertDoesNotThrow(() -> new StationDomain(value));
     }
 
-    @Test
-    void 역_이름이_null이거나_공백일_경우_예외가_발생한다() {
-        assertAll(
-                () -> assertThatThrownBy(() -> new StationDomain(null))
-                        .isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new StationDomain(""))
-                        .isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new StationDomain(" "))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 역_이름이_null이거나_공백일_경우_예외가_발생한다(final String value) {
+        assertThatThrownBy(() -> new StationDomain(value))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 역_이름_길이가_1미만_10초과일_경우_예외가_발생한다() {
-        assertThatThrownBy(() -> new StationDomain("01234567891"))
+    @ParameterizedTest
+    @ValueSource(strings = {"", "12345678901"})
+    void 역_이름_길이가_1미만_10초과일_경우_예외가_발생한다(final String value) {
+        assertThatThrownBy(() -> new StationDomain(value))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
