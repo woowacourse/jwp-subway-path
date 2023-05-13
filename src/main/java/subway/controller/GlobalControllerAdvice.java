2619bca1c80573a5004de4e1b43913387b4bbcfe
gitchan <eunkeeeee@gmail.com>
2023-05-14 02:21:15 +0900

    style(GlobalControllerAdvice): 예외 메세지 수정

--- src/main/java/subway/controller/GlobalControllerAdvice.java
+++ src/main/java/subway/controller/GlobalControllerAdvice.java
@@ -11,7 +11,7 @@ public class GlobalControllerAdvice {
     public ResponseEntity<Object> handleException(Exception exception) {
         return ResponseEntity
                 .internalServerError()
-                .body(exception.getMessage());
+                .body("알 수 없는 오류가 발생했습니다. 잠시 후에 다시 시도해주세요.");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
