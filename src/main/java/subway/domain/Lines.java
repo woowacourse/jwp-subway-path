3a9ad293a52e5785067033ceb077bf7ff249a388
dhkim <akdlzmf1123@gmail.com>
2023-05-14 00:42:20 +0900

    refactor: 노선 이름 중복 확인 기능 Lines로 이동

---
+++ src/main/java/subway/domain/Lines.java
@@ -0,0 +1,31 @@
+package subway.domain;
+
+import subway.exception.DuplicateLineNameException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    public Lines(List<Line> lines) {
+        this.lines = new ArrayList<>(lines);
+    }
+
+    public List<Line> getLines() {
+        return new ArrayList<>(lines);
+    }
+
+    public void addLine(Line lineToAdd) {
+        if (isLineNameExists(lineToAdd)) {
+            throw new DuplicateLineNameException("이미 존재하는 노선 이름입니다");
+        }
+    }
+
+    private boolean isLineNameExists(Line lineToAdd) {
+        return lines.stream()
+                    .map(Line::getName)
+                    .anyMatch(lineName -> lineName.equals(lineToAdd.getName()));
+    }
+}
