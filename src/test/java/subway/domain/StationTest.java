65eafac1f955cd4ba3a60baeef111739ded14745
kdkdhoho <hkim4410@naver.com>
2023-05-14 13:04:08 +0900

    test(StationTest): 객체 동등성 테스트 추가

---
+++ src/test/java/subway/domain/StationTest.java
@@ -0,0 +1,16 @@
+package subway.domain;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class StationTest {
+
+    @Test
+    void ID와_이름이_같으면_같은_객체이다() {
+        Station station1 = new Station(1L, "강남");
+        Station station2 = new Station(1L, "강남");
+
+        assertThat(station1).isEqualTo(station2);
+    }
+}
