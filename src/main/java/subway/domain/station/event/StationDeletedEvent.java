cd8fe47096bc7d3fd18341a2c17a5e5a6d1495ed
Be-student <presentsong@naver.com>
2023-05-14 19:06:49 +0900

    feat: station delete service 추가

--- src/main/java/subway/domain/station/event/StationDeletedEvent.java
+++ src/main/java/subway/domain/station/event/StationDeletedEvent.java
@@ -1,10 +1,12 @@
 package subway.domain.station.event;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class StationDeletedEvent {
 
     private final long id;
---
+++ src/test/java/subway/adapter/subway/out/FakeStationRepository.java
@@ -0,0 +1,48 @@
+package subway.adapter.subway.out;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import subway.application.station.port.out.StationRepository;
+import subway.domain.station.Station;
+
+public class FakeStationRepository implements StationRepository {
+
+    private final Map<Long, Station> stations = new HashMap<>();
+    private long id = 1;
+
+    @Override
+    public List<Station> findAll() {
+        return List.copyOf(stations.values());
+    }
+
+    @Override
+    public Optional<Station> findById(final long id) {
+        return Optional.ofNullable(stations.get(id));
+    }
+
+    @Override
+    public Optional<Station> findByName(final String name) {
+        return stations.values()
+            .stream()
+            .filter(station -> station.getName().getValue().equals(name))
+            .findAny();
+    }
+
+    @Override
+    public Station save(final Station station) {
+        stations.put(id, new Station(id, station.getName().getValue()));
+        return stations.get(id++);
+    }
+
+    @Override
+    public void update(final Station station) {
+        stations.get(station.getId()).updateName(station.getName().getValue());
+    }
+
+    @Override
+    public void deleteById(final long id) {
+        stations.remove(id);
+    }
+}
