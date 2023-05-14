d7dcc1c51c4fee4999f36f308c712d4298bca3ac
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-14 11:17:12 +0900

    feat: 새로운 구간 등록시 중복 및 거리 검증 기능 추가

--- src/main/java/subway/domain/SectionsDomain.java
+++ src/main/java/subway/domain/SectionsDomain.java
@@ -2,18 +2,17 @@ package subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SectionsDomain {
 
-    private final List<SectionDomain> sections;
+    private final List<SectionDomain> sections = new ArrayList<>();
 
-    public SectionsDomain(final List<SectionDomain> sections) {
-        this.sections = sections;
-    }
-
-    public static SectionsDomain from(final List<SectionDomain> sections) {
-        return new SectionsDomain(sections);
+    public static SectionsDomain from(final List<SectionDomain> newSections) {
+        final SectionsDomain sections = new SectionsDomain();
+        newSections.forEach(sections::addSection);
+        return sections;
     }
 
     public void addSection(final SectionDomain newSection) {
@@ -22,17 +21,70 @@ public class SectionsDomain {
     }
 
     private void validate(final SectionDomain newSection) {
-        if (sections.contains(newSection)) {
+        if (isNotEmpty() && isBaseStationNotExist(newSection)) {
+            throw new IllegalArgumentException("구간이 하나 이상 존재하는 노선에 새로운 구간을 등록할 때 기준이 되는 지하철역이 존재해야 합니다.");
+        }
+        if (isAlreadyExist(newSection)) {
             throw new IllegalArgumentException("노선에 이미 존재하는 구간을 등록할 수 없습니다.");
         }
-        if (!isBaseStationExists(newSection)) {
-            throw new IllegalArgumentException("구간이 하나 이상 존재하는 노선에 새로운 구간을 등록할 때 기준이 되는 지하철역이 존재하지 않는다면 예외가 발생한다.");
+        if (isDistanceRangeNotOk(newSection)) {
+            throw new IllegalArgumentException("기존 구간 사이에 들어갈 새로운 구간의 길이가 더 크거나 같을 수 없습니다.");
         }
     }
 
-    private boolean isBaseStationExists(final SectionDomain newSection) {
-        return sections.isEmpty() || sections.stream()
-                .anyMatch(section -> section.isBaseStationExists(newSection));
+    private boolean isNotEmpty() {
+        return !sections.isEmpty();
+    }
+
+    private boolean isBaseStationNotExist(final SectionDomain newSection) {
+        return sections.stream().noneMatch(section -> section.isBaseStationExist(newSection));
+    }
+
+    private boolean isAlreadyExist(final SectionDomain newSection) {
+        return sections.contains(newSection);
+    }
+
+    private boolean isDistanceRangeNotOk(final SectionDomain newSection) {
+        if (isDistanceRangeOverSectionWithOldUpStation(newSection)) {
+            return true;
+        }
+        return isDistanceRangeOverSectionWithOldDownStation(newSection);
+    }
+
+    private boolean isDistanceRangeOverSectionWithOldUpStation(final SectionDomain newSection) {
+        final Optional<SectionDomain> maybeSectionWithOldUpStation
+                = findSectionWithOldUpStation(newSection.getUpStation());
+
+        if (maybeSectionWithOldUpStation.isPresent()) {
+            final SectionDomain sectionWithOldUpStation = maybeSectionWithOldUpStation.get();
+
+            return sectionWithOldUpStation.isDistanceLessThan(newSection);
+        }
+        return false;
+    }
+
+    private Optional<SectionDomain> findSectionWithOldUpStation(final StationDomain station) {
+        return sections.stream()
+                .dropWhile(section -> !section.isSameUpStationBy(station))
+                .findFirst();
+    }
+
+    private boolean isDistanceRangeOverSectionWithOldDownStation(final SectionDomain newSection) {
+        final Optional<SectionDomain> maybeSectionWithOldDownStation
+                = findSectionWithOldDownStation(newSection.getDownStation());
+
+        if (maybeSectionWithOldDownStation.isPresent()) {
+            final SectionDomain findSection = maybeSectionWithOldDownStation.get();
+
+            return findSection.isDistanceLessThan(newSection);
+        }
+        return false;
+    }
+
+    private Optional<SectionDomain> findSectionWithOldDownStation(final StationDomain station) {
+        return sections.stream()
+                .dropWhile(section -> !section.isSameDownStationBy(station))
+                .findFirst();
     }
 
     public List<StationDomain> collectAllStations() {
--- src/test/java/subway/domain/SectionsDomainTest.java
+++ src/test/java/subway/domain/SectionsDomainTest.java
@@ -1,49 +1,237 @@
 package subway.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class SectionsDomainTest {
-    @DisplayName("이미 존재하는 구간을 추가할 경우 예외가 발생한다.")
+
     @Test
-    void throwExceptionWhenSectionDuplicated() {
+    void 비어있는_구간_목록에_새로운_구간을_추가할_수_있다() {
         // given
-        final StationDomain 잠실역 = new StationDomain(1L, "잠실");
-        final StationDomain 잠실새내역 = new StationDomain(2L, "잠실새내");
+        final StationDomain 잠실역 = new StationDomain("잠실");
+        final StationDomain 잠실새내역 = new StationDomain("잠실새내");
+        final Distance 거리10 = new Distance(10);
+        final SectionDomain 잠실역_잠실새내역_구간 = SectionDomain.startFrom(잠실역, 잠실새내역, 거리10);
+
+        final SectionsDomain 구간_목록 = SectionsDomain.from(List.of());
+
+        // expect
+        assertDoesNotThrow(() -> 구간_목록.addSection(잠실역_잠실새내역_구간));
+    }
+
+    @Test
+    void 기존_구간과_같은_새로운_구간을_추가할_경우_예외가_발생한다() {
+        // given
+        final StationDomain 잠실역 = new StationDomain("잠실");
+        final StationDomain 잠실새내역 = new StationDomain("잠실새내");
         final Distance 거리10 = new Distance(10);
         final SectionDomain 잠실역_잠실새내역_구간 = SectionDomain.startFrom(잠실역, 잠실새내역, 거리10);
 
         final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(잠실역_잠실새내역_구간));
-        구간_목록.addSection(잠실역_잠실새내역_구간);
 
         // expect
         assertThatThrownBy(() -> 구간_목록.addSection(잠실역_잠실새내역_구간))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("구간이 하나 이상 존재하는 노선에 새로운 구간을 등록할 때 기준이 되는 지하철역이 존재하지 않는다면 예외가 발생한다.")
-    @Test
-    void throwExceptionWhenStationBaseIsNotExists() {
-        // given
-        final StationDomain 잠실역 = new StationDomain(1L, "잠실");
-        final StationDomain 잠실새내역 = new StationDomain(2L, "잠실새내");
-        final Distance 거리10 = new Distance(10);
-        final SectionDomain 잠실역_잠실새내역_구간 = SectionDomain.startFrom(잠실역, 잠실새내역, 거리10);
+    @DisplayName("구간이 하나 이상 존재하는 노선에 새로운 구간을 등록할 때 기준이 되는 지하철역이")
+    @Nested
+    class SectionAddValidation {
 
-        final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(잠실역_잠실새내역_구간));
-        구간_목록.addSection(잠실역_잠실새내역_구간);
+        @Test
+        void 존재하지않는_경우_예외가_발생한다() {
+            // given
+            final Distance 거리10 = new Distance(10);
 
-        final StationDomain 창동역 = new StationDomain(3L, "창동");
-        final StationDomain 녹천역 = new StationDomain(4L, "녹천");
+            final StationDomain 잠실역 = new StationDomain("잠실");
+            final StationDomain 잠실새내역 = new StationDomain("잠실새내");
+            final SectionDomain 잠실역_잠실새내역_구간 = SectionDomain.startFrom(잠실역, 잠실새내역, 거리10);
 
-        // expect
-        assertThatThrownBy(() -> 구간_목록.addSection(잠실역_잠실새내역_구간))
-                .isInstanceOf(IllegalArgumentException.class);
+            final StationDomain 창동역 = new StationDomain("창동");
+            final StationDomain 녹천역 = new StationDomain("녹천");
+            final SectionDomain 창동역_녹천역_구간 = SectionDomain.startFrom(창동역, 녹천역, 거리10);
+
+            final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(잠실역_잠실새내역_구간));
+
+            // expect
+            assertThatThrownBy(() -> 구간_목록.addSection(창동역_녹천역_구간))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 존재하는_경우_등록_성공한다() {
+            // given
+            final Distance 거리10 = new Distance(10);
+
+            final StationDomain 잠실역 = new StationDomain("잠실");
+            final StationDomain 잠실새내역 = new StationDomain("잠실새내");
+            final SectionDomain 잠실역_잠실새내역_구간 = SectionDomain.startFrom(잠실역, 잠실새내역, 거리10);
+
+            final StationDomain 창동역 = new StationDomain("창동");
+            final SectionDomain 잠실새내역_창동역_구간 = SectionDomain.notStartFrom(잠실새내역, 창동역, 거리10);
+
+            final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(잠실역_잠실새내역_구간));
+
+            // expect
+            assertDoesNotThrow(() -> 구간_목록.addSection(잠실새내역_창동역_구간));
+        }
+
+    }
+
+    @DisplayName("기존 구간의 상행역이 기준이 되고")
+    @Nested
+    class OldSectionWithOldUpStation {
+
+        @DisplayName("새로운 구간의 상행역과 같을 경우")
+        @Nested
+        class NewSectionWithNewDownStation {
+
+            @Test
+            void 새로운_구간의_길이가_기존_구간의_길이보다_클_경우_예외가_발생한다() {
+                // given
+                final Distance 거리10 = new Distance(10);
+                final Distance 거리20 = new Distance(20);
+
+                final StationDomain 기존_상행역 = new StationDomain("기존_상행역");
+                final StationDomain 기존_하행역 = new StationDomain("기존_하행역");
+                final SectionDomain 기존_구간 = SectionDomain.startFrom(기존_상행역, 기존_하행역, 거리10);
+
+                final StationDomain 새로운_하행역 = new StationDomain("새로운_하행역");
+                final SectionDomain 새로운_구간 = SectionDomain.notStartFrom(기존_상행역, 새로운_하행역, 거리20);
+
+                final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(기존_구간));
+
+                // expect
+                assertThatThrownBy(() -> 구간_목록.addSection(새로운_구간))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @Test
+            void 새로운_구간의_길이가_기존_구간의_길이가_같을_경우_예외가_발생한다() {
+                // given
+                final Distance 거리10 = new Distance(10);
+
+                final StationDomain 기존_상행역 = new StationDomain("기존_상행역");
+                final StationDomain 기존_하행역 = new StationDomain("기존_하행역");
+                final SectionDomain 기존_구간 = SectionDomain.startFrom(기존_상행역, 기존_하행역, 거리10);
+
+                final StationDomain 새로운_하행역 = new StationDomain("새로운_하행역");
+                final SectionDomain 새로운_구간 = SectionDomain.notStartFrom(기존_상행역, 새로운_하행역, 거리10);
+
+                final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(기존_구간));
+
+                // expect
+                assertThatThrownBy(() -> 구간_목록.addSection(새로운_구간))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @DisplayName("새로운 구간의 하행역과 같을 경우")
+        @Nested
+        class NewSectionWithNewUpStation {
+
+            @Test
+            void 새로운_구간의_길이가_0보다_클_경우_등록_성공한다() {
+                // given
+                final Distance 거리10 = new Distance(10);
+                final Distance 거리1 = new Distance(1);
+
+                final StationDomain 기존_상행역 = new StationDomain("기존_상행역");
+                final StationDomain 기존_하행역 = new StationDomain("기존_하행역");
+                final SectionDomain 기존_구간 = SectionDomain.startFrom(기존_상행역, 기존_하행역, 거리10);
+
+                final StationDomain 새로운_상행역 = new StationDomain("새로운_상행역");
+                final SectionDomain 새로운_구간 = SectionDomain.notStartFrom(새로운_상행역, 기존_하행역, 거리1);
+
+                final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(기존_구간));
+
+                // expect
+                assertDoesNotThrow(() -> 구간_목록.addSection(새로운_구간));
+            }
+        }
+    }
+
+    @DisplayName("기존 구간의 하행역이 기준이 되고")
+    @Nested
+    class OldSectionWithOldDownStation {
+
+        @DisplayName("새로운 구간의 상행역과 같을 경우")
+        @Nested
+        class NewSectionWithNewUpStation {
+
+            @Test
+            void 새로운_구간의_길이가_0보다_클_경우_등록_성공한다() {
+                // given
+                final Distance 거리10 = new Distance(10);
+                final Distance 거리1 = new Distance(1);
+
+                final StationDomain 기존_상행역 = new StationDomain("기존_상행역");
+                final StationDomain 기존_하행역 = new StationDomain("기존_하행역");
+                final SectionDomain 기존_구간 = SectionDomain.startFrom(기존_상행역, 기존_하행역, 거리10);
+
+                final StationDomain 새로운_하행역 = new StationDomain("새로운_하행역");
+                final SectionDomain 새로운_구간 = SectionDomain.notStartFrom(기존_상행역, 새로운_하행역, 거리1);
+
+                final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(기존_구간));
+
+                // expect
+                assertDoesNotThrow(() -> 구간_목록.addSection(새로운_구간));
+            }
+        }
+
+        @DisplayName("새로운 구간의 하행역과 같을 경우")
+        @Nested
+        class NewSectionWithNewDownStation {
+
+            @Test
+            void 새로운_구간의_길이가_기존_구간의_길이보다_클_경우_예외가_발생한다() {
+                // given
+                final Distance 거리10 = new Distance(10);
+                final Distance 거리20 = new Distance(20);
+
+                final StationDomain 기존_상행역 = new StationDomain("기존_상행역");
+                final StationDomain 기존_하행역 = new StationDomain("기존_하행역");
+                final SectionDomain 기존_구간 = SectionDomain.startFrom(기존_상행역, 기존_하행역, 거리10);
+
+                final StationDomain 새로운_상행역 = new StationDomain("새로운_상행역");
+                final SectionDomain 새로운_구간 = SectionDomain.notStartFrom(새로운_상행역, 기존_하행역, 거리20);
+
+                final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(기존_구간));
+
+                // expect
+                assertThatThrownBy(() -> 구간_목록.addSection(새로운_구간))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @Test
+            void 새로운_구간의_길이가_기존_구간의_길이가_같을_경우_예외가_발생한다() {
+                // given
+                final Distance 거리10 = new Distance(10);
+
+                final StationDomain 기존_상행역 = new StationDomain("기존_상행역");
+                final StationDomain 기존_하행역 = new StationDomain("기존_하행역");
+                final SectionDomain 기존_구간 = SectionDomain.startFrom(기존_상행역, 기존_하행역, 거리10);
+
+                final StationDomain 새로운_상행역 = new StationDomain("새로운_상행역");
+                final SectionDomain 새로운_구간 = SectionDomain.notStartFrom(새로운_상행역, 기존_하행역, 거리10);
+
+                final SectionsDomain 구간_목록 = SectionsDomain.from(List.of(기존_구간));
+
+                // expect
+                assertThatThrownBy(() -> 구간_목록.addSection(새로운_구간))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
     }
 
     @Test
