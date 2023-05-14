f845ee72ee0a8937d0671ce4f360d55df6dd5f74
ingpyo <juninpyo@naver.com>
2023-05-14 21:08:16 +0900

    refactor: 사용하지 않는 생성자 삭제

--- src/main/java/subway/domain/Section.java
+++ src/main/java/subway/domain/Section.java
@@ -20,24 +20,6 @@ public class Section {
     }
 
 
-    public static Section of(final Long lineId, final String upStationName, final String downStationName, final Long distance) {
-        return new Section(lineId, new Station(upStationName), new Station(downStationName), distance);
-    }
-
-    public static Section of(final String upStation, final String downStation) {
-        return new Section(null, new Station(upStation), new Station(downStation), null);
-    }
-
-    public static Section of(final Long lineId, final String upStationName, final String downStationName) {
-        return new Section(lineId, new Station(upStationName), new Station(downStationName), null);
-    }
-
-
-    public Long getDistance() {
-        return distance;
-    }
-
-
     private void validateDuplication(final Station nowStation, final Station nextStation) {
         if (nowStation.equals(nextStation)) {
             throw new IllegalArgumentException("중복된 이름입니다. 다른 이름을 입력해주세요.");
@@ -96,6 +78,14 @@ public class Section {
         return upEndPoint.equals(this.downStation) ^ downEndPoint.equals(this.upStation);
     }
 
+    public boolean isNowStation(final Station station) {
+        return station.equals(this.upStation);
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
     public Station getUpStation() {
         return upStation;
     }
@@ -104,7 +94,7 @@ public class Section {
         return downStation;
     }
 
-    public boolean isNowStation(final Station station) {
-        return station.equals(this.upStation);
+    public Long getDistance() {
+        return distance;
     }
 }
