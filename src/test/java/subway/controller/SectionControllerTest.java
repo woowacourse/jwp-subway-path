d614b42ab5bc5476539198e619b5e645ed822804
hectick <ch.yeondev@gmail.com>
2023-05-14 17:27:39 +0900

    test: 노선에 역을 추가하는 테스트 작성

--- src/test/java/subway/controller/SectionControllerTest.java
+++ src/test/java/subway/controller/SectionControllerTest.java
@@ -1,217 +1,188 @@
-/*
 package subway.controller;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+import subway.dto.request.LineRequest;
 import subway.dto.request.SectionCreationRequest;
 
+import static subway.TestFixtures.강변;
+import static subway.TestFixtures.이_호선;
+import static subway.TestFixtures.잠실;
+import static subway.TestFixtures.잠실나루;
+import static subway.TestFixtures.잠실새내;
+import static subway.TestFixtures.종합운동장;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/schema.sql")
-@DisplayName("SectionController는 ")
+@Sql({"/schema.sql", "/dummyData.sql"})
+@DisplayName("SectionController 테스트")
 class SectionControllerTest {
 
     @LocalServerPort
     int port;
 
-    @BeforeEach
-    public void setUp() {
-        RestAssured.port = port;
-    }
+    @Nested
+    @DisplayName("노선에 구간을 추가하는 createSection 메서드 테스트")
+    class CreateSectionTest {
 
-    @DisplayName("처음 노선에 구간을 등록할 수 있다.")
-    @Test
-    void createInitSections() {
-        InitSectionRequest request = new InitSectionRequest(2L, 1L, 2L, 10);
+        @BeforeEach
+        void setUp() {
+            RestAssured.port = port;
+            LineRequest request = new LineRequest("2호선", "초록색");
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when().post("/lines")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+        }
 
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when().post("/lines/2/init-sections")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-    }
+        @DisplayName("노선에 두 역을 최초로 추가할 때, 정상 추가가 되었다면 상태코드 201 Created 를 반환하는지 확인한다")
+        @Test
+        void successTest1() {
+            SectionCreationRequest request = new SectionCreationRequest(이_호선, 강변, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+        }
 
-    @DisplayName("노선에 역을 추가할 수 있다.")
-    @Test
-    void createSection() {
-        // 3L 강변
-        // 2L 잠실
-        // 1L 잠실나루
+        @DisplayName("하행 종점에 역을 추가할 때, 정상 추가가 되었다면 상태코드 201 Created 를 반환하는지 확인한다")
+        @Test
+        void successTest2() {
+            //given
+            SectionCreationRequest initRequest = new SectionCreationRequest(이_호선, 강변, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(initRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+            //then
+            SectionCreationRequest mainRequest = new SectionCreationRequest(이_호선, 잠실, 잠실새내, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(mainRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+        }
 
-        InitSectionRequest initRequest = new InitSectionRequest(2L, 3L, 2L, 10);
+        @DisplayName("상행 종점에 역을 추가할 때, 정상 추가가 되었다면 상태코드 201 Created 를 반환하는지 확인한다")
+        @Test
+        void successTest3() {
+            //given
+            SectionCreationRequest initRequest = new SectionCreationRequest(이_호선, 잠실나루, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(initRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+            //then
+            SectionCreationRequest mainRequest = new SectionCreationRequest(이_호선, 강변, 잠실나루, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(mainRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+        }
 
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest)
-                .when().post("/lines/2/init-sections")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
+        @DisplayName("존재하는 역들의 사이에 역을 추가할 때, 정상 추가가 되었다면 상태코드 201 Created 를 반환하는지 확인한다 case1")
+        @Test
+        void successTest4() {
+            //given
+            SectionCreationRequest initRequest = new SectionCreationRequest(이_호선, 강변, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(initRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+            //then
+            SectionCreationRequest mainRequest = new SectionCreationRequest(이_호선, 잠실나루, 잠실, 6);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(mainRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+        }
 
-        SectionCreationRequest mainRequest = new SectionCreationRequest(
-                2L,
-                1L,
-                3L,
-                2L,
-                3,
-                7);
+        @DisplayName("존재하는 역들의 사이에 역을 추가할 때, 정상 추가가 되었다면 상태코드 201 Created 를 반환하는지 확인한다 case2")
+        @Test
+        void successTest5() {
+            //given
+            SectionCreationRequest initRequest = new SectionCreationRequest(이_호선, 강변, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(initRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+            //then
+            SectionCreationRequest mainRequest = new SectionCreationRequest(이_호선, 강변, 잠실나루, 6);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(mainRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+        }
 
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(mainRequest)
-                .when().post("/lines/2/section")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-    }
+        @DisplayName("노선에 이미 등록된 역이 있을 때, 새로운 두개의 역을 추가하면 400 BadRequest 를 반환하는지 확인한다")
+        @Test
+        void failTest1() {
+            //given
+            SectionCreationRequest initRequest = new SectionCreationRequest(이_호선, 강변, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(initRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+            //then
+            SectionCreationRequest mainRequest = new SectionCreationRequest(이_호선, 잠실새내, 종합운동장, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(mainRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
 
-    @DisplayName("노선에 종착역을 추가할 수 있다.")
-    @Test
-    void createEndSection() {
-        // 3L 강변
-        // 2L 잠실
-        // 1L 잠실나루
-
-        InitSectionRequest initRequest = new InitSectionRequest(2L, 1L, 2L, 10);
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest)
-                .when().post("/lines/2/init-sections")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        EndSectionRequest mainRequest = new EndSectionRequest(
-                2L,
-                3L,
-                1L,
-                5
-        );
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(mainRequest)
-                .when().post("/lines/2/end-section")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-    }
-
-    @DisplayName("노선에서 하나의 역을 제거할 수 있다.")
-    @Test
-    void deleteSection() {
-        // 3L 강변
-        // 2L 잠실
-        // 1L 잠실나루
-
-        InitSectionRequest initRequest1 = new InitSectionRequest(2L, 3L, 2L, 10);
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest1)
-                .when().post("/lines/2/init-sections")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        SectionCreationRequest initRequest2 = new SectionCreationRequest(
-                2L,
-                1L,
-                3L,
-                2L,
-                3,
-                7);
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest2)
-                .when().post("/lines/2/section")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/lines/2/section?station-id=1")
-                .then()
-                .statusCode(HttpStatus.NO_CONTENT.value());
-    }
-
-
-    @DisplayName("노선에서 하나의 종착역을 제거할 수 있다.")
-    @Test
-    void deleteEndSection() {
-        // 3L 강변
-        // 2L 잠실
-        // 1L 잠실나루
-
-        InitSectionRequest initRequest1 = new InitSectionRequest(2L, 3L, 2L, 10);
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest1)
-                .when().post("/lines/2/init-sections")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        SectionCreationRequest initRequest2 = new SectionCreationRequest(
-                2L,
-                1L,
-                3L,
-                2L,
-                3,
-                7);
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest2)
-                .when().post("/lines/2/section")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/lines/2/end-section?station-id=3")
-                .then()
-                .statusCode(HttpStatus.NO_CONTENT.value());
-    }
-
-    @DisplayName("노선에서 마지막 남은 두개의 역을 제거할 수 있다.")
-    @Test
-    void deleteLastSection() {
-        // 3L 강변
-        // 2L 잠실
-        // 1L 잠실나루
-
-        InitSectionRequest initRequest1 = new InitSectionRequest(2L, 3L, 2L, 10);
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(initRequest1)
-                .when().post("/lines/2/init-sections")
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/lines/2/last-sections?upward-id=3&downward-id=2")
-                .then()
-                .statusCode(HttpStatus.NO_CONTENT.value());
+        @DisplayName("기존 구간 사이에 역을 추가할 경우, 쪼갠 결과로 나온 두개의 거리 중 양수가 아닌 수가 있다면 400 BadRequest 를 반환하는지 확인한다")
+        @ParameterizedTest
+        @ValueSource(ints = {10, 11, 0, -1})
+        void failTest2(int input) {
+            //given
+            SectionCreationRequest initRequest = new SectionCreationRequest(이_호선, 강변, 잠실, 10);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(initRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+            //then
+            SectionCreationRequest mainRequest = new SectionCreationRequest(이_호선, 잠실나루, 잠실, input);
+            RestAssured.given()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(mainRequest)
+                    .when().post("/lines/" + 이_호선 + "/stations")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
+        }
     }
 }
-*/
