5e7bd9f2ee43874dcd559de1a3c9e684bb72875b
jiwon <ljwon77@daum.net>
2023-05-14 15:56:38 +0900

    build: 테이블 스크립트 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,30 @@
-create table if not exists STATION
+CREATE TABLE IF NOT EXISTS station
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
+    id bigint auto_increment NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
 );
 
-create table if not exists LINE
+CREATE TABLE IF NOT EXISTS line
 (
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key(id)
+    id bigint auto_increment NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    color VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id)
 );
 
-create table if not exists SECTION
+CREATE TABLE IF NOT EXISTS section
 (
-    id bigint auto_increment not null,
-    line_id bigint not null,
-    source_station_id bigint not null,
-    target_station_id bigint not null,
-    distance int not null,
-    primary key(id),
-    foreign key(line_id) references line(id) on delete cascade,
-    foreign key(source_station_id) references station(id) on delete cascade,
-    foreign key(target_station_id) references station(id) on delete cascade
+    id bigint auto_increment NOT NULL,
+    line_id bigint NOT NULL,
+    source_station_id bigint NOT NULL,
+    target_station_id bigint NOT NULL,
+    distance int NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(line_id) REFERENCES line(id) ON DELETE CASCADE,
+    FOREIGN KEY(source_station_id) REFERENCES station(id) ON DELETE CASCADE,
+    FOREIGN KEY(target_station_id) REFERENCES station(id) ON DELETE CASCADE
 );
+
+CREATE INDEX IF NOT EXISTS idx_station_name ON STATION (name);
+CREATE INDEX IF NOT EXISTS idx_line_name ON LINE (name);
--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -2,6 +2,6 @@ DELETE FROM line;
 DELETE FROM station;
 DELETE FROM section;
 
-alter table line auto_increment = 1;
-alter table station auto_increment = 1;
-alter table section auto_increment = 1;
+ALTER TABLE line auto_increment = 1;
+ALTER TABLE station auto_increment = 1;
+ALTER TABLE section auto_increment = 1;
