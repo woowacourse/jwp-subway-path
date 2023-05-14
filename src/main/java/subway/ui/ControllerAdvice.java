bd14c671dde568df1d2b457e86497ace33636157
drunkenhw <drunkenhw@gmail.com>
2023-05-14 17:22:25 +0900

    refactor: 예외 처리 추가

--- src/main/java/subway/ui/ControllerAdvice.java
+++ src/main/java/subway/ui/ControllerAdvice.java
@@ -16,6 +16,11 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(SQLException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
