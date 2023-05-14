7f1601eb869f19a433e97a6a43c60899896fa651
kiarakim <kiarakim413@gmail.com>
2023-05-14 22:30:44 +0900

    test: 전체 역 조회 repo 테스트

--- src/test/java/subway/application/StationServiceTest.java
+++ src/test/java/subway/application/StationServiceTest.java
@@ -1,17 +1,18 @@
 package subway.application;
 
-
 import static org.mockito.BDDMockito.*;
+
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import subway.domain.Station;
 import subway.persistence.StationRepositoryImpl;
 import subway.ui.dto.StationCreateRequest;
 import subway.ui.dto.StationResponse;
@@ -39,8 +40,20 @@ class StationServiceTest {
 		Assertions.assertThat(1L).isEqualTo(response.getId());
 	}
 
+	@DisplayName("전체 역 조회 서비스 테스트")
 	@Test
 	void findAll() {
+		// given
+		final Station jamsil = new Station(1L,"잠실");
+		given(repository.findAll()).willReturn(List.of(jamsil));
+
+		// when
+		final List<StationResponse> stations = service.findAll();
+
+		// then
+		Assertions.assertThat(stations)
+			.usingRecursiveComparison()
+			.isEqualTo(List.of(new StationResponse(1L, "잠실")));
 	}
 
 	@Test
