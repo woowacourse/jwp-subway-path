e1be7d0f19105d071fd7c892c19c85de230a08a2
Be-student <presentsong@naver.com>
2023-05-14 20:22:26 +0900

    feat: 컨트롤러 작성

---
+++ src/main/java/subway/ui/line/LineFindAllController.java
@@ -0,0 +1,14 @@
+package subway.ui.line;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Tag(name = "노선")
+@RestController
+@RequestMapping("/lines")
+public class LineFindAllController {
+
+}
