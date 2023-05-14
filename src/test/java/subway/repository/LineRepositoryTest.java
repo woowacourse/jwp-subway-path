d1dddcf79188113ecb066d09c14f9c0a67d06f8a
Miseong Kim <miseongk@naver.com>
2023-05-14 18:52:49 +0900

    test: Line 예외 테스트 추가

--- src/test/java/subway/repository/LineRepositoryTest.java
+++ src/test/java/subway/repository/LineRepositoryTest.java
@@ -1,16 +1,19 @@
 package subway.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import subway.domain.Line;
 import subway.domain.Section;
+import subway.exception.LineNotFoundException;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -21,94 +24,132 @@ class LineRepositoryTest {
     @Autowired
     private LineRepository lineRepository;
 
-    @Test
-    void 노선을_저장한다() {
-        // given
-        final Line line = new Line("2호선", "RED", List.of(
-                new Section("B", "C", 3),
-                new Section("A", "B", 2),
-                new Section("D", "E", 5),
-                new Section("C", "D", 4)
-        ));
+    @Nested
+    class 성공_테스트 {
+        @Test
+        void 노선을_저장한다() {
+            // given
+            final Line line = new Line("2호선", "RED", List.of(
+                    new Section("B", "C", 3),
+                    new Section("A", "B", 2),
+                    new Section("D", "E", 5),
+                    new Section("C", "D", 4)
+            ));
 
-        // when
-        final Line savedLine = lineRepository.save(line);
+            // when
+            final Line savedLine = lineRepository.save(line);
 
-        // then
-        assertThat(savedLine).usingRecursiveComparison().isEqualTo(line);
+            // then
+            assertThat(savedLine).usingRecursiveComparison().isEqualTo(line);
+        }
+
+        @Test
+        void 노선을_조회한다() {
+            // given
+            final Line line = new Line("2호선", "RED", List.of(
+                    new Section("B", "C", 3),
+                    new Section("A", "B", 2),
+                    new Section("D", "E", 5),
+                    new Section("C", "D", 4)
+            ));
+            lineRepository.save(line);
+
+            // when
+            final List<Line> result = lineRepository.findAll();
+
+            // then
+            assertThat(result.get(0)).usingRecursiveComparison().isEqualTo(line);
+        }
+
+        @Test
+        void 노선을_삭제한다() {
+            // given
+            final Line line = new Line("2호선", "RED", List.of(
+                    new Section("B", "C", 3),
+                    new Section("A", "B", 2),
+                    new Section("D", "E", 5),
+                    new Section("C", "D", 4)
+            ));
+            final Line savedLine = lineRepository.save(line);
+            final Long id = lineRepository.findIdByName(savedLine.getName());
+
+            // when
+            lineRepository.deleteById(id);
+
+            // then
+            assertThat(lineRepository.findAll()).isEmpty();
+        }
+
+        @Test
+        void 이름을_입력받아_라인의_id를_반환한다() {
+            // given
+            final Line line = new Line("2호선", "RED", List.of(
+                    new Section("B", "C", 3),
+                    new Section("A", "B", 2),
+                    new Section("D", "E", 5),
+                    new Section("C", "D", 4)
+            ));
+            lineRepository.save(line);
+
+            // when
+            final Long id = lineRepository.findIdByName("2호선");
+
+            // then
+            assertThat(id).isPositive();
+        }
+
+        @Test
+        void id를_입력받아_라인을_반환한다() {
+            // given
+            final Line line = new Line("2호선", "RED", List.of(
+                    new Section("B", "C", 3),
+                    new Section("A", "B", 2),
+                    new Section("D", "E", 5),
+                    new Section("C", "D", 4)
+            ));
+            lineRepository.save(line);
+            final Long id = lineRepository.findIdByName(line.getName());
+
+            // when
+            final Line result = lineRepository.findById(id);
+
+            // then
+            assertThat(result).usingRecursiveComparison().isEqualTo(line);
+        }
     }
 
-    @Test
-    void 노선을_조회한다() {
-        // given
-        final Line line = new Line("2호선", "RED", List.of(
-                new Section("B", "C", 3),
-                new Section("A", "B", 2),
-                new Section("D", "E", 5),
-                new Section("C", "D", 4)
-        ));
-        lineRepository.save(line);
+    @Nested
+    class 예외_테스트 {
+        @Test
+        void 존재하지_않는_id를_입력받아_삭제할_경우_예외를_던진다() {
+            // expect
+            assertThatThrownBy(() -> lineRepository.deleteById(1L))
+                    .isInstanceOf(LineNotFoundException.class)
+                    .hasMessage("노선을 찾을 수 없습니다.");
+        }
 
-        // when
-        final List<Line> result = lineRepository.findAll();
+        @Test
+        void 존재하지_않는_id를_입력받아_수정할_경우_예외를_던진다() {
+            // expect
+            assertThatThrownBy(() -> lineRepository.updateNameAndColorById(1L, "1호선", "RED"))
+                    .isInstanceOf(LineNotFoundException.class)
+                    .hasMessage("노선을 찾을 수 없습니다.");
+        }
 
-        // then
-        assertThat(result.get(0)).usingRecursiveComparison().isEqualTo(line);
-    }
+        @Test
+        void 존재하지_않는_id를_입력받아_조회할_경우_예외를_던진다() {
+            // expect
+            assertThatThrownBy(() -> lineRepository.findById(1L))
+                    .isInstanceOf(LineNotFoundException.class)
+                    .hasMessage("노선을 찾을 수 없습니다.");
+        }
 
-    @Test
-    void 노선을_삭제한다() {
-        // given
-        final Line line = new Line("2호선", "RED", List.of(
-                new Section("B", "C", 3),
-                new Section("A", "B", 2),
-                new Section("D", "E", 5),
-                new Section("C", "D", 4)
-        ));
-        final Line savedLine = lineRepository.save(line);
-        final Long id = lineRepository.findIdByName(savedLine.getName());
-
-        // when
-        lineRepository.deleteById(id);
-
-        // then
-        assertThat(lineRepository.findAll()).isEmpty();
-    }
-
-    @Test
-    void 이름을_입력받아_라인의_id를_반환한다() {
-        // given
-        final Line line = new Line("2호선", "RED", List.of(
-                new Section("B", "C", 3),
-                new Section("A", "B", 2),
-                new Section("D", "E", 5),
-                new Section("C", "D", 4)
-        ));
-        lineRepository.save(line);
-
-        // when
-        final Long id = lineRepository.findIdByName("2호선");
-
-        // then
-        assertThat(id).isPositive();
-    }
-
-    @Test
-    void id를_입력받아_라인을_반환한다() {
-        // given
-        final Line line = new Line("2호선", "RED", List.of(
-                new Section("B", "C", 3),
-                new Section("A", "B", 2),
-                new Section("D", "E", 5),
-                new Section("C", "D", 4)
-        ));
-        lineRepository.save(line);
-        final Long id = lineRepository.findIdByName(line.getName());
-
-        // when
-        final Line result = lineRepository.findById(id);
-
-        // then
-        assertThat(result).usingRecursiveComparison().isEqualTo(line);
+        @Test
+        void 존재하지_않는_노선_이름을_입력받아_조회할_경우_예외를_던진다() {
+            // expect
+            assertThatThrownBy(() -> lineRepository.findIdByName("1호선"))
+                    .isInstanceOf(LineNotFoundException.class)
+                    .hasMessage("노선을 찾을 수 없습니다.");
+        }
     }
 }
--- src/test/java/subway/ui/LineControllerTest.java
+++ src/test/java/subway/ui/LineControllerTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -42,132 +43,249 @@ public class LineControllerTest {
         RestAssured.port = port;
     }
 
-    @Test
-    void 노선을_추가한다() {
-        // given
-        final LineSaveRequest request = new LineSaveRequest("1호선", "RED");
+    @Nested
+    class 성공_테스트 {
+        @Test
+        void 노선을_추가한다() {
+            // given
+            final LineSaveRequest request = new LineSaveRequest("1호선", "RED");
 
-        // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when().post("/lines")
-                .then().log().all()
-                .extract();
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when().post("/lines")
+                    .then().log().all()
+                    .extract();
 
-        // then
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
-                () -> assertThat(response.header("Location")).isNotBlank()
-        );
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                    () -> assertThat(response.header("Location")).isNotBlank()
+            );
+        }
+
+        @Test
+        void 노선id를_입력받아_노선을_조회한다() {
+            // given
+            lineRepository.save(new Line("1호선", "RED", List.of(
+                    new Section("A", "B", 2)
+            )));
+            final Long id = lineRepository.findIdByName("1호선");
+
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().get("/lines/" + id)
+                    .then().log().all()
+                    .extract();
+
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                    () -> assertThat(response.jsonPath().getObject(".", LineResponse.class)).usingRecursiveComparison()
+                            .isEqualTo(new LineResponse("1호선", "RED", List.of("A", "B")))
+            );
+        }
+
+        @Test
+        void 노선을_전체_조회한다() {
+            // given
+            lineRepository.save(new Line("1호선", "RED", List.of(
+                    new Section("B", "C", 3),
+                    new Section("A", "B", 2),
+                    new Section("D", "E", 5),
+                    new Section("C", "D", 4)
+            )));
+            lineRepository.save(new Line("2호선", "BLUE", List.of(
+                    new Section("Z", "B", 3),
+                    new Section("B", "Y", 2)
+            )));
+
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().get("/lines")
+                    .then().log().all()
+                    .extract();
+
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                    () -> assertThat(response.jsonPath().getList(".", LineResponse.class)).usingRecursiveComparison()
+                            .isEqualTo(List.of(
+                                    new LineResponse("1호선", "RED", List.of("A", "B", "C", "D", "E")),
+                                    new LineResponse("2호선", "BLUE", List.of("Z", "B", "Y"))
+                            ))
+            );
+        }
+
+        @Test
+        void 노선을_수정한다() {
+            // given
+            lineRepository.save(new Line("1호선", "RED", List.of(
+                    new Section("A", "B", 2)
+            )));
+            final Long id = lineRepository.findIdByName("1호선");
+            final LineUpdateRequest request = new LineUpdateRequest("2호선", "BLUE");
+
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .body(request)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().put("/lines/" + id)
+                    .then().log().all()
+                    .extract();
+
+            // then
+            final Line line = lineRepository.findById(id);
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                    () -> assertThat(line.getName()).isEqualTo("2호선"),
+                    () -> assertThat(line.getColor()).isEqualTo("BLUE")
+            );
+        }
+
+        @Test
+        void 노선을_제거한다() {
+            // given
+            lineRepository.save(new Line("1호선", "RED", List.of(
+                    new Section("A", "B", 2)
+            )));
+            final Long id = lineRepository.findIdByName("1호선");
+
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().delete("/lines/" + id)
+                    .then().log().all()
+                    .extract();
+
+            // then
+            final List<Line> result = lineRepository.findAll();
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                    () -> assertThat(result).isEmpty()
+            );
+        }
     }
 
-    @Test
-    void 노선id를_입력받아_노선을_조회한다() {
-        // given
-        lineRepository.save(new Line("1호선", "RED", List.of(
-                new Section("A", "B", 2)
-        )));
-        final Long id = lineRepository.findIdByName("1호선");
+    @Nested
+    class 예외_테스트 {
+        @Test
+        void 존재하지_않는_id의_노선을_조회할_경우_예외를_던진다() {
+            // given
+            final int nonExistId = 1;
 
-        // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines/" + id)
-                .then().log().all()
-                .extract();
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().get("/lines/" + nonExistId)
+                    .then().log().all()
+                    .extract();
 
-        // then
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.jsonPath().getObject(".", LineResponse.class)).usingRecursiveComparison()
-                        .isEqualTo(new LineResponse("1호선", "RED", List.of("A", "B")))
-        );
-    }
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value()),
+                    () -> assertThat(response.body().jsonPath().getObject("message", String.class))
+                            .isEqualTo("노선을 찾을 수 없습니다.")
+            );
+        }
 
-    @Test
-    void 노선을_전체_조회한다() {
-        // given
-        lineRepository.save(new Line("1호선", "RED", List.of(
-                new Section("B", "C", 3),
-                new Section("A", "B", 2),
-                new Section("D", "E", 5),
-                new Section("C", "D", 4)
-        )));
-        lineRepository.save(new Line("2호선", "BLUE", List.of(
-                new Section("Z", "B", 3),
-                new Section("B", "Y", 2)
-        )));
+        @Test
+        void 존재하지_않는_id의_노선을_수정할_경우_예외를_던진다() {
+            // given
+            final int nonExistId = 1;
+            final LineUpdateRequest request = new LineUpdateRequest("2호선", "BLUE");
 
-        // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines")
-                .then().log().all()
-                .extract();
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when().put("/lines/" + nonExistId)
+                    .then().log().all()
+                    .extract();
 
-        // then
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.jsonPath().getList(".", LineResponse.class)).usingRecursiveComparison()
-                        .isEqualTo(List.of(
-                                new LineResponse("1호선", "RED", List.of("A", "B", "C", "D", "E")),
-                                new LineResponse("2호선", "BLUE", List.of("Z", "B", "Y"))
-                        ))
-        );
-    }
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value()),
+                    () -> assertThat(response.body().jsonPath().getObject("message", String.class))
+                            .isEqualTo("노선을 찾을 수 없습니다.")
+            );
+        }
 
-    @Test
-    void 노선을_수정한다() {
-        // given
-        lineRepository.save(new Line("1호선", "RED", List.of(
-                new Section("A", "B", 2)
-        )));
-        final Long id = lineRepository.findIdByName("1호선");
-        final LineUpdateRequest request = new LineUpdateRequest("2호선", "BLUE");
+        @Test
+        void 존재하지_않는_id의_노선을_삭제할_경우_예외를_던진다() {
+            // given
+            final int nonExistId = 1;
 
-        // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put("/lines/" + id)
-                .then().log().all()
-                .extract();
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().delete("/lines/" + nonExistId)
+                    .then().log().all()
+                    .extract();
 
-        // then
-        final Line line = lineRepository.findById(id);
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
-                () -> assertThat(line.getName()).isEqualTo("2호선"),
-                () -> assertThat(line.getColor()).isEqualTo("BLUE")
-        );
-    }
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value()),
+                    () -> assertThat(response.body().jsonPath().getObject("message", String.class))
+                            .isEqualTo("노선을 찾을 수 없습니다.")
+            );
+        }
 
-    @Test
-    void 노선을_제거한다() {
-        // given
-        lineRepository.save(new Line("1호선", "RED", List.of(
-                new Section("A", "B", 2)
-        )));
-        final Long id = lineRepository.findIdByName("1호선");
+        @Test
+        void 노선_추가시_노선_이름을_입력하지_않으면_예외가_발생한다() {
+            // given
+            final LineSaveRequest request = new LineSaveRequest("", "RED");
 
-        // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/lines/" + id)
-                .then().log().all()
-                .extract();
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when().post("/lines")
+                    .then().log().all()
+                    .extract();
 
-        // then
-        final List<Line> result = lineRepository.findAll();
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
-                () -> assertThat(result).isEmpty()
-        );
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                    () -> assertThat(response.body().jsonPath().getObject("message", String.class))
+                            .isEqualTo("노선명을 입력해주세요.")
+
+            );
+        }
+
+        @Test
+        void 노선_추가시_노선_색을_입력하지_않으면_예외가_발생한다() {
+            // given
+            final LineSaveRequest request = new LineSaveRequest("1호선", "");
+
+            // when
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when().post("/lines")
+                    .then().log().all()
+                    .extract();
+
+            // then
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                    () -> assertThat(response.body().jsonPath().getObject("message", String.class))
+                            .isEqualTo("노선 색을 입력해주세요.")
+
+            );
+        }
     }
 }
