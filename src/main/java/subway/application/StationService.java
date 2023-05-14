582966f645457d8ec1b34eaa43634839e0f41812
kiarakim <kiarakim413@gmail.com>
2023-05-14 14:02:53 +0900

    feat: 중복 이름 입력 시 예외 발생 로직 추가 및 예외Handler 추가

--- src/main/java/subway/application/StationService.java
+++ src/main/java/subway/application/StationService.java
@@ -1,6 +1,7 @@
 package subway.application;
 
 import org.springframework.stereotype.Service;
+
 import subway.domain.Station;
 import subway.domain.repository.StationRepository;
 import subway.ui.dto.StationCreateRequest;
@@ -10,27 +11,33 @@ import java.util.List;
 
 @Service
 public class StationService {
-    private final StationRepository stationRepository;
+	private final StationRepository stationRepository;
 
-    public StationService(final StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
-    }
+	public StationService(final StationRepository stationRepository) {
+		this.stationRepository = stationRepository;
+	}
 
-    public void createStation(final StationCreateRequest stationCreateRequest) {
-        final Station station = new Station(stationCreateRequest.getName());
+	public void createStation(final StationCreateRequest stationCreateRequest) {
+		final List<StationResponse> stations = findAll();
+		for (StationResponse station : stations) {
+			if (station.getName().equals(stationCreateRequest.getName())) {
+				throw new IllegalArgumentException("이미 존재하는 역입니다");
+			}
+		}
+		final Station station = new Station(stationCreateRequest.getName());
 
-        stationRepository.createStation(station);
-    }
+		stationRepository.createStation(station);
+	}
 
-    public List<StationResponse> findAll() {
-        return StationResponse.of(stationRepository.findAll());
-    }
+	public List<StationResponse> findAll() {
+		return StationResponse.of(stationRepository.findAll());
+	}
 
-    public Station findById(final Long stationIdRequest) {
-        return stationRepository.findById(stationIdRequest);
-    }
+	public Station findById(final Long stationIdRequest) {
+		return stationRepository.findById(stationIdRequest);
+	}
 
-    public void deleteById(final Long stationIdRequest) {
-        stationRepository.deleteById(stationIdRequest);
-    }
+	public void deleteById(final Long stationIdRequest) {
+		stationRepository.deleteById(stationIdRequest);
+	}
 }
