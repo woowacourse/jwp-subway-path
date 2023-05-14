6699254697e7e69696139a91e7f051cf0bcafb33
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-14 16:54:53 +0900

    feat: 역 서비스 구간 저장 및 조회 기능 추가

--- src/test/java/subway/application/v2/StationServiceV2Test.java
+++ src/test/java/subway/application/v2/StationServiceV2Test.java
@@ -1,53 +1,113 @@
 package subway.application.v2;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
-import subway.config.ServiceTestConfig;
-import subway.dto.StationResponse;
+import subway.application.request.CreateSectionRequest;
 import subway.application.request.CreateStationRequest;
+import subway.config.ServiceTestConfig;
+import subway.domain.Distance;
+import subway.dto.StationResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class StationServiceV2Test extends ServiceTestConfig {
 
     StationServiceV2 stationService;
 
     @BeforeEach
     void setUp() {
-        stationService = new StationServiceV2(stationRepository);
+        stationService = new StationServiceV2(stationRepository, sectionRepository, lineRepository);
     }
 
-    @DisplayName("역을 등록한다.")
     @Test
-    void saveStation() {
+    void 역을_등록한다() {
         // given
-        final CreateStationRequest request = new CreateStationRequest("잠실");
+        final CreateStationRequest 잠실역_요청 = new CreateStationRequest("잠실");
 
         // when
-        final Long saveStationId = stationService.saveStation(request);
+        final Long 잠실역_식별자값 = stationService.saveStation(잠실역_요청);
 
         // then
-        assertThat(saveStationId)
+        assertThat(잠실역_식별자값)
                 .isNotNull()
                 .isNotZero();
     }
 
-    @DisplayName("역을 조회한다.")
     @Test
-    void findByStationId() {
+    void 노선에_새로운_구간을_추가할_때_등록되지_않은_새로운_역일_경우_역을_저장한다() {
         // given
-        final CreateStationRequest request = new CreateStationRequest("잠실");
-        final Long saveStationId = stationService.saveStation(request);
+        final Long 노선2_식별자값 = lineDao.save("2", "초록");
+        final Long 잠실_식별자값 = stationDao.save("잠실");
+        final Long 잠실새내_식별자값 = stationDao.save("잠실새내");
+        final Distance 거리_10 = new Distance(10);
+
+        sectionDao.save(잠실_식별자값, 잠실새내_식별자값, 노선2_식별자값, false, 거리_10);
 
         // when
-        final StationResponse response = stationService.findByStationId(saveStationId);
+        final CreateSectionRequest 구간_요청
+                = new CreateSectionRequest("창동", "녹천", 노선2_식별자값, 10);
 
         // then
-        assertThat(response)
+        assertThatThrownBy(() -> stationService.saveSection(구간_요청))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 노선에_중복되는_구간을_추가할_경우_예외가_발생한다() {
+        // given
+        final Long 노선2_식별자값 = lineDao.save("2", "초록");
+        final Long 잠실_식별자값 = stationDao.save("잠실");
+        final Long 잠실새내_식별자값 = stationDao.save("잠실새내");
+        final Distance 거리_10 = new Distance(10);
+
+        sectionDao.save(잠실_식별자값, 잠실새내_식별자값, 노선2_식별자값, false, 거리_10);
+
+        // when
+        final CreateSectionRequest 구간_요청
+                = new CreateSectionRequest("잠실", "잠실새내", 노선2_식별자값, 10);
+
+        // then
+        assertThatThrownBy(() -> stationService.saveSection(구간_요청))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 노선에_구간이_하나_이상_존재할_때_상행역_하행역_모두_노선에_없는_새로운_구간을_추가할_경우_예외가_발생한다() {
+        // given
+        final Long 노선2_식별자값 = lineDao.save("2", "초록");
+        final Long 잠실_식별자값 = stationDao.save("잠실");
+        final Long 잠실새내_식별자값 = stationDao.save("잠실새내");
+        final Distance 거리_10 = new Distance(10);
+
+        sectionDao.save(잠실_식별자값, 잠실새내_식별자값, 노선2_식별자값, false, 거리_10);
+
+        // when
+        final CreateSectionRequest 구간_요청
+                = new CreateSectionRequest("창동", "녹천", 노선2_식별자값, 10);
+
+        // then
+        assertThatThrownBy(() -> stationService.saveSection(구간_요청))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 역_식별자값으로_역을_조회한다() {
+        // given
+        final CreateStationRequest 잠실_요청 = new CreateStationRequest("잠실");
+        final Long 잠실역_식별자값 = stationService.saveStation(잠실_요청);
+
+        // when
+        final StationResponse 역_조회_응답 = stationService.findByStationId(잠실역_식별자값);
+
+        // then
+        assertThat(역_조회_응답)
                 .usingRecursiveComparison()
                 .isEqualTo(new StationResponse(
-                        saveStationId, "잠실"
+                        잠실역_식별자값, "잠실"
                 ));
     }
 }
