bfaf51ab981deca2c45481c994e89bf34b56ac22
ingpyo <juninpyo@naver.com>
2023-05-14 20:04:14 +0900

    refactor: station id 삭제

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -3,23 +3,13 @@ package subway.domain;
 import java.util.Objects;
 
 public class Station {
-    private Long id;
     private String name;
 
     public Station() {
     }
 
-    public Station(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     public Station(String name) {
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
