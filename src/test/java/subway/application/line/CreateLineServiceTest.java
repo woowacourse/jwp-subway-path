964e5f5d26afa1cf05a0ae71252e816cd2be5f61
ingpyo <juninpyo@naver.com>
2023-05-15 00:41:02 +0900

    test: CreateLineService 테스트 작성

---
+++ src/test/java/subway/application/line/CreateLineServiceTest.java
@@ -0,0 +1,38 @@
+package subway.application.line;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import subway.domain.Line;
+import subway.domain.repository.LineRepository;
+import subway.ui.dto.request.LineRequest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+class CreateLineServiceTest {
+
+    private LineRepository lineRepository;
+    private CreateLineService createLineService;
+
+    @BeforeEach
+    void setUp() {
+        lineRepository = Mockito.mock(LineRepository.class);
+        createLineService = new CreateLineService(lineRepository);
+    }
+
+    @Test
+    @DisplayName("존재하는 노선이면 예외처리")
+    void createLine() {
+        given(lineRepository.findByName(any()))
+                .willReturn(Optional.of(new Line(1L, "1호선")));
+
+        assertThatThrownBy(
+                () -> createLineService.createLine(new LineRequest("1호선"))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
