82a052a6eea30d393bdb1756923c9dda9a217bac
hectick <ch.yeondev@gmail.com>
2023-05-14 16:59:09 +0900

    refactor: 테스트용 더미 데이터 분리

---
+++ src/test/resources/dummyData.sql
@@ -0,0 +1,10 @@
+INSERT INTO line(name, color) VALUES ('1호선', '남색');
+INSERT INTO section(upward_id, downward_id, distance, line_id) VALUES (null, null, null, 1);
+
+INSERT INTO station(name) VALUES ('잠실나루');
+INSERT INTO station(name) VALUES ('잠실');
+INSERT INTO station(name) VALUES ('강변');
+
+INSERT INTO station(name) VALUES ('노량진');
+INSERT INTO station(name) VALUES ('용산');
+INSERT INTO station(name) VALUES ('서울역');
