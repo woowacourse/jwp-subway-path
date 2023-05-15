b471bb9a9ae7dc68676b726ad3c0f654f2a3a47d
ChoiYoungHoon <dudgns2865@naver.com>
2023-05-15 21:10:31 +0900

    feat: cost 계산 로직 구현

--- src/main/java/subway/domain/Path.java
+++ src/main/java/subway/domain/Path.java
@@ -1,43 +1,27 @@
 package subway.domain;
 
 import java.util.List;
-import org.jgrapht.Graph;
-import org.jgrapht.GraphPath;
-import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import subway.domain.vo.Distance;
 
 public class Path {
 
-    private final Graph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
-    private final ShortestPathAlgorithm<Station, DefaultWeightedEdge> algorithm;
+    private final List<Station> stations;
+    private final Distance distance;
 
-    public Path(final Lines lines) {
-        addLines(lines);
-        algorithm = new DijkstraShortestPath<>(graph);
+    public Path(final List<Station> stations, final Distance distance) {
+        this.stations = stations;
+        this.distance = distance;
     }
 
-    private void addLines(final Lines lines) {
-        final List<Section> sections = lines.findAllSections();
-        for (final Section section : sections) {
-            final Station upStation = section.getUpStation();
-            final Station downStation = section.getDownStation();
-            final Distance distance = section.getDistance();
-            graph.addVertex(upStation);
-            graph.addVertex(downStation);
-            graph.setEdgeWeight(graph.addEdge(upStation, downStation), distance.getValue());
-        }
+    public long calculateCost() {
+        return 0;
     }
 
-    public List<Station> findShortest(final Station start, final Station end) {
-        final GraphPath<Station, DefaultWeightedEdge> path = algorithm.getPath(start, end);
-        return path.getVertexList();
+    public List<Station> getStations() {
+        return stations;
     }
 
-    public long findTotalDistanceAsLong(final Station start, final Station end) {
-        final GraphPath<Station, DefaultWeightedEdge> path = algorithm.getPath(start, end);
-        return (long) path.getWeight();
+    public Distance getDistance() {
+        return distance;
     }
 }
