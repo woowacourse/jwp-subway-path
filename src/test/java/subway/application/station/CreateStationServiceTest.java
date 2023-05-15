72c24193927c2855ebe3e60a690ac3de82f19b57
ingpyo <juninpyo@naver.com>
2023-05-15 14:01:19 +0900

    test: CreateStationService 테스트 작성

---
+++ src/test/java/subway/application/station/CreateStationServiceTest.java
@@ -0,0 +1,37 @@
+package subway.application.station;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import subway.domain.Station;
+import subway.domain.repository.StationRepository;
+import subway.ui.dto.request.StationCreateRequest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+class CreateStationServiceTest {
+    private StationRepository stationRepository;
+    private CreateStationService createStationService;
+
+    @BeforeEach
+    void setUp() {
+        stationRepository = Mockito.mock(StationRepository.class);
+        createStationService = new CreateStationService(stationRepository);
+    }
+
+    @Test
+    @DisplayName("존재하는 역이면 예외처리")
+    void createStation() {
+        given(stationRepository.findByName(any()))
+                .willReturn(Optional.of(new Station("라빈")));
+
+        assertThatThrownBy(
+                () -> createStationService.createStation(new StationCreateRequest("라빈"))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
