01ed41121d9063bc4bc947ba0207c133a7844413
jaeyeon kim <jakind@naver.com>
2023-05-15 16:13:43 +0900

    feat: VO 에 Equals, HashCode 추가

--- src/main/java/subway/service/domain/Distance.java
+++ src/main/java/subway/service/domain/Distance.java
@@ -1,5 +1,7 @@
 package subway.service.domain;
 
+import java.util.Objects;
+
 public class Distance {
 
     private static final int LOWER_BOUND_INTEGER_INCLUSIVE = 0;
@@ -25,6 +27,26 @@ public class Distance {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance = (Distance) o;
+        return value == distance.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Distance{" +
+                "value=" + value +
+                '}';
     }
 
 }
