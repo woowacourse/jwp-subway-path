850fe8d97f0de543ba7b7cd058f3845c745ad0fa
Miseong Kim <miseongk@naver.com>
2023-05-15 15:29:02 +0900

    test: 실패하는 테스트 수정

--- src/test/java/subway/ui/StationControllerTest.java
+++ src/test/java/subway/ui/StationControllerTest.java
@@ -210,7 +210,7 @@ public class StationControllerTest {
             lineRepository.save(new Line("1호선", "RED", List.of(
                     new Section("A", "B", 2)
             )));
-            final StationSaveRequest request = new StationSaveRequest("1호선", "B", "C", RIGHT, 3);
+            final StationSaveRequest request = new StationSaveRequest("1호선", "B", "C", null, 3);
 
             // when
             ExtractableResponse<Response> response = RestAssured
