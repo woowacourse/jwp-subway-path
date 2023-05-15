51960d3eb616f0a58d12604410b4b7bfff3e7f05
ingpyo <juninpyo@naver.com>
2023-05-15 14:25:28 +0900

    feat: randomport 추상클래스 정의 및 CreateLineController

---
+++ src/test/java/subway/ui/line/CreateLineControllerTest.java
@@ -0,0 +1,46 @@
+package subway.ui.line;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import subway.common.IntegrationTest;
+import subway.domain.Line;
+import subway.persistence.repository.LineRepositoryImpl;
+import subway.ui.dto.request.LineRequest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CreateLineControllerTest extends IntegrationTest {
+
+    @Autowired
+    private LineRepositoryImpl lineRepository;
+
+    @Test
+    @DisplayName("노선을 추가한다.")
+    void createLine() {
+        final LineRequest lineRequest = new LineRequest("1호선");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(lineRequest)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.header("Location")).isNotBlank(),
+                () -> assertThat(lineRepository.findAll()).usingRecursiveComparison().isEqualTo(List.of(new Line(1L, "1호선"))
+                ));
+    }
+}
