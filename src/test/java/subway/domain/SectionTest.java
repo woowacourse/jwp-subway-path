97ce9316e567100b7505bebc8e11fd808b3797d5
hong-sile <gurwns9325@naver.com>
2023-05-15 14:28:36 +0900

    test: prevStation과 nextStation이 동일한지 확인하는 기능 추가

--- src/test/java/subway/domain/SectionTest.java
+++ src/test/java/subway/domain/SectionTest.java
@@ -2,6 +2,7 @@ package subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,11 +22,33 @@ class SectionTest {
                 .isTrue();
     }
 
-    @DisplayName("section을 생성할 때 beforeStation과 nextStation이 같은 경우 예외처리")
+    @DisplayName("section을 생성할 때 prevStation과 nextStation이 같은 경우 예외처리")
     @Test
     void validateSameStation() {
         assertThatThrownBy(() -> new Section(A_STATION, A_STATION, new Distance(10)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("구간은 서로 다른 두 역이어야 합니다.");
+    }
+
+    @DisplayName("section의 prevStation이 같은지 확인한다.")
+    @Test
+    void isEqualPrevStation() {
+        final Section section = new Section(A_STATION, B_STATION, new Distance(10));
+
+        assertAll(
+                () -> assertThat(section.isEqualPrevStation(A_STATION)).isTrue(),
+                () -> assertThat(section.isEqualPrevStation(B_STATION)).isFalse()
+        );
+    }
+
+    @DisplayName("section의 nextStation이 같은지 확인한다.")
+    @Test
+    void isEqualNextStation() {
+        final Section section = new Section(A_STATION, B_STATION, new Distance(10));
+
+        assertAll(
+                () -> assertThat(section.isEqualNextStation(B_STATION)).isTrue(),
+                () -> assertThat(section.isEqualNextStation(A_STATION)).isFalse()
+        );
     }
 }
