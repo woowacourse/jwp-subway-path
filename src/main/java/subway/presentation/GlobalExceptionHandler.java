c5f3082e1cad3f7af9b58a3caf270a5215e8fd0b
apptie <appti@naver.com>
2023-05-15 19:19:49 +0900

    refactor: ControllerAdvice 클래스 이름 변경 및 예외 처리 메소드 추가

---
+++ src/main/java/subway/presentation/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package subway.presentation;
+
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import subway.presentation.dto.response.ExceptionResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String EXCEPTION_FORMAT = "%s : ";
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(final Exception ex) {
+        logger.error(String.format(EXCEPTION_FORMAT, "Exception"), ex);
+
+        return ResponseEntity.internalServerError().body(new ExceptionResponse("예상치 못한 예외가 발생했습니다."));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(final IllegalArgumentException ex) {
+        logger.info(String.format(EXCEPTION_FORMAT, "IllegalArgumentException"), ex);
+
+        return ResponseEntity.badRequest().body(new ExceptionResponse(ex.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+            final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.info(String.format(EXCEPTION_FORMAT, "MethodArgumentNotValidException"), ex);
+
+        final String fieldErrorMessages = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        return ResponseEntity.badRequest().body(new ExceptionResponse(fieldErrorMessages));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            final Exception ex,
+            final Object body,
+            final HttpHeaders headers,
+            final HttpStatus status,
+            final WebRequest request
+    ) {
+        logger.warn(String.format(EXCEPTION_FORMAT, ex.getClass().getSimpleName()), ex);
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+}
