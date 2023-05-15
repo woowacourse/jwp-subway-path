78e29b285f646549f641d23f66095d10fcbd4a8e
kiarakim <kiarakim413@gmail.com>
2023-05-15 10:01:35 +0900

    test: 전체 노선 조회 서비스 테스트 코드 작성

--- src/test/java/subway/application/LineServiceTest.java
+++ src/test/java/subway/application/LineServiceTest.java
@@ -2,6 +2,8 @@ package subway.application;
 
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import subway.domain.Line;
 import subway.persistence.LineJdbcRepository;
 import subway.ui.dto.LineRequest;
 import subway.ui.dto.LineResponse;
@@ -37,8 +40,20 @@ class LineServiceTest {
 		Assertions.assertThat(1L).isEqualTo(response.getId());
 	}
 
+	@DisplayName("전체 노선 조회 서비스 테스트")
 	@Test
 	void findAll() {
+		// given
+		final Line line2 = new Line(1L, "2호선");
+		given(repository.findAll()).willReturn(List.of(line2));
+
+		// when
+		final List<LineResponse> lines = service.findAll();
+
+		// then
+		Assertions.assertThat(lines)
+			.usingRecursiveComparison()
+			.isEqualTo(List.of(new LineResponse(1L, "2호선")));
 	}
 
 	@Test
