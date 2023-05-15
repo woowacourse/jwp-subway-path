11676ba17d9082d08e9351a0b3dafe566ca6702e
Combi153 <jcm04@naver.com>
2023-05-15 16:22:02 +0900

    feat: Name 객체 로직 추가

---
+++ src/main/java/subway/domain/Name.java
@@ -0,0 +1,47 @@
+package subway.domain;
+
+import java.util.Objects;
+
+public class Name {
+
+    private final String name;
+
+    public Name(final String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("노선 이름이 입력되지 않았습니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Name name = (Name) o;
+        return Objects.equals(this.name, name.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "LineName{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
