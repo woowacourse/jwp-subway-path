d27d59ec4b7aa63f9d7eed7745435e9656017560
LJW25 <dbjwlee@gmail.com>
2023-05-15 20:28:06 +0900

    feat(LineResponse): 노선 조회 응답 형식 변경

--- src/main/java/subway/dto/LineResponse.java
+++ src/main/java/subway/dto/LineResponse.java
@@ -2,18 +2,26 @@ package subway.dto;
 
 import subway.domain.Line;
 
-public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
+import java.util.List;
 
-    public LineResponse(Long id, String name, String color) {
+public class LineResponse {
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final List<String> stations;
+
+    public LineResponse(final Long id, final String name, final String color) {
+        this(id, name, color, List.of());
+    }
+
+    public LineResponse(final Long id, final String name, final String color, final List<String> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.stations = stations;
     }
 
-    public static LineResponse of(Line line) {
+    public static LineResponse of(final Line line) {
         return new LineResponse(line.getId(), line.getName(), line.getColor());
     }
 
@@ -27,5 +35,9 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<String> getStations() {
+        return stations;
     }
 }
