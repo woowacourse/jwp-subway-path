ceba05ce9108b0ebf58a16bbf3d195837839f870
zillionme <beatus96@naver.com>
2023-05-15 17:29:49 +0900

    feat: 구간 테이블 추가 및 기존 테이블 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,3 @@
-create table if not exists STATION
-(
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
-);
-
 create table if not exists LINE
 (
     id bigint auto_increment not null,
@@ -13,12 +6,23 @@ create table if not exists LINE
     primary key(id)
 );
 
---//todo 물어볼 것 : 기본 키 id를 없애도 되는지?
+create table if not exists STATION
+(
+    id          bigint            not null      auto_increment ,
+    line_id     bigint            not null      references line(id)        on delete cascade,
+    name        varchar(255)      not null,
+    primary key(id),
+    unique (line_id, name)
+);
+
+
 create table if not exists SECTION
 (
+    id                 bigint    not null      auto_increment,
     line_id            bigint    not null      references line(id)        on delete cascade,
     up_station_id      bigint    not null      references station(id)     on delete cascade,
     down_station_id    bigint    not null      references station(id)     on delete cascade,
     distance           int       not null,
-    primary key(up_station_id, down_station_id)
+    primary key(id),
+    unique(up_station_id, down_station_id)
 );
