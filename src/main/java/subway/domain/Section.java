f842c1e8ac5ffa4099ba988c93a6bd08f49262ff
kiarakim <kiarakim413@gmail.com>
2023-05-15 14:47:16 +0900

    refactor: 구간이 노선을 알고 있도록 수정

--- src/main/java/subway/domain/Section.java
+++ src/main/java/subway/domain/Section.java
@@ -1,102 +1,120 @@
 package subway.domain;
 
-import java.util.List;
-
 public class Section {
-    private Long id;
-    private Station upStation;
-    private Station downStation;
-    private Long distance;
+	private Long id;
+	private Line line;
+	private Station upStation;
+	private Station downStation;
+	private Long distance;
 
-    public Section(final Station upStation, final Station downStation, final Long distance) {
-        this.upStation = upStation;
-        this.downStation = downStation;
-        this.distance = distance;
-    }
+	public Section(final Long id, final Line line, final Station upStation, final Station downStation, final Long distance) {
+		this.id = id;
+		this.line = line;
+		this.upStation = upStation;
+		this.downStation = downStation;
+		this.distance = distance;
+	}
 
-    public Section(final Long id, final Station upStation, final Station downStation, final Long distance) {
-        this.id = id;
-        this.upStation = upStation;
-        this.downStation = downStation;
-        this.distance = distance;
-    }
+	public Section(final Line line, final Station upStation, final Station downStation, final Long distance) {
+		this.line = line;
+		this.upStation = upStation;
+		this.downStation = downStation;
+		this.distance = distance;
+	}
+
+	public Section(final Station upStation, final Station downStation, final Long distance) {
+		this.upStation = upStation;
+		this.downStation = downStation;
+		this.distance = distance;
+	}
+
+	public static Section of(final Line line, final String upStation, final String downStation, final Long distance) {
+		return new Section(line, new Station(upStation), new Station(downStation), distance);
+	}
+	public static Section of(final String line, final String upStation, final String downStation, final Long distance) {
+		return new Section(new Line(line), new Station(upStation), new Station(downStation), distance);
+	}
+
+	public static Section of(final String upStation, final String downStation) {
+		return new Section(null, new Station(upStation), new Station(downStation), null);
+	}
 
 
-    public static Section of(final String upStation, final String downStation, final Long distance) {
-        return new Section(null, new Station(upStation), new Station(downStation), distance);
-    }
+	private void validateDuplication(final Station nowStation, final Station nextStation) {
+		if (nowStation.equals(nextStation)) {
+			throw new IllegalArgumentException("중복된 이름입니다. 다른 이름을 입력해주세요.");
+		}
+	}
 
-    public static Section of(final String upStation, final String downStation) {
-        return new Section(null, new Station(upStation), new Station(downStation), null);
-    }
+	public boolean validateEqualEndPoint(final Station upEndPoint, final Station downEndPoint) {
+		return upEndPoint.equals(this.downStation) ^ downEndPoint.equals(this.upStation);
+	}
 
-    public Long getDistance() {
-        return distance;
-    }
+	public boolean validateDuplicateSection(final Section newSection) {
+		return this.downStation.equals(newSection.downStation)
+			&& this.upStation.equals(newSection.upStation) ||
+			this.upStation.equals(newSection.downStation)
+				&& this.downStation.equals(newSection.upStation);
+	}
 
+	public boolean validateMatchOne(final Section section) {
+		return this.downStation.equals(section.downStation)
+			|| this.upStation.equals(section.upStation);
+	}
 
-    private void validateDuplication(final Station nowStation, final Station nextStation) {
-        if (nowStation.equals(nextStation)) {
-            throw new IllegalArgumentException("중복된 이름입니다. 다른 이름을 입력해주세요.");
-        }
-    }
-    public boolean validateEqualEndPoint(final Station upEndPoint,final Station downEndPoint) {
-        return upEndPoint.equals(this.downStation) ^ downEndPoint.equals(this.upStation);
-    }
+	public void changeSection(final Section newSection) {
+		changeStation(newSection);
+		changeDistance(newSection);
+	}
 
-    public boolean validateDuplicateSection(final Section newSection) {
-        return this.downStation.equals(newSection.downStation)
-                && this.upStation.equals(newSection.upStation) ||
-                this.upStation.equals(newSection.downStation)
-                        && this.downStation.equals(newSection.upStation);
-    }
+	private void changeStation(final Section newSection) {
+		if (this.upStation.equals(newSection.upStation)) {
+			this.upStation = newSection.downStation;
+		}
 
-    public boolean validateMatchOne(final Section section) {
-        return this.downStation.equals(section.downStation)
-                || this.upStation.equals(section.upStation);
-    }
+		if (this.downStation.equals(newSection.downStation)) {
+			this.downStation = newSection.upStation;
+		}
+	}
 
-    public void changeSection(final Section newSection) {
-        changeStation(newSection);
-        changeDistance(newSection);
-    }
+	private void changeDistance(final Section newSection) {
+		distance -= newSection.distance;
+	}
 
-    private void changeStation(final Section newSection) {
-        if (this.upStation.equals(newSection.upStation)) {
-            this.upStation = newSection.downStation;
-        }
+	public boolean hasStation(final Station station) {
+		return upStation.equals(station) || downStation.equals(station);
+	}
 
-        if (this.downStation.equals(newSection.downStation)) {
-            this.downStation = newSection.upStation;
-        }
-    }
+	public boolean isNowStation(final Station station) {
+		return station.equals(this.upStation);
+	}
 
-    private void changeDistance(final Section newSection) {
-        distance -= newSection.distance;
-    }
+	public Section mergedSection(final Section section, final Station station) {
+		final Long distance = this.distance + section.getDistance();
 
-    public boolean hasStation(final Station station) {
-        return upStation.equals(station) || downStation.equals(station);
-    }
+		if (this.downStation.equals(station)) {
+			return new Section(this.upStation, section.downStation, distance);
+		}
+		return new Section(this.downStation, section.upStation, distance);
+	}
 
-    public Section mergedSection(final Section section, final Station station) {
-        final Long distance = this.distance + section.getDistance();
+	public Long getId() {
+		return id;
+	}
 
-        if (this.downStation.equals(station)) {
-            return new Section(this.upStation, section.downStation, distance);
-        }
-        return new Section(this.downStation, section.upStation, distance);
-    }
+	public Line getLine() {
+		return line;
+	}
 
-    public Station getUpStation() {
-        return upStation;
-    }
+	public Station getUpStation() {
+		return upStation;
+	}
 
-    public Station getDownStation() {
-        return downStation;
-    }
+	public Station getDownStation() {
+		return downStation;
+	}
 
-    public boolean isNowStation(final Station station) {
-        return station.equals(this.upStation);
-    }
+	public Long getDistance() {
+		return distance;
+	}
 }
