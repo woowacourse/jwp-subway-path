2a742dfd0f9eba354b978f7c4be13cf872e5a620
donghae <donghae1999@gmail.com>
2023-05-15 19:13:24 +0900

    refactor: 사용하지 않는 코드 삭제
    StationRequest, StationDao의 isExistStationById, findStations

--- src/main/java/subway/dao/StationDao.java
+++ src/main/java/subway/dao/StationDao.java
@@ -1,22 +1,19 @@
 package subway.dao;
 
-import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
 public class StationDao {
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert insertAction;
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     private RowMapper<StationEntity> rowMapper = (rs, rowNum) ->
             new StationEntity(
@@ -25,12 +22,11 @@ public class StationDao {
             );
 
 
-    public StationDao(JdbcTemplate jdbcTemplate, DataSource dataSource, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    public StationDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
         this.insertAction = new SimpleJdbcInsert(dataSource)
                 .withTableName("station")
                 .usingGeneratedKeyColumns("id");
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     public Long insert(StationEntity stationEntity) {
@@ -48,23 +44,9 @@ public class StationDao {
         jdbcTemplate.update(sql, id);
     }
 
-    public Boolean isExistStationById(Long id) {
-        String sql = "select exists(select * from station where id = ?)";
-
-        return jdbcTemplate.queryForObject(sql, Boolean.class, id);
-    }
-
-    public List<StationEntity> findStations(List<String> stations) {
-        String sql = "select * from station where station.name in (:name)";
-
-        SqlParameterSource parameters = new MapSqlParameterSource("name",stations);
-
-        return namedParameterJdbcTemplate.query(sql, parameters, rowMapper);
-    }
-
     public List<StationEntity> findAll() {
         String sql = "select * from station";
 
-        return jdbcTemplate.query(sql,rowMapper);
+        return jdbcTemplate.query(sql, rowMapper);
     }
 }
