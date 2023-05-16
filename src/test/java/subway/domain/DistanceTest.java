be51e07d3f1551d1e92f3223ca1eeabcb5416984
cpot5620 <=cpot5620@naver.com>
2023-05-16 16:15:23 +0900

    refactor: Distance 제약 조건 변경

--- src/test/java/subway/domain/DistanceTest.java
+++ src/test/java/subway/domain/DistanceTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 import subway.exception.GlobalException;
 
 class DistanceTest {
-    @ParameterizedTest(name = "두 역 사이 거리는 10km 이하 양의 정수만 가능하다.")
-    @ValueSource(ints = {1, 10})
+    @ParameterizedTest(name = "두 역 사이 거리는 양의 정수만 가능하다.")
+    @ValueSource(ints = {1, 10,100})
     void createDistanceSuccessTest(int distance) {
         assertDoesNotThrow(() -> new Distance(distance));
     }
 
-    @ParameterizedTest(name = "두 역 사이 거리는 10km 이하 양의 정수만 가능하다.")
-    @ValueSource(ints = {-1, 0, 11})
+    @ParameterizedTest(name = "두 역 사이 거리는 양의 정수만 가능하다.")
+    @ValueSource(ints = {-1, 0})
     void createDistanceFailTestByRange(int distance) {
         assertThatThrownBy(() -> new Distance(distance))
                 .isInstanceOf(GlobalException.class)
