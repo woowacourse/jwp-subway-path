edd8a887a1a2940e06cea039f975c4e0a6871f60
LJW25 <dbjwlee@gmail.com>
2023-05-16 16:53:32 +0900

    feat(Line): Line에 역 추가, 삭제 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,24 +1,39 @@
 package subway.domain;
 
+import java.util.Map;
 import java.util.Objects;
 
 public class Line {
-    private Long id;
-    private String name;
-    private String color;
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final SectionMap sectionMap;
 
-    public Line() {
+    public Line(final String name, final String color) {
+        this(null, name, color, new SectionMap());
     }
 
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
-
-    public Line(Long id, String name, String color) {
+    public Line(final Long id, final String name, final String color, final SectionMap sectionMap) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.sectionMap = sectionMap;
+    }
+
+    public Line(final Long id, final String name, final String color) {
+        this(id, name, color, new SectionMap());
+    }
+
+    public void addInitialSection(final Station upStation, final Station downStation, final int distance) {
+        sectionMap.addInitialSection(upStation, downStation, distance);
+    }
+
+    public void addSection(final Station upStation, final Station downStation, final int distance) {
+        sectionMap.addSection(upStation, downStation, distance);
+    }
+
+    public void deleteStation(final Station station) {
+        sectionMap.deleteStation(station);
     }
 
     public Long getId() {
@@ -33,16 +48,20 @@ public class Line {
         return color;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Line line = (Line) o;
-        return Objects.equals(id, line.id) && Objects.equals(name, line.name) && Objects.equals(color, line.color);
+    public Map<Station, Section> getSectionMap() {
+        return sectionMap.getSectionMap();
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, name, color);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Line line = (Line) o;
+        return Objects.equals(id, line.id) && Objects.equals(name, line.name) && Objects.equals(color, line.color);
     }
 }
