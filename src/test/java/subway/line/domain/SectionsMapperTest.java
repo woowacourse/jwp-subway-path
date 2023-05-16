269384162b995fb3afacd6d41a7b9b28dcfb28e1
thdwoqor <celebarte01@gmail.com>
2023-05-16 16:42:51 +0900

    refactor: 테스트 패키지명 변경

--- src/test/java/subway/line/domain/SectionsMapperTest.java
+++ src/test/java/subway/line/domain/SectionsMapperTest.java
@@ -1,4 +1,4 @@
-package subway.subwayMap.domain;
+package subway.line.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/subway/lineDetail/dao/LineDetailDaoTest.java
+++ src/test/java/subway/lineDetail/dao/LineDetailDaoTest.java
@@ -1,4 +1,4 @@
-package subway.line.dao;
+package subway.lineDetail.dao;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
