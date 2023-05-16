5c2401221f6db5612fc0f2e45519d3b72bdf36d4
seokhwan-an <asok369258@naver.com>
2023-05-16 15:40:08 +0900

    style: 가독성 높게 띄어쓰기 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,27 @@
 CREATE TABLE IF NOT EXISTS LINE
 (
-    id BIGINT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    color VARCHAR(20) NOT NULL,
+    id    BIGINT AUTO_INCREMENT NOT NULL,
+    name  VARCHAR(255)          NOT NULL UNIQUE,
+    color VARCHAR(20)           NOT NULL,
     PRIMARY KEY (id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS STATION
 (
-    id BIGINT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    line_id BIGINT NOT NULL,
+    id      BIGINT AUTO_INCREMENT NOT NULL,
+    name    VARCHAR(255)          NOT NULL,
+    line_id BIGINT                NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (line_id) REFERENCES LINE (id)
 );
 
 CREATE TABLE IF NOT EXISTS SECTION
 (
-    id BIGINT AUTO_INCREMENT NOT NULL,
-    left_station_id BIGINT NOT NULL,
-    right_station_id BIGINT NOT NULL,
-    line_id BIGINT NOT NULL,
-    distance INT NOT NULL,
+    id               BIGINT AUTO_INCREMENT NOT NULL,
+    left_station_id  BIGINT                NOT NULL,
+    right_station_id BIGINT                NOT NULL,
+    line_id          BIGINT                NOT NULL,
+    distance         INT                   NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (line_id) REFERENCES LINE (id)
 );
