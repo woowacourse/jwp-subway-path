21f8bfe634f1fd5a410daad33a5cbed9c12ffbd0
cpot5620 <=cpot5620@naver.com>
2023-05-16 20:28:27 +0900

    feat(Repository): Station findAll 메서드 추가

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -1,7 +1,11 @@
 package subway.domain;
 
+import java.util.List;
+
 public interface StationRepository {
     Long save(Station station);
+
+    List<Station> findAll();
 
     Station findById(Long id);
 
