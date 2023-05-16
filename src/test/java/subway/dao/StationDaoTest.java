5f9ca3d607045df153c5094e3c12082a2a90e0ab
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-16 14:31:22 +0900

    test: StationDao 테스트 작성

---
+++ src/test/java/subway/dao/StationDaoTest.java
@@ -0,0 +1,92 @@
+package subway.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import subway.dao.entity.StationEntity;
+
+@JdbcTest
+class StationDaoTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    StationDao stationDao;
+
+    @BeforeEach
+    void setUp() {
+        stationDao = new StationDao(jdbcTemplate, jdbcTemplate.getDataSource());
+    }
+
+    @Test
+    @DisplayName("역을 추가한다.")
+    void insert() {
+        StationEntity stationEntity = new StationEntity(null, "부산역");
+
+        StationEntity savedStationEntity = stationDao.insert(stationEntity);
+
+        assertAll(
+                () -> assertThat(savedStationEntity.getId()).isNotNull(),
+                () -> assertThat(savedStationEntity.getName()).isEqualTo(stationEntity.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("ID에 해당하는 역을 가져온다.")
+    void findById() {
+        StationEntity stationEntity = new StationEntity(null, "부산역");
+        StationEntity savedStationEntity = stationDao.insert(stationEntity);
+
+        StationEntity findStationEntity = stationDao.findById(savedStationEntity.getId()).get();
+
+        assertAll(
+                () -> assertThat(findStationEntity.getId()).isEqualTo(savedStationEntity.getId()),
+                () -> assertThat(findStationEntity.getName()).isEqualTo(savedStationEntity.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("이름에 해당하는 역을 가져온다.")
+    void findByName() {
+        StationEntity stationEntity = new StationEntity(null, "부산역");
+        StationEntity savedStationEntity = stationDao.insert(stationEntity);
+
+        StationEntity findStationEntity = stationDao.findByName(savedStationEntity.getName()).get();
+
+        assertAll(
+                () -> assertThat(findStationEntity.getId()).isEqualTo(savedStationEntity.getId()),
+                () -> assertThat(findStationEntity.getName()).isEqualTo(savedStationEntity.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("모든 역을 가져온다.")
+    void findAll() {
+        StationEntity stationEntity1 = new StationEntity(null, "부산역");
+        StationEntity stationEntity2 = new StationEntity(null, "서면역");
+        stationDao.insert(stationEntity1);
+        stationDao.insert(stationEntity2);
+
+        List<StationEntity> stations = stationDao.findAll();
+
+        assertThat(stations).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("ID에 해당하는 역을 삭제한다.")
+    void deleteById() {
+        StationEntity stationEntity = new StationEntity(null, "부산역");
+        StationEntity savedStationEntity = stationDao.insert(stationEntity);
+
+        stationDao.deleteById(savedStationEntity.getId());
+
+        assertThat(stationDao.findById(savedStationEntity.getId()).isEmpty()).isTrue();
+    }
+}
