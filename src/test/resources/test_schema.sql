c4accafb7c160308000c24a0ad5f09389bf700aa
jjongwa <troas96@naver.com>
2023-05-16 22:12:05 +0900

    refactor: 필요없는 테스트 제거

---
+++ src/test/resources/test_schema.sql
@@ -0,0 +1,25 @@
+drop table STATION;
+drop table LINE;
+drop table SECTION;
+
+create table if not exists STATION
+(
+    id   bigint auto_increment not null,
+    name varchar(255)          not null unique
+);
+
+create table if not exists LINE
+(
+    id    bigint auto_increment not null,
+    name  varchar(255)          not null unique,
+    color varchar(20)           not null
+);
+
+create table if not exists SECTION
+(
+    id       bigint auto_increment not null,
+    line_id  bigint                not null,
+    from_id  bigint                not null,
+    to_id    bigint                not null,
+    distance bigint                not null
+);
