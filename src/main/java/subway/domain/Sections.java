297b802cd5a2d62174584866a4138bb06026064c
Gyuseong Lee <lks971003@gmail.com>
2023-05-16 14:52:40 +0900

    test : 삭제, 조회 테스트 추가

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -82,7 +82,8 @@ public class Sections {
     }
 
     private boolean isInit(Station station) {
-        return sections.size() == 1 && sections.get(0).isUp(station);
+        return sections.size() == 1
+                && (sections.get(0).isUp(station) || sections.get(0).isDown(station));
     }
 
     private boolean isMid(Station station) {
@@ -134,10 +135,6 @@ public class Sections {
                 .orElseThrow(() -> new IllegalArgumentException("라인에 등록되지 않은 구간입니다."));
     }
 
-    public List<Section> getSections() {
-        return List.copyOf(sections);
-    }
-
     public List<Station> getAllStations() {
         List<Station> stations = sections.stream()
                 .map(Section::getDown)
@@ -152,5 +149,9 @@ public class Sections {
         return sections.stream()
                 .map(Section::getDistance)
                 .collect(Collectors.toList());
+    }
+
+    public List<Section> getSections() {
+        return List.copyOf(sections);
     }
 }
--- src/test/java/subway/domain/SectionsTest.java
+++ src/test/java/subway/domain/SectionsTest.java
@@ -1,17 +1,18 @@
 package subway.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.Fixture;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class SectionsTest {
-
-    @Test
     @DisplayName("역 A와 B를 초기 역등록한다.")
+    @Test
     void addInitStations() {
         // given
         Sections sections = new Sections();
@@ -25,8 +26,8 @@ class SectionsTest {
         assertThat(section.getDown()).isEqualTo(Fixture.stationB);
     }
 
-    @Test
     @DisplayName("구간 BC에 상행 종점 A를 추가하면 구간 AB가 생긴다")
+    @Test
     void addUpEndpoint() {
         // given
         Sections sections = new Sections(List.of(Fixture.sectionBC));
@@ -40,8 +41,8 @@ class SectionsTest {
         assertThat(section.getDown()).isEqualTo(Fixture.stationB);
     }
 
-    @Test
     @DisplayName("구간 AB에 하행 종점 C를 추가하면 구간 BC가 생긴다")
+    @Test
     void addDownEndpoint() {
         // given
         Sections sections = new Sections(List.of(Fixture.sectionAB));
@@ -55,8 +56,8 @@ class SectionsTest {
         assertThat(section.getDown()).isEqualTo(Fixture.stationC);
     }
 
-    @Test
     @DisplayName("구간 AB 중간에 C를 추가하면 구간 AB, BC가 생긴다")
+    @Test
     void addIntermediate() {
         // given
         Sections sections = new Sections(List.of(Fixture.sectionAB));
@@ -72,5 +73,83 @@ class SectionsTest {
         assertThat(prevToThis.getDown()).isEqualTo(Fixture.stationC);
         assertThat(thisToNext.getUp()).isEqualTo(Fixture.stationC);
         assertThat(thisToNext.getDown()).isEqualTo(Fixture.stationB);
+    }
+
+    @DisplayName("구간 중간의 역을 삭제하면 앞의 역과 다음 역이 이어지고 거리는 두 구간 거리의 합이다")
+    @Test
+    void deleteMid() {
+        Sections sections = new Sections(List.of(Fixture.sectionAB, Fixture.sectionBC));
+
+        // when
+        sections.delete(Fixture.stationB);
+
+        //then
+        Assertions.assertThat(sections.getSections()).contains(
+                new Section(Fixture.stationA,
+                        Fixture.stationC,
+                        Fixture.sectionAB.getDistance().sum(Fixture.sectionBC.getDistance())
+                )
+        );
+    }
+
+    @DisplayName("역이 두 개이면 모든 구간을 삭제한다")
+    @Test
+    void deleteInit() {
+        Sections sections = new Sections(List.of(Fixture.sectionAB));
+
+        // when
+        sections.delete(Fixture.stationA);
+
+        //then
+        assertThat(sections.getSections().size()).isEqualTo(0);
+    }
+
+    @DisplayName("상행 종점을 삭제한다")
+    @Test
+    void deleteUp() {
+        Sections sections = new Sections(List.of(Fixture.sectionAB, Fixture.sectionBC));
+
+        // when
+        sections.delete(Fixture.stationA);
+
+        //then
+        Assertions.assertThat(sections.getSections()).containsOnly(Fixture.sectionBC);
+    }
+
+    @DisplayName("하행 종점을 삭제한다")
+    @Test
+    void deleteDown() {
+        Sections sections = new Sections(List.of(Fixture.sectionAB, Fixture.sectionBC));
+
+        // when
+        sections.delete(Fixture.stationC);
+
+        //then
+        Assertions.assertThat(sections.getSections()).containsOnly(Fixture.sectionAB);
+    }
+
+
+    @DisplayName("모든 역을 조회한다")
+    @Test
+    void getAllStations() {
+        Sections sections = new Sections(List.of(Fixture.sectionAB, Fixture.sectionBC));
+
+        // when
+        List<Station> stations = sections.getAllStations();
+
+        //then
+        Assertions.assertThat(stations).containsOnly(Fixture.stationA, Fixture.stationB, Fixture.stationC);
+    }
+
+    @DisplayName("모든 구간의 거리를 조회한다")
+    @Test
+    void getAllDistances() {
+        Sections sections = new Sections(List.of(Fixture.sectionAB, Fixture.sectionBC));
+
+        // when
+        List<Distance> distances = sections.getAllDistances();
+
+        //then
+        Assertions.assertThat(distances).containsOnly(Fixture.sectionAB.getDistance(), Fixture.sectionBC.getDistance());
     }
 }
