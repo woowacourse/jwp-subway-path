19d7f6a2654c5b6c102b64dbe1e4bfc82ca5666c
mcodnjs <codnjs8989@gmail.com>
2023-05-17 00:19:18 +0900

    feat: 에러 핸들링 메서드 추가

--- src/main/java/subway/GlobalExceptionHandler.java
+++ src/main/java/subway/GlobalExceptionHandler.java
@@ -1,15 +1,48 @@
 package subway;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(final HttpRequestMethodNotSupportedException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        return ResponseEntity.badRequest()
+                .body(ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        return ResponseEntity.badRequest()
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(final IllegalArgumentException exception) {
+        return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElement(final NoSuchElementException exception) {
+        return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(final Exception exception) {
         exception.printStackTrace();
-        return ResponseEntity.badRequest().body(exception.getMessage());
+        return ResponseEntity.internalServerError()
+                .body("알 수 없는 서버 에러가 발생했습니다.");
     }
 }
