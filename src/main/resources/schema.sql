2f8512b278df8b002eaf14e284a9359fc7d6f273
jiwon <ljwon77@daum.net>
2023-05-16 18:59:40 +0900

    build: 테이블 스크립트 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS line
     id bigint auto_increment NOT NULL,
     name VARCHAR(255) NOT NULL UNIQUE,
     color VARCHAR(20) NOT NULL,
+    extra_fare int NOT NULL,
     PRIMARY KEY(id)
 );
 
