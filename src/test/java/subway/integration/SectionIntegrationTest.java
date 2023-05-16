d1df184b3ae72f46c7f43ecd80fadbf0b7fec03a
kiarakim <kiarakim413@gmail.com>
2023-05-16 10:50:30 +0900

    test: 지하철 노선에 따른 역 조회 인수테스트 코드 작성

--- src/test/java/subway/integration/SectionIntegrationTest.java
+++ src/test/java/subway/integration/SectionIntegrationTest.java
@@ -28,6 +28,7 @@ public class SectionIntegrationTest extends IntegrationTest{
 	@DisplayName("지하철 구간을 생성한다")
 	@Test
 	void createSection(){
+		// when
 		ExtractableResponse<Response> response = RestAssured.given().log().all()
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.body(sectionRequest1)
@@ -37,5 +38,28 @@ public class SectionIntegrationTest extends IntegrationTest{
 
 		// then
 		Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+	}
+
+	@DisplayName("특정 노선의 지하철역을 조회한다")
+	@Test
+	void findAllByLine(){
+		// given
+		ExtractableResponse<Response> createResponse = RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(sectionRequest1)
+			.when().post("/sections")
+			.then().log().all()
+			.extract();
+
+		// when
+		final long lineId = 1;
+		final ExtractableResponse<Response> findResponse = RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when().get("/sections/{lineId}", lineId)
+			.then().log().all()
+			.extract();
+
+		// then
+		Assertions.assertThat(findResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
 	}
 }
