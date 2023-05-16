1ee8af6462969ecf34f3f95c53db0d9d4e1595ab
Combi153 <jcm04@naver.com>
2023-05-16 15:59:41 +0900

    refactor: LineDao 코드 리팩터링
    변경 내용:
    - 테스트 픽스쳐 추가
    - LineEntity 추가
    - 검증 로직 추가

--- src/main/java/subway/dao/LineDao.java
+++ src/main/java/subway/dao/LineDao.java
@@ -1,69 +1,65 @@
-//package subway.dao;
-//
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.RowMapper;
-//import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-//import org.springframework.stereotype.Repository;
-//import subway.domain.Line;
-//
-//import javax.sql.DataSource;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//@Repository
-//public class LineDao {
-//    private final JdbcTemplate jdbcTemplate;
-//    private final SimpleJdbcInsert insertAction;
-//
-//    private RowMapper<Line> rowMapper = (rs, rowNum) ->
-//            new Line(
-//                    rs.getLong("id"),
-//                    rs.getString("name"),
-//                    rs.getString("color"),
-//                    sections);
-//
-//    private final RowMapper<Boolean> booleanMapper = (resultSet, rowNum) -> resultSet.getBoolean("isExist");
-//
-//
-//    public LineDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
-//        this.jdbcTemplate = jdbcTemplate;
-//        this.insertAction = new SimpleJdbcInsert(dataSource)
-//                .withTableName("line")
-//                .usingGeneratedKeyColumns("id");
-//    }
-//
-//    public Line insert(Line line) {
-//        Map<String, Object> params = new HashMap<>();
-//        params.put("id", line.getId());
-//        params.put("name", line.getNameValue());
-//        params.put("color", line.getColorValue());
-//
-//        Long lineId = insertAction.executeAndReturnKey(params).longValue();
-//        return new Line(lineId, line.getNameValue(), line.getColorValue(), sections);
-//    }
-//
-//    public List<Line> findAll() {
-//        String sql = "select id, name, color from LINE";
-//        return jdbcTemplate.query(sql, rowMapper);
-//    }
-//
-//    public Line findById(Long id) {
-//        String sql = "select id, name, color from LINE WHERE id = ?";
-//        return jdbcTemplate.queryForObject(sql, rowMapper, id);
-//    }
-//
-//    public Boolean hasId(final Long id) {
-//        String sql = "select exists(select id, name, color from LINE WHERE id = ?) as isExist";
-//        return jdbcTemplate.queryForObject(sql, booleanMapper, id);
-//    }
-//
-//    public void update(Line newLine) {
-//        String sql = "update LINE set name = ?, color = ? where id = ?";
-//        jdbcTemplate.update(sql, new Object[]{newLine.getNameValue(), newLine.getColorValue(), newLine.getId()});
-//    }
-//
-//    public void deleteById(Long id) {
-//        jdbcTemplate.update("delete from Line where id = ?", id);
-//    }
-//}
+package subway.dao;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class LineDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insertAction;
+
+    private final RowMapper<LineEntity> rowMapper = (rs, rowNum) ->
+            new LineEntity(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("color")
+            );
+
+    public LineDao(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.insertAction = new SimpleJdbcInsert(dataSource)
+                .withTableName("line")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public LineEntity insert(final LineEntity lineEntity) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", lineEntity.getId());
+        params.put("name", lineEntity.getName());
+        params.put("color", lineEntity.getColor());
+
+        Long lineId = insertAction.executeAndReturnKey(params).longValue();
+        return new LineEntity(lineId, lineEntity.getName(), lineEntity.getColor());
+    }
+
+    public List<LineEntity> findAll() {
+        String sql = "select id, name, color from LINE";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public LineEntity findById(final Long id) {
+        try {
+            String sql = "select id, name, color from LINE WHERE id = ?";
+            return jdbcTemplate.queryForObject(sql, rowMapper, id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new IllegalArgumentException("존재하지 않는 호선입니다.");
+        }
+    }
+
+    public void update(final LineEntity lineEntity) {
+        String sql = "update LINE set name = ?, color = ? where id = ?";
+        jdbcTemplate.update(sql, new Object[]{lineEntity.getName(), lineEntity.getColor(), lineEntity.getId()});
+    }
+
+    public void deleteById(final Long id) {
+        jdbcTemplate.update("delete from Line where id = ?", id);
+    }
+}
