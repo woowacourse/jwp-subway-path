56e7b8765d59272d7fecd29b8d925c3b988fe206
eunbii0213 <eunbii0213@naver.com>
2023-05-16 14:47:25 +0900

    feat: Station의 일급 컬렉션 생성

---
+++ src/main/java/subway/domain/Stations.java
@@ -0,0 +1,20 @@
+package subway.domain;
+
+import java.util.List;
+
+public class Stations {
+
+    private final List<Station> stations;
+
+    public Stations(List<Station> stations) {
+        this.stations = stations;
+    }
+
+    public void addStation(Station station){
+        stations.add(station);
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+}
