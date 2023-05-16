e40bc4ae4c6f3baccdee32a5bdc37b7fa0083fee
kiarakim <kiarakim413@gmail.com>
2023-05-16 10:52:59 +0900

    refactor: 중복 코드 처리

--- src/test/java/subway/integration/SectionIntegrationTest.java
+++ src/test/java/subway/integration/SectionIntegrationTest.java
@@ -17,25 +17,24 @@ import subway.ui.dto.request.SectionRequest;
 @Sql("/test-data.sql")
 public class SectionIntegrationTest extends IntegrationTest{
 
-	private SectionRequest sectionRequest1;
+	private SectionRequest request;
+	private ExtractableResponse<Response> response;
 
 	@BeforeEach
 	public void setUp(){
 		super.setUp();
-		sectionRequest1 = new SectionRequest("2호선","잠실","역삼", 5L);
+		request = new SectionRequest("2호선","잠실","역삼", 5L);
+		response = RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(request)
+			.when().post("/sections")
+			.then().log().all()
+			.extract();
 	}
 
 	@DisplayName("지하철 구간을 생성한다")
 	@Test
 	void createSection(){
-		// when
-		ExtractableResponse<Response> response = RestAssured.given().log().all()
-			.contentType(MediaType.APPLICATION_JSON_VALUE)
-			.body(sectionRequest1)
-			.when().post("/sections")
-			.then().log().all()
-			.extract();
-
 		// then
 		Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 	}
@@ -43,14 +42,6 @@ public class SectionIntegrationTest extends IntegrationTest{
 	@DisplayName("특정 노선의 지하철역을 조회한다")
 	@Test
 	void findAllByLine(){
-		// given
-		ExtractableResponse<Response> createResponse = RestAssured.given().log().all()
-			.contentType(MediaType.APPLICATION_JSON_VALUE)
-			.body(sectionRequest1)
-			.when().post("/sections")
-			.then().log().all()
-			.extract();
-
 		// when
 		final long lineId = 1;
 		final ExtractableResponse<Response> findResponse = RestAssured.given().log().all()
