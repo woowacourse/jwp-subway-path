1e4893bb3016639927178926e5402035899820ba
gitchan <eunkeeeee@gmail.com>
2023-05-16 10:56:17 +0900

    refactor(GlobalControllerAdvice): 로그 세부내용 변경

--- src/main/java/subway/controller/GlobalControllerAdvice.java
+++ src/main/java/subway/controller/GlobalControllerAdvice.java
@@ -13,7 +13,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleException(Exception exception) {
-        logger.error(exception.getMessage());
+        logger.error("알 수 없는 오류가 발생했습니다.", exception);
         return ResponseEntity
                 .internalServerError()
                 .body("알 수 없는 오류가 발생했습니다. 잠시 후에 다시 시도해주세요.");
@@ -21,7 +21,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgumentException(final IllegalArgumentException exception) {
-        logger.warn(exception.getMessage());
+        logger.warn("잘못된 인자가 전달되었습니다.", exception);
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
