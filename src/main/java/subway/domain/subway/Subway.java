085ad8f56f6ab554cf016644421a75e2ebd88976
woo-chang <clllickme@naver.com>
2023-05-16 17:40:37 +0900

    feat: 지하철에서 최단 경로 조회 기능 구현

--- src/main/java/subway/domain/subway/Subway.java
+++ src/main/java/subway/domain/subway/Subway.java
@@ -1,55 +1,19 @@
 package subway.domain.subway;
 
-import java.util.List;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
-import subway.domain.line.Line;
-import subway.domain.section.Section;
 import subway.domain.station.Station;
-import subway.exception.InvalidStationNameException;
 
 public class Subway {
 
-    private final DijkstraShortestPath dijkstraShortestPath;
+    private final SubwayGraph subwayGraph;
 
-    public Subway(final List<Line> lines) {
-        this.dijkstraShortestPath = new DijkstraShortestPath(generateGraph(lines));
+    public Subway(final SubwayGraph subwayGraph) {
+        this.subwayGraph = subwayGraph;
     }
 
-    private WeightedMultigraph<Station, DefaultWeightedEdge> generateGraph(final List<Line> lines) {
-        final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(
-                DefaultWeightedEdge.class);
-
-        for (final Line line : lines) {
-            drawGraph(graph, line);
-        }
-        return graph;
-    }
-
-    private void drawGraph(final WeightedMultigraph<Station, DefaultWeightedEdge> graph, final Line line) {
-        for (final Section section : line.getSections()) {
-            final Station upward = section.getUpward();
-            final Station downward = section.getDownward();
-            graph.addVertex(upward);
-            graph.addVertex(downward);
-            graph.setEdgeWeight(graph.addEdge(upward, downward), section.getDistance());
-        }
-    }
-
-    public List<Station> getShortestPath(final Station start, final Station end) {
-        try {
-            return dijkstraShortestPath.getPath(start, end).getVertexList();
-        } catch (IllegalArgumentException e) {
-            throw new InvalidStationNameException("노선 구간에 등록되지 않은 역 이름을 통해 경로를 조회할 수 없습니다.");
-        }
-    }
-
-    public int getShortestDistance(final Station start, final Station end) {
-        try {
-            return (int) dijkstraShortestPath.getPath(start, end).getWeight();
-        } catch (IllegalArgumentException e) {
-            throw new InvalidStationNameException("노선 구간에 등록되지 않은 역 이름을 통해 경로를 조회할 수 없습니다.");
-        }
+    public Path findShortestPath(final Station start, final Station end) {
+        return new Path(
+                subwayGraph.calculateShortestDistance(start, end),
+                subwayGraph.findShortestPath(start, end)
+        );
     }
 }
