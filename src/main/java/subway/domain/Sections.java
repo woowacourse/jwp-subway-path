b5fb095630b83a061a8ac85694e6c5bdc6871fca
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-18 08:31:16 +0900

    refactor: Sections sort 메서드 분리

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,34 +13,43 @@ public class Sections {
     private final List<Section> sections;
 
     public Sections(List<Section> sections) {
-        this.sections = sections;
+        this.sections = new ArrayList<>(sections);
     }
 
     public List<Station> findStations() {
-
-        List<Station> upStations = sections.stream()
-                .map(Section::getUpStation)
-                .collect(Collectors.toList());
-
-        List<Station> downStations = sections.stream()
-                .map(Section::getDownStation)
-                .collect(Collectors.toList());
-
-        Station startStation = upStations.stream()
-                .filter(station -> !downStations.contains(station))
-                .findFirst().orElseThrow(()->new IllegalStateException("상행 종점을 찾을 수 없습니다"));
+        List<Station> upStations = getUpStations();
+        List<Station> downStations = getDownStations();
+        Station startStation = findStartStation(upStations, downStations);
 
         List<Station> stations = new LinkedList<>(List.of(startStation));
-
         while (stations.size() <= sections.size()) {
-            for (Section section : sections) {
-                if (stations.get(stations.size()-1).equals(section.getUpStation())) {
-                    stations.add(section.getDownStation());
-                }
-            }
+            sections.addAll(sort(stations));
         }
 
         return stations;
     }
 
+    private static Station findStartStation(List<Station> upStations, List<Station> downStations) {
+        return upStations.stream()
+                         .filter(station -> !downStations.contains(station))
+                         .findFirst().orElseThrow(()->new IllegalStateException("상행 종점을 찾을 수 없습니다"));
+    }
+
+    private List<Station> getUpStations() {
+        return sections.stream()
+                       .map(Section::getUpStation)
+                       .collect(Collectors.toList());
+    }
+
+    private List<Station> getDownStations() {
+        return sections.stream()
+                       .map(Section::getDownStation)
+                       .collect(Collectors.toList());
+    }
+
+    private List<Section> sort(List<Station> stations) {
+        return sections.stream()
+                .filter(section -> stations.get(stations.size()-1).equals(section.getUpStation()))
+                .collect(Collectors.toList());
+    }
 }
