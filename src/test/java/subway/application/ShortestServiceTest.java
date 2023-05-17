84de39fc1cd4f2dc43394920686658b63f70d36b
nuyh <hjaehyun25@gmail.com>
2023-05-17 11:08:52 +0900

    test: ShortestService 테스트 더블로 변경

--- src/test/java/subway/application/ShortestServiceTest.java
+++ src/test/java/subway/application/ShortestServiceTest.java
@@ -3,28 +3,36 @@ package subway.application;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-import subway.dto.LineRequest;
-import subway.dto.PathRequest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import subway.dao.PathDao;
+import subway.dao.StationDao;
+import subway.domain.FareStrategy;
+import subway.domain.Path;
+import subway.domain.Paths;
+import subway.domain.Station;
 import subway.dto.ShortestResponse;
-import subway.dto.StationRequest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class ShortestServiceTest {
 
-    @Autowired
-    private LineService lineService;
-    @Autowired
-    private StationService stationService;
-    @Autowired
+    @InjectMocks
     private ShortestService shortestService;
+    @Mock
+    private PathDao pathDao;
+    @Mock
+    private StationDao stationDao;
+    @Mock
+    private FareStrategy fareStrategy;
 
     @Nested
     @DisplayName("최단 경로 찾기 테스트")
@@ -38,26 +46,31 @@ class ShortestServiceTest {
         @DisplayName("여러 경로 중 최단 경로를 구할 수 있다")
         @Test
         void findShortest() {
-            Long lineId1 = lineService.saveLine(new LineRequest("1호선", "red")).getId();
-            Long lineId2 = lineService.saveLine(new LineRequest("2호선", "blue")).getId();
+            //given
+            final Station source = new Station("광안");
+            final Station target = new Station("노포");
 
-            Long source = stationService.saveStation(new StationRequest("광안역")).getId();
-            Long destination = stationService.saveStation(new StationRequest("노포역")).getId();
+            final Station station1 = new Station("전포");
+            final Station station2 = new Station("부산");
+            final Station station3 = new Station("해운대");
 
-            Long stationId2 = stationService.saveStation(new StationRequest("전포역")).getId();
-            Long stationId3 = stationService.saveStation(new StationRequest("부산역")).getId();
-            Long stationId4 = stationService.saveStation(new StationRequest("해운대역")).getId();
+            final Path path1 = new Path(source, station1, 5);
+            final Path path2 = new Path(station1, target, 5);
+
+            final Path path3 = new Path(source, station2, 2);
+            final Path path4 = new Path(station2, station3, 1);
+            final Path path5 = new Path(station3, target, 1);
 
 
-            lineService.addPathToLine(lineId1, new PathRequest(source, stationId2, 5));
-            lineService.addPathToLine(lineId1, new PathRequest(stationId2, destination, 5));
-
-            lineService.addPathToLine(lineId2, new PathRequest(source, stationId3, 2));
-            lineService.addPathToLine(lineId2, new PathRequest(stationId3, stationId4, 1));
-            lineService.addPathToLine(lineId2, new PathRequest(stationId4, destination, 1));
+            final Paths paths = new Paths(List.of(path1, path2, path3, path4, path5));
+            when(pathDao.findAll()).thenReturn(paths);
+            when(stationDao.findById(any()))
+                    .thenReturn(source)
+                    .thenReturn(target);
+            when(fareStrategy.calculate(anyLong())).thenReturn(1250);
 
             //when
-            final ShortestResponse shortest = shortestService.findShortest(source, destination);
+            final ShortestResponse shortest = shortestService.findShortest(source.getId(), target.getId());
 
             //then
             assertAll(
@@ -70,20 +83,23 @@ class ShortestServiceTest {
         @Test
         void findShortest_fail() {
             //given
-            final Long lineId1 = lineService.saveLine(new LineRequest("1호선", "red")).getId();
-            final Long lineId2 = lineService.saveLine(new LineRequest("2호선", "blue")).getId();
+            final Station source = new Station("광안");
+            final Station target = new Station("노포");
 
-            Long source = stationService.saveStation(new StationRequest("광안역")).getId();
-            Long destination = stationService.saveStation(new StationRequest("노포역")).getId();
+            final Station station1 = new Station("전포");
+            final Station station2 = new Station("부산");
 
-            Long stationId1 = stationService.saveStation(new StationRequest("전포역")).getId();
-            Long stationId2 = stationService.saveStation(new StationRequest("부산역")).getId();
+            final Path path1 = new Path(source, station1, 5);
+            final Path path2 = new Path(target, station2, 5);
 
-            lineService.addPathToLine(lineId1, new PathRequest(source, stationId1, 5));
-            lineService.addPathToLine(lineId2, new PathRequest(destination, stationId2, 5));
+            final Paths paths = new Paths(List.of(path1, path2));
+            when(pathDao.findAll()).thenReturn(paths);
+            when(stationDao.findById(any()))
+                    .thenReturn(source)
+                    .thenReturn(target);
 
             //when, then
-            assertThatThrownBy(() -> shortestService.findShortest(source, destination))
+            assertThatThrownBy(() -> shortestService.findShortest(source.getId(), target.getId()))
                     .isInstanceOf(IllegalStateException.class)
                     .hasMessage("경로가 존재하지 않습니다.");
         }
