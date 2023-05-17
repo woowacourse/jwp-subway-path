b73f5c989d15661b70d991d3102715ad295a2846
yujamint <woawoa0513@gmail.com>
2023-05-17 22:16:38 +0900

    test: Repository 레벨에서의 역 이름 중복 검사 기능 테스트 작성

--- src/test/java/subway/repository/StationRepositoryTest.java
+++ src/test/java/subway/repository/StationRepositoryTest.java
@@ -1,6 +1,7 @@
 package subway.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import subway.domain.Station;
 import subway.entity.StationEntity;
+import subway.exception.DuplicatedNameException;
 import subway.repository.dao.StationDao;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -53,5 +55,16 @@ class StationRepositoryTest {
         //then
         assertThat(station).usingRecursiveComparison()
                 .isEqualTo(findStation);
+    }
+
+    @Test
+    void 이미_존재하는_역을_저장하면_예외가_발생한다() {
+        // given
+        stationRepository.save("강남역");
+
+        // when, then
+        assertThatThrownBy(() -> stationRepository.save("강남역"))
+                .isInstanceOf(DuplicatedNameException.class)
+                .hasMessage("이미 존재하는 이름입니다 입력값 : 강남역");
     }
 }
