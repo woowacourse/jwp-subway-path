b9f9b09dff64ea8ba4297124a10536030417c938
jaeyeon kim <jakind@naver.com>
2023-05-17 23:32:23 +0900

    test: Path 생성을 테스트

---
+++ src/test/java/subway/domain/PathTest.java
@@ -0,0 +1,25 @@
+package subway.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import subway.service.domain.Direction;
+import subway.service.domain.Distance;
+import subway.service.domain.Path;
+import subway.service.domain.Station;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PathTest {
+
+    @Test
+    @DisplayName("Path 를 생성한다.")
+    void createPath() {
+        Station station = new Station("pathTest");
+        Path path = new Path(Direction.UP, station, Distance.from(10));
+
+        assertThat(path.getDirection()).isEqualTo(Direction.UP);
+        assertThat(path.getNextStation()).isEqualTo(station);
+        assertThat(path.getDistance()).isEqualTo(10);
+    }
+
+}
