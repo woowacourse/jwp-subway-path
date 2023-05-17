81fc3c90c473811708933cfb55931fdfac772a64
rawfishthelgh <rg970604@naver.com>
2023-05-17 15:30:11 +0900

    feat: 컨트롤러 구현

--- src/main/java/subway/ui/LineController.java
+++ src/main/java/subway/ui/LineController.java
@@ -5,13 +5,9 @@ import org.springframework.web.bind.annotation.*;
 import subway.application.LineService;
 import subway.dto.LineRequest;
 import subway.dto.LineResponse;
-import subway.dto.StationRequest;
-import subway.dto.StationResponse;
-import subway.exception.LineNotFoundException;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/lines")
@@ -23,48 +19,31 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<LineResponse>> showAllLines() {
-        final List<LineResponse> result = lineService.findLines().stream()
-                .map(LineResponse::of)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(result);
-    }
-
     @PostMapping
-    public ResponseEntity<Void> addLine(@RequestBody final LineRequest lineRequest) {
-        final Long lineId = lineService.saveLine(lineRequest);
-
-        return ResponseEntity.created(URI.create("/lines/" + lineId)).build();
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+        LineResponse line = lineService.saveLine(lineRequest);
+        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
-    @GetMapping("/{id}/stations")
-    public ResponseEntity<List<StationResponse>> showStations(@PathVariable final Long id) {
-        final List<StationResponse> result = lineService.findAllStations(id).stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(result);
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> findAllLines() {
+        return ResponseEntity.ok(lineService.findLineResponses());
     }
 
-
-    @PostMapping("/{id}/stations")
-    public ResponseEntity<Void> addStation(@PathVariable final Long id, @RequestBody final StationRequest stationRequest) {
-        final Long stationId = lineService.addStation(id, stationRequest);
-
-        return ResponseEntity.created(URI.create("/lines/" + id + "/stations/" + stationId)).build();
+    @GetMapping("/{id}")
+    public ResponseEntity<LineResponse> findLineById(@PathVariable Long id) {
+        return ResponseEntity.ok(lineService.findLineResponseById(id));
     }
 
-    @DeleteMapping("/{line-id}/stations/{id}")
-    public ResponseEntity<Void> deleteStation(@PathVariable("line-id") final Long lineId, @PathVariable final Long id) {
-        lineService.deleteStation(id);
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequest lineUpdateRequest) {
+        lineService.updateLine(id, lineUpdateRequest);
+        return ResponseEntity.ok().build();
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
+        lineService.deleteLineById(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<Void> handleLineNotFoundException(final LineNotFoundException lineNotFoundException) {
-        return ResponseEntity.notFound().build();
     }
 }
