d5d90ad85664f88c1d88932f73e23e7f1f8c5aff
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-18 08:53:47 +0900

    test: 애플리케이션 테스트 추가

---
+++ src/test/java/subway/SubwayApplicationTests.java
@@ -0,0 +1,12 @@
+package subway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
