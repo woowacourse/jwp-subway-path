509ce832dfe55ba8dec2078f9f05200e7d953328
rawfishthelgh <rg970604@naver.com>
2023-05-17 15:57:11 +0900

    refactor: 생성자 추가

--- src/main/java/subway/dto/SectionRequest.java
+++ src/main/java/subway/dto/SectionRequest.java
@@ -10,8 +10,11 @@ public class SectionRequest {
     @NotBlank(message = "역 이름을 입력해야 합니다")
     private String downStation;
     @NotNull(message = "거리를 입력해야 합니다")
-    @Size(min = 1,message = "1이상의 값을 입력해야 합니다")
+    @Size(min = 1, message = "1이상의 값을 입력해야 합니다")
     private Long distance;
+
+    public SectionRequest() {
+    }
 
     public SectionRequest(final String upStation, final String downStation, final Long distance) {
         this.upStation = upStation;
