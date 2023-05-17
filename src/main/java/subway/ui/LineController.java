c668a28c732b20f9d982d53a7c13a72c9cfb9162
yujamint <woawoa0513@gmail.com>
2023-05-18 00:48:31 +0900

    style: 주석문 삭제

--- src/main/java/subway/ui/LineController.java
+++ src/main/java/subway/ui/LineController.java
@@ -28,33 +28,17 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    /**
-     * 노선을 생성한다.
-     * @param lineRequest (lineName, sourceStation, targetStation, distance)
-     * @return 생성한 Line Id
-     */
     @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody @Valid LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
-    /**
-     * 한 노선의 정보를 조회한다.
-     * @param lineId
-     * @return StationsResponse - 노선 내에 존재하는 모든 역의 이름 목록
-     */
     @GetMapping("/{lineId}")
     public ResponseEntity<LineSelectResponse> findLineById(@PathVariable Long lineId) {
         return ResponseEntity.ok(lineService.getStationsByLineId(lineId));
     }
 
-    /**
-     * 노선에 역을 추가한다.
-     * @param lineId
-     * @param stationSaveRequest (SourceStation, TargetStation, Distance)
-     * @return StationResponse 추가한 역의 이름
-     */
     @PostMapping("/{lineId}/stations")
     public ResponseEntity<StationSelectResponse> createStation(@PathVariable Long lineId,
                                                                @RequestBody StationSaveRequest stationSaveRequest) {
@@ -67,19 +51,12 @@ public class LineController {
         lineService.deleteStation(lineId, stationId);
         return ResponseEntity.noContent().build();
     }
-    /**
-     * 노선을 여러개 생성한다.
-     * @return 생성한 노선들의 Id 목록
-     */
+
     @GetMapping
     public ResponseEntity<LinesSelectResponse> findAllLines() {
         return ResponseEntity.ok(lineService.findAllLine());
     }
 
-    /**
-     * 노선을 삭제한다.
-     * @param lineId
-     */
     @DeleteMapping("/{lineId}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long lineId) {
         lineService.deleteLineById(lineId);
