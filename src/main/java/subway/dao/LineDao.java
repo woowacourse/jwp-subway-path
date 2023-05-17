a9d816492fc36d664ab9f757391cb657ca3c4981
LJW25 <dbjwlee@gmail.com>
2023-05-17 14:43:13 +0900

    feat(LineDao): DB 구조 변경에 따른 DAO 수정

--- src/main/java/subway/dao/LineDao.java
+++ src/main/java/subway/dao/LineDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import subway.domain.Line;
+import subway.dto.LineEntity;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -16,46 +17,48 @@ public class LineDao {
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert insertAction;
 
-    private RowMapper<Line> rowMapper = (rs, rowNum) ->
-            new Line(
+    private final RowMapper<LineEntity> rowMapper = (rs, rowNum) ->
+            new LineEntity(
                     rs.getLong("id"),
                     rs.getString("name"),
-                    rs.getString("color")
+                    rs.getString("color"),
+                    rs.getLong("up_endpoint_id")
             );
 
-    public LineDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+    public LineDao(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
         this.insertAction = new SimpleJdbcInsert(dataSource)
                 .withTableName("line")
                 .usingGeneratedKeyColumns("id");
     }
 
-    public Line insert(Line line) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("id", line.getId());
-        params.put("name", line.getName());
-        params.put("color", line.getColor());
+    public LineEntity insert(final LineEntity lineEntity) {
+        final Map<String, Object> params = new HashMap<>();
+        params.put("id", lineEntity.getId());
+        params.put("name", lineEntity.getName());
+        params.put("color", lineEntity.getColor());
+        params.put("up_endpoint_id", lineEntity.getUpEndpointId());
 
-        Long lineId = insertAction.executeAndReturnKey(params).longValue();
-        return new Line(lineId, line.getName(), line.getColor());
+        final Long lineId = insertAction.executeAndReturnKey(params).longValue();
+        return new LineEntity(lineId, lineEntity.getName(), lineEntity.getColor(), lineEntity.getUpEndpointId());
     }
 
-    public List<Line> findAll() {
-        String sql = "select id, name, color from LINE";
+    public List<LineEntity> findAll() {
+        final String sql = "select id, name, color, up_endpoint_id from LINE";
         return jdbcTemplate.query(sql, rowMapper);
     }
 
-    public Line findById(Long id) {
-        String sql = "select id, name, color from LINE WHERE id = ?";
+    public LineEntity findById(final Long id) {
+        final String sql = "select id, name, color, up_endpoint_id from LINE WHERE id = ?";
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
-    public void update(Line newLine) {
-        String sql = "update LINE set name = ?, color = ? where id = ?";
-        jdbcTemplate.update(sql, new Object[]{newLine.getName(), newLine.getColor(), newLine.getId()});
+    public void update(final Line newLine) {
+        final String sql = "update LINE set name = ?, color = ? where id = ?";
+        jdbcTemplate.update(sql, newLine.getName(), newLine.getColor(), newLine.getId());
     }
 
-    public void deleteById(Long id) {
+    public void deleteById(final Long id) {
         jdbcTemplate.update("delete from Line where id = ?", id);
     }
 }
---
+++ src/main/java/subway/dto/LineEntity.java
@@ -0,0 +1,35 @@
+package subway.dto;
+
+public class LineEntity {
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final Long upEndpointId;
+
+    public LineEntity(final String name, final String color) {
+        this(null, name, color, null);
+    }
+
+    public LineEntity(final Long id, final String name, final String color, final Long upEndpointId) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.upEndpointId = upEndpointId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpEndpointId() {
+        return upEndpointId;
+    }
+}
