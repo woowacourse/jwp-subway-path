fa739240976466d30cafa7a6bd3c320523d4e115
Combi153 <jcm04@naver.com>
2023-05-17 18:00:09 +0900

    refactor: SectionDao 로직 수정
    변경 내용 :
    - 사용하지 않는 메서드 삭제
    - 기본 로직 수정

--- src/main/java/subway/dao/SectionDao.java
+++ src/main/java/subway/dao/SectionDao.java
@@ -1,5 +1,6 @@
 package subway.dao;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -8,7 +9,6 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,11 +16,13 @@ public class SectionDao {
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert insertAction;
-
-    private final RowMapper<List<Long>> sectionIdRowMapper = (rs, rowNum) ->
-            new ArrayList<>(
-                    List.of(rs.getLong("from_id"),
-                            rs.getLong("to_id"))
+    private final RowMapper<SectionEntity> sectionEntityRowMapper = (rs, rowNum) ->
+            new SectionEntity(
+                    rs.getLong("id"),
+                    rs.getLong("from_id"),
+                    rs.getLong("to_id"),
+                    rs.getInt("distance"),
+                    rs.getLong("line_id")
             );
 
     public SectionDao(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
@@ -30,48 +32,33 @@ public class SectionDao {
                 .usingGeneratedKeyColumns("id");
     }
 
-    public Long insert(final Long fromId, final Long toId, final Integer distance, final Long lineId) {
-        SqlParameterSource params = new MapSqlParameterSource()
-                .addValue("from_id", fromId)
-                .addValue("to_id", toId)
-                .addValue("distance", distance)
-                .addValue("line_id", lineId);
+    public SectionEntity insert(final SectionEntity sectionEntity) {
+        final SqlParameterSource params = new MapSqlParameterSource()
+                .addValue("from_id", sectionEntity.getFromId())
+                .addValue("to_id", sectionEntity.getToId())
+                .addValue("distance", sectionEntity.getDistance())
+                .addValue("line_id", sectionEntity.getLineId());
 
-        return insertAction.executeAndReturnKey(params).longValue();
+        final Long id = insertAction.executeAndReturnKey(params).longValue();
+        return new SectionEntity(id, sectionEntity.getFromId(), sectionEntity.getToId(), sectionEntity.getDistance(), sectionEntity.getLineId());
     }
 
-    public Integer count(final Long lineId, final Long stationId) {
-        final String sql = "SELECT COUNT (*) FROM section WHERE (section.from_id = ? OR section.to_id = ?) AND section.line_id = ?";
-        return jdbcTemplate.queryForObject(sql, Integer.class, stationId, stationId, lineId);
+    public List<SectionEntity> findSectionsByLineId(final Long lineId) {
+        final String sql = "SELECT * FROM section WHERE line_id = ?";
+        return jdbcTemplate.query(sql, sectionEntityRowMapper, lineId);
     }
 
-    public List<List<Long>> findAdjacentStationId(final Long lineId, final Long stationId) {
-        final String sql = "SELECT section.from_id, section.to_id FROM section WHERE (section.from_id =? OR section.to_id = ?)  AND section.line_id = ?";
-        return jdbcTemplate.query(sql, sectionIdRowMapper, stationId, stationId, lineId);
+    public void deleteById(final Long id) {
+        final String sql = "DELETE FROM section WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
-    public Long findLeftStationId(final Long lineId, final Long stationId) {
-        final String sql = "SELECT section.from_id FROM section WHERE section.to_id = ?  AND section.line_id = ?";
-        return jdbcTemplate.queryForObject(sql, Long.class, stationId, lineId);
-    }
-
-    public Long findRightStationId(final Long lineId, final Long stationId) {
-        final String sql = "SELECT section.to_id FROM section WHERE section.from_id = ? AND section.line_id = ?";
-        return jdbcTemplate.queryForObject(sql, Long.class, stationId, lineId);
-    }
-
-    public void deleteSection(final Long lineId, final Long stationId) {
-        final String sql = "DELETE FROM section WHERE line_id = ? and (section.from_id =? OR section.to_id = ?)";
-        jdbcTemplate.update(sql, lineId, stationId, stationId);
-    }
-
-    public Integer findLeftSectionDistance(final Long lineId, final Long stationId) {
-        final String sql = "SELECT section.distance FROM section WHERE section.to_id = ? AND section.line_id = ?";
-        return jdbcTemplate.queryForObject(sql, Integer.class, stationId, lineId);
-    }
-
-    public Integer findRightSectionDistance(final Long lineId, final Long stationId) {
-        final String sql = "SELECT section.distance FROM section WHERE section.from_id = ? AND section.line_id = ?";
-        return jdbcTemplate.queryForObject(sql, Integer.class, stationId, lineId);
+    public SectionEntity findById(final Long id) {
+        try {
+            final String sql = "SELECT * FROM section WHERE id = ?";
+            return jdbcTemplate.queryForObject(sql, sectionEntityRowMapper, id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new IllegalArgumentException("존재하지 않는 구간입니다.");
+        }
     }
 }
