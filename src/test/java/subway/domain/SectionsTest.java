0337bf9564c4d3b900799b09d568c915cef22ff8
Cyma-s <ssol0319@naver.com>
2023-05-17 15:31:13 +0900

    test: beforeeach 제거

--- src/test/java/subway/domain/SectionsTest.java
+++ src/test/java/subway/domain/SectionsTest.java
@@ -5,72 +5,86 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import subway.exception.DuplicateException;
+import subway.exception.ErrorCode;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
 class SectionsTest {
-    private List<Section> sectionList;
-    private Section section1;
-    private Section section2;
-    private Station station1;
-    private Station station2;
-    private Station station3;
-    private Sections sections;
+    private Station 역을_생성한다(final Long 역_ID, final String 역_이름) {
+        return new Station(역_ID, 역_이름);
+    }
 
-    @BeforeEach
-    void setUp() {
-        station1 = new Station(1L, "잠실역");
-        station2 = new Station(2L, "잠실새내역");
-        station3 = new Station(3L, "봉천역");
-
-        section1 = new Section(station1, station2, 10);
-        section2 = new Section(station2, station3, 4);
-
-        sectionList = List.of(section2, section1);
-
-        sections = Sections.from(sectionList);
+    private Section 구간을_생성한다(final Station 시작역, final Station 도착역, final int 거리) {
+        return new Section(시작역, 도착역, 거리);
     }
 
     @Test
-    @DisplayName("생성 테스트")
+    @DisplayName("Sections 를 from 메서드로 생성한다.")
     void createFrom() {
         // given
-        sections = Sections.from(sectionList);
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections sections = Sections.from(List.of(첫번째_구간, 두번째_구간));
 
         // expected
         assertAll(
-                () -> assertThat(sections.getSections().get(0)).isEqualTo(section1),
-                () -> assertThat(sections.getSections().get(1)).isEqualTo(section2)
+                () -> assertThat(sections.getSections().get(0)).isEqualTo(첫번째_구간),
+                () -> assertThat(sections.getSections().get(1)).isEqualTo(두번째_구간)
         );
     }
 
     @Test
     @DisplayName("노선의 모든 역을 반환한다.")
     void getStations() {
+        // given
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
+
         // when
-        List<Station> stations = sections.getStations();
+        List<Station> stations = 구간들.getStations();
 
         // expected
-        assertThat(stations).contains(station1, station2, station3);
+        assertThat(stations).containsExactly(첫번째_역, 두번째_역, 세번째_역);
     }
 
     @Test
     @DisplayName("새로운 구간을 추가한다.")
     void addSection() {
         // given
-        Station upStation = new Station(4L, "종합운동장역");
-        Station downStation = new Station(3L, "봉천역");
-        Section newSection = new Section(upStation, downStation, 2);
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
 
         // when
-        sections.addSection(newSection);
+        Station 새로운_도착역 = new Station(4L, "삼성역");
+        Section 새로운_구간 = new Section(세번째_역, 새로운_도착역, 2);
+        구간들.addSection(새로운_구간);
 
         // expected
         assertAll(
-                () -> assertThat(sections.getStations()).hasSize(4),
-                () -> assertThat(sections.getSections().get(1)).isEqualTo(newSection)
+                () -> assertThat(구간들.getStations()).hasSize(4),
+                () -> assertThat(구간들.getSections().get(2)).isEqualTo(새로운_구간)
         );
     }
 
@@ -78,63 +92,112 @@ class SectionsTest {
     @DisplayName("새로운 역들이 노선에 이미 존재하는 경우 예외가 발생한다.")
     void validateDuplicateSection() {
         // given
-        Section newSection = new Section(station1, station3, 2);
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
+
+        Section 새로운_구간 = new Section(첫번째_역, 세번째_역, 5);
 
         // expected
-        assertThatThrownBy(() -> sections.addSection(newSection)).isInstanceOf(DuplicateException.class);
+        assertThatThrownBy(() -> 구간들.addSection(새로운_구간))
+                .isInstanceOf(DuplicateException.class)
+                .hasMessage(ErrorCode.DUPLICATE_STATION.getErrorMessage());
     }
 
     @Test
     @DisplayName("새로운 역이 상행 종점으로 추가된다.")
     void addSectionFirstStation() {
         // given
-        Station newUpStation = new Station(4L, "베로역");
-        Section newSection = new Section(newUpStation, station1, 10);
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
+
+        Station 새로운_상행_종점_역 = new Station(4L, "새로운상행종점역");
+        Section 새로운_구간 = new Section(새로운_상행_종점_역, 첫번째_역, 10);
 
         // when
-        sections.addSection(newSection);
+        구간들.addSection(새로운_구간);
 
         // expected
-        assertThat(sections.getSections().get(0)).isEqualTo(newSection);
+        assertThat(구간들.getSections().get(0)).isEqualTo(새로운_구간);
     }
 
     @Test
     @DisplayName("새로운 역이 하행 종점으로 추가된다.")
     void addSectionLastStation() {
         // given
-        Station newDownStation = new Station(4L, "베로역");
-        Section newSection = new Section(station3, newDownStation, 10);
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
+
+        Station 새로운_하행_종점_역 = new Station(4L, "베로역");
+        Section 새로운_구간 = new Section(세번째_역, 새로운_하행_종점_역, 10);
 
         // when
-        sections.addSection(newSection);
+        구간들.addSection(새로운_구간);
 
         // expected
-        assertThat(sections.getSections().get(2)).isEqualTo(newSection);
+        assertThat(구간들.getSections().get(2)).isEqualTo(새로운_구간);
     }
 
     @Test
     @DisplayName("중간 역을 삭제하면 연결된 구간이 삭제된다.")
     void delete() {
+        // given
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
+
         // when
-        sections.deleteStation(station2);
+        구간들.deleteStation(두번째_역);
 
         // expected
         assertAll(
-                () -> assertThat(sections.getStations()).hasSize(2),
-                () -> assertThat(sections.getSections()).hasSize(1)
+                () -> assertThat(구간들.getStations()).hasSize(2),
+                () -> assertThat(구간들.getSections()).hasSize(1)
         );
     }
 
     @Test
     @DisplayName("종점을 삭제한다.")
     void deleteTerminal() {
+        // given
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+        Station 세번째_역 = 역을_생성한다(3L, "종합운동장역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+        Section 두번째_구간 = 구간을_생성한다(두번째_역, 세번째_역, 20);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간, 두번째_구간));
+
         // when
-        sections.deleteStation(station1);
+        구간들.deleteStation(첫번째_역);
 
         // expected
         assertAll(
-                () -> assertThat(sections.getStations()).hasSize(2),
-                () -> assertThat(sections.getSections()).hasSize(1)
+                () -> assertThat(구간들.getStations()).hasSize(2),
+                () -> assertThat(구간들.getSections()).hasSize(1)
         );
     }
 
@@ -142,15 +205,20 @@ class SectionsTest {
     @DisplayName("노선에 역이 두 개 존재할 때, 모든 노선이 삭제된다.")
     void deleteAll() {
         // given
-        sections = Sections.from(List.of(section1));
+        Station 첫번째_역 = 역을_생성한다(1L, "잠실역");
+        Station 두번째_역 = 역을_생성한다(2L, "잠실새내역");
+
+        Section 첫번째_구간 = 구간을_생성한다(첫번째_역, 두번째_역, 10);
+
+        Sections 구간들 = Sections.from(List.of(첫번째_구간));
 
         // when
-        sections.deleteStation(station1);
+        구간들.deleteStation(첫번째_역);
 
         // expected
         assertAll(
-                () -> assertThat(sections.getStations()).hasSize(0),
-                () -> assertThat(sections.getSections()).hasSize(0)
+                () -> assertThat(구간들.getStations()).hasSize(0),
+                () -> assertThat(구간들.getSections()).hasSize(0)
         );
     }
 }
