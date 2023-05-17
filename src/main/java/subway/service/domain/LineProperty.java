7133e66784137dfb350656eddea0778f5f7ef1d1
jaeyeon kim <jakind@naver.com>
2023-05-17 23:36:20 +0900

    test: LineProperty 생성을 테스트

---
+++ src/main/java/subway/service/domain/LineProperty.java
@@ -0,0 +1,32 @@
+package subway.service.domain;
+
+public class LineProperty {
+
+    private Long id;
+    private final String name;
+    private final String color;
+
+    public LineProperty(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public LineProperty(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
