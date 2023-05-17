7d3c2d224651350cf8cd094726e5d0d1b2400758
Combi153 <jcm04@naver.com>
2023-05-17 18:06:23 +0900

    refactor: sql 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,30 +19,3 @@ create table if not exists SECTION
     to_id    bigint                not null,
     distance bigint                not null
 );
-
-insert into Station (name)
-values ('후추');
-insert into Station (name)
-values ('디노');
-insert into Station (name)
-values ('조앤');
-insert into Station (name)
-values ('로운');
-insert into Station (name)
-values ('포비');
-
-insert into Line (name, color)
-values ('2호선', 'Green');
-insert into Line (name, color)
-values ('8호선', 'pink');
-
-insert into SECTION (line_id, from_id, to_id, distance)
-values (1, 1, 2, 5);
-insert into SECTION (line_id, from_id, to_id, distance)
-values (1, 2, 3, 4);
-insert into SECTION (line_id, from_id, to_id, distance)
-values (1, 3, 4, 6);
-insert into SECTION (line_id, from_id, to_id, distance)
-values (2, 5, 4, 3);
-insert into SECTION (line_id, from_id, to_id, distance)
-values (2, 3, 5, 3);
