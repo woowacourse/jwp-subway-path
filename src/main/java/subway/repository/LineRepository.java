32b6452867924a131168864342c1f1050ead027e
Combi153 <jcm04@naver.com>
2023-05-17 19:27:34 +0900

    feat: LineRepository 로직 추가

---
+++ src/main/java/subway/repository/LineRepository.java
@@ -0,0 +1,57 @@
+package subway.repository;
+
+import org.springframework.stereotype.Repository;
+import subway.dao.*;
+import subway.domain.Line;
+import subway.domain.Section;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class LineRepository {
+
+    private final LineDao lineDao;
+    private final SectionDao sectionDao;
+    private final StationDao stationDao;
+
+    public LineRepository(final LineDao lineDao, final SectionDao sectionDao, final StationDao stationDao) {
+        this.lineDao = lineDao;
+        this.sectionDao = sectionDao;
+        this.stationDao = stationDao;
+    }
+
+    public Line save(final LineEntity lineEntity) {
+        return lineDao.insert(lineEntity).toLine();
+    }
+
+    public List<Line> findLines() {
+        final List<LineEntity> lineEntities = lineDao.findAll();
+        return lineEntities.stream()
+                .map(this::generateLine)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private Line generateLine(final LineEntity lineEntity) {
+        return lineEntity.toLine(findSectionsByLineId(lineEntity.getId()));
+    }
+
+    private List<Section> findSectionsByLineId(final Long lineId) {
+        return sectionDao.findSectionsByLineId(lineId)
+                .stream()
+                .map(this::generateSection)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private Section generateSection(final SectionEntity sectionEntity) {
+        return new Section(
+                sectionEntity.getId(),
+                stationDao.findById(sectionEntity.getFromId()).toStation(),
+                stationDao.findById(sectionEntity.getToId()).toStation(),
+                sectionEntity.getDistance());
+    }
+
+    public Line findLineById(final Long id) {
+        return generateLine(lineDao.findById(id));
+    }
+}
---
+++ src/test/java/subway/repository/LineRepositoryTest.java
@@ -0,0 +1,72 @@
+package subway.repository;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import subway.domain.Line;
+import subway.integration.IntegrationTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static subway.fixture.EntityFixture.일호선_남색_Entity;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class LineRepositoryTest extends IntegrationTest {
+
+    @Autowired
+    private LineRepository lineRepository;
+
+    @Test
+    void 호선을_저장한다() {
+        //when
+        final Line line = lineRepository.save(일호선_남색_Entity);
+
+        //then
+        assertSoftly(softly -> {
+            softly.assertThat(line.getId()).isEqualTo(1L);
+            softly.assertThat(line.getNameValue()).isEqualTo("일호선");
+            softly.assertThat(line.getColorValue()).isEqualTo("남색");
+            softly.assertThat(line.getSections()).isEmpty();
+        });
+    }
+
+    @Test
+    void 전체_호선을_조회한다() {
+        //given
+        lineRepository.save(일호선_남색_Entity);
+
+        //when
+        final List<Line> lines = lineRepository.findLines();
+
+        //then
+        assertSoftly(softly -> {
+            softly.assertThat(lines).hasSize(1);
+
+            final Line line = lines.get(0);
+            softly.assertThat(line.getId()).isEqualTo(1L);
+            softly.assertThat(line.getNameValue()).isEqualTo("일호선");
+            softly.assertThat(line.getColorValue()).isEqualTo("남색");
+            softly.assertThat(line.getSections()).isEmpty();
+        });
+    }
+
+    @Test
+    void id로_호선을_조회한다() {
+        //given
+        final Long id = lineRepository.save(일호선_남색_Entity).getId();
+
+        //when
+        final Line line = lineRepository.findLineById(id);
+
+        //then
+        assertSoftly(softly -> {
+            softly.assertThat(line.getId()).isEqualTo(1L);
+            softly.assertThat(line.getNameValue()).isEqualTo("일호선");
+            softly.assertThat(line.getColorValue()).isEqualTo("남색");
+            softly.assertThat(line.getSections()).isEmpty();
+        });
+    }
+}
