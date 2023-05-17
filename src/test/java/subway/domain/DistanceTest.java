0f70d0f40f0ca5783f31ae6f6efe77771b5579e5
0chil <0@chll.it>
2023-05-17 13:47:52 +0900

    feat(Distance): 거리끼리 비교한다, 더한다

--- src/test/java/subway/domain/DistanceTest.java
+++ src/test/java/subway/domain/DistanceTest.java
@@ -19,11 +19,29 @@ class DistanceTest {
 
     @DisplayName("거리끼리 차를 구한다")
     @Test
-    void diff() {
+    void gapBetweenOneAndOther() {
         var distance = new Distance(10);
         var other = new Distance(6);
 
-        assertThat(distance.diff(other)).isEqualTo(new Distance(4));
-        assertThat(other.diff(distance)).isEqualTo(new Distance(4));
+        assertThat(distance.gapBetween(other)).isEqualTo(new Distance(4));
+        assertThat(other.gapBetween(distance)).isEqualTo(new Distance(4));
+    }
+
+    @DisplayName("거리끼리 더한다")
+    @Test
+    void add() {
+        var distance = new Distance(10);
+        var other = new Distance(6);
+
+        assertThat(distance.sum(other)).isEqualTo(new Distance(16));
+    }
+
+    @DisplayName("어느 거리가 더 긴지 알 수 있다")
+    @Test
+    void isDistanceLongerThanOther() {
+        var distance = new Distance(10);
+        var other = new Distance(9);
+
+        assertThat(distance.isLongerThan(other)).isTrue();
     }
 }
