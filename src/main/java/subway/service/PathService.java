3c858402f43bd5a431e8684f7c4b1418daa21f80
hong-sile <gurwns9325@naver.com>
2023-05-17 11:44:35 +0900

    feat: 출발지와 도착지를 입력받으면, 경로의 역과 길이와 요금이 반환된다.

--- src/main/java/subway/service/PathService.java
+++ src/main/java/subway/service/PathService.java
@@ -1,13 +1,66 @@
 package subway.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import subway.domain.Distance;
+import subway.domain.Fare;
+import subway.domain.FarePolicy;
+import subway.domain.Station;
+import subway.domain.path.ShortestPathFinder;
+import subway.domain.section.EmptySections;
+import subway.domain.section.Section;
+import subway.domain.section.Sections;
+import subway.persistence.dao.SectionDao;
+import subway.persistence.dao.StationDao;
 import subway.service.dto.PathRequest;
 import subway.service.dto.PathResponse;
+import subway.service.dto.StationResponse;
 
 @Service
 public class PathService {
 
+    private final SectionDao sectionDao;
+    private final StationDao stationDao;
+    private final ShortestPathFinder finder;
+    private final FarePolicy farePolicy;
+
+    public PathService(final SectionDao sectionDao, final StationDao stationDao, final ShortestPathFinder finder,
+                       final FarePolicy farePolicy) {
+        this.sectionDao = sectionDao;
+        this.stationDao = stationDao;
+        this.finder = finder;
+        this.farePolicy = farePolicy;
+    }
+
     public PathResponse findPath(final PathRequest pathRequest) {
-        return null;
+        final List<Section> allSections = sectionDao.findAll();
+        final Station startStation = stationDao.findByName(pathRequest.getStartStationName())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 역입니다."));
+        final Station endStation = stationDao.findByName(pathRequest.getEndStationName())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 역입니다."));
+        final List<Section> path = finder.findShortestPath(allSections, startStation, endStation);
+        final Sections sections = createSections(path);
+        return toPathResponse(sections);
+    }
+
+    private Sections createSections(final List<Section> path) {
+        Sections sections = new EmptySections();
+        for (final Section section : path) {
+            sections = sections.addSection(section);
+        }
+        return sections;
+    }
+
+    private PathResponse toPathResponse(final Sections sections) {
+        final List<StationResponse> stationResponses = sections.getAllStations().stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toUnmodifiableList());
+        final Distance totalDistance = sections.getSections().stream()
+                .map(Section::getDistance)
+                .reduce(Distance::plusValue)
+                .orElseThrow(() -> new IllegalArgumentException("존재하는 경로가 없습니다."));
+        final Fare fare = farePolicy.calculate(sections.getSections());
+        return new PathResponse(stationResponses, fare.getValue(), totalDistance.getValue());
     }
 }
---
+++ src/test/java/subway/service/PathServiceTest.java
@@ -0,0 +1,48 @@
+package subway.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static subway.integration.TestFixture.GANGNAM;
+import static subway.integration.TestFixture.SAMSUNG;
+import static subway.integration.TestFixture.SEONGLENUG;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import subway.domain.DistanceProportionalFarePolicy;
+import subway.domain.path.DijkstraShortestPathFinder;
+import subway.persistence.dao.SectionDao;
+import subway.persistence.dao.StationDao;
+import subway.service.dto.PathRequest;
+import subway.service.dto.PathResponse;
+import subway.service.dto.StationResponse;
+
+@JdbcTest
+@Import({PathService.class, SectionDao.class, StationDao.class,
+        DijkstraShortestPathFinder.class, DistanceProportionalFarePolicy.class})
+class PathServiceTest {
+
+    @Autowired
+    private PathService pathService;
+
+    @DisplayName("출발지와 목적지를 입력하면, 최단 경로와 거리와 요금을 반환하는 기능 테스트")
+    @Test
+    void findPath() {
+        final PathRequest pathRequest = new PathRequest("강남", "삼성");
+
+        final PathResponse pathResponse = pathService.findPath(pathRequest);
+
+        final List<StationResponse> stationResponses = pathResponse.getStationResponses();
+        assertAll(
+                () -> assertThat(stationResponses)
+                        .extracting(StationResponse::getName)
+                        .containsExactly(GANGNAM.getName(), SEONGLENUG.getName(), SAMSUNG.getName()),
+                () -> assertThat(pathResponse)
+                        .extracting(PathResponse::getTotalDistance, PathResponse::getTotalFare)
+                        .containsExactly(10, 1250)
+        );
+    }
+}
