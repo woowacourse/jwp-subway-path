988294cbc20bfbd68c194d16e4e9608e2fa59a05
dhkim <akdlzmf1123@gmail.com>
2023-05-17 11:27:26 +0900

    refactor: 테스트 코드 및 객체 상수 추출

--- src/main/java/subway/route/domain/jgraph/DistanceFareCalculator.java
+++ src/main/java/subway/route/domain/jgraph/DistanceFareCalculator.java
@@ -4,19 +4,40 @@ import subway.route.domain.FareCalculator;
 
 public class DistanceFareCalculator implements FareCalculator {
 
+    static final int MINIMUM_FARE = 1250;
+    static final int INCREMENTAL_FARE = 100;
+    static final int MID_DISTANCE_THRESHOLD = 10;
+    static final int FAR_DISTANCE_THRESHOLD = 50;
+    static final int MID_DISTANCE_INCREMENT_UNIT = 5;
+    static final int FAR_DISTANCE_INCREMENT_UNIT = 8;
+    static final int MAXIMUM_MID_DISTANCE_INCREMENT_COUNT = 8;
+
     @Override
     public int calculate(int totalDistance) {
-        if (totalDistance < 0) {
-            throw new IllegalArgumentException("디버깅 거리는 음수가 될 수 없습니다. totalDistance: " + totalDistance);
+        if (totalDistance <= 0) {
+            throw new IllegalArgumentException("디버깅 거리는 양수여야 합니다. totalDistance: " + totalDistance);
         }
-        if (totalDistance == 0) {
+        if (totalDistance < MID_DISTANCE_THRESHOLD) {
+            return MINIMUM_FARE;
+        }
+
+        int midDistanceIncrementCount = getMidDistanceIncrementCount(totalDistance);
+        int farDistanceIncrementCount = getFarDistanceIncrementCount(totalDistance);
+
+        return MINIMUM_FARE + INCREMENTAL_FARE * (midDistanceIncrementCount + farDistanceIncrementCount);
+    }
+
+    private int getMidDistanceIncrementCount(int totalDistance) {
+        if (totalDistance >= FAR_DISTANCE_THRESHOLD) {
+            return MAXIMUM_MID_DISTANCE_INCREMENT_COUNT;
+        }
+        return (totalDistance - MID_DISTANCE_THRESHOLD) / MID_DISTANCE_INCREMENT_UNIT;
+    }
+
+    private int getFarDistanceIncrementCount(int totalDistance) {
+        if (totalDistance < FAR_DISTANCE_THRESHOLD) {
             return 0;
         }
-
-        int fare = 1250;
-        int count5 = Math.min((totalDistance > 10 ? (totalDistance - 10) / 5 : 0), 40 / 5);
-        int count8 = totalDistance > 50 ? (totalDistance - 50) / 8 : 0;
-
-        return fare + 100 * (count5 + count8);
+        return (totalDistance - FAR_DISTANCE_THRESHOLD) / FAR_DISTANCE_INCREMENT_UNIT;
     }
 }
--- src/test/java/subway/route/domain/jgraph/DistanceFareCalculatorTest.java
+++ src/test/java/subway/route/domain/jgraph/DistanceFareCalculatorTest.java
@@ -1,25 +1,38 @@
 package subway.route.domain.jgraph;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static subway.route.domain.jgraph.DistanceFareCalculator.*;
 
 class DistanceFareCalculatorTest {
 
-    @Test
-    void calculate() {
-        final DistanceFareCalculator fareCalculator = new DistanceFareCalculator();
+    private static final int FAR_DISTANCE_MINIMUM_FARE = MINIMUM_FARE + MAXIMUM_MID_DISTANCE_INCREMENT_COUNT * INCREMENTAL_FARE;
 
+    final DistanceFareCalculator fareCalculator = new DistanceFareCalculator();
+
+    @Test
+    @DisplayName("입력된 거리에 따라 총 요금을 계산한다")
+    void calculate() {
         assertAll(
-                () -> assertThat(fareCalculator.calculate(0)).isEqualTo(0),
-                () -> assertThat(fareCalculator.calculate(1)).isEqualTo(1250),
-                () -> assertThat(fareCalculator.calculate(10)).isEqualTo(1250),
-                () -> assertThat(fareCalculator.calculate(14)).isEqualTo(1250),
-                () -> assertThat(fareCalculator.calculate(15)).isEqualTo(1350),
-                () -> assertThat(fareCalculator.calculate(50)).isEqualTo(2050),
-                () -> assertThat(fareCalculator.calculate(57)).isEqualTo(2050),
-                () -> assertThat(fareCalculator.calculate(58)).isEqualTo(2150)
+                () -> assertThat(fareCalculator.calculate(1)).isEqualTo(MINIMUM_FARE),
+                () -> assertThat(fareCalculator.calculate(MID_DISTANCE_THRESHOLD)).isEqualTo(MINIMUM_FARE),
+                () -> assertThat(fareCalculator.calculate(MID_DISTANCE_THRESHOLD + MID_DISTANCE_INCREMENT_UNIT - 1)).isEqualTo(MINIMUM_FARE),
+                () -> assertThat(fareCalculator.calculate(MID_DISTANCE_THRESHOLD + MID_DISTANCE_INCREMENT_UNIT)).isEqualTo(MINIMUM_FARE + INCREMENTAL_FARE),
+                () -> assertThat(fareCalculator.calculate(FAR_DISTANCE_THRESHOLD)).isEqualTo(FAR_DISTANCE_MINIMUM_FARE),
+                () -> assertThat(fareCalculator.calculate(FAR_DISTANCE_THRESHOLD + FAR_DISTANCE_INCREMENT_UNIT - 1)).isEqualTo(FAR_DISTANCE_MINIMUM_FARE),
+                () -> assertThat(fareCalculator.calculate(FAR_DISTANCE_THRESHOLD + FAR_DISTANCE_INCREMENT_UNIT)).isEqualTo(FAR_DISTANCE_MINIMUM_FARE + INCREMENTAL_FARE)
         );
+    }
+
+    @Test
+    @DisplayName("입력된 거리가 0이하면 예외가 발생된다")
+    void calculateFail1() {
+        assertThatThrownBy(() -> fareCalculator.calculate(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("거리는");
     }
 }
