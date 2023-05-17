09bad6c7e29275dfc0c99d09d64d3e71ce4c10d8
parkmuhyeun <pjhg410@gmail.com>
2023-05-18 00:16:09 +0900

    feat: StationRepository 생성

---
+++ src/main/java/subway/station/persistence/H2StationRepository.java
@@ -0,0 +1,29 @@
+package subway.station.persistence;
+
+import subway.station.domain.Station;
+import subway.station.domain.StationRepository;
+
+public class H2StationRepository implements StationRepository {
+
+  private final StationDao stationDao;
+
+  public H2StationRepository(StationDao stationDao) {
+    this.stationDao = stationDao;
+  }
+
+  @Override
+  public Station findById(Long id) {
+    final StationEntity stationEntity = stationDao.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 이름의 역이 존재하지 않습니다."));
+
+    return new Station(stationEntity.getId(), stationEntity.getStationName());
+  }
+
+  @Override
+  public Station createStation(String name) {
+    final Station station = new Station(name);
+    final Long stationId = stationDao.insert(new StationEntity(station.getName()));
+    return new Station(stationId, station.getName());
+  }
+
+}
