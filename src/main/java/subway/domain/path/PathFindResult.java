1d57a55b71ae99a1687dd6b6853cabdc21ec5302
greeng00se <thegreengoose7@gmail.com>
2023-05-17 23:37:53 +0900

    remove: 경로 상의 구간을 라인 기준으로 반환하는 기능 제거

--- src/main/java/subway/domain/path/PathFindResult.java
+++ src/main/java/subway/domain/path/PathFindResult.java
@@ -1,9 +1,7 @@
 package subway.domain.path;
 
-import java.util.ArrayList;
 import java.util.List;
 import subway.domain.core.Distance;
-import subway.domain.core.Section;
 
 public class PathFindResult {
 
@@ -13,22 +11,6 @@ public class PathFindResult {
     public PathFindResult(final Distance distance, final List<SectionEdge> path) {
         this.distance = distance;
         this.path = path;
-    }
-
-    public List<List<Section>> toSections() {
-        final List<List<Section>> result = new ArrayList<>();
-
-        List<Section> currentLine = new ArrayList<>();
-        long currentLineId = 0;
-        for (SectionEdge sectionEdge : path) {
-            if (currentLine.isEmpty() || currentLineId != sectionEdge.getLineId()) {
-                currentLine = new ArrayList<>();
-                currentLineId = sectionEdge.getLineId();
-                result.add(currentLine);
-            }
-            currentLine.add(sectionEdge.toSection());
-        }
-        return result;
     }
 
     public int getDistanceValue() {
