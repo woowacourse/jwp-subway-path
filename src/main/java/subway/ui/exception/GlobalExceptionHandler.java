e3223c89de81fb934cff1ebdb7ec2cc13b9f282a
chaewon121 <cw121@naver.com>
2023-05-17 18:06:21 +0900

    refactor: ExceptionHandler 예외처리 코드 잘못된 반환값 수정 및 로그 추가

--- src/main/java/subway/ui/exception/GlobalExceptionHandler.java
+++ src/main/java/subway/ui/exception/GlobalExceptionHandler.java
@@ -15,13 +15,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnExpectedException(Exception exception) {
         final String errorMessage = exception.getMessage();
-        log.warn("예상치 못한 예외가 발생했습니다 : {}", errorMessage);
+        log.error("예상치 못한 예외가 발생했습니다 : {}", errorMessage);
 
         return ResponseEntity.internalServerError().body(errorMessage);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(Exception exception) {
-        return ResponseEntity.internalServerError().body(exception.getMessage());
+    public ResponseEntity<String> ha가ndleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("잘못된 입력 정보 입니다 : {}", exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
