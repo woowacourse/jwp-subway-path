feb987c92ac15bab4e656206a8be7c5764cc565e
amaran-th <songsy405@gmail.com>
2023-05-17 22:10:47 +0900

    feat: DB 환경을 실제 환경과 테스트 환경으로 분리

---
+++ src/test/resources/data.sql
@@ -0,0 +1,43 @@
+INSERT INTO LINE (name, color, head_station)
+values('1호선', '파란색', 1);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('강남역', 2, 10, 1);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('역삼역', 4, 5, 1);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('잠실역', 0, null, 1);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('선릉역', 6, 10, 1);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('건대입구역', 3, 10, 1);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('삼성역', 5, 8, 1);
+
+
+
+INSERT INTO LINE (name, color, head_station)
+values('2호선', '초록색', 7);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('성수역', 8, 10, 2);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('뚝섬역', 9, 5, 2);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('잠실역', 10, 15, 2);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('삼성역', 11, 10, 2);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('왕십리역', 12, 4, 2);
+
+INSERT INTO STATION (name, next_station, distance, line_id)
+values('용답역', 0, null, 2);
