97060ce780c615811b0a43cd8876e0d0f3638d39
0chil <0@chll.it>
2023-05-18 16:52:40 +0900

    test: data.sql 갱신

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,14 @@
 insert into station (name)
-values ('잠실나루'), ('잠실'), ('잠실새내'), ('정자'), ('판교');
+values ('잠실나루'),
+       ('잠실'),
+       ('잠실새내'),
+       ('정자'),
+       ('판교'),
+       ('종합운동장'),
+       ('몽촌토성'),
+       ('석촌');
 
 insert into line (name, color)
-values ('1호선', 'blue'), ('2호선', 'green'), ('3호선', 'brown');
+values ('1호선', 'blue'),
+       ('2호선', 'green'),
+       ('3호선', 'brown');
