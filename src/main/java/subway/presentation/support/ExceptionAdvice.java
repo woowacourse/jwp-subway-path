358c7197a8280d4c41ec6d0cac191c8c2c3954fe
woosung1223 <dntjd991223@naver.com>
2023-05-18 13:04:45 +0900

    refactor: logback-access가 아닌 인터셉터로 로깅 처리

--- src/main/java/subway/presentation/support/ExceptionAdvice.java
+++ src/main/java/subway/presentation/support/ExceptionAdvice.java
@@ -1,9 +1,8 @@
-package subway.presentation.advice;
+package subway.presentation.support;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
