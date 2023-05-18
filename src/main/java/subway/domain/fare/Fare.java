fb950a9c1e525d2fe1a7faac657add621b710d14
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-18 11:29:47 +0900

    ✨ feat: 요금 객체간 연산 기능 추가

--- src/main/java/subway/domain/fare/Fare.java
+++ src/main/java/subway/domain/fare/Fare.java
@@ -1,5 +1,7 @@
 package subway.domain.fare;
 
+import java.util.Objects;
+
 public class Fare {
 
     private final int fare;
@@ -13,5 +15,30 @@ public class Fare {
         if (value <= 0) {
             throw new IllegalArgumentException("요금은 0보다 작을 수 없습니다.");
         }
+    }
+
+    public Fare multiply(final int times) {
+        return new Fare(this.fare * times);
+    }
+
+    public Fare add(final Fare other) {
+        return new Fare(this.fare + other.fare);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Fare fare1 = (Fare) o;
+        return fare == fare1.fare;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fare);
     }
 }
--- src/test/java/subway/domain/fare/FareTest.java
+++ src/test/java/subway/domain/fare/FareTest.java
@@ -1,7 +1,9 @@
 package subway.domain.fare;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,5 +14,23 @@ class FareTest {
     void 요금이_0보다_작거나_같은_경우_예외_발생(final int value) {
         assertThatThrownBy(() -> new Fare(value))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 요금_곱_적용_테스트() {
+        final Fare fare = new Fare(300);
+
+        final Fare multiply = fare.multiply(3);
+
+        assertThat(multiply).isEqualTo(new Fare(900));
+    }
+
+    @Test
+    void 요금_덧셈_적용_테스트() {
+        final Fare fare = new Fare(300);
+
+        final Fare multiply = fare.add(new Fare(700));
+
+        assertThat(multiply).isEqualTo(new Fare(1000));
     }
 }
