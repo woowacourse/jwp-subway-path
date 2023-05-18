68827bcff944a916ba97a1b9d59820b460d94422
jaeyeon kim <jakind@naver.com>
2023-05-18 18:19:13 +0900

    test: LineController delete, update 테스트

--- src/test/java/subway/integration/LineIntegrationTest.java
+++ src/test/java/subway/integration/LineIntegrationTest.java
@@ -143,7 +143,7 @@ class LineIntegrationTest extends IntegrationTest {
                 .then().log().all()
                 .extract();
 
-        // then
+
         StationResponse 잠실 = new StationResponse(1L, "잠실");
         StationResponse 잠실새내 = new StationResponse(2L, "잠실새내");
         StationResponse 종합운동장 = new StationResponse(3L, "종합운동장");
@@ -163,59 +163,59 @@ class LineIntegrationTest extends IntegrationTest {
         assertThat(result.get(1).getLinePropertyResponse().getId()).isEqualTo(2L);
         assertThat(result.get(1).getLinePropertyResponse().getName()).isEqualTo("6호선");
         assertThat(result.get(1).getLinePropertyResponse().getColor()).isEqualTo("bg-olive-600");
-        assertThat(result.get(1).getStationResponses()).containsAll(List.of(잠실새내, 종합운동장));
+        assertThat(result.get(1).getStationResponses()).containsAll(List.of(잠실, 종합운동장));
     }
 
-//    @DisplayName("지하철 노선을 수정한다.")
-//    @Test
-//    @Sql("/station_test_data.sql")
-//    void updateLine() {
-//        // given
-//        ExtractableResponse<Response> createResponse = RestAssured
-//                .given().log().all()
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .body(lineRequest1)
-//                .when().post("/lines")
-//                .then().log().all().
-//                extract();
-//
-//        // when
-//        Long lineId = Long.parseLong(createResponse.header("Location").split("/")[2]);
-//        ExtractableResponse<Response> response = RestAssured
-//                .given().log().all()
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .body(lineRequest2)
-//                .when().put("/lines/{lineId}", lineId)
-//                .then().log().all()
-//                .extract();
-//
-//        // then
-//        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-//    }
-//
-//    @DisplayName("지하철 노선을 제거한다.")
-//    @Test
-//    @Sql("/station_test_data.sql")
-//    void deleteLine() {
-//        // given
-//        ExtractableResponse<Response> createResponse = RestAssured
-//                .given().log().all()
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .body(lineRequest1)
-//                .when().post("/lines")
-//                .then().log().all().
-//                extract();
-//
-//        // when
-//        Long lineId = Long.parseLong(createResponse.header("Location").split("/")[2]);
-//        ExtractableResponse<Response> response = RestAssured
-//                .given().log().all()
-//                .when().delete("/lines/{lineId}", lineId)
-//                .then().log().all()
-//                .extract();
-//
-//        // then
-//        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-//    }
+    /**
+     * INSERT INTO station(name) VALUES('잠실'), ('잠실새내'), ('종합운동장'), ('석촌'), ('송파');
+     */
+    @DisplayName("지하철 노선을 수정한다.")
+    @Test
+    @Sql("/station_test_data.sql")
+    void updateLine() {
+        ExtractableResponse<Response> createResponse = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(lineRequest1)
+                .when().post("/lines")
+                .then().log().all().
+                extract();
+
+        Long lineId = Long.parseLong(createResponse.header("Location").split("/")[2]);
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(lineRequest2)
+                .when().put("/lines/{lineId}", lineId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    /**
+     * INSERT INTO station(name) VALUES('잠실'), ('잠실새내'), ('종합운동장'), ('석촌'), ('송파');
+     */
+    @DisplayName("지하철 노선을 제거한다.")
+    @Test
+    @Sql("/station_test_data.sql")
+    void deleteLine() {
+        ExtractableResponse<Response> createResponse = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(lineRequest1)
+                .when().post("/lines")
+                .then().log().all().
+                extract();
+
+        Long lineId = Long.parseLong(createResponse.header("Location").split("/")[2]);
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when().delete("/lines/{lineId}", lineId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
 
 }
