638dcd78fdb2936ae6665b093847f4cb43b639e1
nuyh <hjaehyun25@gmail.com>
2023-05-18 15:13:58 +0900

    test: 요금 조회 테스트 네임 추가

--- src/test/java/subway/domain/FareStrategyImplTest.java
+++ src/test/java/subway/domain/FareStrategyImplTest.java
@@ -1,6 +1,5 @@
 package subway.domain;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,9 +9,8 @@ class FareStrategyImplTest {
 
     private FareStrategyImpl fareStrategy = new FareStrategyImpl();
 
-    @DisplayName("요금 계산 테스트")
     @CsvSource(value = {"10,1250", "25,1550", "50,2050", "60,2250", "68,2350", "106,2750", "178,3650"})
-    @ParameterizedTest
+    @ParameterizedTest(name = "거리가 {0}일 때, 요금은 {1}원이다.")
     void calculate(long distance, int expectedFare) {
         //when
         final int fare = fareStrategy.calculate(distance);
