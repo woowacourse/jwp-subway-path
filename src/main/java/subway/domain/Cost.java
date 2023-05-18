e1771dcfe92608f87b976a62a78ea1c9b0ab73ec
cpot5620 <=cpot5620@naver.com>
2023-05-18 12:59:09 +0900

    refactor(Cost): Function 인터페이스 적용

--- src/main/java/subway/domain/Cost.java
+++ src/main/java/subway/domain/Cost.java
@@ -1,9 +1,16 @@
 package subway.domain;
 
+import java.util.function.Function;
+
 public class Cost {
     private static final Distance DEFAULT_DISTANCE_STANDARD = new Distance(10);
     private static final Distance MIDDLE_DISTANCE_STANDARD = new Distance(50);
     private static final int DEFAULT_FARE = 1250;
+
+    private static final Function<Distance, Integer> calculateOverCostByLongDistance =
+            (distance) -> (int) ((Math.ceil((distance.getDistance() - 1) / 8) + 1) * 100);
+    private static final Function<Distance, Integer> calculateOverCostByMiddleDistance =
+            (distance) -> (int) ((Math.ceil((distance.getDistance() - 1) / 5) + 1) * 100);
 
     private final int fare;
 
@@ -17,19 +24,10 @@ public class Cost {
         }
 
         if (distance.isLessThanOrEqualTo(MIDDLE_DISTANCE_STANDARD)) {
-            return DEFAULT_FARE + calculateOverFareByMiddleDistance(distance);
+            return DEFAULT_FARE + calculateOverCostByMiddleDistance.apply(distance);
         }
 
-        return DEFAULT_FARE + calculateOverFareByLongDistance(distance);
-    }
-
-    private int calculateOverFareByMiddleDistance(Distance distance) {
-        return (int) ((Math.ceil((distance.getDistance() - 1) / 5) + 1) * 100);
-    }
-
-
-    private int calculateOverFareByLongDistance(Distance distance) {
-        return (int) ((Math.ceil((distance.getDistance() - 1) / 8) + 1) * 100);
+        return DEFAULT_FARE + calculateOverCostByLongDistance.apply(distance);
     }
 
     public int getFare() {
