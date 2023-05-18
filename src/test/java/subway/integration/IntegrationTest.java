6967722862e5c693ebd06c7c4ef80817e9178ee6
thdwoqor <celebarte01@gmail.com>
2023-05-18 09:45:57 +0900

    chore: testDb, prodDb 분리
    (cherry picked from commit 6844615f2ec818ab29e42cdf4bef351eb9f91a83)

--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -1,12 +1,17 @@
 package subway.integration;
 
 import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 @Sql({"classpath:schema.sql", "classpath:data.sql"})
+@TestPropertySource(properties = "spring.config.location = classpath:application.yml")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
     @LocalServerPort
@@ -15,5 +20,10 @@ public class IntegrationTest {
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+    }
+
+    @Test
+    void 테스트_프로퍼티_설정(@Value("${spring.datasource.url}") String url) {
+        Assertions.assertThat(url).isEqualTo("jdbc:h2:~/test;MODE=MySQL");
     }
 }
