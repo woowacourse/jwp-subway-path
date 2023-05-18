09ba2886de8b2e259a23d7b11d58ce65aa68eac3
thdwoqor <celebarte01@gmail.com>
2023-05-18 09:47:55 +0900

    refactor: property 변경
    (cherry picked from commit 56dd8ba6e1e45ecf80b2319959198ad8313c86f2)

--- src/test/java/subway/SubwayApplicationTests.java
+++ src/test/java/subway/SubwayApplicationTests.java
@@ -2,8 +2,10 @@ package subway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.location = classpath:application.yml")
 class SubwayApplicationTests {
 
 	@Test
