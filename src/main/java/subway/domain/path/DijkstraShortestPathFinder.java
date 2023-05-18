19de04ba3d25b7b748c662917ee0504c3b72362e
hong-sile <gurwns9325@naver.com>
2023-05-18 16:15:04 +0900

    refactor: 상속을 이용하지 않고 edge의 가중치를 직접 설정하는 방식으로 리팩터링

--- src/main/java/subway/domain/path/DijkstraShortestPathFinder.java
+++ src/main/java/subway/domain/path/DijkstraShortestPathFinder.java
@@ -1,6 +1,5 @@
 package subway.domain.path;
 
-import static java.util.stream.Collectors.toUnmodifiableList;
 import static java.util.stream.Collectors.toUnmodifiableSet;
 
 import java.util.List;
@@ -18,25 +17,24 @@ public class DijkstraShortestPathFinder implements ShortestPathFinder {
     @Override
     public Path findShortestPath(final List<Section> sections, final Station startStation,
                                  final Station endStation) {
-        final WeightedMultigraph<Station, SectionDijkstraEdge> graph
-                = new WeightedMultigraph<>(SectionDijkstraEdge.class);
+        final WeightedMultigraph<Station, Section> graph
+                = new WeightedMultigraph<>(Section.class);
         initGraph(sections, graph);
-        final DijkstraShortestPath<Station, SectionDijkstraEdge> shortestPath
+        final DijkstraShortestPath<Station, Section> shortestPath
                 = new DijkstraShortestPath<>(graph);
 
-        final GraphPath<Station, SectionDijkstraEdge> graphPath = shortestPath.getPath(startStation, endStation);
-        final List<Section> sectionList = graphPath.getEdgeList().stream()
-                .map(SectionDijkstraEdge::getSection)
-                .collect(toUnmodifiableList());
+        final GraphPath<Station, Section> graphPath = shortestPath.getPath(startStation, endStation);
+        final List<Section> sectionList = graphPath.getEdgeList();
         return new Path(graphPath.getVertexList(), sectionList);
     }
 
-    private void initGraph(final List<Section> sections, final WeightedMultigraph<Station, SectionDijkstraEdge> graph) {
+    private void initGraph(final List<Section> sections, final WeightedMultigraph<Station, Section> graph) {
         for (final Station station : findAllStation(sections)) {
             graph.addVertex(station);
         }
         for (final Section section : sections) {
-            graph.addEdge(section.getPrevStation(), section.getNextStation(), new SectionDijkstraEdge(section));
+            graph.addEdge(section.getPrevStation(), section.getNextStation(), section);
+            graph.setEdgeWeight(section, section.getDistance().getValue());
         }
     }
 
