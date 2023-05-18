a35db8724264be3a3a0188ed16c7360da237dde2
cpot5620 <=cpot5620@naver.com>
2023-05-18 12:51:32 +0900

    test(Path): 연결되지 않은 경로 조회 테스트 구현

--- src/test/java/subway/domain/PathTest.java
+++ src/test/java/subway/domain/PathTest.java
@@ -1,11 +1,14 @@
 package subway.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import subway.exception.GlobalException;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static subway.domain.PathTest.Data.*;
 
 class PathTest {
@@ -24,6 +27,18 @@ class PathTest {
         //then
         assertThat(shortestPath).containsExactly(건대입구, 왕십리, 청량리);
         assertThat(shortestDistance.getDistance()).isEqualTo(16);
+    }
+
+    @DisplayName("연결되어 있지 않은 경로로는 이동할 수 없다.")
+    @Test
+    void findPathFailTest() {
+        List<Station> stations = List.of(잠실, 건대입구, 왕십리, 청량리);
+        List<Section> sections = List.of(잠실_건대입구, 왕십리_청량리);
+        Path path = new Path(stations, sections);
+
+        assertThatThrownBy(() -> path.getShortestPath(잠실, 청량리))
+                .isInstanceOf(GlobalException.class)
+                .hasMessage("이동할 수 없는 경로입니다.");
     }
 
     static class Data {
