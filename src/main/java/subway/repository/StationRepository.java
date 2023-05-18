335edcfacfe01ca640f27a1e348427d4b7c422ee
jaeyeon kim <jakind@naver.com>
2023-05-18 15:33:19 +0900

    feat: StationRepository 의 findById 기능 구현

--- src/main/java/subway/repository/StationRepository.java
+++ src/main/java/subway/repository/StationRepository.java
@@ -3,8 +3,11 @@ package subway.repository;
 import org.springframework.stereotype.Repository;
 import subway.dao.StationDao;
 import subway.entity.StationEntity;
+import subway.exception.StationNotFoundException;
 import subway.repository.converter.StationConverter;
 import subway.service.domain.Station;
+
+import java.util.List;
 
 @Repository
 public class StationRepository {
@@ -20,6 +23,17 @@ public class StationRepository {
         Long id = stationDao.insert(stationEntity);
 
         return StationConverter.entityToDomain(id, stationEntity);
+    }
+
+    public Station findById(Long id) {
+        List<StationEntity> stationEntities = stationDao.findById(id);
+
+        if (stationEntities.isEmpty()) {
+            throw new StationNotFoundException("해당 역은 존재하지 않습니다.");
+        }
+
+        StationEntity stationEntity = stationEntities.get(0);
+        return StationConverter.entityToDomain(stationEntity);
     }
 
 }
--- src/test/java/subway/repository/StationRepositoryTest.java
+++ src/test/java/subway/repository/StationRepositoryTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import subway.exception.StationNotFoundException;
 import subway.service.domain.Station;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -27,6 +29,28 @@ class StationRepositoryTest {
         assertThat(save.getName()).isEqualTo("hello");
     }
 
+    /**
+     * INSERT INTO station(name)
+     * VALUES('가산'), ('남구로'), ('대림'), ('신풍'), ('구로'), ('독산'), ('신도림'), ('구로디지털단지');
+     */
+    @Test
+    @DisplayName("findById 를 통해 Station 을 찾는다.")
+    void findById() {
+        Station station = stationRepository.findById(4L);
 
+        assertThat(station.getId()).isEqualTo(4L);
+        assertThat(station.getName()).isEqualTo("신풍");
+    }
+
+    /**
+     * INSERT INTO station(name)
+     * VALUES('가산'), ('남구로'), ('대림'), ('신풍'), ('구로'), ('독산'), ('신도림'), ('구로디지털단지');
+     */
+    @Test
+    @DisplayName("findById 를 통해 Station 을 찾는다. (실패)")
+    void findById_fail() {
+        assertThatThrownBy(() -> stationRepository.findById(100L))
+                .isInstanceOf(StationNotFoundException.class);
+    }
 
 }
