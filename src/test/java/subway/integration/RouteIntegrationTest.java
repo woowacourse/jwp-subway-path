32b7750b599f3966ae4be09a8277c49402ce1b8c
yoondgu <doyou2ing@naver.com>
2023-05-18 17:28:47 +0900

    test: 경로 조회 통합테스트 잘못된 요청 케이스 작성

--- src/test/java/subway/integration/RouteIntegrationTest.java
+++ src/test/java/subway/integration/RouteIntegrationTest.java
@@ -8,6 +8,8 @@ import io.restassured.response.Response;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
@@ -36,5 +38,27 @@ public class RouteIntegrationTest extends IntegrationTest {
                 .isEqualTo(18);
         assertThat(response.body().jsonPath().getInt("totalFare"))
                 .isEqualTo(1450);
+    }
+
+    @DisplayName("잘못된 요청 정보로 조회 시 404 상태코드와 적절한 에러 메시지를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "100:1:출발 역이 존재하지 않습니다.", "1:100:도착 역이 존재하지 않습니다.",
+            "1:1:출발 역과 도착 역이 동일한 경로를 찾을 수 없습니다.", "100:100:출발 역이 존재하지 않습니다."}, delimiter = ':')
+    void findShortestRouteBadRequest(final int sourceStationId,
+                                     final int targetStationId,
+                                     final String errorMessage) {
+        // given, when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/routes?sourceStationId={sourceStationId}&targetStationId={targetStationId}",
+                        Map.of("sourceStationId", sourceStationId, "targetStationId", targetStationId))
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.body().jsonPath().getString("message")).isEqualTo(errorMessage);
     }
 }
