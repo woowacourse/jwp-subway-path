4e40d5470b354440018d9bf7216f2c093e37f7b7
kiarakim <kiarakim413@gmail.com>
2023-05-18 21:31:02 +0900

    feat: 존재하지 않는 노선 id로 조회를 시도했을 때 예외를 터트리는 기능 구현

--- src/main/java/subway/persistence/LineJdbcRepository.java
+++ src/main/java/subway/persistence/LineJdbcRepository.java
@@ -15,67 +15,71 @@ import subway.domain.repository.LineRepository;
 @Repository
 public class LineJdbcRepository implements LineRepository {
 
-    private static final int DELETED_COUNT = 1;
-    private static final int UPDATED_COUNT = 1;
-    private final JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert insert;
+	private static final int DELETED_COUNT = 1;
+	private static final int UPDATED_COUNT = 1;
+	private final JdbcTemplate jdbcTemplate;
+	private final SimpleJdbcInsert insert;
 
-    private final RowMapper<Line> lineRowMapper = (rs, rowNum) ->
-            new Line(
-                    rs.getLong("id"),
-                    rs.getString("name")
-            );
+	private final RowMapper<Line> lineRowMapper = (rs, rowNum) ->
+		new Line(
+			rs.getLong("id"),
+			rs.getString("name")
+		);
 
-    public LineJdbcRepository(final JdbcTemplate jdbcTemplate) {
-        this.insert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("line")
-                .usingGeneratedKeyColumns("id");
-        this.jdbcTemplate = jdbcTemplate;
-    }
+	public LineJdbcRepository(final JdbcTemplate jdbcTemplate) {
+		this.insert = new SimpleJdbcInsert(jdbcTemplate)
+			.withTableName("line")
+			.usingGeneratedKeyColumns("id");
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    @Override
-    public long createLine(final Line line) {
-        SqlParameterSource params = new BeanPropertySqlParameterSource(line);
-        return insert.executeAndReturnKey(params).longValue();
-    }
+	@Override
+	public long createLine(final Line line) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(line);
+		return insert.executeAndReturnKey(params).longValue();
+	}
 
-    @Override
-    public List<Line> findAll() {
-        String sql = "SELECT * FROM line";
-        return jdbcTemplate.query(sql, lineRowMapper);
-    }
+	@Override
+	public List<Line> findAll() {
+		String sql = "SELECT * FROM line";
+		return jdbcTemplate.query(sql, lineRowMapper);
+	}
 
-    @Override
-    public Line findById(final Long lineId) {
-        String sql = "SELECT * FROM line WHERE id = ?";
-        return jdbcTemplate.queryForObject(sql, lineRowMapper, lineId);
-    }
+	@Override
+	public Line findById(final long lineId) {
+		String sql = "SELECT * FROM line WHERE id = ?";
+		final Line line = jdbcTemplate.queryForObject(sql, lineRowMapper, lineId);
+		if (line == null) {
+			throw new NullPointerException("존재하지 않는 노선입니다");
+		}
+		return line;
+	}
 
-    @Override
-    public boolean updateLine(final long lineId, final Line line) {
-        final String sql = "UPDATE line SET name = ? WHERE id = ?";
-        final int updateCount = jdbcTemplate.update(sql, line.getName(), lineId);
+	@Override
+	public boolean updateLine(final long lineId, final Line line) {
+		final String sql = "UPDATE line SET name = ? WHERE id = ?";
+		final int updateCount = jdbcTemplate.update(sql, line.getName(), lineId);
 
-        return updateCount == UPDATED_COUNT;
-    }
+		return updateCount == UPDATED_COUNT;
+	}
 
-    @Override
-    public Line findByName(final String lineName) {
-        final String sql = "SELECT * FROM line WHERE name = ?";
-        final List<Line> lines = jdbcTemplate.query(sql, lineRowMapper, lineName);
+	@Override
+	public Line findByName(final String lineName) {
+		final String sql = "SELECT * FROM line WHERE name = ?";
+		final List<Line> lines = jdbcTemplate.query(sql, lineRowMapper, lineName);
 
-        if(lines.isEmpty()){
-            throw new IllegalArgumentException("존재하지 않는 노선입니다");
-        }
+		if (lines.isEmpty()) {
+			throw new IllegalArgumentException("존재하지 않는 노선입니다");
+		}
 
-        return lines.get(0);
-    }
+		return lines.get(0);
+	}
 
-    @Override
-    public boolean deleteById(final Long lineId) {
-        String sql = "DELETE FROM line WHERE id = ?";
-        final int deleteCount = jdbcTemplate.update(sql, lineId);
+	@Override
+	public boolean deleteById(final long lineId) {
+		String sql = "DELETE FROM line WHERE id = ?";
+		final int deleteCount = jdbcTemplate.update(sql, lineId);
 
-        return deleteCount == DELETED_COUNT;
-    }
+		return deleteCount == DELETED_COUNT;
+	}
 }
