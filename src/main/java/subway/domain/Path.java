13858d847563afe4ae6087d35d6389c3947a5718
0chil <0@chll.it>
2023-05-18 19:46:52 +0900

    feat: 경로 구현

---
+++ src/main/java/subway/domain/Path.java
@@ -0,0 +1,34 @@
+package subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import subway.domain.exception.EmptyPathException;
+
+public class Path {
+
+    private final List<Section> sections;
+
+    public Path(List<Section> sections) {
+        this.sections = new ArrayList<>(sections);
+    }
+
+    // TODO: ENUM 리팩터링
+    public int calculateFair() {
+        int distance = getDistanceOf(sections);
+        if (distance <= 10) {
+            return 1250;
+        }
+        if (distance < 50) {
+            return 1250 + (distance - 10) / 5 * 100;
+        }
+        return 2050 + (distance - 50) / 8 * 100;
+    }
+
+    private int getDistanceOf(List<Section> sections) {
+        return sections.stream()
+                .map(Section::getDistance)
+                .reduce(Integer::sum)
+                .orElseThrow(EmptyPathException::new);
+    }
+}
---
+++ src/test/java/subway/domain/PathTest.java
@@ -0,0 +1,53 @@
+package subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static subway.TestFixture.STATION_A;
+import static subway.TestFixture.STATION_C;
+import static subway.TestFixture.STATION_D;
+import static subway.TestFixture.STATION_E;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PathTest {
+
+    @DisplayName("10km 이내는 운임이 1250원이다.")
+    @Test
+    void calculate_fair_under_10km() {
+        var path = new Path(List.of(
+                new Section(STATION_A, STATION_C, 2),
+                new Section(STATION_C, STATION_D, 3),
+                new Section(STATION_D, STATION_E, 5)
+        ));
+
+        assertThat(path.calculateFair()).isEqualTo(1250);
+    }
+
+    @DisplayName("10km~50km는 5km 당 100원이다")
+    @ParameterizedTest
+    @CsvSource({"4,1250", "5,1350", "6,1350", "10,1450", "40,2050"})
+    void calculate_fair_between_10km_50km(int extraDistance, int fair) {
+        var path = new Path(List.of(
+                new Section(STATION_A, STATION_C, 10),
+                new Section(STATION_C, STATION_D, extraDistance)
+        ));
+
+        assertThat(path.calculateFair()).isEqualTo(fair);
+    }
+
+    @DisplayName("50km 초과는 8km 당 100원이다")
+    @ParameterizedTest
+    @CsvSource({"48,2150", "56, 2250", "64, 2350"})
+    void calculate_fair_over_50km(int extraDistance, int fair) {
+        var path = new Path(List.of(
+                new Section(STATION_A, STATION_C, 10),
+                new Section(STATION_C, STATION_D, extraDistance)
+        ));
+
+        assertThat(path.calculateFair()).isEqualTo(fair);
+    }
+}
