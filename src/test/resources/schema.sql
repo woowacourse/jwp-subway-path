76ec4cddea59850e7cfd8fadbab5b21263244b8a
Woojin Lee <dhkep03@gmail.com>
2023-05-18 09:08:15 +0900

    chore: application.yml 테스트와 프로덕션 분리

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS line
+(
+    id                  BIGINT AUTO_INCREMENT  NOT NULL,
+    name                VARCHAR(255)           NOT NULL,
+    created_at          TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS section
+(
+    id                  BIGINT AUTO_INCREMENT  NOT NULL,
+    line_id             BIGINT                 NOT NULL,
+    upward_station_id   BIGINT                 NOT NULL,
+    downward_station_id BIGINT                 NOT NULL,
+    distance            INT                    NOT NULL,
+    created_at          TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (line_id) REFERENCES LINE (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS station
+(
+    id                  BIGINT AUTO_INCREMENT  NOT NULL,
+    line_id             BIGINT                 NOT NULL,
+    name                VARCHAR(255)           NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (line_id) REFERENCES LINE (id) ON DELETE CASCADE
+);
