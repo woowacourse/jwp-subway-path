0d1d28a4e3228f6e3b40e9bcf6ae63f8aae254c8
yoondgu <doyou2ing@naver.com>
2023-05-18 22:37:48 +0900

    build: 프로덕션 코드 구간 더미데이터 DDL 삭제
    - 구간 데이터(SECTIONS 테이블)는 프로그램 사용에 따라 변하기 때문에 insert ignore문 사용이 부적절
    - 어플리케이션 실행 시마다 기존 DDL 사용하면 잘못된 데이터 값이 들어가게 됨
    - 따라서 해당 더미데이터 기본 실행되지 않도록 삭제함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,17 +12,3 @@ insert ignore into STATION (id, name) VALUES(7, '서울역');
 insert ignore into STATION (id, name) VALUES(8, '종각역');
 insert ignore into STATION (id, name) VALUES(9, '종로3가역');
 insert ignore into STATION (id, name) VALUES(10, '종로5가역');
-
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(1, 1, 2, 3, 10);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(2, 1, 3, 4, 10);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(3, 1, 4, 5, 10);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(4, 1, 1, 2, 8);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(5, 1, 5, 6, 10);
-
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(6, 2, 7, 1, 10);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(7, 2, 1, 8, 10);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(8, 2, 8, 9, 10);
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(9, 2, 9, 10, 10);
-
-insert ignore into SECTIONS (id, line_id, left_station_id, right_station_id, distance) VALUES(10, 3, 2, 9, 10);
-
