ba2fe46384a56cac121375188e40686638fc37d1
woosung1223 <dntjd991223@naver.com>
2023-05-18 11:33:50 +0900

    refactor: 동등성 비교조건 변경

--- src/main/java/subway/application/domain/Station.java
+++ src/main/java/subway/application/domain/Station.java
@@ -20,16 +20,20 @@ public class Station {
         return name;
     }
 
+    public boolean hasSameNameWith(Station target) {
+        return name.equals(target.name);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Station station = (Station) o;
-        return Objects.equals(name, station.name);
+        return Objects.equals(id, station.id) && Objects.equals(name, station.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(id, name);
     }
 }
