c9b628580f5782818b1ae376abb4d3fbd7167e89
yujamint <woawoa0513@gmail.com>
2023-05-18 20:47:18 +0900

    test: LineDao 조회 기능 테스트 작성

--- src/test/java/subway/repository/dao/LineDaoTest.java
+++ src/test/java/subway/repository/dao/LineDaoTest.java
@@ -2,6 +2,7 @@ package subway.repository.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -35,5 +36,37 @@ class LineDaoTest {
 
         // then
         assertThat(saveLine.getId()).isPositive();
+    }
+
+    @Test
+    void Line을_전체_조회한다() {
+        // given
+        final LineEntity firstLine = new LineEntity("1호선");
+        final LineEntity secondLine = new LineEntity("2호선");
+        lineDao.insert(firstLine);
+        lineDao.insert(secondLine);
+
+        // when
+        final List<LineEntity> lines = lineDao.findAll();
+
+        // then
+        assertThat(lines).map(LineEntity::getName)
+                .containsExactly(firstLine.getName(), secondLine.getName());
+
+    }
+
+    @Test
+    void Line을_ID_기준으로_단건_조회한다() {
+        // given
+        final LineEntity saveLine1 = lineDao.insert(new LineEntity("1호선"));
+        final LineEntity saveLine2 = lineDao.insert(new LineEntity("2호선"));
+
+        // when
+        final LineEntity findLine1 = lineDao.findById(saveLine1.getId());
+        final LineEntity findLine2 = lineDao.findById(saveLine2.getId());
+
+        // then
+        assertThat(findLine1).isEqualTo(saveLine1);
+        assertThat(findLine2).isEqualTo(saveLine2);
     }
 }
