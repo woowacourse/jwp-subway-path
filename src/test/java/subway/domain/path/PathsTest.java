14b2c9040dfecfbdd7c3eb496b7e3e4d26023196
nuyh <hjaehyun25@gmail.com>
2023-05-18 16:21:02 +0900

    test: Paths 생성 테스트 추가

--- src/test/java/subway/domain/path/PathsTest.java
+++ src/test/java/subway/domain/path/PathsTest.java
@@ -1,6 +1,7 @@
 package subway.domain.path;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import subway.domain.Station;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class PathsTest {
 
@@ -16,6 +18,49 @@ class PathsTest {
     private final Station station2 = new Station("해운대");
     private final Station station3 = new Station("광안리");
     private final Station station4 = new Station("전포");
+
+    @DisplayName("생성 테스트")
+    @Nested
+    class Construct {
+        @DisplayName("여러 경로들로 Paths를 생성할 수 있다")
+        @Test
+        void construct() {
+            //given
+            final Path path1 = new Path(station, station2, 3);
+            final Path path2 = new Path(station2, station3, 3);
+            final Path path3 = new Path(station3, station4, 3);
+
+            //when, then
+            assertDoesNotThrow(() -> new Paths(List.of(path1, path2, path3)));
+        }
+
+        @DisplayName("이어지지 않은 경로들로 Paths를 생성하면 예외가 발생한다")
+        @Test
+        void construct_fail() {
+            //given
+            final Path path1 = new Path(station, station2, 3);
+            final Path path2 = new Path(station3, station4, 3);
+
+            //when, then
+            assertThatThrownBy(() -> new Paths(List.of(path1, path2)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("연결되지 않은 경로들입니다.");
+        }
+
+        @DisplayName("1:1 연결이 아닌 경로들로 Paths를 생성하면 예외가 발생한다")
+        @Test
+        void construct_fail2() {
+            //given
+            final Path path1 = new Path(station, station2, 3);
+            final Path path2 = new Path(station, station3, 3);
+            final Path path3 = new Path(station3, station4, 3);
+
+            //when, then
+            assertThatThrownBy(() -> new Paths(List.of(path1, path2, path3)))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("연결되지 않은 경로들입니다.");
+        }
+    }
 
     @DisplayName("경로가 하나도 없을 때 경로를 추가할 수 있다")
     @Test
