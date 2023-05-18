15d67dff08f2f9f95a27d58ca43cf16829ec4348
kiarakim <kiarakim413@gmail.com>
2023-05-18 22:01:18 +0900

    refactor: 엔티티에 id 추가

--- src/main/java/subway/persistence/SectionEntity.java
+++ src/main/java/subway/persistence/SectionEntity.java
@@ -6,43 +6,50 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SectionEntity {
-    private final String line;
-    private final String upStation;
-    private final String downStation;
-    private final Long distance;
+	private final long id;
+	private final String line;
+	private final String upStation;
+	private final String downStation;
+	private final Long distance;
 
-    public SectionEntity(final String line, final String upStation, final String downStation,
-        final Long distance) {
-        this.line = line;
-        this.upStation = upStation;
-        this.downStation = downStation;
-        this.distance = distance;
-    }
+	public SectionEntity(final long id, final String line, final String upStation, final String downStation,
+		final Long distance) {
+		this.id = id;
+		this.line = line;
+		this.upStation = upStation;
+		this.downStation = downStation;
+		this.distance = distance;
+	}
 
-    public static List<SectionEntity> of(final List<Section> sections) {
-        return sections.stream()
-                .map(section -> new SectionEntity(
-                        section.getLine().getName(),
-                        section.getUpStation().getName(),
-                        section.getDownStation().getName(),
-                        section.getDistance()
-                ))
-                .collect(Collectors.toList());
-    }
+	public static List<SectionEntity> of(final List<Section> sections) {
+		return sections.stream()
+			.map(section -> new SectionEntity(
+				section.getId(),
+				section.getLine().getName(),
+				section.getUpStation().getName(),
+				section.getDownStation().getName(),
+				section.getDistance()
+			))
+			.collect(Collectors.toList());
+	}
 
-    public String getLine() {
-        return line;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public String getUpStation() {
-        return upStation;
-    }
+	public String getLine() {
+		return line;
+	}
 
-    public String getDownStation() {
-        return downStation;
-    }
+	public String getUpStation() {
+		return upStation;
+	}
 
-    public Long getDistance() {
-        return distance;
-    }
+	public String getDownStation() {
+		return downStation;
+	}
+
+	public Long getDistance() {
+		return distance;
+	}
 }
