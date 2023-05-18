47faf7e173292dff2028f2f12f708fc5e5b392ab
gitchan <eunkeeeee@gmail.com>
2023-05-18 17:45:06 +0900

    feat(PathController): 최단 경로 조회 API 컨트롤러 작성

---
+++ src/main/java/subway/service/PathService.java
@@ -0,0 +1,12 @@
+package subway.service;
+
+import org.springframework.stereotype.Service;
+import subway.dto.PathResponse;
+
+@Service
+public class PathService {
+
+    public PathResponse findShortestPath(final Long sourceId, final Long targetId) {
+        return null;
+    }
+}
