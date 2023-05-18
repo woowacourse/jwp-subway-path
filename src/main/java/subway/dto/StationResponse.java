2c686ecc8d6f482c802aef3f6cf7bc7e4f876b1b
dooboocookie <bkh3030@gmail.com>
2023-05-18 17:31:52 +0900

    test: StationService 테스트 추가

--- src/main/java/subway/dto/StationResponse.java
+++ src/main/java/subway/dto/StationResponse.java
@@ -2,9 +2,11 @@ package subway.dto;
 
 import subway.domain.Station;
 
+import java.util.Objects;
+
 public class StationResponse {
-    private Long id;
-    private String name;
+    private final Long id;
+    private final String name;
 
     public StationResponse(Long id, String name) {
         this.id = id;
@@ -21,5 +23,26 @@ public class StationResponse {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final StationResponse response = (StationResponse) o;
+        return Objects.equals(id, response.id) && Objects.equals(name, response.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "StationResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/subway/application/StationServiceTest.java
+++ src/test/java/subway/application/StationServiceTest.java
@@ -1,168 +1,90 @@
-/*
 package subway.application;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import subway.dao.LineDao;
-import subway.dao.SectionDao;
-import subway.dao.StationDao;
-import subway.dao.StationLineDao;
-import subway.domain.Distance;
-import subway.domain.Line;
-import subway.domain.Station;
-import subway.dto.DeleteStationRequest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import subway.domain.*;
 import subway.dto.StationRequest;
+import subway.dto.StationResponse;
+import subway.repository.LineRepository;
+import subway.repository.StationRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
 
-@JdbcTest
+@ExtendWith(MockitoExtension.class)
 class StationServiceTest {
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
+    @Mock
+    StationRepository stationRepository;
+    @Mock
+    LineRepository lineRepository;
+    @InjectMocks
     StationService stationService;
 
-    StationDao stationDao;
-
-    SectionDao sectionDao;
-
-    LineDao lineDao;
-
-    @BeforeEach
-    void setUp() {
-        stationDao = new StationDao(jdbcTemplate);
-        sectionDao = new SectionDao(jdbcTemplate);
-        lineDao = new LineDao(jdbcTemplate);
-
+    @DisplayName("역을 저장한다")
+    @Test
+    void 역을_저장한다() {
+        //given
+        StationRequest request = new StationRequest("테스트");
+        Station station = new Station("테스트");
+        when(stationRepository.save(station)).thenReturn(new Station(1L, "테스트"));
+        when(lineRepository.findAll())
+                .thenReturn(
+                        List.of(
+                                new Line(new LineName("2호선"), new LineColor("초록"), Sections.create()),
+                                new Line(new LineName("8호선"), new LineColor("핑크"), Sections.create())
+                        ));
+        //when
+        Long saveId = stationService.saveStation(request);
+        //then
+        assertThat(saveId).isEqualTo(1L);
     }
 
-    @DisplayName("upStation과 downStation이 이미 같은 노선에 있고, 추가하려는 구간이 그 노선일 때 예외를 발생한다.")
+    @DisplayName("역 아이디로 정보를 조회한다")
     @Test
-    void throwExceptionWHenUpStationAndDownStationAndLineIsSame() {
-        // given
-        StationRequest stationRequest = new StationRequest(
-                "잠실",
-                "잠실새내",
-                "2",
-                "초록",
-                10
-        );
-
-        final Long upStationId = stationDao.save("잠실");
-        final Long downStationId = stationDao.save("잠실새내");
-        final Long lineId = lineDao.save("2", "초록");
-        //sectionDao.save(upStationId, downStationId, lineId, true, new Distance(10));
-
-        // expect
-        assertThatThrownBy(() -> stationService.saveStation(stationRequest))
-                .isInstanceOf(IllegalArgumentException.class);
+    void 역_아이디로_정보를_조회한다() {
+        //given
+        Long stationId = 1L;
+        when(stationRepository.findById(stationId)).thenReturn(new Station(1L, "테스트"));
+        //when
+        StationResponse response = stationService.findStation(stationId);
+        //then
+        assertThat(response).isEqualTo(new StationResponse(1L, "테스트"));
     }
 
-    @DisplayName("downStation이 상행 종점이 아니면 해당 upStation이 상행 종점 아니다.")
+    @DisplayName("역을 수정한다")
     @Test
-    void saveStationNotStart() {
-        // given
-        final Long upStationId = stationDao.save("잠실");
-        final Long downStationId = stationDao.save("잠실새내");
-        final Long lineId = lineDao.save("2", "초록");
-        //sectionDao.save(upStationId, downStationId, lineId, true, new Distance(10));
-
-        final StationRequest stationRequest = new StationRequest(
-                "강변",
-                "잠실새내",
-                "2",
-                "초록",
-                5
-        );
-
-        // when
-        stationService.saveStation(stationRequest);
-
-        final Long firstStationId = sectionDao.findFirstStationIdByLineId(lineId);
-
-        // then
-        assertThat(firstStationId).isEqualTo(upStationId);
+    void 역을_수정한다() {
+        //given
+        Long stationId = 1L;
+        StationRequest request = new StationRequest("수정");
+        Station before = new Station(stationId, "테스트");
+        Station after = new Station(null, "수정");
+        when(stationRepository.findById(stationId)).thenReturn(before);
+        //when
+        stationService.editStation(stationId, request);
+        //then
+        verify(stationRepository, times(1)).update(before, after);
     }
 
-    @DisplayName("line 이 존재하지 않을 때, line 과 section을 저장한다.")
+
+
+    @DisplayName("역을 수정한다")
     @Test
-    void saveStationNewLine() {
-        // given
-        StationRequest stationRequest = new StationRequest(
-                "잠실",
-                "잠실새내",
-                "2",
-                "초록",
-                10
-        );
-        //final Optional<Line> before = lineDao.findByName("2");
-
-        // when
-        stationService.saveStation(stationRequest);
-
-        // expect
-        final Optional<Line> after = lineDao.findByName("2");
-        assertAll(
-                () -> assertThat(before).isEmpty(),
-                () -> assertThat(after).isPresent()
-        );
-    }
-
-    @DisplayName("역을 삭제할 때 역이 존재하지 않을 경우 예외가 발생한다.")
-    @Test
-    void throwExceptionWhenStationIsNotExists() {
-        // given
-        lineDao.save("2", "초록");
-
-        final DeleteStationRequest deleteStationRequest = new DeleteStationRequest("없는역", "2");
-
-        // expect
-        assertThatThrownBy(() -> stationService.deleteStationByStationNameAndLineName(deleteStationRequest))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("역을 삭제할 때 노선이 존재하지 않을 경우 예외가 발생한다.")
-    @Test
-    void throwExceptionWhenLineIsNotExists() {
-        // given
-        stationDao.save("잠실");
-
-        final DeleteStationRequest deleteStationRequest = new DeleteStationRequest("잠실", "없는노선");
-
-        // expect
-        assertThatThrownBy(() -> stationService.deleteStationByStationNameAndLineName(deleteStationRequest))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("역을 삭제한다.")
-    @Test
-    void deleteStationByStationNameAndLineName() {
-        // given
-        final Long upStationId = stationDao.save("잠실");
-        final Long downStationId = stationDao.save("잠실새내");
-        final Long lineId = lineDao.save("2", "초록");
-        stationLineDao.save(upStationId, lineId);
-        stationLineDao.save(downStationId, lineId);
-        sectionDao.save(upStationId, downStationId, lineId, true, new Distance(10));
-
-        final DeleteStationRequest deleteStationRequest = new DeleteStationRequest("잠실", "2");
-
-        // when
-        stationService.deleteStationByStationNameAndLineName(deleteStationRequest);
-
-        final Optional<Station> maybeStation = stationDao.findByName("잠실");
-
-        // then
-        assertThat(maybeStation).isEmpty();
+    void 역을_삭제한다() {
+        //given
+        Long stationId = 1L;
+        Station station = new Station(stationId, "테스트");
+        when(stationRepository.findById(stationId)).thenReturn(station);
+        //when
+        stationService.removeStation(stationId);
+        //then
+        verify(stationRepository, times(1)).delete(station);
     }
 }
-*/
