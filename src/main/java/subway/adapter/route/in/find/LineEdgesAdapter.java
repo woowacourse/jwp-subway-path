4ec5155adcbad33c03a268c4806411c80b3b7aef
Be-student <presentsong@naver.com>
2023-05-19 23:35:28 +0900

    feat: 모든 구간을 경로 도메인 모델로 변경해주는 어뎁터 구현

---
+++ src/main/java/subway/adapter/route/in/find/LineEdgesAdapter.java
@@ -0,0 +1,45 @@
+package subway.adapter.route.in.find;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import subway.application.line.port.in.InterStationResponseDto;
+import subway.application.line.port.in.LineFindAllUseCase;
+import subway.application.line.port.in.LineResponseDto;
+import subway.application.route.port.in.find.RouteAllEdgesUseCase;
+import subway.domain.route.Graph;
+import subway.domain.route.InterStationEdge;
+
+@Component
+public class LineEdgesAdapter implements RouteAllEdgesUseCase {
+
+    private final LineFindAllUseCase lineFindAllUseCase;
+
+    public LineEdgesAdapter(final LineFindAllUseCase lineFindAllUseCase) {
+        this.lineFindAllUseCase = lineFindAllUseCase;
+    }
+
+    @Override
+    public Graph findAllEdges() {
+        final List<LineResponseDto> allLines = lineFindAllUseCase.findAllLines();
+        final List<InterStationEdge> edges = new ArrayList<>();
+        for (final LineResponseDto lineResponseDto : allLines) {
+            addEdges(edges, lineResponseDto);
+        }
+        return new Graph(edges);
+    }
+
+    private void addEdges(final List<InterStationEdge> edges, final LineResponseDto lineResponseDto) {
+        final List<InterStationResponseDto> interStationResponseDtos =
+                lineResponseDto.getInterStations();
+        for (final InterStationResponseDto interStationResponseDto : interStationResponseDtos) {
+            final InterStationEdge edge = createEdge(interStationResponseDto, lineResponseDto.getId());
+            edges.add(edge);
+        }
+    }
+
+    private InterStationEdge createEdge(final InterStationResponseDto responseDto, final long lineId) {
+        return new InterStationEdge(responseDto.getUpStationId(), responseDto.getDownStationId(),
+                responseDto.getDistance(), lineId);
+    }
+}
