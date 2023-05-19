53eb3d2b04c02f5c4f4fde039627a5b28ea8a774
wugawuga <dnr3655@naver.com>
2023-05-19 19:40:36 +0900

    refactor: 거리 최대 제한 삭제

--- src/test/java/subway/domain/DistanceTest.java
+++ src/test/java/subway/domain/DistanceTest.java
@@ -15,7 +15,7 @@ class DistanceTest {
     }
 
     @ParameterizedTest(name = "두 역 사이 거리는 10km 이하 양의 정수만 가능하다.")
-    @ValueSource(ints = {-1, 0, 11})
+    @ValueSource(ints = {-1, 0, -10})
     void createDistanceFailTestByRange(int distance) {
         assertThatThrownBy(() -> new Distance(distance))
                 .isInstanceOf(GlobalException.class);
