ac9382c26a632b7d597decc5b447c88f6d93bd5a
woo-chang <clllickme@naver.com>
2023-05-19 15:24:55 +0900

    refactor: 행위에 대한 의미 부여를 위한 메서드 추출

--- src/main/java/subway/domain/fare/DistancePolicy.java
+++ src/main/java/subway/domain/fare/DistancePolicy.java
@@ -6,17 +6,11 @@ public enum DistancePolicy {
 
     BASE_FIFTY(50, distance -> {
         final long additionDistance = distance - 50;
-        if (additionDistance <= 0) {
-            return 0L;
-        }
-        return (long) ((Math.ceil((additionDistance - 1) / 8) + 1) * 100);
+        return calculateDistanceFarePerStep(additionDistance, 8);
     }),
     BASE_TEN(10, distance -> {
         final long additionDistance = Math.min(BASE_FIFTY.base, distance) - 10;
-        if (additionDistance <= 0) {
-            return 0L;
-        }
-        return (long) ((Math.ceil((additionDistance - 1) / 5) + 1) * 100);
+        return calculateDistanceFarePerStep(additionDistance, 5);
     });
 
     private final int base;
@@ -25,6 +19,13 @@ public enum DistancePolicy {
     DistancePolicy(final int base, final Function<Long, Long> policy) {
         this.base = base;
         this.policy = policy;
+    }
+
+    private static long calculateDistanceFarePerStep(final long distance, final int step) {
+        if (distance <= 0) {
+            return 0L;
+        }
+        return (long) ((Math.ceil((distance - 1) / step) + 1) * 100);
     }
 
     public long calculateAdditionFare(final long distance) {
