67a6e05bdd23f1c35266ffce9bfefae4fb34756b
nuyh <hjaehyun25@gmail.com>
2023-05-20 01:08:04 +0900

    test: 최단 경로 찾기 테스트 추가

--- src/test/java/subway/domain/ShortestTest.java
+++ src/test/java/subway/domain/ShortestTest.java
@@ -60,6 +60,51 @@ class ShortestTest {
                     () -> assertThat(found.toList()).hasSize(3));
         }
 
+        /**
+         * 광안 <-5-> 전포 <-5-> 노포
+         * 광안 <-2-> 부산 <-1-> 해운대 <-1-> 노포
+         * |
+         * 1
+         * |
+         * 다대포 <-1-> 노포
+         * expect: 2
+         */
+        @DisplayName("여러 경로 중 최단 경로를 구할 수 있다")
+        @Test
+        void findShortest2() {
+            //given
+            final Station source = new Station("광안");
+            final Station target = new Station("노포");
+
+            final Station station1 = new Station("전포");
+            final Station station2 = new Station("부산");
+            final Station station3 = new Station("해운대");
+            final Station station4 = new Station("다대포");
+
+            final Path path1 = new Path(source, station1, 5);
+            final Path path2 = new Path(station1, target, 5);
+
+            final Path path3 = new Path(source, station2, 2);
+            final Path path4 = new Path(station2, station3, 1);
+            final Path path5 = new Path(station3, target, 1);
+
+            final Path path6 = new Path(source, station4, 1);
+            final Path path7 = new Path(station4, target, 1);
+
+            final Paths paths1 = new Paths(List.of(path1, path2));
+            final Paths paths2 = new Paths(List.of(path3, path4, path5));
+            final Paths paths3 = new Paths(List.of(path6, path7));
+            final Shortest shortest = Shortest.from(List.of(paths1, paths2, paths3));
+
+            //when
+            final Paths found = shortest.findShortest(source, target);
+
+            //then
+            assertAll(
+                    () -> assertThat(found.getTotalDistance()).isEqualTo(2),
+                    () -> assertThat(found.toList()).hasSize(2));
+        }
+
         @DisplayName("존재하지 않는 경로로 최단 경로를 조회하면 빈 경로가 반환된다")
         @Test
         void findShortest_fail() {
