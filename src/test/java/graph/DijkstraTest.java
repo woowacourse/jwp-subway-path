fbf03812ce547258ba4c5587e467bb263e4f4982
be-student <presentsong@naver.com>
2023-05-19 17:49:54 +0900

    test: 다익스트라의 같은 정점을 추가해도 문제가 없다

--- src/test/java/graph/DijkstraTest.java
+++ src/test/java/graph/DijkstraTest.java
@@ -47,4 +47,24 @@ class DijkstraTest {
 
         assertThat(shortestPath).hasSize(3);
     }
+
+    @Test
+    void 같은_정점을_여러번_더해도_문제_없다() {
+        final WeightedMultigraph<Integer, DefaultWeightedEdge> graph
+                = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.setEdgeWeight(graph.addEdge(1, 2), 2);
+        graph.setEdgeWeight(graph.addEdge(2, 3), 2);
+        graph.setEdgeWeight(graph.addEdge(1, 3), 100);
+
+        final DijkstraShortestPath<Integer, DefaultWeightedEdge> dijkstraShortestPath
+                = new DijkstraShortestPath<>(graph);
+        final List<Integer> shortestPath
+                = dijkstraShortestPath.getPath(3, 1).getVertexList();
+
+        assertThat(shortestPath).hasSize(3);
+    }
 }
