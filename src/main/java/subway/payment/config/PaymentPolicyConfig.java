ef0434c827f7323873061ad722df1c4fc17aee53
shin-mallang <huipulci1@naver.com>
2023-05-19 21:12:45 +0900

    refactor: PaymentPolicyConfig에서 등록되는 빈에 @Primary

--- src/main/java/subway/payment/config/PaymentPolicyConfig.java
+++ src/main/java/subway/payment/config/PaymentPolicyConfig.java
@@ -3,14 +3,16 @@ package subway.payment.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import subway.path.domain.PaymentPolicy;
 import subway.payment.domain.payment.PaymentPolicyComposite;
 
 @Configuration
 public class PaymentPolicyConfig {
 
+    @Primary
     @Bean
-    public PaymentPolicy paymentPolicy(List<PaymentPolicy> paymentPolicies) {
+    public PaymentPolicy paymentPolicy(final List<PaymentPolicy> paymentPolicies) {
         return new PaymentPolicyComposite(paymentPolicies);
     }
 }
