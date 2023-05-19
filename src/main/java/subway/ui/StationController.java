a40632e670f1fce9442ea788dbf2c66c917ccd0f
pilyang <30036534+pilyang@users.noreply.github.com>
2023-05-19 23:58:28 +0900

    refactor: 역생성 api response body타입 수정

--- src/main/java/subway/ui/StationController.java
+++ src/main/java/subway/ui/StationController.java
@@ -1,16 +1,15 @@
 package subway.ui;
 
-import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import subway.ui.dto.StationRequest;
-import subway.ui.dto.StationResponse;
 import subway.service.StationService;
+import subway.ui.dto.StationRequest;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/stations")
@@ -23,7 +22,7 @@ public class StationController {
     }
 
     @PostMapping
-    public ResponseEntity<StationResponse> createStation(@Valid @RequestBody final StationRequest stationRequest) {
+    public ResponseEntity<Void> createStation(@Valid @RequestBody final StationRequest stationRequest) {
         Long stationId = stationService.create(stationRequest.getName());
         return ResponseEntity.created(URI.create("/stations/" + stationId)).build();
     }
