3e7239ecf3b9463995883a179ce482622ef627bb
splitube <voicesplit@daum.net>
2023-05-19 10:57:33 +0900

    test: ShortestPathIntegrationTest 작성

---
+++ src/main/java/subway/domain/LineStation.java
@@ -0,0 +1,47 @@
+package subway.domain;
+
+import java.util.Objects;
+
+public class LineStation {
+
+    private Line line;
+    private Station station;
+
+    private LineStation(final Line line, final Station station) {
+        this.line = line;
+        this.station = station;
+    }
+
+    public static LineStation withNullLine(final Station station) {
+        return new LineStation(null, station);
+    }
+
+    public static LineStation of(final Line line, final Station station) {
+        return new LineStation(line, station);
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final LineStation that = (LineStation) o;
+        return Objects.equals(station, that.station);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(station);
+    }
+}
---
+++ src/main/java/subway/dto/ShortestPathResponse.java
@@ -0,0 +1,33 @@
+package subway.dto;
+
+import java.util.List;
+
+public class ShortestPathResponse {
+
+    private List<LineStationResponse> route;
+    private Integer totalDistance;
+    private Integer totalFare;
+
+    ShortestPathResponse() {
+
+    }
+
+    public ShortestPathResponse(final List<LineStationResponse> route, final Integer totalDistance,
+        final Integer totalFare) {
+        this.route = route;
+        this.totalDistance = totalDistance;
+        this.totalFare = totalFare;
+    }
+
+    public List<LineStationResponse> getRoute() {
+        return route;
+    }
+
+    public Integer getTotalDistance() {
+        return totalDistance;
+    }
+
+    public Integer getTotalFare() {
+        return totalFare;
+    }
+}
