f26bf161de9c724aba3678389906d3bdb98658be
Be-student <presentsong@naver.com>
2023-05-20 01:20:40 +0900

    feat: 역 추가 전략 구현

---
+++ src/main/java/subway/domain/interstation/add/AddInterStationPolicy.java
@@ -0,0 +1,29 @@
+package subway.domain.interstation.add;
+
+import java.util.Arrays;
+import java.util.List;
+import subway.domain.interstation.InterStation;
+import subway.domain.interstation.exception.InterStationsException;
+
+public enum AddInterStationPolicy {
+    ADD_FIRST(new AddFirstInterStationStrategy()),
+    ADD_LAST(new AddMiddleStationStrategy()),
+    ADD_MIDDLE(new AddLastInterStationStrategy());
+
+    private final AddInterStationStrategy addInterStationStrategy;
+
+    AddInterStationPolicy(final AddInterStationStrategy addInterStationStrategy) {
+        this.addInterStationStrategy = addInterStationStrategy;
+    }
+
+    public static AddInterStationStrategy of(final List<InterStation> interStations,
+            final Long upStationId,
+            final Long downStationId,
+            final Long newStationId) {
+        return Arrays.stream(values())
+                .map(it -> it.addInterStationStrategy)
+                .filter(it -> it.isSatisfied(interStations, upStationId, downStationId, newStationId))
+                .findAny()
+                .orElseThrow(() -> new InterStationsException("역을 추가할 수 없습니다"));
+    }
+}
