8d839e45dd55e0f89b878d59594a00864b1a3d66
Choi-JJunho <junho5336@gmail.com>
2023-05-19 14:16:33 +0900

    refactor: 요금 계산로직 수정 및 테스트 추가

--- src/main/java/subway/domain/FareCalculator.java
+++ src/main/java/subway/domain/FareCalculator.java
@@ -2,24 +2,28 @@ package subway.domain;
 
 public class FareCalculator {
 
-    private static final Integer DEFAULT_FARE = 1250;
+    private static final int DEFAULT_FARE = 1250;
+    private static final int DEFAULT_FARE_OVER_50KM = 2050;
+
+    private FareCalculator() {
+    }
 
     public static long calculate(int distance) {
         int result = DEFAULT_FARE;
-        if (distance >= 10) {
-            result += calculateOverFare(distance);
-        }
         if (distance >= 50) {
-            result += calculateOverFare2(distance);
+            result = DEFAULT_FARE_OVER_50KM;
+            result += calculateFareOver50(distance);
+        } else if (distance >= 10) {
+            result += calculateFareOver10(distance);
         }
         return result;
     }
 
-    private static long calculateOverFare(int distance) {
-        return (long) ((Math.ceil((distance - 1) / 5) + 1) * 100);
+    private static long calculateFareOver10(int distance) {
+        return (long) (((distance - 10) / 5) + 1) * 100;
     }
 
-    private static long calculateOverFare2(int distance) {
-        return (long) ((Math.ceil((distance - 1) / 8) + 1) * 100);
+    private static long calculateFareOver50(int distance) {
+        return (long) ((distance - 50) / 8) * 100;
     }
 }
