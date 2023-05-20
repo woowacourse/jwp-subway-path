5868c82ad49530e6e1e5ab73b4b808ddb2f39ca1
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-20 21:23:04 +0900

    feat: 거리에 대해 요금을 계산하는 기능 구현

---
+++ src/main/java/subway/domain/Fare.java
@@ -0,0 +1,37 @@
+package subway.domain;
+
+public enum Fare {
+
+    BASIC(1, 10, 10, 1250),
+    MEDIUM_DISTANCE_ADDITION(11, 50, 5, 100),
+    LONG_DISTANCE_ADDITION(51, Integer.MAX_VALUE, 8, 100);
+
+    private final int lowerBoundDistance;
+    private final int upperBoundDistance;
+    private final int interval;
+    private final int additionalCost;
+
+    Fare(int lowerBoundDistance, int upperBoundDistance, int interval, int additionalCost) {
+        this.lowerBoundDistance = lowerBoundDistance;
+        this.upperBoundDistance = upperBoundDistance;
+        this.interval = interval;
+        this.additionalCost = additionalCost;
+    }
+
+
+    public static int getFare(int distance) {
+        int totalFare = 0;
+        for (Fare fare : values()) {
+            totalFare += fare.calculateFare(distance);
+        }
+        return totalFare;
+    }
+
+    private int calculateFare(int distance) {
+        if (lowerBoundDistance > distance) {
+            return 0;
+        }
+        int limitedDistance = Integer.min(upperBoundDistance, distance);
+        return ((limitedDistance - lowerBoundDistance) / interval + 1) * additionalCost;
+    }
+}
