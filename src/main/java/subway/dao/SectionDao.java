c45af2d8471cc2a98a517179eb98fcce6b1c3d1b
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-20 17:56:49 +0900

    refactor: section 객체 내 Line 필드 추가 및 dao 조회 기능에 반영

--- src/main/java/subway/dao/SectionDao.java
+++ src/main/java/subway/dao/SectionDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import subway.domain.Distance;
+import subway.domain.Line;
 import subway.domain.Section;
 import subway.domain.Station;
 
@@ -15,6 +16,7 @@ public class SectionDao {
 
     private RowMapper<Section> rowMapper = (rs, rowNum) ->
             new Section(
+                    new Line(rs.getLong("line_id"), rs.getString("line_name"), rs.getString("line_color")),
                     new Station(rs.getLong("left_station_id"), rs.getString("left_station_name")),
                     new Station(rs.getLong("right_station_id"), rs.getString("right_station_name")),
                     new Distance(rs.getInt("distance"))
@@ -31,32 +33,41 @@ public class SectionDao {
 
     public List<Section> findAll() {
         String sql =
-                "select left_station_id, left_st.name as left_station_name, right_station_id, right_st.name as right_station_name, distance from SECTIONS as se"
+                "select left_station_id, left_st.name as left_station_name, right_station_id, right_st.name as right_station_name, "
+                        + " se.line_id as line_id, line.name as line_name, line.color as line_color, distance from SECTIONS as se"
                         + " LEFT JOIN STATION as left_st"
                         + " ON se.left_station_id = left_st.id"
                         + " LEFT JOIN STATION as right_st"
-                        + " ON se.right_station_id = right_st.id";
+                        + " ON se.right_station_id = right_st.id"
+                        + " LEFT JOIN LINE as line" +
+                          " ON se.line_id = line.id";
         return jdbcTemplate.query(sql, rowMapper);
     }
 
     public List<Section> findByLineId(final Long lineId) {
         String sql =
-                "select left_station_id, left_st.name as left_station_name, right_station_id, right_st.name as right_station_name, distance from SECTIONS as se"
+                "select left_station_id, left_st.name as left_station_name, right_station_id, right_st.name as right_station_name, "
+                        + " se.line_id as line_id, line.name as line_name, line.color as line_color, distance from SECTIONS as se"
                         + " LEFT JOIN STATION as left_st"
                         + " ON se.left_station_id = left_st.id"
                         + " LEFT JOIN STATION as right_st"
                         + " ON se.right_station_id = right_st.id"
+                        + " LEFT JOIN LINE as line"
+                        + " ON se.line_id = line.id"
                         + " WHERE line_id = ?";
         return jdbcTemplate.query(sql, rowMapper, lineId);
     }
 
     public Section findById(final Long id) {
         String sql =
-                "select left_station_id, left_st.name as left_station_name, right_station_id, right_st.name as right_station_name, distance from SECTIONS as se"
+                "select left_station_id, left_st.name as left_station_name, right_station_id, right_st.name as right_station_name, "
+                        + " se.line_id as line_id, line.name as line_name, line.color as line_color, distance from SECTIONS as se"
                         + " LEFT JOIN STATION as left_st"
                         + " ON se.left_station_id = left_st.id"
                         + " LEFT JOIN STATION as right_st"
                         + " ON se.right_station_id = right_st.id"
+                        + " LEFT JOIN LINE as line"
+                        + " ON se.line_id = line.id"
                         + " WHERE se.id = ?";
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
--- src/main/java/subway/domain/Section.java
+++ src/main/java/subway/domain/Section.java
@@ -7,13 +7,23 @@ import java.util.Objects;
 
 public class Section {
 
-    public static final String INVALID_SECTION_MESSAGE = "동일한 역 간 구간을 생성할 수 없습니다.";
-    private final Station left;
-    private final Station right;
-    private final Distance distance;
+    private static final String INVALID_SECTION_MESSAGE = "동일한 역 간 구간을 생성할 수 없습니다.";
+
+    private Line line;
+    private Station left;
+    private Station right;
+    private Distance distance;
 
     public Section(final Station left, final Station right, final Distance distance) {
         validate(left, right);
+        this.left = left;
+        this.right = right;
+        this.distance = distance;
+    }
+
+    public Section(Line line, final Station left, final Station right, final Distance distance) {
+        validate(left, right);
+        this.line = line;
         this.left = left;
         this.right = right;
         this.distance = distance;
@@ -23,6 +33,10 @@ public class Section {
         if (left.equals(right)) {
             throw new SubwayServiceException(INVALID_SECTION_MESSAGE);
         }
+    }
+
+    public Line getLine() {
+        return line;
     }
 
     public Station getLeft() {
@@ -45,23 +59,17 @@ public class Section {
         return right.getId();
     }
 
-
     @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Section section = (Section) o;
-        return Objects.equals(left, section.left) && Objects.equals(right, section.right)
-                && Objects.equals(distance, section.distance);
+        return Objects.equals(line, section.line) && Objects.equals(left, section.left) && Objects.equals(right, section.right) && Objects.equals(distance, section.distance);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(left, right, distance);
+        return Objects.hash(line, left, right, distance);
     }
 
     @Override
