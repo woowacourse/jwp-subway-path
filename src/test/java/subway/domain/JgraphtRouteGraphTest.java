e53adc0144fa70d2268f5ed82de4d6a9c445fa9b
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-20 15:28:32 +0900

    test: 환승을 이용한 최단 경로, 거리 테스트 추가

--- src/test/java/subway/domain/JgraphtRouteGraphTest.java
+++ src/test/java/subway/domain/JgraphtRouteGraphTest.java
@@ -2,6 +2,7 @@ package subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static subway.fixture.StationFixture.강남역;
 import static subway.fixture.StationFixture.남영역;
 import static subway.fixture.StationFixture.사당역;
 import static subway.fixture.StationFixture.서울역;
@@ -29,35 +30,39 @@ class JgraphtRouteGraphTest {
     @Test
     @DisplayName("출발역과 도착역 까지 최단 경로를 찾는다.")
     void findShortedRoute1() {
-        Station start = 남영역;
-        Station end = 선릉역;
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        List<Station> shortestRoute = jgraphtRouteGraph.findShortestRoute(start, end);
+        List<Station> shortestRoute = jgraphtRouteGraph.findShortestRoute(남영역, 선릉역);
 
-        assertThat(shortestRoute).containsExactly(start, 서울역, 시청역, end);
+        assertThat(shortestRoute).containsExactly(남영역, 서울역, 시청역, 선릉역);
     }
 
     @Test
-    @DisplayName("출발역과 도착역 까지 최단 경로를 찾는다.")
-    void findShortedRoute2() {
-        Station start = 사당역;
-        Station end = 종각역;
+    @DisplayName("출발역과 도착역 까지 환승하여 최단 경로를 찾는다.")
+    void findShortedRouteWithTransfer() {
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        List<Station> shortestRoute = jgraphtRouteGraph.findShortestRoute(start, end);
+        List<Station> shortestRoute = jgraphtRouteGraph.findShortestRoute(사당역, 종각역);
 
-        assertThat(shortestRoute).containsExactly(start, 용산역, 시청역, end);
+        assertThat(shortestRoute).containsExactly(사당역, 용산역, 시청역, 종각역);
     }
 
     @Test
     @DisplayName("출발역과 도착역 까지 최단 거리를 찾는다.")
     void findShortedDistance() {
-        Station start = 남영역;
-        Station end = 선릉역;
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        Distance distance = jgraphtRouteGraph.findShortestDistance(start, end);
+        Distance distance = jgraphtRouteGraph.findShortestDistance(남영역, 선릉역);
+
+        assertThat(distance.getValue()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역 까지 환승하여 최단 거리를 찾는다.")
+    void findShortedDistanceWithTransfer() {
+        JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
+
+        Distance distance = jgraphtRouteGraph.findShortestDistance(강남역, 선릉역);
 
         assertThat(distance.getValue()).isEqualTo(20);
     }
@@ -65,36 +70,27 @@ class JgraphtRouteGraphTest {
     @Test
     @DisplayName("출발역이 전체 경로의 역에 존재하지 않는 경우 예외가 발생한다.")
     void findShortestRouteFailWithWrongStart() {
-        Station start = 역삼역;
-        Station end = 선릉역;
-
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(start, end))
+        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(역삼역, 선릉역))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("도착역이 전체 경로의 역에 존재하지 않는 경우 예외가 발생한다.")
     void findShortestRouteFailWithWrongEnd() {
-        Station start = 선릉역;
-        Station end = 회현역;
-
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(start, end))
+        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(선릉역, 회현역))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("출발역과 도착역 모두 전체 경로의 역에 존재하지 않는 경우 예외가 발생한다.")
     void findShortestRouteFailWithWrongStations() {
-        Station start = 서초역;
-        Station end = 회현역;
-
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(start, end))
+        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(서초역, 회현역))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -106,12 +102,11 @@ class JgraphtRouteGraphTest {
 
         JgraphtRouteGraph jgraphtRouteGraph = createRouteMap();
 
-        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(start, end))
+        assertThatThrownBy(() -> jgraphtRouteGraph.findShortestRoute(신사역, 서울역))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(start.getName() + "과 " + end.getName() + " 사이의 경로가 존재하지 않습니다.");
     }
-
-
+    
     private static JgraphtRouteGraph createRouteMap() {
         Line 일호선 = LineFixture.일호선;
         Line 이호선 = LineFixture.이호선;
