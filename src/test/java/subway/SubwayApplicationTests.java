25682ec79be4472e4d62212f8cbc99ef1380f062
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-20 17:01:38 +0900

    refactor: test db와 프로덕션 db 분리

--- src/test/java/subway/SubwayApplicationTests.java
+++ src/test/java/subway/SubwayApplicationTests.java
@@ -2,7 +2,12 @@ package subway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import subway.config.TestConfiguration;
 
+@ActiveProfiles("test")
+@ContextConfiguration(classes = TestConfiguration.class)
 @SpringBootTest
 class SubwayApplicationTests {
 
--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -2,15 +2,25 @@ package subway.integration;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import subway.config.TestConfiguration;
+
+import javax.sql.DataSource;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class IntegrationTest {
     @LocalServerPort
     int port;
+
+    @Autowired
+    DataSource dataSource;
 
     @BeforeEach
     public void setUp() {
