ebb734253806bde4114b43d9a960c5548872a80c
echo <echo.backend@gmail.com>
2023-05-20 21:06:55 +0900

    refactor: 서비스 메서드에 @Transactional 추가

---
+++ src/test/java/subway/application/StationServiceTest.java
@@ -0,0 +1,69 @@
+package subway.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+import subway.dao.StationDao;
+import subway.dto.StationRequest;
+import subway.dto.StationResponse;
+
+@JdbcTest
+@Import({StationService.class, StationDao.class})
+class StationServiceTest {
+    
+    @Autowired
+    private StationService stationService;
+    
+    @Test
+    @Transactional
+    @DisplayName("역 저장 테스트")
+    void saveStationTest() {
+        // given
+        final int initialSize = this.stationService.findAllStationResponses().size();
+        final StationRequest stationRequest = new StationRequest("테스트역");
+        // when
+        final StationResponse stationResponse = this.stationService.saveStation(stationRequest);
+        // then
+        final int finalSize = this.stationService.findAllStationResponses().size();
+        assertThat(stationResponse).isNotNull();
+        assertThat(stationResponse.getName()).isEqualTo(stationRequest.getName());
+        System.out.println(stationResponse.getId());
+        assertThat(finalSize).isEqualTo(initialSize + 1);
+    }
+    
+    @Test
+    @Transactional
+    @DisplayName("역 수정 테스트")
+    void updateStationTest() {
+        // given
+        final StationRequest stationRequest = new StationRequest("테스트역");
+        final StationResponse stationResponse = this.stationService.saveStation(stationRequest);
+        // when
+        this.stationService.updateStation(stationResponse.getId(), new StationRequest("수정역"));
+        // then
+        final StationResponse updatedStationResponse = this.stationService.findStationResponseById(
+                stationResponse.getId());
+        assertThat(updatedStationResponse).isNotNull();
+        assertThat(updatedStationResponse.getName()).isEqualTo("수정역");
+    }
+    
+    @Test
+    @Transactional
+    @DisplayName("역 삭제 테스트")
+    void deleteStationTest() {
+        // given
+        final StationRequest stationRequest = new StationRequest("테스트역");
+        final StationResponse stationResponse = this.stationService.saveStation(stationRequest);
+        final int initialSize = this.stationService.findAllStationResponses().size();
+        // when
+        this.stationService.deleteStationById(stationResponse.getId());
+        // then
+        final int finalSize = this.stationService.findAllStationResponses().size();
+        assertThat(finalSize).isEqualTo(initialSize - 1);
+    }
+}
