3a36b797fbf4b8460b14e80754501afe6c10212d
jiwon <ljwon77@daum.net>
2023-05-20 15:53:18 +0900

    refactor: 요금 할인 정책에 대한 클래스 분리

---
+++ src/main/java/subway/domain/fare/discount/TeenagerFarePolicy.java
@@ -0,0 +1,16 @@
+package subway.domain.fare.discount;
+
+import subway.domain.fare.Fare;
+
+public class TeenagerFarePolicy implements DiscountFarePolicy {
+
+    private static final int TEENAGER_DISCOUNT_RATE = 20;
+    private static final Fare TAX = new Fare(350);
+
+    @Override
+    public Fare calculateFare(final Fare basicFare) {
+        final Fare deductedFare = basicFare.subtract(TAX);
+        final double discountPercentage = (100 - TEENAGER_DISCOUNT_RATE) * 0.01;
+        return deductedFare.multiply(new Fare(discountPercentage));
+    }
+}
