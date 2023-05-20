55102c1979f94ddedab4ebd0204fd4ec92c5cc6c
apptie <appti@naver.com>
2023-05-20 22:34:51 +0900

    chore: 테스트 환경 변경

--- src/test/java/subway/SubwayApplicationTests.java
+++ src/test/java/subway/SubwayApplicationTests.java
@@ -2,9 +2,7 @@ package subway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class SubwayApplicationTests {
 
