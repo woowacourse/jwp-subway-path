a35c27c0fd79de4ad8babe672ca4ddac412154de
ezzanzzan <cjsdmswjd821@naver.com>
2023-05-22 03:32:14 +0900

    test: StationStep 불필요한 코드 제거

--- src/test/java/subway/integration/step/StationStep.java
+++ src/test/java/subway/integration/step/StationStep.java
@@ -1,15 +1,13 @@
 package subway.integration.step;
 
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 import subway.dto.StationCreateRequest;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class StationStep {
-    public static Long 역을_생성한다(StationCreateRequest stationCreateRequest) {
-        ExtractableResponse<Response> response = RestAssured
+    public static void 역을_생성한다(StationCreateRequest stationCreateRequest) {
+        RestAssured
                 .given().log().all()
                 .body(stationCreateRequest)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -17,8 +15,5 @@ public class StationStep {
                 .post("/stations")
                 .then().log().all()
                 .extract();
-
-        String location = response.header("Location");
-        return Long.parseLong(location.substring(location.lastIndexOf("/") + 1));
     }
 }
