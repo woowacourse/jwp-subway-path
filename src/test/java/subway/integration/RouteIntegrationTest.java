15acec4e78d6d3142a914a665734f9e14a6f8f18
xxeol2 <jhj01177@naver.com>
2023-05-22 01:24:39 +0900

    test: 경로 조회 통합테스트 작성

---
+++ src/test/java/subway/integration/RouteIntegrationTest.java
@@ -0,0 +1,168 @@
+package subway.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import io.restassured.RestAssured;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import subway.adapter.out.persistence.LineJdbcAdapter;
+import subway.adapter.out.persistence.StationJdbcAdapter;
+import subway.application.port.in.route.dto.response.RouteQueryResponse;
+import subway.domain.Line;
+import subway.domain.LineInfo;
+import subway.domain.Section;
+import subway.domain.Station;
+import subway.fixture.StationFixture.강남역;
+import subway.fixture.StationFixture.고터역;
+import subway.fixture.StationFixture.교대역;
+import subway.fixture.StationFixture.논현역;
+import subway.fixture.StationFixture.반포역;
+import subway.fixture.StationFixture.방배역;
+import subway.fixture.StationFixture.서초역;
+import subway.fixture.StationFixture.신논현역;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+public class RouteIntegrationTest extends IntegrationTest {
+
+    @Autowired
+    private LineJdbcAdapter lineJdbcAdapter;
+
+    @Autowired
+    private StationJdbcAdapter stationJdbcAdapter;
+
+    @Test
+    void 최단경로를_조회한다() {
+        // given
+        long 방배역Id = stationJdbcAdapter.create(방배역.STATION);
+        long 서초역Id = stationJdbcAdapter.create(서초역.STATION);
+        long 교대역Id = stationJdbcAdapter.create(교대역.STATION);
+        long 강남역Id = stationJdbcAdapter.create(강남역.STATION);
+        long 고터역Id = stationJdbcAdapter.create(고터역.STATION);
+        long 신논현역Id = stationJdbcAdapter.create(신논현역.STATION);
+        long 논현역Id = stationJdbcAdapter.create(논현역.STATION);
+        long 반포역Id = stationJdbcAdapter.create(반포역.STATION);
+
+        long 이호선Id = lineJdbcAdapter.create(new LineInfo("2호선", "GREEN"));
+        long 삼호선Id = lineJdbcAdapter.create(new LineInfo("3호선", "ORANGE"));
+        long 신분당선Id = lineJdbcAdapter.create(new LineInfo("신분당선", "RED"));
+        long 칠호선Id = lineJdbcAdapter.create(new LineInfo("7호선", "DARK_GREEN"));
+
+        lineJdbcAdapter.updateSections(new Line(이호선Id, "2호선", "GREEN", List.of(
+                new Section(new Station(방배역Id, "방배역"), new Station(서초역Id, "서초역"), 1),
+                new Section(new Station(서초역Id, "서초역"), new Station(교대역Id, "교대역"), 1),
+                new Section(new Station(교대역Id, "교대역"), new Station(강남역Id, "강남역"), 2)
+        )));
+
+        lineJdbcAdapter.updateSections(new Line(삼호선Id, "3호선", "GREEN", List.of(
+                new Section(new Station(교대역Id, "교대역"), new Station(고터역Id, "고터역"), 3)
+        )));
+
+        lineJdbcAdapter.updateSections(new Line(신분당선Id, "신분당선", "GREEN", List.of(
+                new Section(new Station(강남역Id, "강남역"), new Station(신논현역Id, "신논현역"), 3),
+                new Section(new Station(신논현역Id, "신논현역"), new Station(논현역Id, "논현역"), 4)
+        )));
+
+        lineJdbcAdapter.updateSections(new Line(칠호선Id, "7호선", "GREEN", List.of(
+                new Section(new Station(고터역Id, "고터역"), new Station(반포역Id, "반포역"), 3),
+                new Section(new Station(반포역Id, "반포역"), new Station(논현역Id, "논현역"), 2)
+        )));
+
+        // when
+        RouteQueryResponse response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("sourceStationId", 방배역Id)
+                .queryParam("targetStationId", 논현역Id)
+                .when().get("/route")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(RouteQueryResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(response.getRoute())
+                        .usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(List.of(
+                                방배역.RESPONSE, 서초역.RESPONSE, 교대역.RESPONSE, 고터역.RESPONSE, 반포역.RESPONSE, 논현역.RESPONSE
+                        )),
+                () -> assertThat(response.getDistance()).isEqualTo(10),
+                () -> assertThat(response.getFare()).isEqualTo(1250)
+        );
+    }
+
+    @Test
+    void 최단경로_조회시_거리가_10이상이면_추가요금이_발생한다() {
+        // given
+        long 방배역Id = stationJdbcAdapter.create(방배역.STATION);
+        long 서초역Id = stationJdbcAdapter.create(서초역.STATION);
+
+        long 이호선Id = lineJdbcAdapter.create(new LineInfo("2호선", "GREEN"));
+
+        lineJdbcAdapter.updateSections(new Line(이호선Id, "2호선", "GREEN", List.of(
+                new Section(new Station(방배역Id, "방배역"), new Station(서초역Id, "서초역"), 11)
+        )));
+
+        // when
+        RouteQueryResponse response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("sourceStationId", 방배역Id)
+                .queryParam("targetStationId", 서초역Id)
+                .when().get("/route")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(RouteQueryResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(response.getRoute())
+                        .usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(List.of(방배역.RESPONSE, 서초역.RESPONSE)),
+                () -> assertThat(response.getDistance()).isEqualTo(11),
+                () -> assertThat(response.getFare()).isEqualTo(1350)
+        );
+    }
+
+    @Test
+    void 최단경로_조회시_거리가_50초과이면_초과_추가요금이_발생한다() {
+        // given
+        long 방배역Id = stationJdbcAdapter.create(방배역.STATION);
+        long 서초역Id = stationJdbcAdapter.create(서초역.STATION);
+
+        long 이호선Id = lineJdbcAdapter.create(new LineInfo("2호선", "GREEN"));
+
+        lineJdbcAdapter.updateSections(new Line(이호선Id, "2호선", "GREEN", List.of(
+                new Section(new Station(방배역Id, "방배역"), new Station(서초역Id, "서초역"), 58)
+        )));
+
+        // when
+        RouteQueryResponse response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("sourceStationId", 방배역Id)
+                .queryParam("targetStationId", 서초역Id)
+                .when().get("/route")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(RouteQueryResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(response.getRoute())
+                        .usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(List.of(방배역.RESPONSE, 서초역.RESPONSE)),
+                () -> assertThat(response.getDistance()).isEqualTo(58),
+                () -> assertThat(response.getFare()).isEqualTo(2150)
+        );
+    }
+}
