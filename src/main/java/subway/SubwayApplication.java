5f33c388af7ee6d61a255a0e1326b984bb55a719
sosow0212 <sosow0212@naver.com>
2023-05-21 10:39:14 +0900

    refactor: 이벤트 초기화를 LifeCycle에서 관리하도록 변경

--- src/main/java/subway/SubwayApplication.java
+++ src/main/java/subway/SubwayApplication.java
@@ -1,23 +1,12 @@
 package subway;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import subway.event.RouteUpdateEvent;
-import subway.service.SubwayMapService;
 
 @SpringBootApplication
 public class SubwayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SubwayApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner initEvent(final SubwayMapService subwayMapService) {
-        return args -> {
-            subwayMapService.updateRoute(new RouteUpdateEvent());
-        };
     }
 }
