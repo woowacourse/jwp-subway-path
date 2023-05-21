83f0308ff1b64c5436b95af774b2612f7c7b729c
kiarakim <kiarakim413@gmail.com>
2023-05-22 04:16:57 +0900

    feat: 구간 도메인 변경 및 테스트 코드 작성

---
+++ src/test/java/subway/domain/subway/SectionTest.java
@@ -0,0 +1,37 @@
+package subway.domain.subway;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import subway.exception.InvalidSectionDistanceException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static subway.fixture.SectionFixture.createSection;
+
+class SectionTest {
+
+	@Test
+	@DisplayName("역이 존재하는지 확인하는 테스트")
+	void hasStation() {
+		// given
+		Section section = createSection();
+
+		// when
+		boolean result = section.hasStation(new Station("삼성역"));
+
+		// then
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	@DisplayName("요청한 역 사이의 거리가 현재 거리보다 같거나 길면 예외가 발생한다")
+	void exception_whenInvalidDistance() {
+		// given
+		Section section = createSection();
+		long requestDistance = 10L;
+
+		// then
+		assertThatThrownBy(() -> section.validateSectionDistance(requestDistance))
+			.isInstanceOf(InvalidSectionDistanceException.class);
+	}
+}
