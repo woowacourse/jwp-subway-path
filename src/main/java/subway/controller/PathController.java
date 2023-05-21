39accf08e951a5ac076db52642df8699c846c7df
iamjooon2 <iamjooon2@gmail.com>
2023-05-21 19:49:19 +0900

    refactor: 경로 조회 API 스펙을 Get으로 변경

--- src/main/java/subway/controller/PathController.java
+++ src/main/java/subway/controller/PathController.java
@@ -1,9 +1,8 @@
 package subway.controller;
 
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,7 @@ public class PathController {
         this.pathService = pathService;
     }
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<PathResponse> findPath(@RequestBody @Valid final ReadPathRequest pathRequest) {
         final PathResponse pathResponse = pathService.findPath(pathRequest.toDto());
         return ResponseEntity.ok(pathResponse);
