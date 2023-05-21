8d16a99b7e470b3fdb2019a52dac436215705f4c
Youngho <cyh6099@gmail.com>
2023-05-21 23:45:11 +0900

    feat: IllegalState, Exception핸들러 추가

--- src/main/java/subway/exception/GlobalExceptionHandler.java
+++ src/main/java/subway/exception/GlobalExceptionHandler.java
@@ -9,7 +9,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+    public ResponseEntity<ExceptionMessage> handleException(Exception e) {
+        System.out.println(e.getMessage());
+        ExceptionMessage exceptionMessage = new ExceptionMessage("현재 서버가 응답할 수 없습니다.");
+
+        return ResponseEntity.internalServerError().body(exceptionMessage);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionMessage> handleIllegalArgumentException(IllegalArgumentException e) {
         System.out.println(e.getMessage());
         ExceptionMessage exceptionMessage = new ExceptionMessage(e.getMessage());
 
@@ -17,9 +25,17 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleNotExistException(NotExistException e) {
+    public ResponseEntity<Void> handleNotExistException(NotExistException e) {
         System.out.println(e.getMessage());
 
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionMessage> handleIllegalStateException(IllegalStateException e) {
+        System.out.println(e.getMessage());
+        ExceptionMessage exceptionMessage = new ExceptionMessage(e.getMessage());
+
+        return ResponseEntity.badRequest().body(exceptionMessage);
     }
 }
