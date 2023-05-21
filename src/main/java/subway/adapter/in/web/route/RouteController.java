948b164c95deeb3d45a46cd6e5fe67e9839dcbc9
ingpyo <juninpyo@naver.com>
2023-05-21 17:15:45 +0900

    refactor: 입력 방식 수정

--- src/main/java/subway/adapter/in/web/route/RouteController.java
+++ src/main/java/subway/adapter/in/web/route/RouteController.java
@@ -2,12 +2,12 @@ package subway.adapter.in.web.route;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import subway.adapter.in.web.route.dto.FindShortCutRequest;
 import subway.application.dto.RouteResponse;
 import subway.application.port.in.route.FindRouteResultUseCase;
-import subway.adapter.in.web.route.dto.FindShortCutRequest;
 
 @RestController
 @RequestMapping("/stations/route")
@@ -20,7 +20,10 @@ public class RouteController {
     }
 
     @GetMapping
-    public ResponseEntity<RouteResponse> findResultRouteResult(@RequestBody FindShortCutRequest findShortCutRequest) {
+    public ResponseEntity<RouteResponse> findResultRouteResult(@RequestParam String fromStation,
+                                                               @RequestParam String toStation,
+                                                               @RequestParam Integer age) {
+        final FindShortCutRequest findShortCutRequest = new FindShortCutRequest(fromStation, toStation, age);
         return ResponseEntity.ok(findRouteResultUseCase.findRouteResult(findShortCutRequest));
     }
 }
