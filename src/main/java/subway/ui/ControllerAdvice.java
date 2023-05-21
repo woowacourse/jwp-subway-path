fcaee3a526c003aab166aa95510dad07ac23dc87
yujamint <woawoa0513@gmail.com>
2023-05-22 04:29:20 +0900

    refactor: 예외 계층화에 따른 핸들러 추가

--- src/main/java/subway/ui/ControllerAdvice.java
+++ src/main/java/subway/ui/ControllerAdvice.java
@@ -6,9 +6,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import subway.dto.ErrorResponse;
 import subway.exception.SubwayException;
+import subway.exception.line.LineException;
+import subway.exception.section.SectionException;
+import subway.exception.station.StationException;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(LineException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(StationException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(SectionException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(SubwayException e) {
