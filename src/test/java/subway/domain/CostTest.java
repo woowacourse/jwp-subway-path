3e65ea23b4e3f9d0d837fa7e3444844a64981b23
cpot5620 <=cpot5620@naver.com>
2023-05-21 13:16:13 +0900

    test: Cost 경계값 테스트 추가

--- src/test/java/subway/domain/CostTest.java
+++ src/test/java/subway/domain/CostTest.java
@@ -1,37 +1,53 @@
 package subway.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CostTest {
 
     @ParameterizedTest(name = "기본 운임(10km 이하) 요금은 1250원이다.")
-    @ValueSource(ints = {3, 5, 10})
+    @ValueSource(ints = {1, 3, 5, 10})
     void calculateDefaultCost(int distance) {
         Cost cost = new Cost(new Distance(distance));
 
         assertThat(cost.getCost()).isEqualTo(1250);
     }
 
-    @DisplayName("중거리 운행(10km ~ 50km)은 5km당 100원의 추가요금이 붙는다.")
-    @Test
-    void calculateMiddleDistanceCost() {
-        Cost cost = new Cost(new Distance(23));
+    @ParameterizedTest(name = "중거리 운행(10km ~ 50km)은 5km당 100원의 추가요금이 붙는다.")
+    @MethodSource("middleDistanceProvider")
+    void calculateMiddleDistanceCost(Distance distance, int expectedCost) {
+        Cost cost = new Cost(distance);
 
-        //기본 요금 1250 + 추가 요금 500
-        assertThat(cost.getCost()).isEqualTo(1750);
+        assertThat(cost.getCost()).isEqualTo(expectedCost);
     }
 
-    @DisplayName("장거리 운행(50km 초과)은 8km당 100원의 추가요금이 붙는다.")
-    @Test
-    void calculateLongDistanceCost() {
-        Cost cost = new Cost(new Distance(55));
+    static Stream<Arguments> middleDistanceProvider() {
+        return Stream.of(
+                Arguments.arguments(new Distance(11), 1550),
+                Arguments.arguments(new Distance(23), 1750),
+                Arguments.arguments(new Distance(50), 2250)
+        );
+    }
 
-        //기본 요금 1250 + 추가 요금 700
-        assertThat(cost.getCost()).isEqualTo(1950);
+    @ParameterizedTest(name = ("장거리 운행(50km 초과)은 8km당 100원의 추가요금이 붙는다."))
+    @MethodSource("longDistanceProvider")
+    void calculateLongDistanceCost(Distance distance, int expectedCost) {
+        Cost cost = new Cost(distance);
+
+        assertThat(cost.getCost()).isEqualTo(expectedCost);
+    }
+
+    static Stream<Arguments> longDistanceProvider() {
+        return Stream.of(
+                Arguments.arguments(new Distance(51), 1950),
+                Arguments.arguments(new Distance(100), 2550),
+                Arguments.arguments(new Distance(200), 3750)
+        );
     }
 }
