715b3cac03f8e86f0497607ec0a12e1a54dc926b
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-05-22 00:01:54 +0900

    refactor: 의미 없는 계층화 제거

--- src/main/java/subway/path/presentation/PathController.java
+++ src/main/java/subway/path/presentation/PathController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import subway.path.facade.PathFacade;
 import subway.path.presentation.dto.response.PathResponse;
+import subway.path.service.PathService;
 
 @RequestMapping("/paths")
 @RestController
 public class PathController {
 
-    private final PathFacade pathFacade;
+    private final PathService pathService;
 
-    public PathController(final PathFacade pathFacade) {
-        this.pathFacade = pathFacade;
+    public PathController(final PathService pathService) {
+        this.pathService = pathService;
     }
 
     @GetMapping
@@ -23,7 +23,7 @@ public class PathController {
             @RequestParam Long upStationId,
             @RequestParam Long downStationId
     ) {
-        PathResponse response = pathFacade.getByDijkstra(upStationId, downStationId);
+        PathResponse response = pathService.findByDijkstra(upStationId, downStationId);
         return ResponseEntity.ok().body(response);
     }
 
