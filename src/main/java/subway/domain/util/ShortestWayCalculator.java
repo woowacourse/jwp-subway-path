f9f4cf72ed4eb3eb16761672d24bfedc294af2ff
kevstevie <jianna442@gmail.com>
2023-05-22 15:56:48 +0900

    refactor: polishing

--- src/main/java/subway/domain/util/ShortestWayCalculator.java
+++ src/main/java/subway/domain/util/ShortestWayCalculator.java
@@ -18,14 +18,7 @@ public class ShortestWayCalculator {
         final List<Station> starts = new ArrayList<>();
         final List<Station> ends = new ArrayList<>();
         final List<Integer> distances = new ArrayList<>();
-        for (final Line line : lines) {
-            final Map<Station, Path> paths = line.getPaths();
-            for (final Map.Entry<Station, Path> entry : paths.entrySet()) {
-                starts.add(entry.getKey());
-                ends.add(entry.getValue().getNext());
-                distances.add(entry.getValue().getDistance());
-            }
-        }
+        mappingPath(lines, starts, ends, distances);
 
         final var result = dijkstra.shortestPath(starts, ends, distances, start, end);
 
@@ -33,6 +26,17 @@ public class ShortestWayCalculator {
             return new ShortestWay(((int) result.getWeight()), result.getVertexList());
         } catch (RuntimeException e) {
             throw new IllegalArgumentException("갈 수 없는 경로입니다.");
+        }
+    }
+
+    private static void mappingPath(final List<Line> lines, final List<Station> starts, final List<Station> ends, final List<Integer> distances) {
+        for (final Line line : lines) {
+            final Map<Station, Path> paths = line.getPaths();
+            for (final Map.Entry<Station, Path> entry : paths.entrySet()) {
+                starts.add(entry.getKey());
+                ends.add(entry.getValue().getNext());
+                distances.add(entry.getValue().getDistance());
+            }
         }
     }
 }
