f1ec4304108e105ceac683a1c0b9fb91b493a11b
SproutMj <mjk54565@gmail.com>
2023-05-22 16:20:58 +0900

    refactor: 의미있는 상수명으로 변경

--- src/main/java/subway/domain/path/CostCalculator.java
+++ src/main/java/subway/domain/path/CostCalculator.java
@@ -3,26 +3,26 @@ package subway.domain.path;
 public final class CostCalculator {
 
     private static final int DEFAULT_COST = 1250;
-    private static final int SECTION_1 = 10;
-    private static final int SECTION_2 = 50;
+    private static final int BASE_COST_DISTANCE = 10;
+    private static final int SECOND_EXTRA_COST_DISTANCE = 50;
     private static final int ADDITIONAL_COST = 100;
     private static final int DISTANCE_THRESHOLD_10KM_TO_50KM = 5;
     private static final int DISTANCE_THRESHOLD_OVER_50KM = 8;
 
 
     public static int calculateCost(int distance) {
-        if (distance < SECTION_1) {
+        if (distance < BASE_COST_DISTANCE) {
             return DEFAULT_COST;
         }
-        if (distance == SECTION_1) {
+        if (distance == BASE_COST_DISTANCE) {
             return DEFAULT_COST + ADDITIONAL_COST;
         }
-        if (distance <= SECTION_2) {
-            distance -= SECTION_1;
+        if (distance <= SECOND_EXTRA_COST_DISTANCE) {
+            distance -= BASE_COST_DISTANCE;
             return DEFAULT_COST + ((int)Math.ceil((double) distance/DISTANCE_THRESHOLD_10KM_TO_50KM) * ADDITIONAL_COST);
         }
-        int cost = DEFAULT_COST + ((int)Math.ceil(((double)SECTION_2-SECTION_1)/DISTANCE_THRESHOLD_10KM_TO_50KM) * ADDITIONAL_COST);
-        distance -= SECTION_2;
+        int cost = DEFAULT_COST + ((int)Math.ceil(((double) SECOND_EXTRA_COST_DISTANCE - BASE_COST_DISTANCE)/DISTANCE_THRESHOLD_10KM_TO_50KM) * ADDITIONAL_COST);
+        distance -= SECOND_EXTRA_COST_DISTANCE;
         return cost + ((int)Math.ceil((double) distance/DISTANCE_THRESHOLD_OVER_50KM) * ADDITIONAL_COST);
     }
 }
