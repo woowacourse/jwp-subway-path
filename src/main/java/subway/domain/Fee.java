853beb125ba474bd96e9597327beafcf6008b759
junpakPark <junpak.park@gmail.com>
2023-05-22 10:09:52 +0900

    feat : Fee 객체 구현

---
+++ src/main/java/subway/domain/Fee.java
@@ -0,0 +1,45 @@
+package subway.domain;
+
+public class Fee {
+
+	private static final int DEFAULT_FEE = 1250;
+	private static final int FIRST_DISTANCE_LIMIT = 10;
+	private static final int SECOND_DISTANCE_LIMIT = 50;
+	private static final int FIRST_UNIT_DISTANCE = 5;
+	private static final int SECOND_UNIT_DISTANCE = 8;
+	private static final int SURCHARGE = 100;
+
+	private final int fee;
+
+	private Fee(final int fee) {
+		this.fee = fee;
+	}
+
+	public static Fee Calculate(final Distance distance) {
+		final int distanceValue = distance.getValue();
+
+		if (distanceValue < FIRST_DISTANCE_LIMIT) {
+			return new Fee(DEFAULT_FEE);
+		}
+
+		if (distanceValue <= SECOND_DISTANCE_LIMIT) {
+			final int fee = DEFAULT_FEE
+				+ calculateOverFare(distanceValue - FIRST_DISTANCE_LIMIT, FIRST_UNIT_DISTANCE);
+			return new Fee(fee);
+		}
+
+		final int fee = DEFAULT_FEE
+			+ calculateOverFare(SECOND_DISTANCE_LIMIT - FIRST_DISTANCE_LIMIT, FIRST_UNIT_DISTANCE)
+			+ calculateOverFare(distanceValue - SECOND_DISTANCE_LIMIT, SECOND_UNIT_DISTANCE);
+		return new Fee(fee);
+
+	}
+
+	private static int calculateOverFare(int distance, int unitDistance) {
+		return (int)((Math.ceil((distance - 1) / unitDistance) + 1) * SURCHARGE);
+	}
+
+	public int getFee() {
+		return fee;
+	}
+}
---
+++ src/test/java/subway/domain/FeeTest.java
@@ -0,0 +1,51 @@
+package subway.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class FeeTest {
+
+	@Test
+	@DisplayName("10km 미만일때는 기본 요금이 부과된다.")
+	void calculateDefaultDistance() {
+		// given
+		final Distance distance = new Distance(9);
+
+		// when
+		final Fee fee = Fee.Calculate(distance);
+
+		// then
+		assertThat(fee.getFee()).isEqualTo(1250);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"12,1350", "16,1450"})
+	@DisplayName("10km 이상, 50km 이하일때는 5km 마다 추가 요금이 부과된다.")
+	void calculateFirstDistanceLimit(int distanceValue, int feeValue) {
+		// given
+		final Distance distance = new Distance(distanceValue);
+
+		// when
+		final Fee fee = Fee.Calculate(distance);
+
+		// then
+		assertThat(fee.getFee()).isEqualTo(feeValue);
+	}
+
+	@Test
+	@DisplayName("50km 초과일때는 8km 마다 추가 요금이 부과된다.")
+	void calculateSecondDistanceLimit() {
+		// given
+		final Distance distance = new Distance(58);
+
+		// when
+		final Fee fee = Fee.Calculate(distance);
+
+		// then
+		assertThat(fee.getFee()).isEqualTo(2150);
+	}
+}
