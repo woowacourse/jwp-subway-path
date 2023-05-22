99e4f93558c0c16f7d25d2da69aaf107c129d822
gitchan <eunkeeeee@gmail.com>
2023-05-22 17:32:54 +0900

    refactor(PathRequest): Request 객체 생성

--- src/main/java/subway/dto/PathRequest.java
+++ src/main/java/subway/dto/PathRequest.java
@@ -1,25 +1,35 @@
 package subway.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class PathRequest {
 
+    private static final int DEFAULT_AGE_VALUE = 19;
     @NotNull(message = "출발 역의 아이디를 입력해 주세요. 입력값 : ${validatedValue}")
-    private final Long sourceId;
+    private final Long source;
 
     @NotNull(message = "도착 역의 아이디를 입력해 주세요. 입력값 : ${validatedValue}")
-    private final Long targetId;
+    private final Long target;
 
-    public PathRequest(final Long sourceId, final Long targetId) {
-        this.sourceId = sourceId;
-        this.targetId = targetId;
+    @Min(value = 1, message = "나이는 최소 1살 이상이어야 합니다. 입력값 : ${validatedValue}")
+    private final Integer age;
+
+    public PathRequest(final Long source, final Long target, final Integer age) {
+        this.source = source;
+        this.target = target;
+        this.age = age != null ? age : DEFAULT_AGE_VALUE;
     }
 
-    public Long getSourceId() {
-        return sourceId;
+    public Long getSource() {
+        return source;
     }
 
-    public Long getTargetId() {
-        return targetId;
+    public Long getTarget() {
+        return target;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 }
