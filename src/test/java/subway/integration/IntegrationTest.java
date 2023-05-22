d9a1f8859ec116fbcb30d580140c69fbb2ea02ee
hanueleee <ezzeezz@naver.com>
2023-05-22 14:15:47 +0900

    refactor: 테스트 환경 수정

--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
-@Sql(value = "/delete.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = {"/delete.sql", "/schema.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
     @LocalServerPort
