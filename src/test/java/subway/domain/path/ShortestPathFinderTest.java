502e8f595511e158cd08857eb1f742b305593b63
hong-sile <gurwns9325@naver.com>
2023-05-22 16:53:11 +0900

    test: 동일한 구간이 있을 때 최단경로를 반환하는지 확인하는 테스트 코드 작성

--- src/test/java/subway/domain/path/ShortestPathFinderTest.java
+++ src/test/java/subway/domain/path/ShortestPathFinderTest.java
@@ -22,13 +22,26 @@ class ShortestPathFinderTest {
 
     @DisplayName("List<Section>과 출발점 도착점을 넣으면 최단 경로를 반환한다.")
     @Test
-    void findShortestPath() {
+    void findShortestPath1() {
         //강남 - 5 - 삼성 - 5 - 선릉
         //given,when
         final Path shortestPath = finder.findShortestPath(SECTIONS.getSections(), GANGNAM, SAMSUNG);
 
         assertThat(shortestPath.getStations())
                 .containsExactly(GANGNAM, SEONGLENUG, SAMSUNG);
+    }
+
+    @DisplayName("동일한 구간이 있을 때, 거리가 다른 경우 그 중 최단 경로를 반환한다.")
+    @Test
+    void findShortestPath2() {
+        final Section section1 = new Section(1L, JAMSIL, GANGNAM, new Distance(2));
+        final Section section2 = new Section(2L, JAMSIL, GANGNAM, new Distance(5));
+
+        final Path shortestPath = finder.findShortestPath(List.of(section1, section2), JAMSIL, GANGNAM);
+
+        assertThat(shortestPath.getSections())
+                .extracting(Section::getId)
+                .containsExactly(1L);
     }
 
     @DisplayName("출발지에서 도착지까지 이동할 수 있는 경로가 없는 경우 예외를 반환한다.")
