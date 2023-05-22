17854e2a8742d3825259290604cd38f94d32f732
yeonkkk <tjddus0302@gmail.com>
2023-05-22 16:17:57 +0900

    feat: PathController 구현

--- src/test/java/subway/fixture/PathRequestFixture.java
+++ src/test/java/subway/fixture/PathRequestFixture.java
@@ -4,4 +4,5 @@ import subway.dto.request.PathRequest;
 
 public class PathRequestFixture {
     public static final PathRequest PATH_REQUEST_1_3 = new PathRequest(1L, 3L);
+    public static final PathRequest PATH_REQUEST_1_7 = new PathRequest(1L, 7L);
 }
