c278a2618eb95c4c098f27acfa056cd1b2a7fbaa
waterricecake <fruturum@nate.com>
2023-05-22 15:08:07 +0900

    refactor: null 상황 방지

--- src/main/java/subway/ui/RouteController.java
+++ src/main/java/subway/ui/RouteController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import subway.application.RouteService;
 import subway.dto.RouteResponse;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @RestController
 @RequestMapping("/route")
 public class RouteController {
@@ -17,10 +20,10 @@ public class RouteController {
         this.routeService = routeService;
     }
 
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<RouteResponse> getShortestRoute(
-            @RequestParam("departure-id") long departureId,
-            @RequestParam("arrival-id") long arrivalId) {
+            @RequestParam("departure-id") @Valid @NotNull(message = "출발역을 입력해야합니다.") long departureId,
+            @RequestParam("arrival-id") @Valid @NotNull(message = "도착역을 입력해야합니다.")  long arrivalId) {
         RouteResponse routeResponse = routeService.findShortcut(departureId, arrivalId);
         return ResponseEntity.ok()
                 .body(routeResponse);
