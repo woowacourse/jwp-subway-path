daba0c550634766d4f0a579838538a567f724a66
mcodnjs <codnjs8989@gmail.com>
2023-05-22 14:31:58 +0900

    refactor: 요금 계산 로직 depth 리팩토링

--- src/main/java/subway/domain/fare/DistanceFareStrategy.java
+++ src/main/java/subway/domain/fare/DistanceFareStrategy.java
@@ -8,14 +8,10 @@ public class DistanceFareStrategy implements FareStrategy {
     public int calculateFare(final int distance) {
         int surcharge = 0;
         if (distance > 10) { // 10km 초과인 경우
-            if (distance <= 50) { // 10km~50km 이내인 경우
-                surcharge += (int) ((Math.floor((distance - 10 - 1) / 5.0) + 1) * 100);
-            }
-
-            if (distance > 50) {
-                surcharge = Math.min((int) ((Math.floor((distance - 10 - 1) / 5.0) + 1) * 100), 800);
-                surcharge += (int) ((Math.floor((distance - 50 - 1) / 8.0) + 1) * 100);
-            }
+            surcharge += Math.min((int) ((Math.floor((distance - 10 - 1) / 5.0) + 1) * 100), 800);
+        }
+        if (distance > 50) {
+            surcharge += (int) ((Math.floor((distance - 50 - 1) / 8.0) + 1) * 100);
         }
         return BASE_FARE + surcharge;
     }
