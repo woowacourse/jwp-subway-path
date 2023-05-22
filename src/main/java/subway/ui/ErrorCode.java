077c72299202fd7e260062a074bbd909227f8c9a
hum02 <ehdgma02@gmail.com>
2023-05-22 17:48:27 +0900

    refacotr: errorCode status값 httpStatus통하도록 수정

--- src/main/java/subway/ui/ErrorCode.java
+++ src/main/java/subway/ui/ErrorCode.java
@@ -1,13 +1,15 @@
 package subway.ui;
 
+import org.springframework.http.HttpStatus;
+
 public enum ErrorCode {
 
-    STATION_NOT_EXIST(400, "존재하지 않는 역입니다."),
-    STATION_ALREADY_EXIST(400, "이미 존재하는 역입니다."),
-    INVALID_SECTION_LENGTH(400, "등록될 수 없는 길이의 구간입니다. 1이상의 정수가 아니거나, 너무 큰 길이입니다."),
-    NOT_FOUND_END_STATION(400,"종점을 찾을 수 없습니다."),
-    NOT_FOUND_SECTION(400,"해당 구간을 찾을 수 없습니다."),
-    INVALID_SECTION_CONNECTION(400,"이어질 수 없는 구간들 입니다. 노선은 일직선으로, 순환노선이 없어야 합니다.");
+    STATION_NOT_EXIST(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 역입니다."),
+    STATION_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 역입니다."),
+    INVALID_SECTION_LENGTH(HttpStatus.BAD_REQUEST.value(), "등록될 수 없는 길이의 구간입니다. 1이상의 정수가 아니거나, 너무 큰 길이입니다."),
+    NOT_FOUND_END_STATION(HttpStatus.BAD_REQUEST.value(),"종점을 찾을 수 없습니다."),
+    NOT_FOUND_SECTION(HttpStatus.BAD_REQUEST.value(),"해당 구간을 찾을 수 없습니다."),
+    INVALID_SECTION_CONNECTION(HttpStatus.BAD_REQUEST.value(),"이어질 수 없는 구간들 입니다. 노선은 일직선으로, 순환노선이 없어야 합니다.");
 
 
     private final int status;
