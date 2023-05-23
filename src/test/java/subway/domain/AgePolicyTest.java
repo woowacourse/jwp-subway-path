a28b11dfd862f3620ded7265e7bbd79189da4f55
Choi-JJunho <junho5336@gmail.com>
2023-05-23 21:20:48 +0900

    test: 유효하지 않은 나이에 대한 테스트 추가

--- src/test/java/subway/domain/AgePolicyTest.java
+++ src/test/java/subway/domain/AgePolicyTest.java
@@ -11,13 +11,13 @@ class AgePolicyTest {
     @ParameterizedTest(name = "나이가 {0}이면 {1}이다.")
     @CsvSource(delimiter = ':', value = {"19:ADULT", "13:TEENAGER", "6:CHILD", "4:BABY"})
     void 나이별_정책_확인(int age, AgePolicy agePolicy) {
-        Assertions.assertThat(AgePolicy.of(age))
+        Assertions.assertThat(AgePolicy.from(age))
                 .isEqualTo(agePolicy);
     }
 
     @Test
     void 유효하지_않은_나이가_들어오면_예외() {
-        Assertions.assertThatThrownBy(() -> AgePolicy.of(-1))
+        Assertions.assertThatThrownBy(() -> AgePolicy.from(-1))
                 .isInstanceOf(ApiIllegalStateException.class)
                 .hasMessage("해당 나이는 유효하지 않습니다.");
     }
