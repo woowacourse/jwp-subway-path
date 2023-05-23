9397c00e6a502ee9ae24fb2bc695e0e7ef5e9c82
Choi-JJunho <junho5336@gmail.com>
2023-05-23 21:19:14 +0900

    test: 유효하지 않은 나이에 대한 테스트 추가

--- src/test/java/subway/domain/AgePolicyTest.java
+++ src/test/java/subway/domain/AgePolicyTest.java
@@ -1,8 +1,10 @@
 package subway.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import subway.exception.ApiIllegalStateException;
 
 class AgePolicyTest {
 
@@ -11,5 +13,12 @@ class AgePolicyTest {
     void 나이별_정책_확인(int age, AgePolicy agePolicy) {
         Assertions.assertThat(AgePolicy.of(age))
                 .isEqualTo(agePolicy);
+    }
+
+    @Test
+    void 유효하지_않은_나이가_들어오면_예외() {
+        Assertions.assertThatThrownBy(() -> AgePolicy.of(-1))
+                .isInstanceOf(ApiIllegalStateException.class)
+                .hasMessage("해당 나이는 유효하지 않습니다.");
     }
 }
