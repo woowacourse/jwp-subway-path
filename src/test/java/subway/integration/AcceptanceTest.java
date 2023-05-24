f5966dc68aeef6931dcaa41e0767992b3847d278
kevstevie <jianna442@gmail.com>
2023-05-24 16:00:44 +0900

    refactor: depricated 제거

--- src/test/java/subway/integration/AcceptanceTest.java
+++ src/test/java/subway/integration/AcceptanceTest.java
@@ -8,7 +8,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 
