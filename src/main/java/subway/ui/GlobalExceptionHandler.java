73b4433be0100b6ab944d7f0cd20106778aae3b6
kevstevie <jianna442@gmail.com>
2023-05-24 16:16:50 +0900

    refactor: 에러 로그 추가

--- src/main/java/subway/ui/GlobalExceptionHandler.java
+++ src/main/java/subway/ui/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package subway.ui;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,31 +10,37 @@ import subway.exception.DuplicatedNameException;
 import subway.exception.LineNotFoundException;
 import subway.exception.StationNotFoundException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException exception) {
+        log.error(exception.getMessage());
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @ExceptionHandler({StationNotFoundException.class, LineNotFoundException.class})
-    public ResponseEntity<String> handleLineNotFoundException() {
+    public ResponseEntity<String> handleLineNotFoundException(RuntimeException exception) {
+        log.error("{}: 잘못된 삭제 요청", exception.getClass());
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler
     public ResponseEntity<String> handleDuplicatedNameException(final DuplicatedNameException exception) {
+        log.error(exception.getMessage());
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @ExceptionHandler
     public ResponseEntity<Void> handleNotFound(final EmptyResultDataAccessException exception) {
+        log.error("{}: 경로를 찾을 수 없습니다", exception.getClass());
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler
     public ResponseEntity<Void> handleException(final Exception exception) {
+        log.error(exception.getMessage());
         return ResponseEntity.internalServerError().build();
     }
 }
