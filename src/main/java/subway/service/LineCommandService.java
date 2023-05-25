10b95bb735e2be4aeb36e9fd092300e0687de158
java-saeng <gusdn85554@naver.com>
2023-05-25 13:21:19 +0900

    refactor : Line 생성 시 이벤트 발행

--- src/main/java/subway/service/LineCommandService.java
+++ src/main/java/subway/service/LineCommandService.java
@@ -1,9 +1,11 @@
 package subway.service;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import subway.dao.LineDao;
 import subway.dao.LineEntity;
+import subway.event.StationChange;
 import subway.exception.line.CanNotDuplicatedLineNameException;
 import subway.service.dto.RegisterLineRequest;
 
@@ -14,15 +16,17 @@ public class LineCommandService {
   private final LineDao lineDao;
   private final SectionCommandService sectionCommandService;
   private final LineQueryService lineQueryService;
+  private final ApplicationEventPublisher applicationEventPublisher;
 
   public LineCommandService(
       final LineDao lineDao,
       final SectionCommandService sectionCommandService,
-      final LineQueryService lineQueryService
-  ) {
+      final LineQueryService lineQueryService,
+      final ApplicationEventPublisher applicationEventPublisher) {
     this.lineDao = lineDao;
     this.sectionCommandService = sectionCommandService;
     this.lineQueryService = lineQueryService;
+    this.applicationEventPublisher = applicationEventPublisher;
   }
 
   public void deleteLine(final Long lineId) {
@@ -45,5 +49,7 @@ public class LineCommandService {
         registerLineRequest.getDistance(),
         savedId
     );
+
+    applicationEventPublisher.publishEvent(new StationChange());
   }
 }
