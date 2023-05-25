cd8b9e065b8ff94d33665b3568fc01380dd6eda7
kokodak <kokodakadokok@gmail.com>
2023-05-25 13:35:40 +0900

    feat: 로그 기능 추가

--- src/main/java/subway/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/subway/controller/advice/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package subway.controller.advice;
 
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +16,18 @@ import subway.exception.notfound.NotFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(InvalidException.class)
     public ResponseEntity<ExceptionResponse> invalidExceptionHandler(final InvalidException e) {
+        log.warn("[ERROR] Invalid Exception, Message = {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ExceptionResponse.from(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> notFoundExceptionHandler(final NotFoundException e) {
+        log.warn("[ERROR] Not Found Exception, Message = {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ExceptionResponse.from(e.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
@@ -31,13 +37,14 @@ public class GlobalExceptionHandler {
         String message = e.getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(", "));
-
+        log.warn("[ERROR] Method Argument Not Valid Exception, Message = {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ExceptionResponse.from(message, HttpStatus.BAD_REQUEST.value()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> internalServerErrorHandler(final Exception e) {
+        log.error("[ERROR] Internal Server Error, Message = {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ExceptionResponse.from(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
