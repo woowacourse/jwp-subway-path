ff9125f34e3f70453e867e13f71f4ca02b5681f8
mcodnjs <codnjs8989@gmail.com>
2023-05-25 14:12:06 +0900

    refactor: application.yml 설정 파일 수정

--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -2,14 +2,12 @@ package subway.integration;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.ActiveProfiles;
 
-@AutoConfigureTestDatabase
-@Sql({"/test-data.sql"})
+@ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
