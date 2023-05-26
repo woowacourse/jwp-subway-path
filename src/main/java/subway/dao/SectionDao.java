5d21da56dd8692c7872b6b99203f07755c925852
Go-Jaecheol <gojaech@naver.com>
2023-05-26 22:44:08 +0900

    refactor: rowMapper에서 문자열 순서가 아닌 컬럼명에 의존하도록 수정

--- src/main/java/subway/dao/SectionDao.java
+++ src/main/java/subway/dao/SectionDao.java
@@ -28,18 +28,19 @@ public class SectionDao {
     }
 
     public List<SectionStationEntity> findByLineId(final Long lineId) {
-        final String sql = "SELECT up.id, up.name, down.id, down.name, section.distance FROM section "
+        final String sql = "SELECT up.id as up_id, up.name as up_name, down.id as down_id, down.name as down_name, "
+                + "section.distance as distance FROM section "
                 + "INNER JOIN station AS up ON section.up_station_id = up.id "
                 + "INNER JOIN station AS down ON section.down_station_id = down.id "
                 + "WHERE line_id = ?";
         return jdbcTemplate.query(sql, (rs, rowNum) ->
                 new SectionStationEntity(
                         lineId,
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getLong(3),
-                        rs.getString(4),
-                        rs.getInt(5)
+                        rs.getLong("up_id"),
+                        rs.getString("up_name"),
+                        rs.getLong("down_id"),
+                        rs.getString("down_name"),
+                        rs.getInt("distance")
                 ), lineId);
     }
 
