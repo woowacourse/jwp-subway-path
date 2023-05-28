f2ca71582ba54e96db773921b9fc4aae2f1ebcc5
kdkdhoho <hkim4410@naver.com>
2023-05-28 10:12:46 +0900

    refactor(PathFinder): 코드 개선
    - 인스턴스화 타입을 추상화 타입으로 수정
    - 각 메서드에서 반복해서 객체 생성하는 것 수정

--- src/main/java/subway/domain/PathFinder.java
+++ src/main/java/subway/domain/PathFinder.java
@@ -3,6 +3,7 @@ package subway.domain;
 import static java.util.stream.Collectors.toList;
 
 import org.jgrapht.Graph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
 import org.jgrapht.graph.GraphDelegator;
@@ -11,6 +12,7 @@ import java.util.List;
 public class PathFinder {
 
     private final Graph<Station, StationEdge> subwayRoute = new DefaultUndirectedWeightedGraph<>(StationEdge.class);
+    private final ShortestPathAlgorithm<Station, StationEdge> shortestPathAlgorithm = new DijkstraShortestPath<>(subwayRoute);
 
     public PathFinder(final List<Section> sections) {
         for (final Section section : sections) {
@@ -31,21 +33,16 @@ public class PathFinder {
     }
 
     public List<Station> findShortestPath(final Station startStation, final Station endStation) {
-        DijkstraShortestPath<Station, StationEdge> dijkstraShortestPath = new DijkstraShortestPath<>(subwayRoute);
-
-        return dijkstraShortestPath.getPath(startStation, endStation).getVertexList();
+        return shortestPathAlgorithm.getPath(startStation, endStation).getVertexList();
     }
 
     public double calculateShortestDistance(final Station startStation, final Station endStation) {
-        DijkstraShortestPath<Station, StationEdge> dijkstraShortestPath = new DijkstraShortestPath<>(subwayRoute);
-
-        return dijkstraShortestPath.getPathWeight(startStation, endStation);
+        return shortestPathAlgorithm.getPathWeight(startStation, endStation);
     }
 
     public List<Line> findPassLine(final Station startStation, final Station endStation) {
-        DijkstraShortestPath<Station, StationEdge> dijkstraShortestPath = new DijkstraShortestPath<>(subwayRoute);
+        List<StationEdge> edges = shortestPathAlgorithm.getPath(startStation, endStation).getEdgeList();
 
-        List<StationEdge> edges = dijkstraShortestPath.getPath(startStation, endStation).getEdgeList();
         return edges.stream()
                 .map(StationEdge::getLine)
                 .distinct()
