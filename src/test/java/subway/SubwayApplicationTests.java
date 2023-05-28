fe524cc2eafebfdbd7fdfdb47a2f9f310e8295a7
Combi153 <jcm04@naver.com>
2023-05-28 21:46:12 +0900

    test: PathIntegrationTest 추가

---
+++ src/test/java/subway/SubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package subway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
