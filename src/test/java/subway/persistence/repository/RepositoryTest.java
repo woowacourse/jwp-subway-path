785f2a57cd041c32ec7c500879ebfcc30306be4c
70825 <hello70825@gmail.com>
2023-05-30 00:27:08 +0900

    refactor: PathService 인터페이스 삭제

--- src/test/java/subway/persistence/repository/RepositoryTest.java
+++ src/test/java/subway/persistence/repository/RepositoryTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 import subway.application.PathService;
-import subway.application.PathServiceImpl;
 import subway.persistence.dao.LineDao;
 import subway.persistence.dao.SectionDao;
 import subway.persistence.dao.StationDao;
@@ -15,7 +14,7 @@ import subway.persistence.dao.StationDao;
 @JdbcTest
 @Sql("/reset-table.sql")
 @Import({LineRepository.class, SectionRepository.class, StationRepository.class,
-        LineDao.class, SectionDao.class, StationDao.class, PathServiceImpl.class})
+        LineDao.class, SectionDao.class, StationDao.class, PathService.class})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class RepositoryTest {
 
