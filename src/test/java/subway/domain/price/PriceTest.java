354bab585531e32a230c3bda48594fbdc8045bf0
70825 <hello70825@gmail.com>
2023-05-30 00:34:00 +0900

    test: 요금 계산 테스트 추가

--- src/test/java/subway/domain/price/PriceTest.java
+++ src/test/java/subway/domain/price/PriceTest.java
@@ -3,7 +3,7 @@ package subway.domain.price;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import subway.domain.price.Price;
+import subway.domain.Distance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,6 +17,15 @@ class PriceTest {
         assertThatThrownBy(() -> Price.from(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("금액은 0원 이상이어야 합니다.");
+    }
+
+    @Test
+    void 요금을_계산할_수_있는지_확인한다() {
+        final Distance distance = Distance.from(20);
+        final Distance farePerDistance = Distance.from(10);
+        final Price additionFare = Price.from(100);
+
+        assertThat(Price.calculateSurcharge(distance, farePerDistance, additionFare)).isEqualTo(Price.from(200));
     }
 
     @Test
