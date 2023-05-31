44e5c371f5e2a8b3d0903066089fcfcdd97b5f8b
ezzanzzan <cjsdmswjd821@naver.com>
2023-05-31 14:15:54 +0900

    feat: LineRepository 생성

---
+++ src/main/java/subway/repository/LineRepository.java
@@ -0,0 +1,61 @@
+package subway.repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Repository;
+import subway.dao.LineDao;
+import subway.dao.SectionDao;
+import subway.dao.StationDao;
+import subway.dao.entity.LineEntity;
+import subway.dao.entity.SectionEntity;
+import subway.dao.entity.StationEntity;
+import subway.domain.Line;
+import subway.domain.Section;
+import subway.domain.Station;
+
+@Repository
+public class LineRepository {
+    private final LineDao lineDao;
+    private final SectionDao sectionDao;
+    private final StationDao stationDao;
+
+    public LineRepository(LineDao lineDao, SectionDao sectionDao, StationDao stationDao) {
+        this.lineDao = lineDao;
+        this.sectionDao = sectionDao;
+        this.stationDao = stationDao;
+    }
+
+    public List<Line> findAllLine() {
+        List<LineEntity> lineEntities = lineDao.findAll();
+
+        return lineEntities.stream()
+                .map(lineEntity -> findLineByLineId(lineEntity.getId()))
+                .collect(Collectors.toList());
+    }
+
+    public Line findLineByLineId(Long lineId) {
+        LineEntity lineEntity = lineDao.findById(lineId);
+        return Line.of(lineEntity.getName(), findSectionsByLineId(lineId));
+    }
+
+    private List<Section> findSectionsByLineId(Long lineId) {
+        List<SectionEntity> sectionEntities = sectionDao.findByLineId(lineId);
+        Map<Long, String> stationEntities = findStations();
+
+        return sectionEntities.stream()
+                .map(sectionEntity -> new Section(
+                        new Station(sectionEntity.getUpStationId(), stationEntities.get(sectionEntity.getUpStationId())),
+                        new Station(sectionEntity.getDownStationId(), stationEntities.get(sectionEntity.getDownStationId())),
+                        sectionEntity.getDistance()
+                )).collect(Collectors.toList());
+    }
+
+    private Map<Long, String> findStations() {
+        return stationDao.findAll().stream()
+                .collect(Collectors.toMap(
+                        StationEntity::getId,
+                        StationEntity::getName)
+                );
+    }
+}
