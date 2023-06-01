a9c589cf27447de0cb2386c8ea0f3d2a9597ea6e
ezzanzzan <cjsdmswjd821@naver.com>
2023-06-01 15:54:29 +0900

    feat: 요금 계산 시 예외 처리 기능
    - 추가거리가 0 이하일 때 예외 처리 기능 구현

--- src/main/java/subway/domain/Fare.java
+++ src/main/java/subway/domain/Fare.java
@@ -1,5 +1,8 @@
 package subway.domain;
 
+import subway.exception.ErrorCode;
+import subway.exception.InvalidException;
+
 public class Fare {
     public static final int DEFAULT_FARE = 1250;
     public static final int UNIT_OVER_TEN = 5;
@@ -16,7 +19,14 @@ public class Fare {
         this(DEFAULT_FARE);
     }
 
+    private void validateDistance(int distance) {
+        if (distance <= 0) {
+            throw new InvalidException(ErrorCode.INVALID_NOT_POSITIVE_DISTANCE);
+        }
+    }
+
     public int calculateFare(int distance) {
+        validateDistance(distance);
         fare += (calculateOverFare(distance - 10, UNIT_OVER_TEN) - calculateOverFare(distance - 50, UNIT_OVER_TEN));
         fare += calculateOverFare(distance - 50, UNIT_OVER_FIFTY);
 
@@ -24,7 +34,7 @@ public class Fare {
     }
 
     private int calculateOverFare(int distance, int unit) {
-        if (distance <= 0) {
+        if (distance < 0) {
             return 0;
         }
         return (int) ((Math.ceil((distance - 1) / unit) + 1) * ADD_FARE);
