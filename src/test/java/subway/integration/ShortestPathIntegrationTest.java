b597146e53669f213c9058a85c240b553ca4d53b
ezzanzzan <cjsdmswjd821@naver.com>
2023-06-01 11:25:02 +0900

    test: 최단 경로 조회 기능 테스트 추가

--- src/test/java/subway/integration/ShortestPathIntegrationTest.java
+++ src/test/java/subway/integration/ShortestPathIntegrationTest.java
@@ -1,6 +1,7 @@
 package subway.integration;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -19,7 +20,7 @@ import subway.integration.step.LineStep;
 import subway.integration.step.SectionStep;
 import subway.integration.step.StationStep;
 
-@DisplayName("최단 경로 구하기 기능")
+@DisplayName("최단 경로 조회 기능")
 @SuppressWarnings("NonAsciiCharacters")
 public class ShortestPathIntegrationTest extends IntegrationTest {
     @Override
@@ -60,5 +61,81 @@ public class ShortestPathIntegrationTest extends IntegrationTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.as(PathResponse.class).getDistance()).isEqualTo(2);
         assertThat(response.as(PathResponse.class).getFare()).isEqualTo(1250);
+    }
+
+    @DisplayName("출발역과 도착역이 같을 때 예외가 발생한다.")
+    @Test
+    void shortestPathWithSameStation() {
+        // given
+        PathRequest pathRequest = new PathRequest("찰리역", "찰리역");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(pathRequest)
+                .when()
+                .get("/paths/shortest")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(response.body().jsonPath().get("message").equals("시작역과 도착역은 같을 수 없습니다.")).isTrue()
+        );
+    }
+
+    @DisplayName("존재하지 않는 역이 입력되면 예외가 발생한다.")
+    @Test
+    void shortestPathWithNotExistStation() {
+        // given
+        PathRequest pathRequest = new PathRequest("베로역", "찰리역");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(pathRequest)
+                .when()
+                .get("/paths/shortest")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(response.body().jsonPath().get("message").equals("존재하지 않는 역입니다.")).isTrue()
+        );
+    }
+
+    @DisplayName("경로를 찾을 수 없을 때 예외가 발생한다.")
+    @Test
+    void shortestPathWithNotFoundPath() {
+        // given
+        LineStep.노선을_생성한다(new LineRequest("4호선"));
+
+        StationStep.역을_생성한다(new StationCreateRequest("잠실역"));
+        StationStep.역을_생성한다(new StationCreateRequest("잠실새내역"));
+
+        SectionStep.구간을_생성한다(new SectionCreateRequest("잠실역", "잠실새내역", 10, 4));
+
+        PathRequest pathRequest = new PathRequest("잠실역", "찰리역");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(pathRequest)
+                .when()
+                .get("/paths/shortest")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(response.body().jsonPath().get("message").equals("존재하지 않는 경로입니다.")).isTrue()
+        );
     }
 }
