a025bdeab0f2e39e99b5696d351cea6281e56525
kokodak <kokodakadokok@gmail.com>
2023-06-02 11:01:22 +0900

    feat: 최단 경로 컨트롤러 구현

---
+++ src/main/java/subway/controller/ShortestPathController.java
@@ -0,0 +1,27 @@
+package subway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import subway.dto.shortestpath.ShortestPathRequest;
+import subway.dto.shortestpath.ShortestPathResponse;
+import subway.service.ShortestPathService;
+
+@RestController
+@RequestMapping("/shortest-path")
+public class ShortestPathController {
+
+    private final ShortestPathService shortestPathService;
+
+    public ShortestPathController(final ShortestPathService shortestPathService) {
+        this.shortestPathService = shortestPathService;
+    }
+
+    @GetMapping
+    ResponseEntity<ShortestPathResponse> showShortestPath(@RequestBody ShortestPathRequest request) {
+        final ShortestPathResponse shortestPath = shortestPathService.findShortestPath(request);
+        return ResponseEntity.ok(shortestPath);
+    }
+}
