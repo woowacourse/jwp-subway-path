9100f67ee159efa88a58ddaf7242a2d26c746c43
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-06-05 07:48:07 +0900

    refactor: 사용하지 않는 생성자 제거

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -3,29 +3,12 @@ package subway.domain;
 import java.util.List;
 import java.util.Objects;
 
-//todo 생각해볼것1: 도메인 객체를 값객체로 감싸야 하는지?
-//todo 생각해볼것2: addStation같은 역할을 부여해야 하는지?
-//todo 생각해볼것3: 비즈니스로직에서 id가 필요한가?
 public class Line {
 
     private final Long id;
     private final String name;
     private final String color;
     private final Sections sections;
-
-    public Line(String name, String color) {
-        this.id = null;
-        this.name = name;
-        this.color = color;
-        this.sections = Sections.EMPTY_SECTION;
-    }
-
-    public Line(Long id, String name, String color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.sections = Sections.EMPTY_SECTION;
-    }
 
     public Line(Long id, String name, String color, Sections sections) {
         this.id = id;
